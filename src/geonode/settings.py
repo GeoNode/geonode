036e7b81fc3fe1efa2a9e19c03963e426e905a6b
dwinslow <dwinslow@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-01 20:57:43 +0000

    Clear out stubbed-out capra site
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@405 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/geonode/settings.py
+++ src/geonode/settings.py
@@ -50,7 +50,7 @@ MEDIA_URL = ''
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+#ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'myv-y4#7j-d*p-__@j#*3z@!y24fz8%^z2v6atuy4bo9vqr1_a'
