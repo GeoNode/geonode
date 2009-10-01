3a2d5c3d9d7d2fb417ef01fd2dbbed5562828ede
dwinslow <dwinslow@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-01 16:40:04 +0000

    Add context_processors.py to version control.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@392 125c47a5-210e-49ec-a0c6-83efc728e0a1

---
+++ src/geonode/maps/context_processors.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+
+def resource_urls(request): 
+    return {
+        "GEOSERVER_BASE_URL": settings.GEOSERVER_BASE_URL,
+        "GOOGLE_API_KEY": settings.GOOGLE_API_KEY
+    }
