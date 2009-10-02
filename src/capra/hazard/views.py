da921d38f00f0caf9660e7600d8d67d872896d32
dwinslow <dwinslow@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-02 19:05:50 +0000

    Set up JavaScript build profile and page for hazards application.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@415 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/capra/hazard/views.py
+++ src/capra/hazard/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from geonode.maps.context_processors import resource_urls
 
 def index(request): 
-    return render_to_response("index.html")
+    return render_to_response("index.html",
+        context_instance=RequestContext(request, {}, [resource_urls])
+    )
--- src/capra/urls.py
+++ src/capra/urls.py
@@ -1,6 +1,17 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 from geonode.urls import urlpatterns
 
 urlpatterns += patterns('capra.hazard.views',
     (r'^hazard/', 'index'),
 )
+
+if settings.DEBUG:
+    import os
+    def here(*x): 
+        return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+    root = here("..", "capra-client", "build", "capra-client") if settings.MINIFIED_RESOURCES else here("..", "capra-client", "")
+    urlpatterns += patterns('',
+        (r'^capra_static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': root}),
+    )
