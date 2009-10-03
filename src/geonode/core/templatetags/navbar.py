0532a5487c39c2aa49aab90a9f177cafb3cbe8a7
whit <whit@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-03 04:36:13 +0000

    navbar rendering tag
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@420 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/geonode/core/templatetags/navbar.py
+++ src/geonode/core/templatetags/navbar.py
@@ -1,12 +1,18 @@
 """
 Tag for rendering a context sensitive and extendable navigation
 """
+from geonode.utils import SectionConfigMap
 from django import template
-from django.utils.translation import ungettext
-from django.utils.translation import ugettext_lazy as _
+from django.template import loader
+from django.template.defaulttags import url
 from django.utils.translation import string_concat
+from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ungettext
+from geonode.utils import path_extrapolate
+
 
 register = template.Library()
+
 
 @register.tag('navbar')
 def do_navbar(parser, token):
@@ -19,17 +25,78 @@ def do_navbar(parser, token):
         tagname, whatpage = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError, _("%r tag requires a single argument (what page should be selected in the nav?)" % token.contents.split()[0])
-    return NavBar()
+    return NavBar(whatpage, parser)
+
+
+def load_navbar_config():
+    fn = path_extrapolate('geonode/core/templatetags/navbar.ini')
+    try:
+        from django import settings
+        newfn = getattr(settings, 'NAVBAR_INI', fn)
+        if newfn is not None:
+            fn = newfn
+    except ImportError:
+        pass
+    
+    #@@ cascade?
+    conf = SectionConfigMap.load(fn)
+    meta = conf['meta']
+    # load defaults
+    for section in conf.keys():
+        cur = conf[section]
+        for key in 'id','class',:
+            if not cur.has_key(key):
+                cur[key] = meta['default_' + key]
+        conf[section] = cur
+    return conf
 
 
 class NavBar(template.Node):
     """
     Renderer for the nav bar
     """
-    def __init__(self, whereami):
+    
+    config = load_navbar_config()
+    
+    def __init__(self, whereami, parser):
         self.whereami = whereami
+        self.parser = parser
+
+    def get_url_node(self, spec):
+        token = template.Token(template.TOKEN_BLOCK, "url %s" %spec)
+        return url(self.parser, token)
+     
+
+    def compile_sections(self, context):
+        sections = list()
+        for section in self.visible:
+            sect_info = self.config[section]
+            if section == self.whereami:
+                oldclass = sect_info['class']
+                sect_info['class'] = "%s %s" %(oldclass, self.meta['active_class'])
+            if sect_info['id'].startswith('%s'):
+                sect_info['id'] = sect_info['id'] %section
+            sect_info['url'] = self.get_url_node(sect_info['url']).render(context)
+            sections.append(sect_info)
+        return sections
+    
     def render(self, context):
-        import pdb;pdb.set_trace()
+        #@@ cache this? doesn't change much
+        sections = self.compile_sections(context)
+        info = dict(active=self.whereami,
+                    sections=sections,
+                    config=self.config)
+        return loader.render_to_string('navbar.html', info)
+
+    @property
+    def meta(self):
+        return self.config['meta']
+
+    @property
+    def visible(self):
+        return self.meta['visible'].split()
+
+
 
 
 
