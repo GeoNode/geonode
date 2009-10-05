766f28f1845c736233b080309ebdb62f49f6eec3
dwinslow <dwinslow@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-05 17:30:59 +0000

    Use a dummy json document instead of "Hello restlet" for the placeholder rest service.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@426 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/geonode-geoserver-ext/src/main/java/org/geonode/rest/ProcessRestlet.java
+++ src/geonode-geoserver-ext/src/main/java/org/geonode/rest/ProcessRestlet.java
@@ -1,11 +1,15 @@
 package org.geonode.rest;
 
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.restlet.Restlet;
 import org.restlet.data.MediaType;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
-import org.restlet.resource.StringRepresentation;
+import org.restlet.resource.OutputRepresentation;
 
 import org.geoserver.catalog.Catalog;
 import org.geoserver.platform.GeoServerResourceLoader;
@@ -13,6 +17,21 @@ import org.geoserver.rest.RestletException;
 
 public class ProcessRestlet extends Restlet {
     public void handle(Request request, Response response) {
-        response.setEntity(new StringRepresentation("Hello Rest", MediaType.TEXT_PLAIN));
+        response.setEntity(new OutputRepresentation(MediaType.APPLICATION_JSON) {
+            public InputStream getStream() {
+                return ProcessRestlet.class.getResourceAsStream("/dummy.json");
+            }
+
+            public void write(OutputStream out) throws IOException { 
+                InputStream in = getStream();
+                byte[] buff = new byte[1024];
+                int len = 0;
+                while ((len = in.read(buff)) != -1) {
+                    out.write(buff, 0, len);
+                }
+                out.flush();
+                out.close();
+            }
+        });
     }
 }
