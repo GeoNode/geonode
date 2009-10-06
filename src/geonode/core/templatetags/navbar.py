724e2584c4b908f9956af2489455c6d831bf4545
whit <whit@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-06 15:14:37 +0000

    * mediate values of NAVBAR to take strings or dicts
    * allow template to be passed in as 3rd argument to tag
    
    * add extra class selector mediation for <a> and <li>
    
    * add function to generate config dict, update ini that generates
    
    * copy over index.html to core/templates
    
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@438 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/geonode/core/templatetags/navbar.py
+++ src/geonode/core/templatetags/navbar.py
@@ -1,54 +1,62 @@
 """
 Tag for rendering a context sensitive and extendable navigation
 """
-from geonode.utils import SectionConfigMap
+from pprint import pformat
 from django import template
 from django.template import loader
 from django.template.defaulttags import url
 from django.utils.translation import string_concat
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ungettext
+from geonode.utils import ConfigMap, DictMixin
 from geonode.utils import path_extrapolate
 
 
 register = template.Library()
 
+DEFAULT_PATH = 'geonode/core/templatetags/navbar.ini'
+DEFAULT_TEMPLATE = 'navbar.html'
 
 @register.tag('navbar')
-def do_navbar(parser, token):
+def do_navbar(parser, token, template=DEFAULT_TEMPLATE):
     """
     token is the page for the template
 
     @@ can we get a path or some route info?
     """
-    try:
-        tagname, whatpage = token.split_contents()
-    except ValueError:
+    spec = token.split_contents()
+    if len(spec) == 2:
+        tagname, whatpage = spec
+    elif len(spec) == 3:
+        tagname, whatpage, template = spec
+    else:
         raise template.TemplateSyntaxError, _("%r tag requires a single argument (what page should be selected in the nav?)" % token.contents.split()[0])
-    return NavBar(whatpage, parser)
+    return NavBar(whatpage, parser, template)
 
 
-def load_navbar_config():
-    fn = path_extrapolate('geonode/core/templatetags/navbar.ini')
-    try:
-        from django import settings
-        newfn = getattr(settings, 'NAVBAR_INI', fn)
-        if newfn is not None:
-            fn = newfn
-    except ImportError:
-        pass
-    
-    #@@ cascade?
-    conf = SectionConfigMap.load(fn)
-    meta = conf['meta']
+def prep_config(mapping):
+    meta = mapping['meta']
     # load defaults
-    for section in conf.keys():
-        cur = conf[section]
-        for key in 'id','class',:
+    for section in mapping.keys():
+        cur = mapping[section]
+        for key in 'id','item_class', 'link_class':
             if not cur.has_key(key):
                 cur[key] = meta['default_' + key]
-        conf[section] = cur
-    return conf
+        mapping[section] = cur
+    return mapping
+
+    
+def load_navbar_config(path=DEFAULT_PATH):
+    try:
+        from django.conf import settings
+        conf = settings.NAVBAR
+    except ImportError:
+        conf = path_extrapolate(path)
+
+    if isinstance(conf, basestring):
+        conf = dict(ConfigMap.load(conf))
+
+    return prep_config(conf)
 
 
 class NavBar(template.Node):
@@ -58,35 +66,46 @@ class NavBar(template.Node):
     
     config = load_navbar_config()
     
-    def __init__(self, whereami, parser):
+    def __init__(self, whereami, parser, template):
         self.whereami = whereami
         self.parser = parser
+        self.template = template
 
     def get_url_node(self, spec):
         token = template.Token(template.TOKEN_BLOCK, "url %s" %spec)
         return url(self.parser, token)
-     
 
     def compile_sections(self, context):
         sections = list()
         for section in self.visible:
-            sect_info = self.config[section]
-            if section == self.whereami:
-                oldclass = sect_info['class']
-                sect_info['class'] = "%s %s" %(oldclass, self.meta['active_class'])
-            if sect_info['id'].startswith('%s'):
+            # copy the section dict to protect against accidental
+            # writes
+            sect_info = dict(self.config[section])
+
+            if section == self.whereami:  # the active tab
+                oldclass = sect_info['link_class']
+                sect_info['link_class'] = "%s %s" %(oldclass, self.meta['active_class'])
+
+            if section == self.visible[-1]: # the last tab
+                oldclass = sect_info['item_class']
+                sect_info['item_class'] = "%s %s" %(oldclass, self.meta['end_class'])
+
+            if sect_info['id'].startswith('%s'): # id needs a section sub
                 sect_info['id'] = sect_info['id'] %section
+                
             sect_info['url'] = self.get_url_node(sect_info['url']).render(context)
+            sect_info['text'] = _(sect_info['text'])
             sections.append(sect_info)
+            
         return sections
-    
+
+    #@@ cache this? doesn't change much
     def render(self, context):
-        #@@ cache this? doesn't change much
         sections = self.compile_sections(context)
         info = dict(active=self.whereami,
                     sections=sections,
                     config=self.config)
-        return loader.render_to_string('navbar.html', info)
+        return loader.render_to_string(self.template, info)
 
     @property
     def meta(self):
@@ -97,6 +116,14 @@ class NavBar(template.Node):
         return self.meta['visible'].split()
 
 
+def export_default_config(path=DEFAULT_PATH):
+    conf = path_extrapolate(path)    
+    conf = ConfigMap.load(conf)
+    conf = prep_config(conf)
+    conf = dict(conf)
+    for name, mapping in sorted(conf.items()):
+        conf[name]=dict(mapping)
+    return pformat(conf)
 
 
 
