12e88a0498e863409cf52508de738e6d661705f2
whit <whit@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-07 01:32:55 +0000

    use type spelling (quote conservation)
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@469 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/geonode/maps/context_processors.py
+++ src/geonode/maps/context_processors.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 
 def resource_urls(request): 
-    return {
-        "GEOSERVER_BASE_URL": settings.GEOSERVER_BASE_URL,
-        "GOOGLE_API_KEY": settings.GOOGLE_API_KEY,
-        "MEDIA_LOCATIONS": settings.MEDIA_LOCATIONS
-    }
+    return dict(GEOSERVER_BASE_URL=settings.GEOSERVER_BASE_URL,
+                GOOGLE_API_KEY=settings.GOOGLE_API_KEY,
+                MEDIA_LOCATIONS=settings.MEDIA_LOCATIONS)
