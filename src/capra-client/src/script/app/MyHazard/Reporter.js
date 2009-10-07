9a2aeb6d1d2ff12a735b47dbe2c72220b12b2271
dwinslow <dwinslow@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-07 17:56:49 +0000

    Connect the report control to an HTTP request for the actual report.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@491 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/capra-client/src/script/app/MyHazard/Reporter.js
+++ src/capra-client/src/script/app/MyHazard/Reporter.js
@@ -2,8 +2,14 @@ Ext.namespace("MyHazard");
 
 MyHazard.Reporter = OpenLayers.Class(OpenLayers.Control, {
     format: null,
+    persist: true,
+
+    EVENT_TYPES: ["report"],
 
     initialize: function(handler, options) {
+        this.EVENT_TYPES = MyHazard.Reporter.prototype.EVENT_TYPES.concat(
+            OpenLayers.Control.prototype.EVENT_TYPES
+        );
         handler = handler || OpenLayers.Handler.Point;
         this.format = (options && options.format) || new OpenLayers.Format.GeoJSON();
         OpenLayers.Control.prototype.initialize.apply(this, [options]);
@@ -19,6 +25,6 @@ MyHazard.Reporter = OpenLayers.Class(OpenLayers.Control, {
     },
 
     report: function(geom) {
-        alert(this.format.write(geom));
+        this.events.triggerEvent("report", {geom: geom});
     }
 });
