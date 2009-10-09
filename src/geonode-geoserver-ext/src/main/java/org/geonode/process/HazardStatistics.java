ecafc7afa508580960e48cb15a2ad5fd77c60554
groldan <groldan@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-09 04:03:09 +0000

    add HazardStatistics process
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@544 125c47a5-210e-49ec-a0c6-83efc728e0a1

---
+++ src/geonode-geoserver-ext/src/main/java/org/geonode/process/HazardStatistics.java
@@ -0,0 +1,250 @@
+package org.geonode.process;
+
+import static org.geonode.process.HazardStatisticsFactory.DATALAYER;
+import static org.geonode.process.HazardStatisticsFactory.GEOMERTY;
+import static org.geonode.process.HazardStatisticsFactory.RADIUS;
+
+import java.awt.geom.Rectangle2D;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.media.jai.Histogram;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.coverage.grid.GridEnvelope2D;
+import org.geotools.coverage.grid.GridGeometry2D;
+import org.geotools.coverage.grid.ViewType;
+import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
+import org.geotools.coverage.grid.io.AbstractGridFormat;
+import org.geotools.coverage.processing.OperationJAI;
+import org.geotools.geometry.GeneralEnvelope;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.parameter.Parameter;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.impl.AbstractProcess;
+import org.geotools.referencing.CRS;
+import org.opengis.parameter.GeneralParameterValue;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.datum.PixelInCell;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.NoninvertibleTransformException;
+import org.opengis.referencing.operation.TransformException;
+import org.opengis.util.ProgressListener;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+final class HazardStatistics extends AbstractProcess {
+
+    protected HazardStatistics(final ProcessFactory factory) {
+        super(factory);
+    }
+
+    /**
+     * @see Process#execute(Map, ProgressListener)
+     */
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> execute(final Map<String, Object> input,
+            final ProgressListener monitor) throws ProcessException {
+
+        final Geometry inputGeometry = (Geometry) input.get(GEOMERTY.key);
+        final Double inputRadius = (Double) input.get(RADIUS.key);
+
+        final List<AbstractGridCoverage2DReader> inputDataLayers;
+        inputDataLayers = (List<AbstractGridCoverage2DReader>) input.get(DATALAYER.key);
+
+        checkInputs(inputGeometry, inputRadius, inputDataLayers);
+
+        final Geometry bufferedGeometry = performBuffer(inputGeometry, inputRadius);
+
+        /**
+         * CoverageReaders have no Name, so we return a list of results in the same order than the
+         * provided readers for client code to match them up
+         */
+        List<Map<String, double[]>> perLayerStats = new ArrayList<Map<String, double[]>>();
+        for (AbstractGridCoverage2DReader layer : inputDataLayers) {
+            Map<String, double[]> stats = gatherCoverageStats(layer, bufferedGeometry);
+            perLayerStats.add(new HashMap<String, double[]>(stats));
+        }
+
+        Map<String, Object> results = new HashMap<String, Object>();
+
+        results.put(HazardStatisticsFactory.RESULT_STATISTICS.key, perLayerStats);
+
+        // TODO: un-fake
+        HashMap<String, Object> politicalData = new HashMap<String, Object>();
+        politicalData.put("country", "Tasmania");
+        politicalData.put("municipality", "Bicheno");
+
+        results.put(HazardStatisticsFactory.RESULT_POLITICAL.key, politicalData);
+
+        return results;
+    }
+
+    private Map<String, double[]> gatherCoverageStats(final AbstractGridCoverage2DReader layer,
+            final Geometry bufferedGeometry) throws ProcessException {
+
+        Map<String, double[]> stats = new HashMap<String, double[]>();
+        stats.put("min", null);
+        stats.put("max", null);
+        stats.put("mean", null);
+        stats.put("stddev", null);
+
+        final GridCoverage2D geophysics;
+        try {
+            final ReferencedEnvelope requesEnvelope;
+            final CoordinateReferenceSystem crs = layer.getCrs();
+            requesEnvelope = new ReferencedEnvelope(bufferedGeometry.getEnvelopeInternal(), crs);
+
+            GridCoverage2D coverage = getGridCoverage(layer, requesEnvelope);
+            if (coverage == null) {
+                // request envelope did not intersect coverage
+                geophysics = null;
+            } else {
+                geophysics = coverage.view(ViewType.GEOPHYSICS);
+            }
+        } catch (IOException e) {
+            throw new ProcessException(e);
+        }
+
+        final OperationJAI op = new OperationJAI("Histogram");
+        ParameterValueGroup params = op.getParameters();
+        params.parameter("Source").setValue(geophysics);
+
+        GridCoverage2D coverage = (GridCoverage2D) op.doOperation(params, null);
+        Histogram histogram = (Histogram) coverage.getProperty("histogram");
+
+        stats.put("min", histogram.getLowValue());
+        stats.put("max", histogram.getHighValue());
+        stats.put("mean", histogram.getMean());
+        stats.put("stddev", histogram.getStandardDeviation());
+
+        return stats;
+    }
+
+    private void checkInputs(final Geometry inputGeometry, final Double inputRadius,
+            final List<AbstractGridCoverage2DReader> inputDataLayers) {
+        if (inputGeometry == null) {
+            throw new NullPointerException("input geometry is required");
+        }
+        if (inputRadius == null) {
+            throw new NullPointerException("input radius is required");
+        }
+        if (inputDataLayers == null || inputDataLayers.size() == 0) {
+            throw new NullPointerException("input data layer is required");
+        }
+    }
+
+    private Geometry performBuffer(Geometry inputGeometry, Double inputRadius) {
+        return inputGeometry.buffer(inputRadius.doubleValue());
+    }
+
+    /**
+     * Loads a grid coverage.
+     * <p>
+     * 
+     * </p>
+     * 
+     * @param info
+     *            The grid coverage metadata.
+     * @param envelope
+     *            The section of the coverage to load.
+     * @param hints
+     *            Hints to use while loading the coverage.
+     * 
+     * @throws IOException
+     *             Any errors that occur loading the coverage.
+     */
+    @SuppressWarnings("deprecation")
+    public GridCoverage2D getGridCoverage(final AbstractGridCoverage2DReader reader,
+            final ReferencedEnvelope env) throws IOException {
+
+        GeneralEnvelope requestEnvelope = new GeneralEnvelope(env);
+
+        // /////////////////////////////////////////////////////////
+        //
+        // Do we need to proceed?
+        // I need to check the requested envelope in order to see if the
+        // coverage we ask intersect it otherwise it is pointless to load it
+        // since its reader might return null;
+        // /////////////////////////////////////////////////////////
+        final CoordinateReferenceSystem sourceCRS = requestEnvelope.getCoordinateReferenceSystem();
+        final CoordinateReferenceSystem destCRS = reader.getCrs();
+
+        if (!CRS.equalsIgnoreMetadata(sourceCRS, destCRS)) {
+            // get a math transform
+            MathTransform transform;
+            try {
+                transform = CRS.findMathTransform(sourceCRS, destCRS, true);
+            } catch (FactoryException e) {
+                final IOException ioe = new IOException("unable to determine coverage crs");
+                ioe.initCause(e);
+                throw ioe;
+            }
+
+            // transform the envelope
+            if (!transform.isIdentity()) {
+                try {
+                    requestEnvelope = CRS.transform(transform, requestEnvelope);
+                } catch (TransformException e) {
+                    throw (IOException) new IOException("error occured transforming envelope")
+                            .initCause(e);
+                }
+            }
+        }
+
+        final GeneralEnvelope coverageBounds = reader.getOriginalEnvelope();
+        // just do the intersection since
+        requestEnvelope.intersect(coverageBounds);
+
+        if (requestEnvelope.isEmpty()) {
+            return null;
+        }
+
+        requestEnvelope.setCoordinateReferenceSystem(destCRS);
+
+        final MathTransform originalGridToWorld = reader
+                .getOriginalGridToWorld(PixelInCell.CELL_CORNER);
+        MathTransform worldToGrid;
+        try {
+            worldToGrid = originalGridToWorld.inverse();
+        } catch (NoninvertibleTransformException e) {
+            throw new RuntimeException(e);
+        }
+        Rectangle2D requestGridRange;
+        try {
+            requestGridRange = CRS.transform(worldToGrid, requestEnvelope).toRectangle2D();
+        } catch (IllegalStateException e) {
+            throw new RuntimeException(e);
+        } catch (TransformException e) {
+            throw new RuntimeException(e);
+        }
+
+        // /////////////////////////////////////////////////////////
+        //
+        // Reading the coverage
+        //
+        // /////////////////////////////////////////////////////////
+        final Parameter<GridGeometry2D> readGG = new Parameter<GridGeometry2D>(
+                AbstractGridFormat.READ_GRIDGEOMETRY2D);
+        readGG.setValue(new GridGeometry2D(new GridEnvelope2D(requestGridRange.getBounds()),
+                requestEnvelope));
+
+        GeneralParameterValue[] parameters = new GeneralParameterValue[] { readGG };
+
+        GridCoverage2D gc = reader.read(parameters);
+
+        if ((gc == null) || !(gc instanceof GridCoverage2D)) {
+            throw new IOException("The requested coverage could not be found.");
+        }
+
+        return gc;
+    }
+
+}
---
+++ src/geonode-geoserver-ext/src/main/java/org/geonode/process/HazardStatisticsFactory.java
@@ -0,0 +1,153 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geonode.process;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.Parameter;
+import org.geotools.feature.NameImpl;
+import org.geotools.process.Process;
+import org.geotools.process.impl.SingleProcessFactory;
+import org.geotools.text.Text;
+import org.opengis.util.InternationalString;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+/**
+ * <p>
+ * Inputs:
+ * <ul>
+ * <li>{@code radius}: {@link Integer}. Buffer radius around the input geometry to create the
+ * intersection query geometry to match the {@code datalayers} with.
+ * <li>{@code geometry}: {@link Geometry},
+ * <li>{@code datalayers:} {@link List List&lt;String&gt;}, names of the layers to intersect with
+ * the buffered input geometry
+ * </ul>
+ * </p>
+ */
+public class HazardStatisticsFactory extends SingleProcessFactory {
+
+    private static final String PROCESS_NAMESPACE = "hazard";
+
+    private static final String PROCESS_NAME = "Statistics";
+
+    /** Geometry for operation. Required. */
+    public static final Parameter<Geometry> GEOMERTY = new Parameter<Geometry>("geometry",
+            Geometry.class, Text.text("Geometry"), Text.text("Geometry to buffer"));
+
+    /** Buffer amount. Required. */
+    public static final Parameter<Double> RADIUS = new Parameter<Double>("radius", Double.class,
+            Text.text("Buffer Amount"), Text.text("Amount to buffer the geometry by"));
+
+    /** Layer names. Required. Cardinality is 1..* */
+    public static final Parameter<AbstractGridCoverage2DReader> DATALAYER = new Parameter<AbstractGridCoverage2DReader>(
+            "datalayer", AbstractGridCoverage2DReader.class, Text.text("Data layers"), Text
+                    .text("List of datalayers to query"), true, 1, -1, null, null);
+
+    @SuppressWarnings("unchecked")
+    public static final Parameter<FeatureSource> POLITICAL_LAYER = new Parameter<FeatureSource>(
+            "politicalLayer", FeatureSource.class, Text.text("Political layer"), Text
+                    .text("Layer to return political info about"), false, 0, 1, null, null);
+
+    /** Layer names. Required. Cardinality is 0..* */
+    public static final Parameter<String> POLITICAL_LAYER_ATTRIBUTES = new Parameter<String>(
+            "politicalLayerAttributes", String.class, Text.text("Political layer attributes"),
+            Text.text("Attributes to return from the political layer for intersecting features"),
+            false, 0, -1, null, null);
+
+    /**
+     * Map used for getParameterInfo; used to describe operation requirements for user interface
+     * creation.
+     */
+    private static final Map<String, Parameter<?>> prameterInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        prameterInfo.put(GEOMERTY.key, GEOMERTY);
+        prameterInfo.put(RADIUS.key, RADIUS);
+        prameterInfo.put(DATALAYER.key, DATALAYER);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static final Parameter<Map> RESULT_STATISTICS = new Parameter<Map>("statistics",
+            Map.class, Text.text("Statistics"), Text
+                    .text("Aggregate result of datalayer statistics"));
+
+    @SuppressWarnings("unchecked")
+    public static final Parameter<Map> RESULT_POLITICAL = new Parameter<Map>("result", Map.class,
+            Text.text("Political"), Text
+                    .text("Political subdivision names where the staistics lie"));
+
+    /**
+     * Map used to describe operation results.
+     */
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+
+    static {
+        resultInfo.put(RESULT_STATISTICS.key, RESULT_STATISTICS);
+        resultInfo.put(RESULT_POLITICAL.key, RESULT_POLITICAL);
+    }
+
+    public HazardStatisticsFactory() {
+        super(new NameImpl(PROCESS_NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return Text
+                .text("Intersect given data layers with given geometry and buffer radius and produce per data layer statistics");
+    }
+
+    @Override
+    public Map<String, Parameter<?>> getParameterInfo() {
+        return Collections.unmodifiableMap(prameterInfo);
+    }
+
+    @Override
+    public Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        // please note that this is a title for display purposes only
+        // finding an specific implementation by name is not possible
+        return Text.text("Hazard Statistics");
+    }
+
+    /**
+     * @return {@code true}
+     */
+    @Override
+    public boolean supportsProgress() {
+        return true;
+    }
+
+    @Override
+    public String getVersion() {
+        return "0.1.0";
+    }
+
+    @Override
+    public Process create() throws IllegalArgumentException {
+        return new HazardStatistics(this);
+    }
+}
