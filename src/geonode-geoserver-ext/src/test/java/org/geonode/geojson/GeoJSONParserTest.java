d46e95c92f362db63adc6f49d644bddc3f7faf9f
groldan <groldan@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-14 19:12:37 +0000

    CRS parsing support for GeoJSON geometries
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@576 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/geonode-geoserver-ext/src/test/java/org/geonode/geojson/GeoJSONParserTest.java
+++ src/geonode-geoserver-ext/src/test/java/org/geonode/geojson/GeoJSONParserTest.java
@@ -1,6 +1,8 @@
 package org.geonode.geojson;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.StringWriter;
 
@@ -8,30 +10,33 @@ import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 
-import org.geotools.feature.SchemaException;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.referencing.CRS;
 import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.opengis.feature.IllegalAttributeException;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
-import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.MultiLineString;
 import com.vividsolutions.jts.geom.MultiPoint;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.WKTReader;
 
 public class GeoJSONParserTest {
+
+    private static final WKTReader wkt = new WKTReader();
 
     private GeoJSONParser parser;
 
@@ -50,10 +55,8 @@ public class GeoJSONParserTest {
     }
 
     @Test
-    public void testPoint() throws IllegalAttributeException, JSONException, SchemaException {
-        GeometryFactory gf = new GeometryFactory();
-        Coordinate coord = new Coordinate(120.0, 0.0);
-        Point origPoint = gf.createPoint(coord);
+    public void testPoint() throws Exception {
+        Point origPoint = (Point) wkt.read("POINT(120 0)");
 
         JSONObject jsonObj = createJSONObject(origPoint);
 
@@ -63,15 +66,9 @@ public class GeoJSONParserTest {
     }
 
     @Test
-    public void testLineString() throws JSONException {
-        GeometryFactory gf = new GeometryFactory();
-        Coordinate[] coords = new Coordinate[4];
-        coords[0] = new Coordinate(20.2, 10.1);
-        coords[1] = new Coordinate(12.4, 1.0);
-        coords[2] = new Coordinate(12.4, 5.5);
-        coords[3] = new Coordinate(14, 42);
-
-        LineString origLine = gf.createLineString(coords);
+    public void testLineString() throws Exception {
+        LineString origLine = (LineString) wkt
+                .read("LINESTRING(20.2 10.1, 12.4 1, 12.4 5.5, 14 42)");
 
         JSONObject jsonObj = createJSONObject(origLine);
 
@@ -81,16 +78,8 @@ public class GeoJSONParserTest {
     }
 
     @Test
-    public void testPolygon() throws JSONException {
-        GeometryFactory gf = new GeometryFactory();
-        Coordinate[] coords = new Coordinate[4];
-        coords[0] = new Coordinate(0.0, 0.1);
-        coords[1] = new Coordinate(2.1, 0.12);
-        coords[2] = new Coordinate(1.4, 1.0);
-        coords[3] = new Coordinate(0.0, 0.1);
-
-        LinearRing ring = gf.createLinearRing(coords);
-        Polygon origPoly = gf.createPolygon(ring, null);
+    public void testPolygon() throws Exception {
+        Polygon origPoly = (Polygon) wkt.read("POLYGON((0 1, 1.2 0.12, 1.4 1, 0 1))");
 
         JSONObject jsonObj = createJSONObject(origPoly);
 
@@ -100,14 +89,8 @@ public class GeoJSONParserTest {
     }
 
     @Test
-    public void testMultiPoint() throws JSONException {
-        GeometryFactory gf = new GeometryFactory();
-        Coordinate[] coords = new Coordinate[3];
-        coords[0] = new Coordinate(142, 142);
-        coords[1] = new Coordinate(202.0, 128.0);
-        coords[2] = new Coordinate(84.0, 8.0);
-
-        MultiPoint origPoints = gf.createMultiPoint(coords);
+    public void testMultiPoint() throws Exception {
+        MultiPoint origPoints = (MultiPoint) wkt.read("MULTIPOINT(142 142, 202 128, 84 8)");
 
         JSONObject jsonObj = createJSONObject(origPoints);
 
@@ -117,13 +100,9 @@ public class GeoJSONParserTest {
     }
 
     @Test
-    public void testMultiLineString() throws JSONException {
-        GeometryFactory gf = new GeometryFactory();
-        LineString line1 = gf.createLineString(new Coordinate[] { new Coordinate(0, 0),
-                new Coordinate(1, 2) });
-        LineString line2 = gf.createLineString(new Coordinate[] { new Coordinate(3, 2),
-                new Coordinate(4, 2) });
-        MultiLineString origMLS = gf.createMultiLineString(new LineString[] { line1, line2 });
+    public void testMultiLineString() throws Exception {
+        MultiLineString origMLS = (MultiLineString) wkt
+                .read("MULTILINESTRING((0 0, 1 2), (3 2, 4 2))");
 
         JSONObject jsonObj = createJSONObject(origMLS);
 
@@ -133,16 +112,9 @@ public class GeoJSONParserTest {
     }
 
     @Test
-    public void testMultiPolygon() throws JSONException {
-        GeometryFactory gf = new GeometryFactory();
-        LinearRing ring1 = gf.createLinearRing(new Coordinate[] { new Coordinate(0, 0),
-                new Coordinate(1, 1), new Coordinate(1, 2), new Coordinate(0, 2),
-                new Coordinate(0, 0) });
-        LinearRing ring2 = gf.createLinearRing(new Coordinate[] { new Coordinate(10, 10),
-                new Coordinate(10, 14), new Coordinate(5, 12), new Coordinate(10, 10) });
-        Polygon poly1 = gf.createPolygon(ring1, null);
-        Polygon poly2 = gf.createPolygon(ring2, null);
-        MultiPolygon origMultiPoly = gf.createMultiPolygon(new Polygon[] { poly1, poly2 });
+    public void testMultiPolygon() throws Exception {
+        MultiPolygon origMultiPoly = (MultiPolygon) wkt
+                .read("MULTIPOLYGON(((0 0, 1 1, 1 2, 0 2, 0 0)), ((10 10, 10 14, 5 12, 10 10)))");
 
         JSONObject jsonObj = createJSONObject(origMultiPoly);
 
@@ -152,25 +124,12 @@ public class GeoJSONParserTest {
     }
 
     @Test
-    public void testGeometryCollection() throws JSONException {
-        GeometryFactory gf = new GeometryFactory();
-        Geometry[] geometries = new Geometry[3];
-        geometries[0] = gf.createPoint(new Coordinate(1.0, 1.4));
-        geometries[1] = gf.createLineString(new Coordinate[] { new Coordinate(32, 64),
-                new Coordinate(192, 160) });
-        geometries[2] = gf.createPoint(new Coordinate(2.0, 2.0));
+    public void testGeometryCollection() throws Exception {
+        GeometryCollection origCollection = (GeometryCollection) wkt
+                .read("GEOMETRYCOLLECTION(POINT(1 1.4), LINESTRING(32 64, 192 160), POINT(2 2))");
 
-        GeometryCollection origCollection = gf.createGeometryCollection(geometries);
+        JSONObject jsonObj = createJSONObject(origCollection);
 
-        // JSONObject jsonObj = createJSONObject(origCollection);
-        // TODO: For some reason GeoJSONBuilder dies on the above GeometryCollection
-        // A hack to manually construct the JSON string
-        String jsonStr = "{\"type\": \"GeometryCollection\", \"geometries\": "
-                + "[ { \"type\": \"Point\", \"coordinates\": [1.0, 1.4] },"
-                + "  { \"type\": \"LineString\", \"coordinates\": [ [32, 64], [192, 160] ]},"
-                + "  { \"type\": \"Point\", \"coordinates\": [2.0, 2.0] },]}";
-
-        JSONObject jsonObj = (JSONObject) JSONSerializer.toJSON(jsonStr);
         GeometryCollection newCollection = (GeometryCollection) GeoJSONParser.parse(jsonObj);
 
         if (origCollection.getNumGeometries() != newCollection.getNumGeometries()) {
@@ -178,9 +137,54 @@ public class GeoJSONParserTest {
         }
 
         for (int i = 0; i < origCollection.getNumGeometries(); i++) {
-            if (!origCollection.getGeometryN(i).equals(newCollection.getGeometryN(i))) {
-                assertTrue(false);
-            }
+            assertTrue(origCollection.getGeometryN(i).equals(newCollection.getGeometryN(i)));
+        }
+    }
+
+    @Test
+    public void testParseNamedCrsUnknownAuthority() throws Exception {
+        // GeoJSON CRS's can't live as standalone objects, they're identified as the "crs" property
+        // of a container object
+        String jsonStr = "{\"type\":\"Point\", \"coordinates\": [100.0, 0.0],"
+                + "\"crs\":{\"type\": \"name\",\"properties\": {\"name\": \"urn:ogc:def:crs:OGC:1.3:CRS84\"}}}";
+        try {
+            GeoJSONParser.parse(jsonStr);
+            fail("expected NoSuchAuthorityCodeException");
+        } catch (JSONException e) {
+            assertTrue(e.getCause() instanceof NoSuchAuthorityCodeException);
+        }
+    }
+
+    @Test
+    public void testParseNamedCrs() throws Exception {
+        // GeoJSON CRS's can't live as standalone objects, they're identified as the "crs" property
+        // of a container object
+        String jsonStr = "{\"type\":\"Point\", \"coordinates\": [100.0, 0.0],"
+                + "\"crs\":{\"type\": \"name\",\"properties\": {\"name\": \"EPSG:4326\"}}}";
+
+        Object parsed = GeoJSONParser.parse(jsonStr);
+        assertNotNull(parsed);
+
+        CoordinateReferenceSystem expected = CRS.decode("EPSG:4326");
+
+        assertTrue(parsed instanceof Point);
+        assertTrue(((Point) parsed).getUserData() instanceof CoordinateReferenceSystem);
+        assertTrue(CRS.equalsIgnoreMetadata(expected, ((Point) parsed).getUserData()));
+    }
+
+    @Test
+    public void testParseLinkedCrsIsUnsupported() throws Exception {
+        // GeoJSON CRS's can't live as standalone objects, they're identified as the "crs" property
+        // of a container object.
+        // Linked CRSs are not supported:
+        String jsonStr = "{\"type\":\"Point\", \"coordinates\": [100.0, 0.0],"
+                + "\"crs\":{\"type\": \"link\",\"properties\": {\"href\": \"http://example.com/crs/42\", \"type\": \"proj4\"}}}";
+
+        try {
+            GeoJSONParser.parse(jsonStr);
+            fail("Expected UnsupportedOperationException for linked CRS's");
+        } catch (UnsupportedOperationException e) {
+            assertTrue(true);
         }
     }
 
