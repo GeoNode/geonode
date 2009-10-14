aa972a6553b7ac7c1ced4c0e0cc242fd8ad4da68
dwinslow <dwinslow@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-14 20:51:32 +0000

    Take out the checkboxes and prevent the opacity slider from marking fully transparent layers as hidden.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@579 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/capra-client/src/script/app/MyHazard/Viewer.js
+++ src/capra-client/src/script/app/MyHazard/Viewer.js
@@ -6,10 +6,6 @@ MyHazard.Viewer = Ext.extend(Ext.util.Observable, {
     sidebar: null,
     hazardConfig: null,
     
-    setLayerTransparencyText: "UT: Set layer transparency...",
-    showLayersText: "UT: Show active layers",
-    hideLayersText: "UT: Hide active layers",
-
     constructor: function (config) {
         this.initialConfig = config;
         Ext.apply(this, this.initialConfig);
@@ -213,24 +209,13 @@ MyHazard.Viewer = Ext.extend(Ext.util.Observable, {
                 tooltip: this.transparencyMenuText,
                 iconCls: "icon-visibility",
                 menu: [
-                    new Ext.menu.CheckItem({
-                        text: this.hideLayersText
-                    }),
-                    new Ext.menu.CheckItem({
-                        text: this.showLayersText
-                    }),
-                    new Ext.menu.Separator(), 
-                    {
-                        text: this.setLayerTransparencyText,
-                        menu: [
-                            new Ext.menu.Adapter(
-                                new MyHazard.LayerGroupOpacitySlider({
-                                    layers: layers,
-                                    width: 150
-                                })
-                            )
-                        ]
-                    }
+                    new Ext.menu.Adapter(
+                        new MyHazard.LayerGroupOpacitySlider({
+                            layers: layers,
+                            vertical: true,
+                            height: 150
+                        })
+                    )
                 ]
             })
         ];
