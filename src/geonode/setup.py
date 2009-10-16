3dabf45fab1d816240b084b8ba71afa270b0ed54
sbenthall <sbenthall@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-16 16:15:54 +0000

    a setup.py file here to try to get this to work with pip
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@596 125c47a5-210e-49ec-a0c6-83efc728e0a1

---
+++ src/geonode/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='geonode',
+      version='0.0',
+      description='geonode webapp',
+      author='',
+      author_email='',
+      url='',
+      packages=['geonode'],
+     )
