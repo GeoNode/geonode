56dd8bd2507b4c9ce384e20cd9d4c605f57ce5e1
whit <whit@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-22 21:10:23 +0000

    add py packages
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@613 125c47a5-210e-49ec-a0c6-83efc728e0a1

---
+++ src/GeoNodePy/geonode/maps/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Map(models.Model):
+    # metadata fields
+    title = models.CharField(max_length=200)
+    abstract = models.CharField(max_length=200)
+    contact = models.CharField(max_length=200)
+    featured = models.BooleanField()
+    endorsed = models.BooleanField()
+
+    # viewer configuration
+    zoom = models.IntegerField()
+    center_lat = models.FloatField()
+    center_lon = models.FloatField()
+
+    def __unicode__(self):
+        return '%s by %s' % (self.title, self.contact)
+
+    def get_absolute_url(self):
+        return '/maps/%i' % self.id
+
+class Layer(models.Model):
+    name = models.CharField(max_length=200)
+    ows_url = models.URLField()
+    group = models.CharField(max_length=200,blank=True)
+    stack_order = models.IntegerField()
+    map = models.ForeignKey(Map)
+
+    class Meta:
+        ordering = ["stack_order"]
+
+    def __unicode__(self):
+        return '%s?layers=%s' % (self.ows_url, self.name)
+
+
---
+++ src/GeoNodePy/geonode/proxy/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from httplib import HTTPConnection
+from urlparse import urlsplit
+
+def proxy(request):
+    if 'url' not in request.GET:
+        return HttpResponse('The proxy service requires a URL-encoded URL as a parameter.', status=400, content_type="text/plain")
+
+    url = urlsplit(request.GET['url'])
+    locator = url.path
+    if url.query != "":
+        locator += '?' + url.query
+    if url.fragment != "":
+        locator += '#' + url.fragment
+
+    conn = HTTPConnection(url.hostname, url.port)
+    conn.request(request.method, locator, request.raw_post_data)
+    result = conn.getresponse()
+    
+    response = HttpResponse(result.read(), status=result.status, content_type=result.getheader("Content-Type", "text/plain"))
+    return response
+
+
