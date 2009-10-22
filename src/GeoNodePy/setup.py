8c0cb1a76b679ea2e1919ddf5eb6c3aa435875ef
whit <whit@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-22 21:11:55 +0000

    fix up numbers and readme
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@614 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/GeoNodePy/setup.py
+++ src/GeoNodePy/setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup, find_packages
+import pkg_resources
 import sys, os
-
-version = '0.1'
 
 name='GeoNodePy'
 version = "1.0.1"
+req = pkg_resource.Requirement.parse(name)
 
 setup(name=name,
       version=version,
       description="Application for serving and sharing geospatial data",
-      long_description=""" """,
+      long_description=pkg_resources.resource_string(req, 'README.rst'),
       classifiers=[
         "Development Status :: 1 - Planning" ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='GeoNode Developers',
+      author='GeoNode Developers',`
       author_email='dev@geonode.org',
       url='http://geonode.org',
       license='GPL',
-      packages = ['capra', 'geonode',],
-      package_dir = {'': 'src'},
+      packages = find_packages(),
       include_package_data=True,
       zip_safe=False,
       entry_points="""
