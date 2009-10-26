322130dde521736d388f748ff5a758145f04047a
whit <whit@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2009-10-26 20:43:58 +0000

    kill old capra py
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@620 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/GeoNodePy/setup.py
+++ src/GeoNodePy/setup.py
@@ -4,12 +4,12 @@ import sys, os
 
 name='GeoNodePy'
 version = "1.0.1"
-req = pkg_resource.Requirement.parse(name)
+req = pkg_resources.Requirement.parse(name)
 
 setup(name=name,
       version=version,
       description="Application for serving and sharing geospatial data",
-      long_description=pkg_resources.resource_string(req, 'README.rst'),
+      long_description=open('README.rst').read(),
       classifiers=[
         "Development Status :: 1 - Planning" ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
