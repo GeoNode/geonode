30308d5e4fa744fde9bfef0bed30672672f24eba
iwillig <iwillig@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2010-01-11 21:34:04 +0000

    removed installed application that was not finished
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@807 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/GeoNodePy/capra/settings.py
+++ src/GeoNodePy/capra/settings.py
@@ -91,7 +91,7 @@ pattern = re.compile("hazard")
 if not pattern.search(NAVBAR['meta']['visible']):
     NAVBAR['meta']['visible'] += "\nhazard" #Why is this a string not a list?
 
-INSTALLED_APPS += ('capra.hazard','capra.safehospitals',)
+INSTALLED_APPS += ('capra.hazard',)
 
 TEMPLATE_DIRS = (path_extrapolate("capra/hazard/templates"),path_extrapolate("capra/safehospitals/templates"),) + TEMPLATE_DIRS 
 
