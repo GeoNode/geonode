c8a3817d8176a92b917c0e0a6afe3b51dd32764e
dwinslow <dwinslow@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2010-01-14 22:44:35 +0000

    Update i18n dictionaries for MyHazard application as well.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@834 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/GeoNodePy/capra/hazard/templates/hazard/lang.js
+++ src/GeoNodePy/capra/hazard/templates/hazard/lang.js
@@ -1,16 +1,17 @@
 {% load i18n %}
 
-if(window.MyHazard && window.MyHazard.Viewer){
-  Ext.apply(MyHazard.Viewer.prototype, {
-      pdfButtonText: gettext("PDF Report"),
-      reportSwitcherTip: gettext("Request hazard data by clicking on the map"),
-      pointReporterTip: gettext("Point reporter"),
-      lineReporterTip: gettext("Line reporter"),
-      polygonReporterTip: gettext("Polygon reporter"),
-      navActionTipText: gettext("Pan map"),
-      zoomSliderTipText: gettext("Zoom level"),
-      transparencyButtonText: gettext("Set layer transparency"),
-      reportFailureMessage: gettext("Failure while retrieving report..."),
-      reportPopupTitle: gettext("MyHazard Report")
-  });
+if (window.MyHazard && window.MyHazard.Viewer) {
+    Ext.apply(MyHazard.Viewer.prototype, {
+        lineReporterTip: gettext("Line reporter"),
+        navActionTipText: gettext("Pan map"),
+        pdfButtonText: gettext("PDF Report"),
+        pointReporterTip: gettext("Point reporter"),
+        polygonReporterTip: gettext("Polygon reporter"),
+        reportEmptyDataSetMessage: gettext("Please select some layers before requesting a report."),
+        reportFailureMessage: gettext("Failure while retrieving report..."),
+        reportPopupTitle: gettext("MyHazard Report"),
+        reportSwitcherTip: gettext("Tooltip here"),
+        transparencyButtonText: gettext("Transparency"),
+        zoomSliderTipText: gettext("Zoom level")
+    });
 }
