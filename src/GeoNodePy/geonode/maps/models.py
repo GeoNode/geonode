e6c6c4374bb4f641329025941ec788235927c99f
dwinslow <dwinslow@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2010-02-08 16:05:22 +0000

    Avoid connecting to GeoServer on module loading; wait for a method to be called that requires it.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@872 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/GeoNodePy/geonode/maps/models.py
+++ src/GeoNodePy/geonode/maps/models.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.db import models
 from owslib.wms import WebMapService
 
+__wms__ = None
+
 class LayerManager(models.Manager):
     def slurp(self):
         wms_url = "%swms?request=GetCapabilities" % settings.GEOSERVER_BASE_URL
@@ -10,8 +12,8 @@ class LayerManager(models.Manager):
             if name is not None:
                 self.model(typename=name).save()
 
+
 class Layer(models.Model):
-    wms = WebMapService("%swms?request=GetCapabilities" % settings.GEOSERVER_BASE_URL)
     objects = LayerManager()
     typename = models.CharField(max_length=128)
 
@@ -41,13 +43,15 @@ class Layer(models.Model):
         return self.metadata().styles
 
     def metadata(self): 
-        if not self.typename in self.__class__.wms.contents:
+        global __wms__
+        if (__wms__ is None) or (self.typename not in __wms__.contents):
             wms_url = "%swms?request=GetCapabilities" % settings.GEOSERVER_BASE_URL
-            self.__class__.wms = WebMapService(wms_url)
-        return self.__class__.wms[self.typename]
+            __wms__ = WebMapService(wms_url)
+        return __wms__[self.typename]
 
     def __str__(self):
         return "%s Layer" % self.typename
+
 
 class Map(models.Model):
     # metadata fields
@@ -68,6 +72,7 @@ class Map(models.Model):
     def get_absolute_url(self):
         return '/maps/%i' % self.id
 
+
 class MapLayer(models.Model):
     name = models.CharField(max_length=200)
     ows_url = models.URLField()
@@ -80,5 +85,3 @@ class MapLayer(models.Model):
 
     def __unicode__(self):
         return '%s?layers=%s' % (self.ows_url, self.name)
-
-
