2be56dd50aca1c1ca2030b918c9fa04362ac2275
dwinslow <dwinslow@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2010-02-23 21:35:04 +0000

    Add MapBrowser base class to house the common functionality between CommunityPage and CuratedPage.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@904 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/geonode-client/src/script/app/CommunityPage.js
+++ src/geonode-client/src/script/app/CommunityPage.js
@@ -1,19 +1,4 @@
-
-var CommunityPage = Ext.extend(Page, {
-
-    /**
-     * Property: capabilities
-     * {GeoExt.data.WMSCapabilitiesStore} A store containing a record for each
-     *     layer on the server.
-     */
-    capabilities: null,
-
-    /**
-     * Property: capGrid
-     * {<Ext.Window>} A window which includes a CapabilitiesGrid panel.
-     */
-    capGrid: null,
-
+var CommunityPage = Ext.extend(MapBrowser, {
     page: {
         'map-description': '',
         sidebar: '<h3> UT: Contributed Maps</h3>' +
@@ -26,105 +11,7 @@ var CommunityPage = Ext.extend(Page, {
             '<p>Or click here to <a href="map.html">create your own map</a>!</p>'
     },
 
-    constructor: function(config) {
-
-	// pass on any proxy config to OpenLayers
-	if(this.proxy) {
-	    OpenLayers.ProxyHost = this.proxy;
-	}
-
-        Page.prototype.constructor.apply(this, arguments);
-    },
-
-
-    /**
-     * Method: owsURL
-     */
-
-    owsURL : function(params){
-        var argIndex = this.ows.indexOf("?");
-        if(argIndex > -1) {
-            var search = this.ows.substring(this.ows.indexOf("?")+1);
-            var url = this.ows.replace(search, Ext.urlEncode(Ext.apply(
-                Ext.urlDecode(search), params)));
-        } else {
-            url = this.ows + "?" + Ext.urlEncode(params);
-        }
-        if(this.proxy) {
-            url = this.proxy + encodeURIComponent(url);
-        }
-
-        return url;
-    },
-
-
-
-    /**
-     * Method: createLayout
-     * Create the various parts that compose the layout.
-     */
-    createLayout: function() {
-
-        // create layer store
-        this.layers = new GeoExt.data.LayerStore({});
-    },
-
-    /**
-     * Method: activate
-     * Activate the application.  Call after application is configured.
-     */
-    activate: function() {
-        this.initPanel();
-
-        // initialize tooltips
-        Ext.QuickTips.init();
-
-        this.fireEvent("ready");
-    },
-
-
-    /**
-     * Method: owsURL
-     */
-
-    restURL : function(params){
-        var argIndex = this.rest.indexOf("?");
-        if(argIndex > -1) {
-            var search = this.rest.substring(this.rest.indexOf("?")+1);
-            var url = this.ows.replace(search, Ext.urlEncode(Ext.apply(
-                Ext.urlDecode(search), params)));
-        } else {
-            url = this.rest + "?" + Ext.urlEncode(params);
-        }
-
-        return url;
-    },
-
-    /**
-     * Method: initPanel
-     * Constructs the map grid.
-     */
-    initPanel: function() {
-
-        var mapGrid = new MapGrid({
-            store : new Ext.data.JsonStore({
-                url: this.restURL(),
-                root: 'maps',
-                id: 'id',
-                fields: [
-                    {name: 'id', mapping: 'id'},
-                    {name: "title", mapping: "config.about.title"},
-                    {name: "abstract", mapping: "config.about.abstract"},
-                    {name: "contact", mapping: "config.about.contact"},
-                    {name: "endorsed", mapping: "config.about.endorsed"}
-                ],
-                autoLoad: true,
-                listeners : {
-                    "load" : function(store){
-                        store.filter("endorsed", false);
-                    }
-                }
-            })
-        });
+    filterStore: function(store) {
+        store.filter("endorsed", false);
     }
 });
