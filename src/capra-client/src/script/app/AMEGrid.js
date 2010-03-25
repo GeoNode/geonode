6cee4de098dd4166edf6046fd6574acc68b1153c
ltucker <ltucker@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2010-03-25 18:33:25 +0000

    move AME file grid javascript to capra-client, internationalize
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@956 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/capra-client/src/script/app/AMEGrid.js
+++ src/capra-client/src/script/app/AMEGrid.js
@@ -1,80 +1,67 @@
 Ext.namespace("capra");
 capra.AMEGrid = Ext.extend(Ext.util.Observable, {
+    gridTitleText: 'UT: AME Data',
     countryLabelText: 'UT: Country',
     scenarioLabelText: 'UT: Scenario',
-    ame_link_prefix: 'http://localhost:8001/geoserver/www',
-    ameLabelText: 'UT:AME File',
-    singularFile: 'UT:File',
-    pluralFiles: 'UT:Files',
-
+    ameLabelText: 'UT: Name',
+    singularFileText: 'UT:File',
+    pluralFilesText: 'UT:Files',
+    downloadText: 'UT:Download',
+    fileDetailsText: 'UT: More Information about this file',
+    
     /**
      * A store containing metadata for the AME files stored on this server.
      */
     ameStore: null, 
     
     constructor: function(config) {
+        /* expects: renderTo, file_info_url */
         Ext.apply(this, config);
         this.loadData();
     }, 
 
     loadData: function() {
-        var json_url = this.ows.replace(/\/...$/, '') + "/www/out.json";
-        if (this.proxy) {
-            json_url = this.proxy + "?" + Ext.urlEncode({ "url": json_url });
-        }
-
         this.ameStore = new Ext.data.GroupingStore({
-            proxy: new Ext.data.HttpProxy({url: json_url}),
-            reader: new Ext.data.JsonReader({
-                fields: ['rel_tifs', 'path', 'scenario', 'country']
-            }),
+            proxy: new Ext.data.HttpProxy({url: this.file_info_url}),
+            reader: new Ext.data.JsonReader({fields: ['name','download_url', 'edit_url', 'filename', 'scenario','country']}),
             groupField: 'scenario',
             sortInfo: {field: 'scenario', direction: 'ASC'},
             groupOnSort: true
         });
-
-        gxp.util.dispatch([
-                function(done) {
-                    this.ameStore.load({
-                        callback: done,
-                        scope: this
-                    });
-                }
-            ], 
-            this.doLayout,
-            this
-        );
+        gxp.util.dispatch(
+            [function(done) {
+                this.ameStore.load({callback: done, scope: this});
+             }], this.doLayout, this);
     }, 
 
     doLayout: function() {
-        var ame_link_prefix = this.ame_link_prefix;
+        var ame_detail = '<p><b>' + this.downloadText + '</b> <a class = "download ame" href="{download_url}">{filename}</a></p>'
+                       + '<p><a href="{edit_url}">' + this.fileDetailsText  + '</a>';
+
+        var ameExpander = new Ext.grid.RowExpander({
+            tpl: new Ext.Template(ame_detail)
+        });
+
         new Ext.grid.GridPanel({
-            title: 'AME Data',
-            height: 200,
+            renderTo: this.renderTo,
+            title: this.gridTitleText,
             store: this.ameStore,
-            columns: [{
-                id: 'path', 
-                header: this.ameLabelText, 
-                dataIndex: 'path', 
-                renderer: function (path) {
-                    return '<a href="' + ame_link_prefix + '/' + path + '">' + path.replace(/.*\//, '') + '</a';
-                }
-            }, { 
-                header: this.scenarioLabelText,
-                dataIndex: 'scenario' 
-            }, { 
-                header: this.countryLabelText, 
-                dataIndex: 'country' 
-            }],
+            plugins: [ameExpander],
+            columns: [
+                ameExpander,
+                {header: this.ameLabelText, dataIndex: 'name' },
+                {header: this.scenarioLabelText, dataIndex: 'scenario'},
+                {header: this.countryLabelText, dataIndex: 'country'}
+            ],
+            height: 200,
             view: new Ext.grid.GroupingView({
                 forceFit:true,
                 // custom grouping text template to display the number of items per group
                 groupTextTpl: '{text} ({[values.rs.length]} ' +
-                    '{[values.rs.length == 1 ? "' + this.singularFile + 
-                    '" : "' + this.pluralFiles +'"]})'
+                    '{[values.rs.length == 1 ? "' + this.singularFileText + 
+                    '" : "' + this.pluralFilesText +'"]})'
             }),
-            viewConfig: {autoFill: true},
-            renderTo: this.renderTo
+            viewConfig: {autoFill: true}
         });
     }
 });
