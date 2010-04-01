1b49ecd88f8ff5b5bfaa6d1b77a7411a12c3e18f
ltucker <ltucker@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2010-04-01 21:57:03 +0000

    #390, #388, #474 ext-ify layer data upload form to provide progress indicator and fix styling issues.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@1010 125c47a5-210e-49ec-a0c6-83efc728e0a1

---
+++ src/geonode-client/src/script/ux/loader.js
@@ -0,0 +1,29 @@
+(function() {
+
+    var jsfiles = new Array(
+        "/static/src/script/ux/RowExpander.js",
+        "/static/src/script/ux/FileUploadField.js"
+    );
+
+    var appendable = !((/MSIE/).test(navigator.userAgent) ||
+                       (/Safari/).test(navigator.userAgent));
+    var pieces = new Array(jsfiles.length);
+
+    var element = document.getElementsByTagName("head").length ?
+                    document.getElementsByTagName("head")[0] :
+                    document.body;
+    var script;
+
+    for(var i=0; i<jsfiles.length; i++) {
+        if(!appendable) {
+            pieces[i] = "<script src='" + jsfiles[i] + "'></script>"; 
+        } else {
+            script = document.createElement("script");
+            script.src = jsfiles[i];
+            element.appendChild(script);
+        }
+    }
+    if(!appendable) {
+        document.write(pieces.join(""));
+    }
+})();
