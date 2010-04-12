f479611eeb3bee64f4d3362bcae5a4e7914c4594
iwillig <iwillig@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2010-04-12 16:03:19 +0000

    some commits towards closing #372
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@1036 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/GeoNodePy/geonode/proxy/views.py
+++ src/GeoNodePy/geonode/proxy/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from httplib import HTTPConnection
 from urlparse import urlsplit
+import httplib2
 
 def proxy(request):
     if 'url' not in request.GET:
@@ -16,8 +17,13 @@ def proxy(request):
     conn = HTTPConnection(url.hostname, url.port)
     conn.request(request.method, locator, request.raw_post_data)
     result = conn.getresponse()
-    
     response = HttpResponse(result.read(), status=result.status, content_type=result.getheader("Content-Type", "text/plain"))
     return response
 
 
+def geoserver(request):
+    url = "http://localhost:8001{url}".format(url=request.environ["PATH_INFO"])
+    h = httplib2.Http()
+    h.add_credentials('admin', 'geoserver')
+    resp, content = h.request(url,request.method)
+    return HttpResponse(content) 
