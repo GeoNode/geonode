b5e806aeaa76df3cf5b1921aa440bc2877b8c64f
ltucker <ltucker@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2010-04-28 19:11:21 +0000

    Add a workaround to enable the "view" privilege for everyone on layers uploaded to GeoNetwork.  This allows items to appear in CSW search results.
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@1104 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/GeoNodePy/geonode/geonetwork.py
+++ src/GeoNodePy/geonode/geonetwork.py
@@ -3,6 +3,7 @@ from datetime import date
 from django.template import Context
 from django.template.loader import get_template
 from xml.dom import minidom
+from xml.etree.ElementTree import XML
 
 class Catalog(object):
 
@@ -57,16 +58,18 @@ class Catalog(object):
             "Accept": "text/plain"
         }
         request = urllib2.Request(url, md_doc, headers)
-
-        if self.opener is None:
-            response = urllib2.urlopen(request)
-        else:
-            response = self.opener.open(request)
+        response = self.urlopen(request)
 
         # print layer.uuid
         # print response.read()
         # TODO: Parse response, check for error report
 
+
+        # Turn on the "view" permission (aka publish) for
+        # the "all" group in GeoNetwork so that the layer
+        # will be searchable via CSW without admin login.
+        self.set_metadata_priv(layer.uuid, "all", "view", True)
+        
         return self.base + "srv/en/csw?" + urllib.urlencode({
             "request": "GetRecordById",
             "service": "CSW",
@@ -78,3 +81,64 @@ class Catalog(object):
 
     def update_from_layer(self, record, layer):
         pass
+
+    def set_metadata_priv(self, uuid, group, operation, enabled):
+        """
+        set the geonetwork privilege 'operation' to the value 
+        of 'enabled' for the group 'group' on the metadata
+        item identified by 'uuid'
+        """
+        
+        # XXX This is a fairly ugly workaround that makes 
+        # requests similar to those made by the GeoNetwork
+        # admin based on the recommendation here: 
+        # http://bit.ly/ccVEU7
+        
+        get_dbid_url = self.base + 'srv/en/portal.search.present?' + urllib.urlencode({'uuid': uuid})
+        get_groups_url = self.base + "srv/en/xml.info?" + urllib.urlencode({'type': 'groups'})
+        get_ops_url = self.base + "srv/en/xml.info?" + urllib.urlencode({'type': 'operations'})
+    
+    
+        # get the id of the data.
+        request = urllib2.Request(get_dbid_url)
+        response = self.urlopen(request)
+        doc = XML(response.read())
+        data_dbid = doc.find('metadata/{http://www.fao.org/geonetwork}info/id').text
+
+        # get the id of the group.
+        request = urllib2.Request(get_groups_url)
+        response = self.urlopen(request)
+        doc = XML(response.read())
+        group_id = None
+        for gp in doc.findall('groups/group'):
+            if gp.find('name').text.lower() == group.lower():
+                group_id = gp.attrib['id']
+        if group_id is None:
+            raise Exception('Unable to locate geonetwork group "%s"' % group)
+        
+        # get the id of the operation    
+        request = urllib2.Request(get_ops_url)
+        response = self.urlopen(request)
+        doc = XML(response.read())
+        operation_id = None
+        for op in doc.findall('operations/operation'):
+            if op.find('name').text.lower() == operation.lower():
+                operation_id = op.attrib['id']
+        if operation_id is None:
+            raise Exception('Unable to locate geonetwork privilege "%s"' % operation)
+
+        # update the privilege 
+        update_privs_url = self.base + "srv/en/metadata.admin?" + urllib.urlencode({
+            "id": data_dbid, # "uuid": layer.uuid, # you can say this instead in newer versions of GN 
+            "_%s_%s" % (group_id, operation_id): "on", # All: Publish=on
+        })
+        request = urllib2.Request(update_privs_url)
+        response = self.urlopen(request)
+
+        # TODO: check for error report
+
+    def urlopen(self, request):
+        if self.opener is None:
+            return urllib2.urlopen(request)
+        else:
+            return self.opener.open(request)
