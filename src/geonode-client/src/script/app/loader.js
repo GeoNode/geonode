778defc0805158ff8283bb8db70e000ecbe3d701
ltucker <ltucker@125c47a5-210e-49ec-a0c6-83efc728e0a1>
2010-05-19 20:09:12 +0000

    #362 #363 bounding box constraint on data search
    git-svn-id: https://svn.opengeo.org/CAPRA/GeoNode/trunk@1142 125c47a5-210e-49ec-a0c6-83efc728e0a1

--- src/geonode-client/src/script/app/loader.js
+++ src/geonode-client/src/script/app/loader.js
@@ -3,7 +3,6 @@
     var jsfiles = new Array(
         "/static/src/script/app/GeoExplorer.js",
         "/static/src/script/app/GeoNode/DataGrid.js",
-        "/static/src/script/app/GeoNode/SearchTable.js",
         "/static/src/script/app/Lang.js",
         "/static/src/script/app/Embed.js",
         "/static/src/script/app/LayerPreview.js",
@@ -13,7 +12,8 @@
         "/static/src/script/app/GeoExplorer/CapabilitiesGrid.js",
         "/static/src/script/app/GeoExplorer/CapabilitiesRowExpander.js",
         "/static/src/script/app/GeoExplorer/GoogleEarthPanel.js",
-        "/static/src/script/app/GeoExplorer/NewSourceWindow.js"
+        "/static/src/script/app/GeoExplorer/NewSourceWindow.js",
+        "/static/src/script/app/GeoNode/SearchTable.js"
     );
 
     var appendable = !((/MSIE/).test(navigator.userAgent) ||
