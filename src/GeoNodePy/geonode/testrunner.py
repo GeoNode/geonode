3982935b359c4d29a7558a0bf9998627176fb3ea
Ariel Núñez <ingenieroariel@gmail.com>
2010-06-15 15:06:33 -0500

    Have the testrunner set the GEOSERVER flag for the tests when port 8001 is used

--- src/GeoNodePy/geonode/testrunner.py
+++ src/GeoNodePy/geonode/testrunner.py
@@ -2,11 +2,32 @@ from django.test.simple import DjangoTestSuiteRunner, TestCase
 from django.test.simple import reorder_suite, build_test, build_suite
 from django.db.models import get_app, get_apps
 import unittest
+import socket
+import os
+
+def isOpen(ip,port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+     s.connect((ip, int(port)))
+     s.shutdown(2)
+     return True
+    except:
+     return False
+
 
 class GeoNodeTestRunner(DjangoTestSuiteRunner):
     """ This test runner allows the exclusion of some
         of the apps from the test suite
     """
+    def __init__(self, verbosity=1, interactive=True, failfast=True, **kwargs):
+        self.verbosity = verbosity
+        self.interactive = interactive
+        self.failfast = failfast
+        # Check if a server is running in port 8080 and set the GEOSERVER flag
+        if "GEOSERVER" not in os.environ.keys():
+            if isOpen('127.0.0.1', '8001'):
+                os.environ["GEOSERVER"]="True"
+
     EXCLUDED_APPS = [
         # Registration complains about email
         # messages in spanish, ticket has been filled at:
