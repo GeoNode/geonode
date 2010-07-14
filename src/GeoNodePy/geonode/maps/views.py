39c7c64f508b8d4223cbb2a0832c2bb25e3dbda1
Luke Tucker <ltucker@openplans.org>
2010-07-14 11:05:36 -0400

    add username field to data/acls

--- src/GeoNodePy/geonode/maps/views.py
+++ src/GeoNodePy/geonode/maps/views.py
@@ -873,6 +873,7 @@ def layer_acls(request):
     result = {
         'rw': read_write,
         'ro': read_only,
+        'username': acl_user.username,
         'is_superuser':  acl_user.is_superuser,
         'is_anonymous': acl_user.is_anonymous()
     }
