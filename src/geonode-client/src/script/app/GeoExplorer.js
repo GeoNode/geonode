1ed14b4287bb914407689b9784ac17101977323c
Sebastian Benthall <sbenthall@gmail.com>
2010-07-14 11:55:22 -0400

    actually, did need this.  see save as copy handler L1648

--- src/geonode-client/src/script/app/GeoExplorer.js
+++ src/geonode-client/src/script/app/GeoExplorer.js
@@ -1704,7 +1704,7 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
      * as - a boolean, True if map should be "Saved as..."
      * instead of overwritten
      */
-    save : function(){
+    save : function(as){
         var config = this.configManager.getConfig(this);
         
         var failure = function(response, options) {
@@ -1719,7 +1719,7 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
             }).show();
         };
 
-        if (!this.mapID) {
+        if (!this.mapID || as) {
             /* create a new map */ 
             Ext.Ajax.request({
                 url: this.rest,
