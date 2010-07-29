1f460d6fffc795a0c0cf8e7f100774294735fd53
ahocevar <ahocevar@opengeo.org>
2010-07-29 09:32:22 +0200

    Use 22 zoom levels instead of 20. This required some changes in OpenLayers, and we will have to revisit this when we start supporting arbitrary projections (closes http://projects.opengeo.org/CAPRA/ticket/540)

--- src/geonode-client/src/script/app/GeoExplorer.js
+++ src/geonode-client/src/script/app/GeoExplorer.js
@@ -215,6 +215,11 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
             fontColor: "#000000"
         };
 
+        if (!config.map) {
+            config.map = {};
+        }
+        config.map.numZoomLevels = config.map.numZoomLevels || 22;
+
         GeoExplorer.superclass.constructor.apply(this, arguments);
 
         this.mapID = this.initialConfig.id;
@@ -851,7 +856,10 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
                 });
                 if (record) {
                     if (record.get("group") === "background") {
-                        layerStore.insert(0, [record]);
+                        var pos = layerStore.queryBy(function(rec) {
+                            return rec.get("group") === "background"
+                        }).getCount();
+                        layerStore.insert(pos, [record]);
                     } else {
                         layerStore.add([record]);
                     }
