c834e30bde43d22ad79cf3aaab7a228a747031e6
Sebastian Benthall <sbenthall@gmail.com>
2010-07-29 15:01:32 -0400

    lazy translating model fields (#668)

--- src/GeoNodePy/capra/hazard/models.py
+++ src/GeoNodePy/capra/hazard/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from geonode.maps.models import Layer
+from django.utils.translation import ugettext as _
 
 class Hazard(models.Model): 
-    name = models.CharField(max_length=128)
+    name = models.CharField(_('name'), max_length=128)
 
     def __str__(self): 
         return "%s Hazard" % self.name
 
 class Period(models.Model):
-    hazard = models.ForeignKey(Hazard)
-    layer = models.OneToOneField(Layer)
-    length = models.IntegerField()
+    hazard = models.ForeignKey(Hazard, verbose_name=_('hazard'))
+    layer = models.OneToOneField(Layer, verbose_name=('layer'))
+    length = models.IntegerField(_('length'))
