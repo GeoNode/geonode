bdfaef2ce23232f87129cfc66935672b5cd8c3e3
David Winslow <dwinslow@opengeo.org>
2010-08-12 10:58:48 -0400

    Use proj4 params instead of srid for spherical mercator
    This fixes http://projects.opengeo.org/CAPRA/ticket/728

--- src/GeoNodePy/geonode/maps/views.py
+++ src/GeoNodePy/geonode/maps/views.py
@@ -253,7 +253,7 @@ def newmap(request):
                 y = (miny + maxy) / 2
                 wkt = "POINT(" + str(x) + " " + str(y) + ")"
                 center = GEOSGeometry(wkt, srid=4326)
-                center.transform(3785)
+                center.transform("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs")
 
                 width_zoom = math.log(360 / (maxx - minx), 2)
                 height_zoom = math.log(360 / (maxy - miny), 2)
