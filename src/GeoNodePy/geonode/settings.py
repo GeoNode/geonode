c4f7188a4b510888053fffa35cc5d2370c9994ef
David Winslow <dwinslow@opengeo.org>
2010-10-06 16:40:27 -0400

    Add OpenStreetMap as a baselayer

--- src/GeoNodePy/geonode/settings.py
+++ src/GeoNodePy/geonode/settings.py
@@ -145,9 +145,16 @@ MAP_BASELAYERS = [{
     "group":"background"
   },{
     "source":"any",
+    "type":"OpenLayers.Layer.OSM",
+    "args":["OpenStreetMap"],
+    "visibility": True,
+    "fixed": True,
+    "group":"background"
+  },{
+    "source":"any",
     "type":"OpenLayers.Layer.WMS",
     "group":"background",
-    "visibility": True,
+    "visibility": False,
     "fixed": True,
     "args":[
       "bluemarble",
