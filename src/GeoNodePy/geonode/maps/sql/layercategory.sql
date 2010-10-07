900f9954fb531a28666cf803a70032a9633f1946
mattbert <mbertrand@cga.harvard.edu>
2010-10-07 14:20:26 -0400

    SQL scripts folder and script to insert default categories in db

---
+++ src/GeoNodePy/geonode/maps/sql/layercategory.sql
@@ -0,0 +1,21 @@
+INSERT into maps_layercategory (name) VALUES ('Environmental');
+INSERT into maps_layercategory (name) VALUES ('Base Mapping');
+INSERT into maps_layercategory (name) VALUES ('Economic');
+INSERT into maps_layercategory (name) VALUES ('Ethno and Linguistic');
+INSERT into maps_layercategory (name) VALUES ('Ethno-Classification');
+INSERT into maps_layercategory (name) VALUES ('Harvard Collections');
+INSERT into maps_layercategory (name) VALUES ('Health');
+INSERT into maps_layercategory (name) VALUES ('Historic Maps 1600 to 1800');
+INSERT into maps_layercategory (name) VALUES ('Historic Maps 1800 to 1950');
+INSERT into maps_layercategory (name) VALUES ('Local Projects');
+INSERT into maps_layercategory (name) VALUES ('Period Maps');
+INSERT into maps_layercategory (name) VALUES ('Place Names');
+INSERT into maps_layercategory (name) VALUES ('Political and Population');
+INSERT into maps_layercategory (name) VALUES ('Trade');
+
+
+
+
+
+
+
