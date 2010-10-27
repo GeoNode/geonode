7cef8735eeecf49773fc15fce62de7fb12af00eb
Ariel Núñez <ingenieroariel@gmail.com>
2010-10-27 14:09:46 -0500

    Removed now obsolete here function and custom root url

--- src/GeoNodePy/geonode/urls.py
+++ src/GeoNodePy/geonode/urls.py
@@ -40,16 +40,6 @@ urlpatterns = patterns('',
     (r'^profiles/', include('profiles.urls')),
 )
 
-#
 # Extra static file endpoint for development use
 if settings.SERVE_MEDIA:
-    import os
-    def here(*x): 
-        return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
-
-    root = here("..", "..", "geonode-client", "build", "geonode-client") if settings.MINIFIED_RESOURCES else here("..", "..", "geonode-client", "")
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve',  {'document_root': root}),
-    )
-
     urlpatterns += staticfiles_urlpatterns()
