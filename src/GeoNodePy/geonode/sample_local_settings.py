602b8ad805458dbd71be237fc8d99ef8728e777c
Jeffrey Johnson <jjohnson@etzlaub.local>
2010-12-22 14:08:43 -0800

    Added sample_local_settings.py for postgres settings.

---
+++ src/GeoNodePy/geonode/sample_local_settings.py
@@ -0,0 +1,9 @@
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'geonode'
+DATABASE_USER = 'geonode'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'geonode'         # Not used with sqlite3.
+DATABASE_HOST = ''             # Not used with sqlite3.
+DATABASE_PORT = ''             # Not used with sqlite3.
+
+CELERY_RESULT_BACKEND = "database"
+CELERY_RESULT_DBURI = "postgresql://geonode:geonode@localhost/geonode"
