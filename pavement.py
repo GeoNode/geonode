e4d3fef1ea2ea277db1102cbd8c2f3c1f31c5e78
Jeffrey Johnson <jjohnson@opengeo.org>
2011-01-04 08:13:01 -0800

    Changed package_client task to unpack geonode-client static files by default (use -w option to deploy with war)

--- pavement.py
+++ pavement.py
@@ -284,32 +284,24 @@ def package_dir(options):
 @task
 @needs('package_dir', 'setup_geonode_client')
 @cmdopts([
-    ('use_staticfiles', 's', 'Use django-staticfiles for deploying geonode-client')
+    ('use_war', 'w', 'Use a war to deploy geonode-client')
 ])
 def package_client(options):
     """Package compressed client resources (JavaScript, CSS, images)."""
 
-    if(hasattr(options, 'use_staticfiles')): 
-    	# Extract static files to static_location 
-    	webapps = path("./webapps")
-    	if not webapps.exists():
-        	webapps.mkdir()
-    	dst_war = webapps / "geonode-client.war"
-    	geonode_media_dir = path("./src/GeoNodePy/geonode/media")
-    	static_location = geonode_media_dir / "static" 
-    	if(os.path.exists(static_location)):
-       		shutil.rmtree(static_location)
-    	zip = zipfile.ZipFile(dst_war)
-    	members = []
-    	for name in zip.namelist():
-        	if(name.startswith('WEB-INF/app/static')):
-               		members.append(name) 
-    	zip_extractall(zipfile.ZipFile(dst_war), static_location, members=members)
-    	for data in glob.glob(static_location / "WEB-INF/app/static/*"):
-        	shutil.move(data,static_location)
-    	shutil.rmtree(static_location / "WEB-INF")
-    else:
+    if(hasattr(options, 'use_war')): 
     	geonode_client_target_war.copy(options.deploy.out_dir)
+    else:
+        # Extract static files to static_location 
+        src_url = str(options.config.parser.get('geonode-client', 'geonode_client_zip_url'))
+    	geonode_media_dir = path("./src/GeoNodePy/geonode/media")
+        dst_zip =  geonode_media_dir / "geonode-client.zip"
+        deployed_url = geonode_media_dir / "static" 
+
+        grab(src_url, dst_zip)
+
+        zip_extractall(zipfile.ZipFile(dst_zip), deployed_url)
+        os.remove(dst_zip)
 
 @task
 @needs('package_dir', 'setup_geoserver')
