565363558ba6e258e9b0313feebc2dbffed15f75
Jeffrey Johnson <jjohnson@opengeo.org>
2011-03-04 14:41:24 -0800

    Assume 4326 initially, but fail if latlon_bbox is invalid for 4326

--- src/GeoNodePy/geonode/maps/views.py
+++ src/GeoNodePy/geonode/maps/views.py
@@ -1011,10 +1011,24 @@ def _handle_layer_upload(request, layer=None):
             gs_resource = cat.get_resource(name=name, store=cat.get_store(name=name))
 
             if gs_resource.latlon_bbox is None:
-                cascading_delete(cat, gs_resource)
-                logger.warn("GeoServer failed to detect the projection for layer [%s]. Cancelling import", name)
-                errors.append(_("GeoNode could not detect the projection for %(layer)s.  Import is cancelled.") % { 'layer': name })
+                # If GeoServer couldn't figure out the projection, we initially 
+                # assume its 4326
+                logger.warn("GeoServer failed to detect the projection for layer [%s]. Guessing EPSG:4326", name)
+                #gs_resource.latlon_bbox = gs_resource.native_bbox
+                gs_resource.projection = "EPSG:4326"
+                cat.save(gs_resource)
+                # If it is still invalid, stop and alert the user
+                gs_resource = cat.get_resource(name=name, store=cat.get_store(name=name))
+                if gs_resource.latlon_bbox is None:
+                    cascading_delete(cat, gs_resource)
+                    logger.warn("GeoServer failed to detect the projection for layer [%s]. Tried (and failed) 4326 Cancelling import", name)
+                    errors.append(_("GeoNode could not detect the projection for %(layer)s.  Import is cancelled.") % { 'layer': name })
+                    valid_bbox=False
+                else:
+                    valid_bbox=True
             else:
+                valid_bbox=True
+            if(valid_bbox): 
                 typename = gs_resource.store.workspace.name + ':' + gs_resource.name
                 logger.info("Got GeoServer info for %s, creating Django record", typename)
 
