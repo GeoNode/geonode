6a7b95eee178ab44e45fefcecc75b926bbb5aa2a
ahocevar <andreas.hocevar@gmail.com>
2011-04-06 12:17:37 +0200

    Use a shared client (thread pool). Thanks tschaub for figuring this out.

--- app/proxy.js
+++ app/proxy.js
@@ -1,4 +1,4 @@
-var Client = require("ringo/httpclient").Client;
+var clientRequest = require("ringo/httpclient").request;
 var Request = require("ringo/webapp/request").Request;
 var MemoryStream = require("io").MemoryStream;
 var merge = require("ringo/utils/objects").merge;
@@ -35,8 +35,7 @@ function proxyPass(request, url, preserveHost) {
     } else {
         // re-issue request
         var host = parts[2];
-        var client = new Client();
-        var exchange = client.request({
+        var exchange = clientRequest({
             url: url,
             method: request.method,
             headers: preserveHost ? merge({host: host}, request.headers) : request.headers,
