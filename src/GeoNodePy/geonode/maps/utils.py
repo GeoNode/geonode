f9609d1795a0647d22b07b9a0389fb220cd4b2ba
David Winslow <cdwinslow@gmail.com>
2011-05-06 15:56:07 -0400

    Never create a new user as a side effect of uploading.
    The new behavior is as follows:
    
      * If a username string is passed in, get_valid_user will look up a
        user based on the 'username' field.  If it can't find one, an
        exception is raised.
      * If a User object is passed, get_valid_user will return it, *unless*
        it is an anonymous user, in which case an exception is thrown.
      * If None is passed, then get_valid_user will look for *the*
        administrator account.  If zero, two, or more than two
        administrators are found it will raise an exception.

--- src/GeoNodePy/geonode/maps/utils.py
+++ src/GeoNodePy/geonode/maps/utils.py
@@ -410,30 +410,24 @@ def save(layer, base_file, user, overwrite = True, keywords = []):
 def get_default_user():
     """Create a default user
     """
-    theuser, __ = User.objects.get_or_create(username='default', defaults=
-                                            {'is_superuser':True, 'password':'!'})
-    return theuser
+    try:
+        return User.objects.get(is_superuser=True)
+    except User.DoesNotExist:
+        raise GeoNodeException("You must have an admin account configured before importing data.  Try: django-admin.py createsuperuser")
+    except User.MultipleObjectsReturned:
+        raise GeoNodeException("You have multiple admin accounts, please specify which I should use.")
 
 def get_valid_user(user=None):
     """Gets the default user or creates it if it does not exist
     """
     if user is None:
         theuser = get_default_user()
-    else:
-        if hasattr(user, 'is_anonymous'):
-            if user.is_anonymous():
-                theuser = get_default_user()
-            else:
-                theuser = user
-        elif isinstance(user, basestring):
-            theuser = User.objects.get(username=user)
-        else:
-            msg = ('Cannot determine the nature of specified user:  %s.'
-                  'If you do not specify a user, the default one will be used.' % user)
-            raise GeoNodeException(msg)
+    elif isinstance(user, basestring):
+        theuser = User.objects.get(username=user)
 
-    # Make sure we got a valid user that has an entry in the database.
-    # Anonymous users are not allowed in GeoNode.
+    if theuser.is_anonymous():
+        raise GeoNodeException("The user uploading files must not be anonymous")
+
     #FIXME: Pass a user in the unit tests that is not yet saved ;)
     assert isinstance(theuser, User)
 
