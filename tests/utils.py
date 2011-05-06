0741fd8f0ee7f2077c865f731a1a30619967fe4a
Jeffrey Johnson <jjohnson@opengeo.org>
2011-05-06 15:17:43 -0700

    Initial add tests/utils.py

---
+++ tests/utils.py
@@ -0,0 +1,57 @@
+import os
+import urllib, urllib2, cookielib
+import contextlib
+
+def get_web_page(url, username=None, password=None, login_url=None):
+    """Get url page possible with username and password.
+    """
+
+    if login_url:
+        # Login via a form
+        cookies = urllib2.HTTPCookieProcessor()
+        opener = urllib2.build_opener(cookies)
+        urllib2.install_opener(opener)
+
+        opener.open(login_url)
+
+        try:
+            token = [x.value for x in cookies.cookiejar if x.name == 'csrftoken'][0]
+        except IndexError:
+            return False, "no csrftoken"
+
+        params = dict(username=username, password=password, \
+            this_is_the_login_form=True,
+            csrfmiddlewaretoken=token,
+            )
+        encoded_params = urllib.urlencode(params)
+
+        with contextlib.closing(opener.open(login_url, encoded_params)) as f:
+            html = f.read()
+
+    elif username is not None:
+        # Login using basic auth
+
+        # Create password manager
+        passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        passman.add_password(None, url, username, password)
+
+        # create the handler
+        authhandler = urllib2.HTTPBasicAuthHandler(passman)
+        opener = urllib2.build_opener(authhandler)
+        urllib2.install_opener(opener)
+
+    try:
+        pagehandle = urllib2.urlopen(url)
+    except urllib2.HTTPError, e:
+        msg = ('The server couldn\'t fulfill the request. '
+                'Error code: %s' % e.code)
+        e.args = (msg,)
+        raise
+    except urllib2.URLError, e:
+        msg = 'Could not open URL "%s": %s' % (url, e)
+        e.args = (msg,)
+        raise
+    else:
+        page = pagehandle.read()
+
+    return page
