5cbd4d666f9e63f443c7e6524b80b059a9169f98
David Winslow <cdwinslow@gmail.com>
2011-08-19 16:47:09 -0400

    Tweaks to match up with geonode-client improvements

--- src/GeoNodePy/geonode/proxy/urls.py
+++ src/GeoNodePy/geonode/proxy/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('geonode.proxy.views',
     (r'^proxy/', 'proxy'),
-    (r'^gs_rest/styles', 'geoserver_style_proxy'),
+    (r'^gs/rest/styles', 'geoserver_rest_proxy', dict(
+        proxy_path='/gs/rest/styles', downstream_path='rest/styles')),
+    (r'^gs/rest/layers', 'geoserver_rest_proxy', dict(
+        proxy_path='/gs/rest/layers', downstream_path='rest/layers')),
 )
