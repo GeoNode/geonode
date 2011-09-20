63a25d514d12d09f94ec11e2c0dc01f3aed55acd
Ariel Nunez <ingenieroariel@gmail.com>
2011-09-20 02:16:02 +0000

    Added virtualenv upgrade via easy_install to avoid aborted installations in Ubuntu Lucid

--- shared/package/install.sh
+++ shared/package/install.sh
@@ -155,8 +155,10 @@ function configuredjango() {
 	#
 	cd $GEONODE_LIB
 
-	# Install the latest version of pip from PyPi to avoid having problems in Ubuntu 10.04
+	# Install the latest version of pip and virtualenv from PyPi to avoid having
+        # problems in Ubuntu 10.04
 	# FIXME: It is less than ideal that this command access the network. Ideas?
+	easy_install -U virtualenv
 	easy_install -U pip
 
 	virtualenv .
