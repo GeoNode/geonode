8de25d0cf58c77bb4ff90c7323cceedb4596ffb4
Ariel Nunez <ingenieroariel@gmail.com>
2011-10-12 15:31:04 -0400

    More verbose and resilient updatelayers, references issue 977

--- src/GeoNodePy/geonode/maps/management/commands/updatelayers.py
+++ src/GeoNodePy/geonode/maps/management/commands/updatelayers.py
@@ -1,13 +1,35 @@
 from django.core.management.base import BaseCommand
+from optparse import make_option
 from geonode.maps.models import Layer
-from urllib2 import URLError
+
 
 class Command(BaseCommand):
     help = 'Update the GeoNode application with data from GeoServer'
-    args = '[none]'
+    option_list = BaseCommand.option_list + (
+        make_option('--ignore-errors',
+            action='store_true',
+            dest='ignore_errors',
+            default=False,
+            help='Stop after any errors are encountered.'),
+        )
 
-    def handle(self, *args, **keywordargs):
-        try:
-            Layer.objects.slurp()
-        except URLError:
-            print "Couldn't connect to GeoServer; is it running? Make sure the GEOSERVER_BASE_URL setting is set correctly."
+    def handle(self, **options):
+        ignore_errors = options.get('ignore_errors')
+        verbosity = options.get('verbosity')
+        output = slurp(ignore_errors, verbosity)
+        updated = [dict_['name'] for dict_ in output if dict_['status']=='updated']
+        created = [dict_['name'] for dict_ in output if dict_['status']=='created']
+        failed = [dict_['name'] for dict_ in output if dict_['status']=='failed']
+        if verbosity > 0:
+            print "\n\nFinished processing.\n"
+            print "%d Available layers" % len(output)
+            print "%d Created layers" % len(created)
+            print "%d Updated layers" % len(updated)
+            print "%d Failed layers" % len(failed)
+        if verbosity > 1:
+            print "\nDetailed report of failures:"
+            for dict_ in output:
+                if dict_['status'] == 'failed':
+                    print "\n=========\n"
+                    print dict_['name'], dict_['error']
+                    print dict_['traceback']
