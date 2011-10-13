be0e1c2d0ddbf1316a9f25bbfab50a08b42719f5
Ariel Nunez <ingenieroariel@gmail.com>
2011-10-13 12:54:07 -0400

    Added duration information to the updatelayers report

--- src/GeoNodePy/geonode/maps/management/commands/updatelayers.py
+++ src/GeoNodePy/geonode/maps/management/commands/updatelayers.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from optparse import make_option
 from geonode.maps.models import Layer
 import traceback
+import datetime
 
 class Command(BaseCommand):
     help = 'Update the GeoNode application with data from GeoServer'
@@ -16,16 +17,23 @@ class Command(BaseCommand):
     def handle(self, **options):
         ignore_errors = options.get('ignore_errors')
         verbosity = options.get('verbosity')
+        start = datetime.datetime.now()
         output = Layer.objects.slurp(ignore_errors, verbosity)
         updated = [dict_['name'] for dict_ in output if dict_['status']=='updated']
         created = [dict_['name'] for dict_ in output if dict_['status']=='created']
         failed = [dict_['name'] for dict_ in output if dict_['status']=='failed']
+        finish = datetime.datetime.now()
+        td = finish - start
+        duration = td.microseconds / 1000000 + td.seconds + td.days * 24 * 3600
+        duration_rounded = round(duration, 2)
+
         if verbosity > 0:
-            print "\n\nFinished processing.\n"
-            print "%d Available layers" % len(output)
+            print "\n\nFinished processing %d layers in %s seconds.\n" %
+                                          (len(output), duration_rounded)
             print "%d Created layers" % len(created)
             print "%d Updated layers" % len(updated)
             print "%d Failed layers" % len(failed)
+            print "%s seconds per layer" % round(duration / len(output), 2)
         if verbosity > 1:
             print "\nDetailed report of failures:"
             for dict_ in output:
