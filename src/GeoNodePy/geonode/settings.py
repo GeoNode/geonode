6080533827a2ed2fe4343d43c2cfc1fb0b265dc9
Chris Jones <chris@brack3t.com>
2011-10-21 15:32:09 -0700

    Formatted installed apps a little better. Added dialogos.

--- src/GeoNodePy/geonode/settings.py
+++ src/GeoNodePy/geonode/settings.py
@@ -252,11 +252,14 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.sitemaps',
+
     'staticfiles',
     'django_extensions',
     'registration',
     'profiles',
     'avatar',
+    'dialogos',
+
     'geonode.core',
     'geonode.maps',
     'geonode.proxy',
