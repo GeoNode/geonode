bdfa9d239b8e8d806a63eef3ae80064c109de106
Matt Bertrand <mbertrand@cga.harvard.edu>
2012-03-13 17:25:03 -0500

    Rename accountforms to registration, profileforms to profiles

--- src/GeoNodePy/geonode/profiles/forms.py
+++ src/GeoNodePy/geonode/profiles/forms.py
@@ -1,11 +1,7 @@
 # -*- coding: UTF-8 -*-
-from django import forms
 from django.forms import ModelForm
-from django.contrib.auth.models import User
-from registration.forms import RegistrationFormUniqueEmail
-from django.utils.translation import ugettext_lazy as _
 from geonode.maps.models import Contact
-from registration.models import RegistrationProfile
+
 
 attrs_dict = { 'class': 'required' }
 
