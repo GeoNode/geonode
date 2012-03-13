6111c8c3d9fe0ba2cad6615355218ba125afd60c
Matt Bertrand <mbertrand@cga.harvard.edu>
2012-03-13 16:01:07 -0500

    Send username via email if user forgets; create geonode.registration app for this and possible future customizations of django-registration (using email address as username for example).

---
+++ src/GeoNodePy/geonode/registration/views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.utils.translation import ugettext as _
+from geonode import settings
+from geonode.registration.forms import ForgotUsernameForm
+
+def forgotUsername(request):
+    """ Look up a username based on an email address, and send an email
+    containing the username if found"""
+    username_form = ForgotUsernameForm()
+
+    message = ''
+
+
+    email_subject = _("Your username for ") + settings.SITENAME
+
+
+    if request.method == 'POST':
+        username_form = ForgotUsernameForm(request.POST)
+        if username_form.is_valid():
+
+            users = User.objects.filter(email=username_form.cleaned_data['email'])
+            if len(users) > 0:
+                username = users[0].username
+                email_message = email_subject + " : " + username
+                send_mail(email_subject, email_message, settings.DEFAULT_FROM_EMAIL,
+                    [username_form.cleaned_data['email']], fail_silently=False)
+                message = _("Your username has been emailed to you.")
+            else:
+                message = _("No user could be found with that email address.")
+
+
+    return render_to_response('registration/username_form.html', RequestContext(request, {
+        'message': message,
+        'form' : username_form
+    }))
