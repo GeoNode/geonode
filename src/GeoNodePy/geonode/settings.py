f32670a8b13deb31e093d61c488181c44b1b3b46
Jeffrey Johnson <jjohnson@opengeo.org>
2012-04-03 13:44:25 -0700

    Switch to ElasticSearch as the default haystack backend

--- src/GeoNodePy/geonode/settings.py
+++ src/GeoNodePy/geonode/settings.py
@@ -272,8 +272,9 @@ DB_DATASTORE_TYPE=''
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
     },
 }
 
