65214572580d2c8932f3e25b6935cdd45fb1c149
Luke Hatcher <lukeman@gmail.com>
2012-04-19 19:02:27 -0400

    move search out to its own file, use hogan to render templates

--- src/GeoNodePy/geonode/core/templatetags/bootstrap_tags.py
+++ src/GeoNodePy/geonode/core/templatetags/bootstrap_tags.py
@@ -21,3 +21,31 @@ def is_checkbox(field):
 @register.filter
 def css_class(field):
     return field.field.widget.__class__.__name__.lower()
+
+
+class VerbatimNode(template.Node):
+
+    def __init__(self, text):
+        self.text = text
+    
+    def render(self, context):
+        return self.text
+
+
+@register.tag
+def verbatim(parser, token):
+    text = []
+    while 1:
+        token = parser.tokens.pop(0)
+        if token.contents == 'endverbatim':
+            break
+        if token.token_type == template.TOKEN_VAR:
+            text.append('{{')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('{%')
+        text.append(token.contents)
+        if token.token_type == template.TOKEN_VAR:
+            text.append('}}')
+        elif token.token_type == template.TOKEN_BLOCK:
+            text.append('%}')
+    return VerbatimNode(''.join(text))
