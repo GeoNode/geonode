826ed059d5a17eb93976dfaf917fae9b487fe98d
David Winslow <cdwinslow@gmail.com>
2012-05-07 14:36:59 -0400

    Hookup Cookie handling again

--- src/geoserver-geonode-ext/src/main/java/org/geonode/security/GeoNodeSecurityProvider.java
+++ src/geoserver-geonode-ext/src/main/java/org/geonode/security/GeoNodeSecurityProvider.java
@@ -4,6 +4,9 @@ import org.geoserver.security.GeoServerSecurityProvider;
 import org.geoserver.security.config.SecurityNamedServiceConfig;
 
 public class GeoNodeSecurityProvider extends GeoServerSecurityProvider {
+    private final HTTPClient httpClient = new HTTPClient(10, 1000, 1000);
+    private final GeoNodeSecurityClient client = new DefaultSecurityClient(httpClient);
+    
     @Override
     public Class<GeoNodeAuthenticationProvider> getAuthenticationProviderClass() {
         return GeoNodeAuthenticationProvider.class;
@@ -13,8 +16,16 @@ public class GeoNodeSecurityProvider extends GeoServerSecurityProvider {
     public GeoNodeAuthenticationProvider createAuthenticationProvider(
             SecurityNamedServiceConfig config)
     {
-        HTTPClient httpClient = new HTTPClient(10, 1000, 1000);
-        GeoNodeSecurityClient client = new DefaultSecurityClient(httpClient );
         return new GeoNodeAuthenticationProvider(client);
+    }
+    
+    @Override
+    public Class<GeoNodeCookieProcessingFilter> getFilterClass() {
+        return GeoNodeCookieProcessingFilter.class;
+    }
+    
+    @Override
+    public GeoNodeCookieProcessingFilter createFilter(SecurityNamedServiceConfig config) {
+        return new GeoNodeCookieProcessingFilter(client);
     }
 }
