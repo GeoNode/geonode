261b87c4f9d9e02765e890cb06bbfd9e13c5a3d2
Ariel Núñez <ingenieroariel@gmail.com>
2012-05-15 22:39:18 -0400

    Added comments and reordered settings.py

--- src/GeoNodePy/geonode/settings.py
+++ src/GeoNodePy/geonode/settings.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
-# Django settings for GeoNode project.
+# Django settings for the GeoNode project.
 from urllib import urlencode
 import os
 
-DEBUG = TEMPLATE_DEBUG = True
-SITENAME = "GeoNode"
-SITEURL = "http://localhost:8000/"
+#
+# General Django development settings
+#
 
+# Defines the directory that contains the settings file as the PROJECT_ROOT
+# It is used for relative settings elsewhere.
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
+# Setting debug to true makes Django serve static media and
+# present pretty error pages.
+DEBUG = TEMPLATE_DEBUG = True
+
+# Defines settings for development
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_ROOT, '..', '..', '..', 'development.db'),
-        'USER': '',     # Not used with sqlite
-        'PASSWORD': '', # Not used with sqlite
-        'HOST': '',     # Not used with sqlite
-        'PORT': ''      # Not used with sqlite
+        'NAME': os.path.join(PROJECT_ROOT, 'development.db'),
     }
 }
 
@@ -41,33 +44,6 @@ LANGUAGES = (
     ('id', 'Bahasa Indonesia'),
     ('zh', '中國的'),
 )
-
-SITE_ID = 1
-
-# Setting a custom test runner to avoid running the tests for some problematic 3rd party apps
-TEST_RUNNER='django_nose.NoseTestSuiteRunner'
-
-NOSE_ARGS = [
-      '--verbosity=2',
-      '--cover-erase',
-      '--nocapture',
-      '--with-coverage',
-      '--cover-package=geonode',
-      '--cover-inclusive',
-      '--cover-tests',
-      '--detailed-errors',
-      '--with-xunit',
-
-# This is very beautiful/usable but requires: pip install rudolf
-#      '--with-color',
-
-# The settings below are useful while debugging test failures or errors
-
-#      '--failed',
-#      '--pdb-failures',
-#      '--stop',
-#      '--pdb',
-      ]
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -95,114 +71,25 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static"),
 ]
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'myv-y4#7j-d*p-__@j#*3z@!y24fz8%^z2v6atuy4bo9vqr1_a'
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.request",
-    "geonode.maps.context_processors.resource_urls",
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
-
-ROOT_URLCONF = 'geonode.urls'
-
 # Note that Django automatically includes the "templates" dir in all the
 # INSTALLED_APPS, se there is no need to add maps/templates or admin/templates
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT,"templates"),    
+    os.path.join(PROJECT_ROOT, "templates"),
 )
 
-# The FULLY QUALIFIED url to the GeoServer instance for this GeoNode.
-GEOSERVER_BASE_URL = "http://localhost:8001/geoserver/"
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'myv-y4#7j-d*p-__@j#*3z@!y24fz8%^z2v6atuy4bo9vqr1_a'
 
-# The username and password for a user that can add and edit layer details on GeoServer
-GEOSERVER_CREDENTIALS = "geoserver_admin", SECRET_KEY
+# Location of url mappings
+ROOT_URLCONF = 'geonode.urls'
 
-# The FULLY QUALIFIED url to the GeoNetwork instance for this GeoNode
-GEONETWORK_BASE_URL = "http://localhost:8001/geonetwork/"
+# Site id in the Django sites framework
+SITE_ID = 1
 
-# The username and password for a user with write access to GeoNetwork
-GEONETWORK_CREDENTIALS = "admin", "admin"
-
-AUTHENTICATION_BACKENDS = ('geonode.core.auth.GranularBackend',)
-
-GOOGLE_API_KEY = "ABQIAAAAkofooZxTfcCv9Wi3zzGTVxTnme5EwnLVtEDGnh-lFVzRJhbdQhQgAhB1eT_2muZtc0dl-ZSWrtzmrw"
-LOGIN_REDIRECT_URL = "/"
-
-# Where should newly created maps be focused?
-DEFAULT_MAP_CENTER = (0, 0)
-
-# How tightly zoomed should newly created maps be?
-# 0 = entire world;
-# maximum zoom is between 12 and 15 (for Google Maps, coverage varies by area)
-DEFAULT_MAP_ZOOM = 0
-
-DEFAULT_LAYER_SOURCE = {
-    "ptype":"gxp_wmscsource",
-    "url":"/geoserver/wms",
-    "restUrl": "/gs/rest"
-}
-
-MAP_BASELAYERS = [{
-    "source": {"ptype": "gx_olsource"},
-    "type":"OpenLayers.Layer",
-    "args":["No background"],
-    "visibility": False,
-    "fixed": True,
-    "group":"background"
-  },{
-    "source": { "ptype":"gx_olsource"},
-    "type":"OpenLayers.Layer.OSM",
-    "args":["OpenStreetMap"],
-    "visibility": False,
-    "fixed": True,
-    "group":"background"
-  },{
-    "source": { "ptype":"gxp_mapquestsource"},
-    "name":"osm",
-    "group":"background",
-    "visibility": True
-  },{
-    "source": { "ptype":"gxp_mapquestsource"},
-    "name":"naip",
-    "group":"background",
-    "visibility": False
-  },{
-    "source": {"ptype": "gxp_mapboxsource"},
-  },{
-    "source": {"ptype":"gx_olsource"},
-    "type":"OpenLayers.Layer.WMS",
-    "group":"background",
-    "visibility": False,
-    "fixed": True,
-    "args":[
-      "bluemarble",
-      "http://maps.opengeo.org/geowebcache/service/wms",
-      {
-        "layers":["bluemarble"],
-        "format":"image/png",
-        "tiled": True,
-        "tilesOrigin":[-20037508.34,-20037508.34]
-      },
-      {"buffer":0}
-    ]
-
-}]
 
 INSTALLED_APPS = (
+
+    # Apps bundled with Django
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -212,6 +99,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.messages',
 
+    # Third party apps
     'django_extensions',
     'registration',
     'profiles',
@@ -221,65 +109,24 @@ INSTALLED_APPS = (
     'taggit',
     'south',
 
+    # GeoNode internal apps
     'geonode.core',
     'geonode.maps',
     'geonode.proxy',
-    'geonode'
+
 )
-
-def get_user_url(u):
-    from django.contrib.sites.models import Site
-    s = Site.objects.get_current()
-    return "http://" + s.domain + "/profiles/" + u.username
-
-
-ABSOLUTE_URL_OVERRIDES = {
-    'auth.user': get_user_url
-}
-
-AUTH_PROFILE_MODULE = 'maps.Contact'
-REGISTRATION_OPEN = False
-
-#GEONODE_CLIENT_LOCATION = "http://localhost:8001/geonode-client/"
-GEONODE_CLIENT_LOCATION = "/static/geonode/"
-
-#Import uploaded shapefiles into a database such as PostGIS?
-DB_DATASTORE=False
-
-#Database datastore connection settings
-DB_DATASTORE_NAME = ''
-DB_DATASTORE_USER = ''
-DB_DATASTORE_PASSWORD = ''
-DB_DATASTORE_HOST = ''
-DB_DATASTORE_PORT = ''
-DB_DATASTORE_TYPE=''
-
-# Agon Ratings
-AGON_RATINGS_CATEGORY_CHOICES = {
-    "maps.Map": {
-        "map": "How good is this map?"
-    },
-    "maps.Layer": {
-        "layer": "How good is this layer?"
-    },
-}
-
-SOUTH_MIGRATION_MODULES = {
-    'registration': 'geonode.migrations.registration',
-    'avatar': 'geonode.migrations.avatar',
-}
 
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {
         "null": {
-            "level":"DEBUG",
-            "class":"django.utils.log.NullHandler",
+            "level": "DEBUG",
+            "class": "django.utils.log.NullHandler",
         },
-        "console":{
-            "level":"DEBUG",
-            "class":"logging.StreamHandler",
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
         },
         "mail_admins": {
             "level": "ERROR",
@@ -300,8 +147,207 @@ LOGGING = {
 }
 
 
+#
+# Customizations to built in Django settings required by GeoNode
+#
+
+# Setting a custom test runner to avoid running the tests for
+# some problematic 3rd party apps
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    # The context processor belows add things like SITEURL
+    # and GEOSERVER_BASE_URL to all pages that use a RequestContext
+    'geonode.maps.context_processors.resource_urls',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # The setting below makes it possible to serve different languages per
+    # user depending on things like headers in HTTP requests.
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+
+# Replacement of default authentication backend in order to support
+# permissions per object.
+AUTHENTICATION_BACKENDS = ('geonode.core.auth.GranularBackend',)
+
+
+def get_user_url(u):
+    from django.contrib.sites.models import Site
+    s = Site.objects.get_current()
+    return "http://" + s.domain + "/profiles/" + u.username
+
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': get_user_url
+}
+
+# Redirects to home page after login
+# FIXME(Ariel): I do not know why this setting is needed,
+# it would be best to use the ?next= parameter
+LOGIN_REDIRECT_URL = "/"
+
+
+#
+# Settings for third party apps
+#
+
+# Agon Ratings
+AGON_RATINGS_CATEGORY_CHOICES = {
+    "maps.Map": {
+        "map": "How good is this map?"
+    },
+    "maps.Layer": {
+        "layer": "How good is this layer?"
+    },
+}
+
+# For South migrations
+SOUTH_MIGRATION_MODULES = {
+    'registration': 'geonode.migrations.registration',
+    'avatar': 'geonode.migrations.avatar',
+}
+
+# For django-profiles
+AUTH_PROFILE_MODULE = 'maps.Contact'
+
+# For django-registration
+REGISTRATION_OPEN = False
+
+# Arguments for the test runner
+NOSE_ARGS = [
+      '--verbosity=2',
+      '--cover-erase',
+      '--nocapture',
+      '--with-coverage',
+      '--cover-package=geonode',
+      '--cover-inclusive',
+      '--cover-tests',
+      '--detailed-errors',
+      '--with-xunit',
+      ]
+
+#
+# GeoNode specific settings
+#
+
+# GeoServer information
+
+# The FULLY QUALIFIED url to the GeoServer instance for this GeoNode.
+GEOSERVER_BASE_URL = "http://localhost:8001/geoserver/"
+
+# The username and password for a user that can add and
+# edit layer details on GeoServer
+GEOSERVER_CREDENTIALS = "geoserver_admin", SECRET_KEY
+
+
+# GeoNetwork information
+
+# The FULLY QUALIFIED url to the GeoNetwork instance for this GeoNode
+GEONETWORK_BASE_URL = "http://localhost:8001/geonetwork/"
+
+# The username and password for a user with write access to GeoNetwork
+GEONETWORK_CREDENTIALS = "admin", "admin"
+
+
+# GeoNode javascript client configuration
+
+# Google Api Key needed for 3D maps / Google Earth plugin
+GOOGLE_API_KEY = "ABQIAAAAkofooZxTfcCv9Wi3zzGTVxTnme5EwnLVtEDGnh-lFVzRJhbdQhQgAhB1eT_2muZtc0dl-ZSWrtzmrw"
+
+# Where should newly created maps be focused?
+DEFAULT_MAP_CENTER = (0, 0)
+
+# How tightly zoomed should newly created maps be?
+# 0 = entire world;
+# maximum zoom is between 12 and 15 (for Google Maps, coverage varies by area)
+DEFAULT_MAP_ZOOM = 0
+
+DEFAULT_LAYER_SOURCE = {
+    "ptype": "gxp_wmscsource",
+    "url": "/geoserver/wms",
+    "restUrl": "/gs/rest"
+}
+
+MAP_BASELAYERS = [{
+    "source": {"ptype": "gx_olsource"},
+    "type":"OpenLayers.Layer",
+    "args":["No background"],
+    "visibility": False,
+    "fixed": True,
+    "group":"background"
+  }, {
+    "source": {"ptype": "gx_olsource"},
+    "type":"OpenLayers.Layer.OSM",
+    "args":["OpenStreetMap"],
+    "visibility": False,
+    "fixed": True,
+    "group":"background"
+  }, {
+    "source": {"ptype": "gxp_mapquestsource"},
+    "name":"osm",
+    "group":"background",
+    "visibility": True
+  }, {
+    "source": {"ptype": "gxp_mapquestsource"},
+    "name":"naip",
+    "group":"background",
+    "visibility": False
+  }, {
+    "source": {"ptype": "gxp_mapboxsource"},
+  }, {
+    "source": {"ptype": "gx_olsource"},
+    "type":"OpenLayers.Layer.WMS",
+    "group":"background",
+    "visibility": False,
+    "fixed": True,
+    "args":[
+      "bluemarble",
+      "http://maps.opengeo.org/geowebcache/service/wms",
+      {
+        "layers":["bluemarble"],
+        "format":"image/png",
+        "tiled": True,
+        "tilesOrigin": [-20037508.34, -20037508.34]
+      },
+      {"buffer": 0}
+    ]
+
+}]
+
+
+#GEONODE_CLIENT_LOCATION = "http://localhost:8001/geonode-client/"
+GEONODE_CLIENT_LOCATION = "/static/geonode/"
+
+
+# GeoNode vector data backend configuration.
+
+#Import uploaded shapefiles into a database such as PostGIS?
+DB_DATASTORE = False
+
+#Database datastore connection settings
+DB_DATASTORE_NAME = ''
+DB_DATASTORE_USER = ''
+DB_DATASTORE_PASSWORD = ''
+DB_DATASTORE_HOST = ''
+DB_DATASTORE_PORT = ''
+DB_DATASTORE_TYPE = ''
+
+
+# Load more settings from a file called local_settings.py if it exists
 try:
     from local_settings import *
 except ImportError:
     pass
-
