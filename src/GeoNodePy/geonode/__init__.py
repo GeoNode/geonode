b4393c47b18133c7136da40a10d8528f5e87119a
Ariel Núñez <ingenieroariel@gmail.com>
2012-05-18 07:56:04 -0400

    Switched to a PEP 386 compliant string version

--- src/GeoNodePy/geonode/__init__.py
+++ src/GeoNodePy/geonode/__init__.py
@@ -1,38 +1,6 @@
 __all__ = ('__version__', '__build__', '__docformat__', 'get_revision')
-__version__= (1, 2, 0, 'alpha7')
-
-import os
-
-
-def _get_git_revision(path):
-    revision_file = os.path.join(path, 'refs', 'heads', 'master')
-    if not os.path.exists(revision_file):
-        return None
-    fh = open(revision_file, 'r')
-    try:
-        return fh.read().strip()
-    finally:
-        fh.close()
-
-
-def get_revision():
-    """
-    :returns: Revision number of this branch/checkout, if available. None if
-        no revision number can be determined.
-    """
-    package_dir = os.path.dirname(__file__)
-    # assumes this file lives in src/GeoNodePy/geonode/__init__.py
-    checkout_dir = os.path.normpath(os.path.join(package_dir, '..', '..', '..'))
-    path = os.path.join(checkout_dir, '.git')
-    if os.path.exists(path):
-        return _get_git_revision(path)
-    return None
-
-__build__ = get_revision()
-
+__version__= (1, 2, 0, 'alpha', 0)
 
 def get_version():
-    base = '.'.join(map(str, __version__))
-    if __build__:
-        base = '%s (%s)' % (base, __build__)
-    return base
+    from geonode.utils import get_version
+    return get_version(__version__)
