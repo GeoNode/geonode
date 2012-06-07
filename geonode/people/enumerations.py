8506b949e6694298fc621cd1d5017f15b098481a
Jeffrey Johnson <jjohnson@opengeo.org>
2012-06-07 10:19:19 -0400

    Move CONTACT_FIELDS to people/enumerations.py

--- geonode/people/enumerations.py
+++ geonode/people/enumerations.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 
+CONTACT_FIELDS = [                   
+    "name",
+    "organization",
+    "position",
+    "voice",
+    "facsimile",
+    "delivery_point",
+    "city",
+    "administrative_area",
+    "postal_code",
+    "country",
+    "email",
+    "role"
+]
+
 ROLE_VALUES = [
     'datasetProvider',
     'custodian',
