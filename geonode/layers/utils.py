ef918195f3f5724a08f80162c218769f1897147c
Ivan Willig <iwillig@gmail.com>
2012-06-07 10:15:14 -0400

    Missed a GeoNodeException import.

--- geonode/layers/utils.py
+++ geonode/layers/utils.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf import settings
 
 # Geonode functionality
+from geonode import GeoNodeException
 from geonode.layers.models import Layer
 from geonode.people.models import Contact
 from geonode.gs_helpers import cascading_delete, get_sld_for, delete_from_postgis
