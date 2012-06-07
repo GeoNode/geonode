212299f2a2afd6e1c271b08192ba9ec73bd4b0c5
Jeffrey Johnson <jjohnson@opengeo.org>
2012-06-07 08:59:20 -0400

    Add some more smoke tests and rename from CamelCase to all lower

--- geonode/tests.py
+++ geonode/tests.py
@@ -16,34 +16,87 @@ class GeoNodeClientTests(TestCase):
     def tearDown(self):
         pass
 
+    #### Basic Pages ####
 
-    def test_HomePage(self):
+    def test_home_page(self):
         '''Test if the homepage renders.'''
         c = Client()
         response = c.get('/')
         self.failUnlessEqual(response.status_code, 200)
 
-    def test_Data(self):
-        'Test if the data page renders.'
-        c = Client()
-        response = c.get('/data/')
-        self.failUnlessEqual(response.status_code, 200)
-
-    def test_Maps(self):
-        '''Test Maps page renders.'''
-
-        c = Client()
-        response = c.get('/maps/')
-        self.failUnlessEqual(response.status_code, 200)
-
-    def test_Help(self):
+    def test_help_page(self):
         '''Test help page renders.'''
 
         c = Client()
         response = c.get('/help/')
         self.failUnlessEqual(response.status_code, 200)
     
-    def test_Profiles(self):
+    def test_developer_page(self):
+        '''Test help page renders.'''
+
+        c = Client()
+        response = c.get('/help/')
+        self.failUnlessEqual(response.status_code, 200)
+    
+    #### Data/Layer Pages ####
+    
+    def test_data_page(self):
+        'Test if the data page renders.'
+        c = Client()
+        response = c.get('/data/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_data_acls(self):
+        'Test if the data page renders.'
+        c = Client()
+        response = c.get('/data/acls')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_data_search(self):
+        'Test if the data page renders.'
+        c = Client()
+        response = c.get('/data/search')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_data_search_api(self):
+        'Test if the data page renders.'
+        c = Client()
+        response = c.get('/data/search/api')
+        self.failUnlessEqual(response.status_code, 200)
+
+    #### Maps Pages ####
+
+    def test_maps_page(self):
+        '''Test Maps page renders.'''
+
+        c = Client()
+        response = c.get('/maps/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_maps_search_page(self):
+        '''Test Maps Search page renders.'''
+
+        c = Client()
+        response = c.get('/maps/search/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    def test_maps_search_api(self):
+        '''Test Maps Search API page renders.'''
+
+        c = Client()
+        response = c.get('/maps/search/api/')
+        self.failUnlessEqual(response.status_code, 200)
+    
+    def test_new_map_page(self):
+        '''Test New Map page renders.'''
+
+        c = Client()
+        response = c.get('/maps/new/')
+        self.failUnlessEqual(response.status_code, 200)
+
+    #### People Pages ####
+
+    def test_profiles(self):
         '''Test the profiles page renders.'''
 
         c = Client()
