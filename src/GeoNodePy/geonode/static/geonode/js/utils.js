5b19552a9a65b8f691f5579e5230a1a689fcdd8a
Luke Hatcher <lukeman@gmail.com>
2012-06-08 18:53:34 -0400

    hook up bulk deletion

--- src/GeoNodePy/geonode/static/geonode/js/utils.js
+++ src/GeoNodePy/geonode/static/geonode/js/utils.js
@@ -14,7 +14,43 @@ var perms_submit = function() {
       }
     );
     return false;
+};
+
+var batch_delete = function() {
+  var form = $(this);
+  var action = form.attr("action");
+
+  var postdata = { layers: [], maps: [] };
+  var selected = $(".asset-selector:checked");
+
+  $.each(selected, function(index, value) {
+    var el = $(value);
+    if (el.data("type") === "map") {
+      postdata.maps.push(el.data("id"));
+    } else if (el.data("type") === "layer") {
+      postdata.layers.push(el.data("id"));
+    }
+  });
+
+  if (postdata.layers.length == 0) {
+    delete postdata.layers;
   };
+  if (postdata.maps.length == 0) {
+    delete postdata.maps;
+  };
+
+  $.ajax(
+    {
+      type: "POST",
+      url: action,
+      data: JSON.stringify(postdata),
+      success: function(data) {
+        $("#delete_form").modal("hide");
+      }
+    }
+  );
+  return false;
+};
 
 var batch_perms_submit = function() {
     var form = $(this);
