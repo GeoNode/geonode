1a64d41a88f35e3cc60a6a3064a971c56bf6732f
Matt Bertrand <mbertrand@cga.harvard.edu>
2012-07-11 21:50:49 -0400

    New help doc; fix FeedSourceDialog in IE

--- src/geonode-client/app/static/script/app/GeoExplorer.js
+++ src/geonode-client/app/static/script/app/GeoExplorer.js
@@ -2927,6 +2927,7 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
             });
         }
         this.feedDialog.show();
+        this.feedDialog.alignTo(document, 't-t');
         if (selectedOption) {
             this.feedDialog.sourceTypeRadioList.setValue(selectedOption);
         }
