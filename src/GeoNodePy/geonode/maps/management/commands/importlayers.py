b28c85521b09ba1435bade48bbd3bbad51c6ac66
Ariel Núñez <ingenieroariel@gmail.com>
2012-08-01 15:49:36 -0400

    More detailed importlayers output

--- src/GeoNodePy/geonode/maps/management/commands/importlayers.py
+++ src/GeoNodePy/geonode/maps/management/commands/importlayers.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from optparse import make_option
 from geonode.maps.utils import upload
+import traceback
+import datetime
 
 class Command(BaseCommand):
     help = ("Brings a data file or a directory full of data files into a"
@@ -12,16 +14,55 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
             make_option('--user', dest="user", default=None,
                 help="Name of the user account which should own the imported layers"),
+            make_option('--ignore-errors',
+                action='store_true',
+                dest='ignore_errors',
+                default=True,
+                help='Stop after any errors are encountered.'),
             make_option('--overwrite', dest='overwrite', default=True, action="store_false",
                 help="Overwrite existing layers if discovered (defaults True)"),
-            make_option('--quiet', dest='quiet', default=False, action="store_true",
-                help="Don't print out a help message if invoked with an empty path list"),
             make_option('--keywords', dest='keywords', default="", 
                 help="The default keywords for the imported layer(s). Will be the same for all imported layers if multiple imports are done in one command")
         )
 
-    def handle(self, *args, **opts):
-        if not opts['quiet'] and len(args) == 0:
-            print "No files passed to import command... Is that what you meant to do?"
+    def handle(self, *args, **options):
+        verbosity = int(options.get('verbosity'))
+        ignore_errors = options.get('ignore_errors')
+        user = options.get('user')
+        overwrite = options.get('overwrite')
+        keywords = options.get('keywords').split()
+        start = datetime.datetime.now()
+        output = []
         for path in args:
-            upload(path, opts['user'], opts['overwrite'], opts['keywords'].split())
+            out = upload(path, user=user, overwrite=overwrite, keywords=keywords, verbosity=verbosity)
+            output.extend(out)
+
+        updated = [dict_['file'] for dict_ in output if dict_['status']=='updated']
+        created = [dict_['file'] for dict_ in output if dict_['status']=='created']
+        skipped = [dict_['file'] for dict_ in output if dict_['status']=='skipped']
+        failed = [dict_['file'] for dict_ in output if dict_['status']=='failed']
+
+        finish = datetime.datetime.now()
+        td = finish - start
+        duration = td.microseconds / 1000000 + td.seconds + td.days * 24 * 3600
+        duration_rounded = round(duration, 2)
+
+        if verbosity > 1:
+            print "\nDetailed report of failures:"
+            for dict_ in output:
+                if dict_['status'] == 'failed':
+                    print "\n\n", dict_['file'], "\n================"
+                    traceback.print_exception(dict_['exception_type'],
+                                              dict_['error'],
+                                              dict_['traceback'])
+
+        if verbosity > 0:
+            print "\n\nFinished processing %d layers in %s seconds.\n" % (
+                                              len(output), duration_rounded)
+            print "%d Created layers" % len(created)
+            print "%d Updated layers" % len(updated)
+            print "%d Skipped layers" % len(skipped)
+            print "%d Failed layers" % len(failed)
+
+            if len(output) > 0:
+                print "%f seconds per layer" % (duration * 1.0 / len(output))
--- src/GeoNodePy/geonode/maps/utils.py
+++ src/GeoNodePy/geonode/maps/utils.py
@@ -7,6 +7,7 @@ import re
 import uuid
 import os
 import glob
+import sys
 
 # Django functionality
 from django.contrib.auth.models import User
@@ -565,7 +566,7 @@ def file_upload(filename, user=None, title=None, overwrite=True, keywords=()):
     return new_layer
 
 
-def upload(incoming, user=None, overwrite=True, keywords = ()):
+def upload(incoming, user=None, overwrite=True, keywords = (), ignore_errors=True, verbosity=1, console=sys.stdout):
     """Upload a directory of spatial data files to GeoNode
 
        This function also verifies that each layer is in GeoServer.
@@ -576,14 +577,13 @@ def upload(incoming, user=None, overwrite=True, keywords = ()):
            [{'file': 'data1.tiff', 'name': 'geonode:data1' },
            {'file': 'data2.shp', 'errors': 'Shapefile requires .prj file'}]
     """
+    if verbosity > 1:
+        print >> console, "Verifying that GeoNode is running ..."
     check_geonode_is_up()
 
+    potential_files = []
     if os.path.isfile(incoming):
-        layer = file_upload(incoming,
-                            user=user,
-                            overwrite=overwrite,
-                            keywords = keywords)
-        return [{'file': incoming, 'name': layer.name}]
+        potential_files.append(incoming)
     elif not os.path.isdir(incoming):
         msg = ('Please pass a filename or a directory name as the "incoming" '
                'parameter, instead of %s: %s' % (incoming, type(incoming)))
@@ -598,22 +598,47 @@ def upload(incoming, user=None, overwrite=True, keywords = ()):
                 basename, extension = os.path.splitext(short_filename)
                 filename = os.path.join(root, short_filename)
                 if extension in ['.tif', '.shp', '.zip']:
-                    try:
-                        layer = file_upload(filename,
-                                            user=user,
-                                            title=basename,
-                                            overwrite=overwrite,
-                                            keywords=keywords
-                                           )
+                    potential_files.append(filename)
 
-                    except GeoNodeException, e:
-                        msg = ('[%s] could not be uploaded. Error was: '
-                               '%s' % (filename, str(e)))
-                        logger.info(msg)
-                        results.append({'file': filename, 'errors': msg})
-                    else:
-                        results.append({'file': filename, 'name': layer.name})
-        return results
+    # After gathering the list of potential files, let's process them one by one.
+    number = len(potential_files)
+    if verbosity > 1:
+        msg =  "Found %d potential layers, importing now ..." % number
+        print >> console, msg
+
+    output = []
+    for i, filename in enumerate(potential_files):
+        try:
+            layer = file_upload(filename,
+                                user=user,
+                                title=basename,
+                                overwrite=overwrite,
+                                keywords=keywords
+                               )
+            status = 'created'
+        except Exception, e:
+            if ignore_errors:
+                status = 'failed'
+                exception_type, error, traceback = sys.exc_info()
+            else:
+                if verbosity > 0:
+                    msg = "Stopping process because --ignore-errors was not set and an error was found."
+                    print >> sys.stderr, msg
+                    raise Exception('Failed to process %s' % filename, e), None, sys.exc_info()[2]
+
+        msg = "[%s] Layer for '%s' (%d/%d)" % (status, filename, i+1, number)
+        info = {'file': filename, 'status': status}
+        if status == 'failed':
+            info['traceback'] = traceback
+            info['exception_type'] = exception_type
+            info['error'] = error
+        else:
+            info['name'] = layer.name
+
+        output.append(info)
+        if verbosity > 0:
+            print >> console, msg
+    return output
 
 
 def _create_db_featurestore(name, data, overwrite = False, charset = None):
