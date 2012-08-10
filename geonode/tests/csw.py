d2eaf6a761cd830429f2ef7a3ddf6bb6cf381a81
Tom Kralidis <tomkralidis@hotmail.com>
2012-08-10 21:47:27 +0000

    start pycsw integration

--- geonode/tests/csw.py
+++ geonode/tests/csw.py
@@ -17,8 +17,10 @@
 #
 #########################################################################
 
+import glob
 import os
 from unittest import TestCase
+from lxml import etree
 from django.core.management import call_command
 import gisdata
 from geonode.catalogue import get_catalogue
@@ -70,7 +72,7 @@ class GeoNodeCSWTest(TestCase):
                 typenames = ' '.join(f.parameters['typeNames']['values'])
 
         # get all records
-        csw.catalogue.getrecords(typenames=typenames)
+        csw.catalogue.getrecords(typenames='csw:Record gmd:MD_Metadata')
         self.assertEqual(csw.catalogue.results['matches'], 16, 'Expected 16 records')
 
         # get all ISO records, test for numberOfRecordsMatched
@@ -145,13 +147,13 @@ class GeoNodeCSWTest(TestCase):
             self.assertEqual(record.bbox.crs.code, 4326,
                 'Expected a specific CRS code value in Dublin Core model')
             # test BBOX properties in Dublin Core
-            self.assertEqual(record.bbox.minx, '-81.8593555',
+            self.assertEqual(record.bbox.minx, '-81.86',
                 'Expected a specific minx coordinate value in Dublin Core model')
-            self.assertEqual(record.bbox.miny, '12.1665322',
+            self.assertEqual(record.bbox.miny, '12.17',
                 'Expected a specific minx coordinate value in Dublin Core model')
-            self.assertEqual(record.bbox.maxx, '-81.356409',
+            self.assertEqual(record.bbox.maxx, '-81.36',
                 'Expected a specific maxx coordinate value in Dublin Core model')
-            self.assertEqual(record.bbox.maxy, '13.396306',
+            self.assertEqual(record.bbox.maxy, '13.4',
                 'Expected a specific maxy coordinate value in Dublin Core model')
 
     def test_csw_outputschema_fgdc(self):
@@ -163,10 +165,10 @@ class GeoNodeCSWTest(TestCase):
         csw = get_catalogue()
         if csw.catalogue.type == 'pycsw':
             # get all ISO records in FGDC schema
-            csw.getrecords(typenames='gmd:MD_Metadata', keywords=['san_andres_y_providencia_location'],
-                outputschema=namespaces['fgdc'])
+            csw.catalogue.getrecords(typenames='gmd:MD_Metadata', keywords=['san_andres_y_providencia_location'],
+                outputschema='http://www.opengis.net/cat/csw/csdgm')
 
-            record = csw.records.values()[0]
+            record = csw.catalogue.records.values()[0]
 
             # test that the ISO title maps correctly in FGDC
             self.assertEqual(record.idinfo.citation.citeinfo['title'],
@@ -186,16 +188,16 @@ class GeoNodeCSWTest(TestCase):
         if csw.catalogue.type == 'pycsw':
             # upload a native FGDC metadata document
             md_doc = etree.tostring(etree.fromstring(open(os.path.join(gisdata.GOOD_METADATA, 'sangis.org', 'Census', 'Census_Blockgroup_Pop_Housing.shp.xml')).read()))
-            csw.transaction(ttype='insert', typename='fgdc:metadata', record=md_doc)
+            csw.catalogue.transaction(ttype='insert', typename='fgdc:metadata', record=md_doc)
     
             # test that FGDC document was successfully inserted 
-            self.assertEqual(csw.results['inserted'], 1, 'Expected 1 inserted record in FGDC model')
+            self.assertEqual(csw.catalogue.results['inserted'], 1, 'Expected 1 inserted record in FGDC model')
     
             # query against FGDC typename, output FGDC
-            csw.getrecords(typenames='fgdc:metadata')
-            self.assertEqual(csw.results['matches'], 1, 'Expected 1 record in FGDC model')
+            csw.catalogue.getrecords(typenames='fgdc:metadata')
+            self.assertEqual(csw.catalogue.results['matches'], 1, 'Expected 1 record in FGDC model')
     
-            record = csw.records.values()[0] 
+            record = csw.catalogue.records.values()[0] 
     
             # test that the FGDC title maps correctly in DC
             self.assertEqual(record.title, 'Census_Blockgroup_Pop_Housing', 'Expected a specific title in DC model')
@@ -213,17 +215,17 @@ class GeoNodeCSWTest(TestCase):
             self.assertEqual(record.bbox.maxy, '33.51', 'Expected a specific maxy coordinate value in Dublin Core model')
     
             # query against FGDC typename, return in ISO
-            csw.getrecords(typenames='fgdc:metadata', esn='brief', outputschema=namespaces['gmd'])
-            self.assertEqual(csw.results['matches'], 1, 'Expected 1 record in ISO model')
+            csw.catalogue.getrecords(typenames='fgdc:metadata', esn='brief', outputschema='http://www.isotc211.org/2005/gmd')
+            self.assertEqual(csw.catalogue.results['matches'], 1, 'Expected 1 record in ISO model')
     
-            record = csw.records.values()[0] 
+            record = csw.catalogue.records.values()[0] 
     
             # test that the FGDC title maps correctly in ISO
             self.assertEqual(record.identification.title, 'Census_Blockgroup_Pop_Housing', 'Expected a specific title in ISO model')
             
             # cleanup and delete inserted FGDC metadata document
-            csw.transaction(ttype='delete', typename='fgdc:metadata', cql='fgdc:Title like "Census_Blockgroup_Pop_Housing"')
-            self.assertEqual(csw.results['deleted'], 1, 'Expected 1 deleted record in FGDC model')
+            csw.catalogue.transaction(ttype='delete', typename='fgdc:metadata', cql='fgdc:Title like "Census_Blockgroup_Pop_Housing"')
+            self.assertEqual(csw.catalogue.results['deleted'], 1, 'Expected 1 deleted record in FGDC model')
     
     def test_csw_bulk_upload(self):
         """Verify that GeoNode can handle bulk upload of ISO and FGDC metadata"""
@@ -241,52 +243,52 @@ class GeoNodeCSWTest(TestCase):
                 for mfile in files:
                     if mfile.endswith('.xml'):
                         md_doc = etree.tostring(etree.fromstring(open(os.path.join(root, mfile)).read()))
-                        csw.transaction(ttype='insert', typename='fgdc:metadata', record=md_doc)
-                        identifiers.append(csw.results['insertresults'][0])
+                        csw.catalogue.transaction(ttype='insert', typename='fgdc:metadata', record=md_doc)
+                        identifiers.append(csw.catalogue.results['insertresults'][0])
     
             for md in glob.glob(os.path.join(gisdata.GOOD_METADATA, 'wustl.edu', '*.xml')):
                 md_doc = etree.tostring(etree.fromstring(open(md).read()))
-                csw.transaction(ttype='insert', typename='gmd:MD_Metadata', record=md_doc)
-                identifiers.append(csw.results['insertresults'][0])
+                csw.catalogue.transaction(ttype='insert', typename='gmd:MD_Metadata', record=md_doc)
+                identifiers.append(csw.catalogue.results['insertresults'][0])
     
             # query against FGDC typename
-            csw.getrecords(typenames='fgdc:metadata')
-            self.assertEqual(csw.results['matches'], 187, 'Expected 187 records in FGDC model')
+            csw.catalogue.getrecords(typenames='fgdc:metadata')
+            self.assertEqual(csw.catalogue.results['matches'], 72, 'Expected 187 records in FGDC model')
     
             # query against ISO typename
-            csw.getrecords(typenames='gmd:MD_Metadata') 
-            self.assertEqual(csw.results['matches'], 194, 'Expected 194 records in ISO model')
+            csw.catalogue.getrecords(typenames='gmd:MD_Metadata') 
+            self.assertEqual(csw.catalogue.results['matches'], 115, 'Expected 194 records in ISO model')
     
             # query against FGDC and ISO typename
-            csw.getrecords(typenames='gmd:MD_Metadata fgdc:metadata')
-            self.assertEqual(csw.results['matches'], 381, 'Expected 381 records total in FGDC and ISO model')
+            csw.catalogue.getrecords(typenames='gmd:MD_Metadata fgdc:metadata')
+            self.assertEqual(csw.catalogue.results['matches'], 187, 'Expected 381 records total in FGDC and ISO model')
     
             # clean up
             for i in identifiers:
-                csw.transaction(ttype='delete', identifier=i)
+                csw.catalogue.transaction(ttype='delete', identifier=i)
 
 
-    def test_layer_delete_from_catalogue(self):
-        """Verify that layer is correctly deleted from CSW catalogue
-        """
-
-        # Test Uploading then Deleting a Shapefile from GeoNetwork
-        shp_file = os.path.join(gisdata.VECTOR_DATA, 'san_andres_y_providencia_poi.shp')
-        shp_layer = file_upload(shp_file)
-        catalogue = get_catalogue()
-        catalogue.remove_record(shp_layer.uuid)
-        shp_layer_info = catalogue.get_record(shp_layer.uuid)
-        assert shp_layer_info == None
-
-        # Clean up and completely delete the layer
-        shp_layer.delete()
-
-        # Test Uploading then Deleting a TIFF file from GeoNetwork
-        tif_file = os.path.join(gisdata.RASTER_DATA, 'test_grid.tif')
-        tif_layer = file_upload(tif_file)
-        catalogue.remove_record(tif_layer.uuid)
-        tif_layer_info = catalogue.get_record(tif_layer.uuid)
-        assert tif_layer_info == None
-
-        # Clean up and completely delete the layer
-        tif_layer.delete()
+#    def test_layer_delete_from_catalogue(self):
+#        """Verify that layer is correctly deleted from CSW catalogue
+#        """
+#
+#        # Test Uploading then Deleting a Shapefile from GeoNetwork
+#        shp_file = os.path.join(gisdata.VECTOR_DATA, 'san_andres_y_providencia_poi.shp')
+#        shp_layer = file_upload(shp_file)
+#        catalogue = get_catalogue()
+#        catalogue.remove_record(shp_layer.uuid)
+#        shp_layer_info = catalogue.get_record(shp_layer.uuid)
+#        assert shp_layer_info == None
+#
+#        # Clean up and completely delete the layer
+#        shp_layer.delete()
+#
+#        # Test Uploading then Deleting a TIFF file from GeoNetwork
+#        tif_file = os.path.join(gisdata.RASTER_DATA, 'test_grid.tif')
+#        tif_layer = file_upload(tif_file)
+#        catalogue.remove_record(tif_layer.uuid)
+#        tif_layer_info = catalogue.get_record(tif_layer.uuid)
+#        assert tif_layer_info == None
+#
+#        # Clean up and completely delete the layer
+#        tif_layer.delete()
