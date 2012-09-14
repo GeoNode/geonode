04fc9263ef1ba1b2fe3b9cd43e29453b015f8fb2
Dave Lowe <mahalowe@gmail.com>
2012-09-13 21:39:51 -0500

    Layer model & UI work
    * New model TopicCategory
    * Initial category data from layers.enumerations
    * Implemented bulk of UI for Explore Layers
    * Added LESS Elements for box-shadow, border-radius mixins
    * Javascript for left nav toggle (slides left offpage)
    * Minor tweaks to map detail template

--- geonode/layers/models.py
+++ geonode/layers/models.py
@@ -131,6 +131,20 @@ class LayerManager(models.Manager):
                 print >> console, msg
         return output
 
+
+class TopicCategory(models.Model):
+
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return u"{0}".format(self.name)
+
+    class Meta:
+        ordering = ("name",)
+
+
 class ResourceBase(models.Model, PermissionLevelMixin):
     """
     Base Resource Object loosely based on ISO 19115:2003
@@ -166,7 +180,8 @@ class ResourceBase(models.Model, PermissionLevelMixin):
 
     # Section 4
     language = models.CharField(_('language'), max_length=3, choices=ALL_LANGUAGES, default='eng', help_text=_('language used within the dataset'))
-    topic_category = models.CharField(_('topic_category'), max_length=255, choices=TOPIC_CATEGORIES, default='location', help_text=_('high-level geographic data thematic classification to assist in the grouping and search of available geographic data sets.'))
+    topic_category = models.CharField(_('topic_category'), editable=False, max_length=255, choices=TOPIC_CATEGORIES, default='location')
+    category = models.ForeignKey(TopicCategory, help_text=_('high-level geographic data thematic classification to assist in the grouping and search of available geographic data sets.'), null=True)
 
     # Section 5
     temporal_extent_start = models.DateField(_('temporal extent start'), blank=True, null=True, help_text=_('time period covered by the content of the dataset (start)'))
