8d48bdd5c879f23c476fc7e681bbc33f6393e6db
Dave Lowe <mahalowe@gmail.com>
2012-09-14 17:38:11 -0500

    Fixes and improvements for layers UI

--- geonode/static/geonode/js/tabslider.js
+++ geonode/static/geonode/js/tabslider.js
@@ -1,5 +1,5 @@
 $(function() {
-    $("#slide-pane a.toggle").click(function(e) {
+    $("#slide-pane a.toggle-pane").click(function(e) {
         e.preventDefault();
         if(!$("#slide-pane").is('.hidden')) {
             $("#slide-pane").addClass("hidden").animate({
@@ -14,6 +14,16 @@ $(function() {
                 marginLeft: "0px"
             }, 500);
             $(this).find("i").attr("class", "icon-chevron-left");
+        }
+    });
+    $("nav a.toggle-nav").click(function(e) {
+        e.preventDefault();
+        if ($(this).parents("h2").siblings(".nav").is(":visible")) {
+            $(this).parents("h2").siblings(".nav").slideUp();
+            $(this).find("i").attr("class", "icon-chevron-left");
+        } else {
+            $(this).parents("h2").siblings(".nav").slideDown();
+            $(this).find("i").attr("class", "icon-chevron-down");
         }
     });
 });
