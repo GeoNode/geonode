00460746b0ec6ee8f4fb29ea64fbd9727c7bda40
Dave Lowe <mahalowe@gmail.com>
2012-09-25 14:05:01 -0500

    Infinite scrolling

--- geonode/static/geonode/js/loadmore.js
+++ geonode/static/geonode/js/loadmore.js
@@ -1,32 +1,27 @@
 $(function () {
-    var hasMore = parseInt($(".pages .current.page").html()) < parseInt($(".pages .page_total").html());
+    var hasMore = parseInt($(".pages .current.page").html()) < parseInt($(".pages .page_total").html()),
+    $loading = $("<div class='loading'><p>Loading more items&hellip;</p></div>"),
+    $pages = $(".pages");
+    opts = {
+        offset: '100%'
+    };
+
     if (hasMore && !$("html.ie8").size()) {
-        $('.pages').hide().after('<div class="loadmore"><a href="#">Show More <i class="icon-chevron-down"></i></a></div>');
-        $('.loadmore a').click(function (e) {
-            e.preventDefault();
-            $('.loadmore').addClass('loading');
-            var nextpage = parseInt($(".pages:last .current.page").html()) + 1;
-            if (nextpage <= parseInt($(".pages:last .page_total").html())) {
-                $.ajax({
-                    url: window.location.pathname+'?page='+nextpage+($.urlParam('q') ? '&q='+$.urlParam('q') : ''),
-                    context: $('.tab-pane'),
-                    dataType: 'html',
-                    success: function (data) {
-                        $(".loadmore").removeClass('loading').before(
-                            $("<div class=\"loaddata\" style=\"display:none\">"+data+"</div>")
-                        );
-                        $(".tab-pane").append($(".loaddata").find("article"));
-                        $('.viewcount span:eq(0)').html($(".tab-pane > article").size());
-                        $(".pages").html($(".loaddata").find(".pages").html());
-                        var pagination = $(".pages:last");
-                        if (parseInt(pagination.find('.current.page').html()) >= parseInt(pagination.find('.page_total').html()))
-                            $('.loadmore a').hide();
-                        pagination.hide();
-                        $(".loaddata").remove();
-                    }
-                });
-            }
-        });
+        $pages.children().hide();
+        $pages.waypoint(function(event, direction) {
+            $pages.waypoint('remove');
+            $pages.before($loading);
+            $.get($('a.more').attr('href'), function(data) {
+                var $data = $(data);
+                $('.tab-pane.active').append($data.find('article'));
+                $loading.detach();
+                if ($data.find(".more").size()) {
+                    $('.more').replaceWith($data.find('.more'));
+                    $pages.waypoint(opts);
+                }
+            });
+        }, opts);
+
     }
 });
 $.urlParam = function(name){
