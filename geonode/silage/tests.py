b917c27fd988cd8a0b3f629dd3eb9b275f2ebac6
Ian Schneider <planablediglet@gmail.com>
2012-09-26 14:41:27 -0600

    test work

--- geonode/silage/tests.py
+++ geonode/silage/tests.py
@@ -2,26 +2,30 @@ from django.core import management
 from django.test import TestCase
 from django.test.client import Client
 import json
+import logging
 
-
-def setup():
-    print 'setup called'
-    management.call_command('loaddata', 'silage_testdata.json', verbosity=0)
-
-def teardown():
-    print 'teardown called'
-    management.call_command('flush', verbosity=0, interactive=False)
-
+logging.getLogger('south').setLevel(logging.INFO)
+setup_ran = False
 
 class SilageTest(TestCase):
 
     c = Client()
+    
+    fixtures = ['initial_data.json','silage_testdata.json']
 
     def _fixture_setup(self):
-        pass
+        global setup_ran
+        if setup_ran:
+            return
+        super(SilageTest, self)._fixture_setup()
+        setup_ran = True
+        
+    def _fixture_teardown(self):
+        #logging.getLogger('south').setLevel(logging.DEBUG)
+        return
 
     def test_basic(self):
-        response = self.c.get('/search/api')
+        response = self.c.get('/search/api',{'limit': 100})
         jsonvalue = json.loads(response.content)
 
         self.assertFalse(jsonvalue.get('errors'))
