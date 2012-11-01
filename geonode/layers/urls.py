62eb6bf1c2ee16dec301fd102eeda862c6900c8e
Ariel Núñez <ingenieroariel@gmail.com>
2012-10-31 20:28:28 -0400

    pep8 geonode.layers.urls

--- geonode/layers/urls.py
+++ geonode/layers/urls.py
@@ -26,27 +26,34 @@ js_info_dict = {
     'packages': ('geonode.layers',),
 }
 
-urlpatterns = patterns('geonode.layers.views',
-  url(r'^$', LayerListView.as_view(), name='layer_browse'),
-  url(r'^popular/$', LayerListView.as_view(
-    layer_filter="popular_count"
-  ), name='layer_browse_popular'),
-  url(r'^shared/$', LayerListView.as_view(
-    layer_filter="share_count"
-  ), name='layer_browse_shared'),
-  url(r'^category/(?P<slug>[-\w]+?)/$', 'layer_category', name='layer_browse_category'),
-  url(r'^tag/(?P<slug>[-\w]+?)/$', 'layer_tag', name='layer_browse_tag'),
-  url(r'^acls/?$', 'layer_acls', name='layer_acls'),
-  url(r'^search/?$', 'layer_search_page', name='layer_search_page'),
-  url(r'^upload$', 'layer_upload', name='layer_upload'),
-  url(r'^download$', 'layer_batch_download', name='layer_batch_download'),
-  url(r'^(?P<layername>[^/]*)$', 'layer_detail', name="layer_detail"),
-  url(r'^(?P<layername>[^/]*)/metadata$', 'layer_metadata', name="layer_metadata"),
-  url(r'^(?P<layername>[^/]*)/remove$', 'layer_remove', name="layer_remove"),
-  url(r'^(?P<layername>[^/]*)/replace$', 'layer_replace', name="layer_replace"),
-  url(r'^(?P<layername>[^/]*)/style$', 'layer_style', name="layer_style"),
-  url(r'^(?P<layername>[^/]*)/permissions$', 'layer_permissions', name='layer_permissions'),
-  url(r'^(?P<layername>[^/]*)/edit-check?$', 'feature_edit_check', name="feature_edit_check")
-  #url(r'^api/batch_permissions/?$', 'batch_permissions', name='batch_permssions'),
-  #url(r'^api/batch_delete/?$', 'batch_delete', name='batch_delete'),
+urlpatterns = patterns(
+    'geonode.layers.views',
+    url(r'^$', LayerListView.as_view(), name='layer_browse'),
+    url(r'^popular/$', LayerListView.as_view(
+        layer_filter="popular_count"),
+        name='layer_browse_popular'),
+    url(r'^shared/$', LayerListView.as_view(
+        layer_filter="share_count"),
+        name='layer_browse_shared'),
+    url(r'^category/(?P<slug>[-\w]+?)/$', 'layer_category',
+        name='layer_browse_category'),
+    url(r'^tag/(?P<slug>[-\w]+?)/$', 'layer_tag', name='layer_browse_tag'),
+    url(r'^acls/?$', 'layer_acls', name='layer_acls'),
+    url(r'^search/?$', 'layer_search_page', name='layer_search_page'),
+    url(r'^upload$', 'layer_upload', name='layer_upload'),
+    url(r'^download$', 'layer_batch_download', name='layer_batch_download'),
+    url(r'^(?P<layername>[^/]*)$', 'layer_detail', name="layer_detail"),
+    url(r'^(?P<layername>[^/]*)/metadata$', 'layer_metadata',
+        name="layer_metadata"),
+    url(r'^(?P<layername>[^/]*)/remove$', 'layer_remove', name="layer_remove"),
+    url(r'^(?P<layername>[^/]*)/replace$', 'layer_replace',
+        name="layer_replace"),
+    url(r'^(?P<layername>[^/]*)/style$', 'layer_style', name="layer_style"),
+    url(r'^(?P<layername>[^/]*)/permissions$', 'layer_permissions',
+        name='layer_permissions'),
+    url(r'^(?P<layername>[^/]*)/edit-check?$', 'feature_edit_check',
+        name="feature_edit_check")
+    #url(r'^api/batch_permissions/?$', 'batch_permissions',
+    #    name='batch_permssions'),
+    #url(r'^api/batch_delete/?$', 'batch_delete', name='batch_delete'),
 )
