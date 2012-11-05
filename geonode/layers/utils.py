7698bd80325cffe90f03e77d0b5538715052d814
Ariel Núñez <ingenieroariel@gmail.com>
2012-11-05 14:19:04 -0500

    Better default title for data loaded in bulk

--- geonode/layers/utils.py
+++ geonode/layers/utils.py
@@ -40,7 +40,7 @@ from geonode.utils import check_geonode_is_up
 from geonode.people.utils import get_valid_user
 from geonode.layers.models import Layer
 from geonode.layers.metadata import set_metadata
-from geonode.people.models import Profile 
+from geonode.people.models import Profile
 from geonode.gs_helpers import cascading_delete
 from geonode.gs_helpers import get_sld_for
 from geonode.gs_helpers import delete_from_postgis
@@ -656,7 +656,6 @@ def upload(incoming, user=None, overwrite=False,
             try:
                 layer = file_upload(filename,
                                     user=user,
-                                    title=basename,
                                     overwrite=overwrite,
                                     keywords=keywords,
                                     )
