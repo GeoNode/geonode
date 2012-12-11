537fd89bef56d128556deecb1f2f7673bb784c20
Simone Dalmasso <simone.dalmasso@gmail.com>
2012-12-11 16:34:13 +0100

    save the map before triggering the map_change signal

--- geonode/maps/models.py
+++ geonode/maps/models.py
@@ -178,10 +178,10 @@ class Map(ResourceBase, GXPMapBase):
 
         self.set_bounds_from_layers(self.local_layers)
 
+        self.save()
+
         if layer_names != set([l.typename for l in self.local_layers]):
             map_changed_signal.send_robust(sender=self,what_changed='layers')
-
-        self.save()
 
     def keyword_list(self):
         keywords_qs = self.keywords.all()
