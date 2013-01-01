487f044cc6d04d05eb637442e2f6b994fe361875
Jeffrey Johnson <jjohnson@opengeo.org>
2012-12-31 17:44:12 -0800

    work

--- scripts/jenkins/jenkins-i18n.sh
+++ scripts/jenkins/jenkins-i18n.sh
@@ -33,7 +33,7 @@ python ../manage.py makemessages --all
 python ../manage.py compilemessages
 git add .
 git commit -am "Daily Update GeoNode i18n"
-git push -u git@github.com:jj0hns0n/geonode.git i18n
+git push -u git@github.com:jj0hns0n/geonode.git HEAD:i18n
 
 # Send the PR against GeoNode dev (Need to export username and password as env vars)
 git pull-request -f "Daily Update GeoNode i18n" -b jj0hns0n/geonode:dev -h jj0hns0n/geonode:i18n
