9200f56ebcf405c950e7192b305f52c175717777
Jeffrey Johnson <jjohnson@opengeo.org>
2013-01-02 15:38:40 -0800

    Adding jenkins-geonode-ami.sh

---
+++ scripts/jenkins/jenkins-geonode-ami.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ~/.bashrc
+cd scripts/cloud/
+#python ec2.py terminate
+rm .gnec2.cfg
+python ec2.py launch_base
+
+host=$(python ec2.py host)
+key=$(python ec2.py key)
+echo $host
+echo $key
+
+cd $WORKSPACE/scripts/cloud/
+fab -i $key -H ubuntu@$host build_geonode_ami
+
+python ec2.py terminate
