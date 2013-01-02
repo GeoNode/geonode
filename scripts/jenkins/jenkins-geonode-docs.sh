635818ea058c7d0f1cdc50a3ed4acba6227ae5a0
Jeffrey Johnson <jjohnson@opengeo.org>
2013-01-01 16:46:00 -0800

    Adding jenkings-geonode-docs.sh

---
+++ scripts/jenkins/jenkins-geonode-docs.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Clean up from previous runs
+rm -rf ${WORKSPACE}/*
+
+# Setup environment variables.
+PYENV_HOME=$HOME/.pyenv/
+
+# Delete previously built virtualenv
+if [ -d $PYENV_HOME ]; then
+    rm -rf $PYENV_HOME
+fi
+
+# Setup the virtualenv
+virtualenv --no-site-packages $PYENV_HOME
+source $PYENV_HOME/bin/activate
+pip install cactus
+pip install sphinx_bootstrap_theme
+
+# Get all the Relevant repos
+git clone git@github.com:GeoNode/geonode.github.com.git
+git clone git://github.com/GeoNode/geonode.git
+git clone git://github.com/GeoNode/geonode-workshops.git
+
+# Compile and Publish docs
+cd geonode/docs
+make html
+make latexpdf
+rm -rf /var/www/docs/*
+cp -R _build/html/* /var/www/docs/
+cp -R _build/latex/GeoNode.pdf /var/www/docs/
+
+# Compile and Publish Workshops
+cd ${WORKSPACE}/geonode-workshops/user/doc
+make html
+make latexpdf
+rm -rf /var/www/workshops/user/*
+cp -R build/html/* /var/www/workshops/user/
+cp -R build/latex/GeoNodeUsersWorkshop.pdf /var/www/workshops/user/
+
+cd ${WORKSPACE}/geonode-workshops/admin
+make html
+make latexpdf
+rm -rf /var/www/workshops/admin/*
+cp -R build/html/* /var/www/workshops/admin/
+cp -R build/latex/GeoNodeAdministratorsWorkshop.pdf /var/www/workshops/admin/
+
+cd ${WORKSPACE}/geonode-workshops/devel
+make html
+make latexpdf
+rm -rf /var/www/workshops/devel/*
+cp -R build/html/* /var/www/workshops/devel/
+cp -R build/latex/GeoNodeDevelopersWorkshop.pdf /var/www/workshops/devel/
+
+# Compile and Publish Website
+cd ${WORKSPACE}/geonode.github.com/
+cactus build
+cp -R build/* .
+git commit -am "Update GeoNode Website"
+git push origin master
+rm -rf /var/www/site/*
+cp -R build/* /var/www/site/
