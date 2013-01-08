6289ce6275b749933122e01685b6b6a0804c1fc4
Matt Bertrand <mbertrand@cga.harvard.edu>
2013-01-08 14:49:41 -0500

    Catch exceptions when retrieving layer resource; alter map detail edit page

--- src/GeoNodePy/geonode/maps/views.py
+++ src/GeoNodePy/geonode/maps/views.py
@@ -82,6 +82,7 @@ def default_map_config():
 
 def bbox_to_wkt(x0, x1, y0, y1, srid="4326"):
     return 'SRID='+srid+';POLYGON(('+x0+' '+y0+','+x0+' '+y1+','+x1+' '+y1+','+x1+' '+y0+','+x0+' '+y0+'))'
+
 class ContactForm(forms.ModelForm):
     keywords = taggit.forms.TagField(required=False)
     class Meta:
@@ -185,12 +186,13 @@ class PocForm(forms.Form):
 
 class MapForm(forms.ModelForm):
     keywords = taggit.forms.TagField(required=False)
+    title = forms.CharField(300)
+    abstract = forms.CharField(1000, widget=forms.Textarea(attrs={'cols': 40, 'rows': 10}), required=False)
+    content = forms.CharField(1000, widget=forms.Textarea(attrs={'cols': 40, 'rows': 10, 'id':'mapdescription'}), required=False)
+
     class Meta:
         model = Map
-        exclude = ('contact', 'zoom', 'projection', 'center_x', 'center_y', 'owner', 'officialurl', 'urlsuffix', 'keywords', 'content', 'use_custom_template', 'group_params')
-        widgets = {
-            'abstract': forms.Textarea(attrs={'cols': 40, 'rows': 10}),
-        }
+        exclude = ('contact', 'zoom', 'projection', 'center_x', 'center_y', 'owner', 'officialurl', 'urlsuffix', 'keywords', 'use_custom_template', 'group_params')
 
 
 
