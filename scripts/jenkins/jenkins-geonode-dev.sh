762fa32f3399fce3832917ce4e01c13894cc098a
Jeffrey Johnson <jjohnson@opengeo.org>
2013-01-11 12:47:47 -0800

    Comment out jenkins code quality tools for now

--- scripts/jenkins/jenkins-geonode-dev.sh
+++ scripts/jenkins/jenkins-geonode-dev.sh
@@ -63,13 +63,13 @@ cp -R coverage integration-coverage
 paver test_javascript
 
 # Run Code Quality Tools
-export DJANGO_SETTINGS_MODULE=geonode.settings
-pylint -f parseable geonode/ | tee pylint.out
-pep8 --repeat geonode | tee pep8.out
-find . -type f -iname "*.py" | egrep -v '^./tests/'|xargs pyflakes  > pyflakes.out || :
-clonedigger --cpd-output . || :
-mv output.xml clonedigger.out
-echo; echo ">>> Reporting FIXME's and TODO's in source code"
+#export DJANGO_SETTINGS_MODULE=geonode.settings
+#pylint -f parseable geonode/ | tee pylint.out
+#pep8 --repeat geonode | tee pep8.out
+#find . -type f -iname "*.py" | egrep -v '^./tests/'|xargs pyflakes  > pyflakes.out || :
+#clonedigger --cpd-output . || :
+#mv output.xml clonedigger.out
+#echo; echo ">>> Reporting FIXME's and TODO's in source code"
 
 # All done, clean up
 git reset --hard
