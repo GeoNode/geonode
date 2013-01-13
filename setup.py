fed618f1e7894ef1dea293826b39098600149693
Ariel Núñez <ingenieroariel@gmail.com>
2013-01-13 03:34:43 +0000

    Bump Django version to 1.4.3

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(name='GeoNode',
         # python dependencies
         "gsconfig==0.6.1",
         "OWSLib==0.6.1",
-        "Django==1.4.2",
+        "Django==1.4.3",
         "httplib2>=0.7",
         # Django Apps
         "pinax-theme-bootstrap==2.2.1",
