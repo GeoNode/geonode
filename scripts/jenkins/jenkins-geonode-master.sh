620451e6ed86ef444b7cd0154ebfaef317e4de99
Jeffrey Johnson <jjohnson@opengeo.org>
2013-01-14 15:19:38 -0800

    Split jenkins script for master and dev temporarily

--- scripts/jenkins/jenkins-geonode-master.sh
+++ scripts/jenkins/jenkins-geonode-master.sh
@@ -2,7 +2,7 @@
 PATH=/home/jenkins/apache-maven-2.2.1/bin/:$PATH
 MAVEN_HOME=/home/jenkins/apache-maven-2.2.1/
 JAVA_HOME=/usr/lib/jvm/java-6-sun/
-PYENV_HOME=$HOME/.pyenv-dev/
+PYENV_HOME=$HOME/.pyenv-master/
 DL_ROOT=/var/www/geonode
 GIT_REV=$(git log -1 --pretty=format:%h)
 
