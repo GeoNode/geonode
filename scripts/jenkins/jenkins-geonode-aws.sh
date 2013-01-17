d0592de835b5def49ef96767199dd7558d93bd8f
Michael Weisman <mweisman@gmail.com>
2013-01-17 15:15:25 -0800

    Setup data before changing ip

--- scripts/jenkins/jenkins-geonode-aws.sh
+++ scripts/jenkins/jenkins-geonode-aws.sh
@@ -12,9 +12,9 @@ echo $key
 
 cd $WORKSPACE/scripts/cloud/
 fab -i $key -H ubuntu@$host deploy_geonode_testing_package
-fab -i $key -H ubuntu@$host geonode_updateip:server_name=alpha.dev.geonode.org 
+
 fab -i $key -H ubuntu@$host install_sample_data
 
 python ec2.py set_alpha_ip
-
+fab -i $key -H ubuntu@$host geonode_updateip:server_name=alpha.dev.geonode.org
 #python ec2.py terminate
