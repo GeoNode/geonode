9d231aa7a1161da2b150925493f452ed4cc8310e
Ivan Willig <iwillig@gmail.com>
2013-01-29 08:14:28 -0500

    Make the qunit bootstrap module output an junit xml file

--- geonode/static/geonode/libs/bootstrap.js
+++ geonode/static/geonode/libs/bootstrap.js
@@ -24,16 +24,92 @@ Envjs({
                 testStart,
                 testCases = [],
                 testFailed = 0,
-                testPassed = 0;
+                testPassed = 0,
+                current_test_assertions = [];
 
             QUnit.moduleStart = function (details) {
                 moduleStart = new Date();
                 module = details;
+                testCases = []; // reset the testCase array
             };
 
             QUnit.testStart = function () {
                 testStart = new Date();
             };
+
+
+            QUnit.testDone = function (context) {
+                var xml, i, l = current_test_assertions.length;
+
+                if (0 === context.failed) {
+                    testPassed += 1;
+                } else {
+                    testFailed += 1;
+                }
+
+                xml = '<testcase classname= "' + module.name + '"' +
+                      'name="' + context.name + '"' +
+                      'time="' + (new Date() - testStart) / 1000  + '"';
+
+
+                if (context.failed) {
+                    xml += '/>\n';
+                    for (i = 0; i < l; i += 1) {
+                        xml += current_test_assertions[i];
+                    }
+                    xml += '</testcase>\n';
+                } else {
+                    xml += '/>\n';
+                }
+
+                // reset the current_test_assertions after a test suite is done
+                current_test_assertions = [];
+
+                testCases.push(xml);
+            };
+
+            QUnit.moduleDone = function (context) {
+                var i, l,
+                    xml = '<testsuite ' +
+                    'name="' + context.name +  '"' +
+                    'errors="0" failures="' + context.failed +  '"' +
+                    'tests="' + context.total + '"' +
+                    'time="'  + (new Date() -  moduleStart) / 1000 + '  "';
+
+                if (testCases.length) {
+                    xml += '>\n';
+                    for (i = 0, l = testCases.length; i < l; i += 1) {
+                        xml += testCases[i];
+                    }
+                    xml += '</testsuite>\n\n';
+                } else {
+                    xml += '/>\n\n';
+                }
+
+                junitXml += xml;
+            };
+
+
+            QUnit.log = function (details) {
+                var xml;
+
+                console.log(
+                    '{%s} (%s) [%s] %s',
+                    module.name,
+                    count += 1,
+                    details.result ? 'PASSED' : 'FAILED',
+                    details.message
+                );
+                // if the result if true then bail
+                if (details.result) {
+                    return;
+                }
+
+                xml = '<failure type="failed" message="' + details.message + '"/>';
+                current_test_assertions.push(xml);
+            };
+
+
 
             QUnit.done = function (details) {
                 var out = new java.io.File('junit.xml'), bw;
@@ -47,32 +123,13 @@ Envjs({
                     out.createNewFile();
                 }
 
+                // really java?
                 bw = new java.io.BufferedWriter(new java.io.FileWriter(out.getAbsoluteFile()));
-                bw.write('test');
-
+                bw.write(junitXml);
                 bw.close();
 
             };
 
-
-            QUnit.testDone = function (result) {
-                if (0 === result.failed) {
-                    testPassed += 1;
-                } else {
-                    testFailed += 1;
-                }
-            };
-
-
-            QUnit.log = function (details) {
-                console.log(
-                    '{%s} (%s) [%s] %s',
-                    module.name,
-                    count += 1,
-                    details.result ? 'PASSED' : 'FAILED',
-                    details.message
-                );
-            };
 
         }
 
