8de05d64ab8ca27a8ff813336d58fcea48bcb574
Ivan Willig <iwillig@gmail.com>
2013-01-28 21:17:14 -0500

    Start of junit output for qunit

--- geonode/static/geonode/libs/bootstrap.js
+++ geonode/static/geonode/libs/bootstrap.js
@@ -1,4 +1,6 @@
-/*global window: true, Envjs:true, load:true, QUnit */
+/*global window: true, java: true, Envjs:true, load:true, QUnit: true, document:true */
+'use strict';
+
 load("libs/env-js/dist/env.rhino.js");
 
 Envjs({
@@ -16,15 +18,57 @@ Envjs({
 
         'qunit': function (script) {
             var count = 0,
-                module;
+                junitXml = '',
+                module,
+                moduleStart,
+                testStart,
+                testCases = [],
+                testFailed = 0,
+                testPassed = 0;
+
             QUnit.moduleStart = function (details) {
+                moduleStart = new Date();
                 module = details;
             };
+
+            QUnit.testStart = function () {
+                testStart = new Date();
+            };
+
+            QUnit.done = function (details) {
+                var out = new java.io.File('junit.xml'), bw;
+
+                console.log('\n');
+                console.log(testPassed + ' tests passed');
+                console.log(testFailed + ' tests failed');
+                console.log('\n');
+
+                if (!out.exists()) {
+                    out.createNewFile();
+                }
+
+                bw = new java.io.BufferedWriter(new java.io.FileWriter(out.getAbsoluteFile()));
+                bw.write('test');
+
+                bw.close();
+
+            };
+
+
+            QUnit.testDone = function (result) {
+                if (0 === result.failed) {
+                    testPassed += 1;
+                } else {
+                    testFailed += 1;
+                }
+            };
+
+
             QUnit.log = function (details) {
                 console.log(
                     '{%s} (%s) [%s] %s',
                     module.name,
-                    count++,
+                    count += 1,
                     details.result ? 'PASSED' : 'FAILED',
                     details.message
                 );
