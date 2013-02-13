ece136a25b20edb22ec40cb5138e31a253db190e
Simone Dalmasso <simone.dalmasso@gmail.com>
2013-02-13 15:07:04 +0100

    add filter by date

--- geonode/static/geonode/js/global_search.js
+++ geonode/static/geonode/js/global_search.js
@@ -1,13 +1,13 @@
 $(function(){
-    $('.trigger-query').click(
-        function(){
+    function query(element){
+            element = element.currentTarget;
             // manage the activation deactivation of the filter on click
-            $(this).hasClass('active') ? $(this).removeClass('active') 
-                : $(this).addClass('active');
+            $(element).hasClass('active') ? $(element).removeClass('active') 
+                : $(element).addClass('active');
 
             // logic to make sure that whne clicking on the layer filter it also 
             //activats/deactivated vector and raster
-            if ($(this).attr('data-class') === 'layer'){
+            if ($(element).attr('data-class') === 'layer'){
                 if($(this).hasClass('active')){
                     $('a[data-class="raster"]').addClass('active');
                     $('a[data-class="vector"]').addClass('active');
@@ -20,7 +20,7 @@ $(function(){
 
             // logic to make sure that clicking on the all categories it also
             // activate/deactivate all other categories
-            if ($(this).parents('ul').attr('id') === 'categories' && $(this).attr('data-class') === 'all'){
+            if ($(element).parents('ul').attr('id') === 'categories' && $(element).attr('data-class') === 'all'){
                 if ($(this).hasClass('active')){
                     $('#categories').find('a').each(function(){
                         $(this).addClass('active');
@@ -35,7 +35,9 @@ $(function(){
             var params = {
                 types: [],
                 categories: [],
-                keywords: []
+                keywords: [],
+                date_start: [],
+                date_end: []
             }
             
             // traverse the active filters to build the query parameters
@@ -50,15 +52,24 @@ $(function(){
             if(params['categories'][0] === 'all'){
                 params['categories'].shift();
             }
+            if(params['date_start'][0] === 'yyyy-mm-dd'){
+                params['date_start'] = ['']
+            }
+            if(params['date_end'][0] === 'yyyy-mm-dd'){
+                params['date_end'] = ['']
+            }
 
             $.ajax({
                 type: 'POST',
                 url: '/search/html',
                 data: {
-                    type: params['types'].join(','),
-                    category: params['categories'].join(','),
-                    kw: params['keywords'].join(',')
-                }, 
+                    'type': params['types'].join(','),
+                    'category': params['categories'].join(','),
+                    'kw': params['keywords'].join(','),
+                    'start_date': params['date_start'][0],
+                    'end_date': params['date_end'][0]
+
+                                    }, 
                 success: function(data){
                     $('#search-content').html(data);
                     //call the pagination
@@ -68,5 +79,14 @@ $(function(){
                 }
             });
         }
+    $('.trigger-query').click(
+        query
+    );
+    $('.datepicker').change(
+        function(){
+            $(this).addClass('active');
+            $(this).attr('data-class', $(this).val());
+            query(this);
+        } 
     );
 });
