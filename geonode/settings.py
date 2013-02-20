f1794a9aa3381434495565935cfb172941aa5621
Ariel Núñez <ingenieroariel@gmail.com>
2013-02-20 09:45:41 -0500

    Upgraded to announcements 1.0.2 and fixed compatibility with notification

--- geonode/settings.py
+++ geonode/settings.py
@@ -254,7 +254,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
-    'announcements.context_processors.site_wide_announcements',
     'account.context_processors.account',
     # The context processor below adds things like SITEURL
     # and GEOSERVER_BASE_URL to all pages that use a RequestContext
