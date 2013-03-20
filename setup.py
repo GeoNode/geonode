9248b187c5aa3193fa50d4b425125c4992b29dc9
Ariel Núñez <ingenieroariel@gmail.com>
2013-03-20 01:01:46 -0400

    Upgraded gsconfig.py

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(name='GeoNode',
         "PIL",
         "lxml",
         # python dependencies
-        "gsconfig==0.6.1",
+        "gsconfig==0.6.2",
         "OWSLib==0.6.1",
         "Django==1.4.3",
         "httplib2>=0.7",
