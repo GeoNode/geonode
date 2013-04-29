8d4616a6c5ddd191736b937d88d3c52e1559e7a4
Matt Bertrand <mbertrand@cga.harvard.edu>
2013-04-29 14:50:03 -0400

    Get rid of stuff that belongs only in wmplus branch

--- src/GeoNodePy/geonode/urls.py
+++ src/GeoNodePy/geonode/urls.py
@@ -66,15 +66,13 @@ urlpatterns = patterns('',
     (r'^profiles/', include('geonode.profile.urls')),
     (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
     (r'^download/(?P<service>[^/]*)/(?P<layer>[^/]*)/(?P<format>[^/]*)/?$','geonode.proxy.views.download'),
-    (r'^gazetteer/', include('geonode.gazetteer.urls')),
-    (r'^bostonhoods/?', include('geonode.hoods.urls')),
+    (r'^gazetteer/', include('geonode.gazetteer.urls'))
     )
 
 urlpatterns += geonode.proxy.urls.urlpatterns
 
 
 official_site_url_patterns = patterns('',
-    (r'^tweetmap/$', 'geonode.maps.views.tweetview'),
     (r'^(?P<site>[A-Za-z0-9_\-]+)/$', 'geonode.maps.views.official_site'),
     (r'^(?P<site>[A-Za-z0-9_\-]+)/mobile/?$', 'geonode.maps.views.official_site_mobile'),
     (r'^(?P<site>[A-Za-z0-9_\-]+)/info$', 'geonode.maps.views.official_site_controller'),
