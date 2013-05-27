a6035e5dc81be2af8d62bc2321941fc43097ba81
Simone Dalmasso <simone.dalmasso@gmail.com>
2013-05-27 15:06:20 +0200

    update all tests to use real data instead of fixtures where needed

--- geonode/documents/tests.py
+++ geonode/documents/tests.py
@@ -19,22 +19,27 @@ from geonode.documents.models import Document
 from geonode.security.enumerations import ANONYMOUS_USERS, AUTHENTICATED_USERS
 import geonode.documents.views
 import geonode.security
+from geonode.search.populate_search_test_data import create_models
 
 imgfile = StringIO.StringIO('GIF87a\x01\x00\x01\x00\x80\x01\x00\x00\x00\x00ccc,\x00'
                                 '\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;')
 
-class EventsTest(TestCase):
-    fixtures = ['map_data.json', 'intial_data.json']
+class LayersTest(TestCase):
+    fixtures = ['intial_data.json']
     
+    def setUp(self):
+        create_models('document')
+        create_models('map')
+
     def test_create_document_with_no_rel(self):
         """Tests the createion of a document with no relations"""
         f = SimpleUploadedFile('test_img_file.gif', imgfile.read(), 'image/gif')
     
         superuser = User.objects.get(pk=2)
-        c, created = Document.objects.get_or_create(id=1, doc_file=f,owner=superuser, title='theimg')
+        c = Document.objects.create(doc_file=f,owner=superuser, title='theimg')
         c.set_default_permissions()
-
-        self.assertEquals(created, True)
+        document = Document.objects.get(pk=c.id)
+        self.assertEquals(Document.objects.get(pk=c.id).title, 'theimg')
 
     def test_create_document_with_rel(self):
         """Tests the createion of a document with no a map related"""
@@ -45,53 +50,46 @@ class EventsTest(TestCase):
         m = Map.objects.all()[0]
         ctype = ContentType.objects.get_for_model(m)
 
-        c, created = Document.objects.get_or_create(id=1, doc_file=f,owner=superuser, 
+        c = Document.objects.create(doc_file=f,owner=superuser, 
             title='theimg', content_type=ctype, object_id=m.id)
 
-        self.assertEquals(created, True)
+        self.assertEquals(Document.objects.get(pk=c.id).title, 'theimg')
 
     def test_document_details(self):
         """/documents/1 -> Test accessing the detail view of a document"""
 
-        if settings.DOCUMENTS_APP:
-            f = SimpleUploadedFile('test_img_file.gif', imgfile.read(), 'image/gif')
-        
-            superuser = User.objects.get(pk=2)
-            d, created = Document.objects.get_or_create(id=1, doc_file=f,owner=superuser, title='theimg')
-            d.set_default_permissions()
+    
+        superuser = User.objects.get(pk=2)
+        d = Document.objects.get(pk=1)
+        d.set_default_permissions()
 
-            c = Client()
-            response = c.get(reverse('document_detail', args=(str(d.id),)))
-            self.assertEquals(response.status_code, 200)
-
-        else: 
-            pass
+        c = Client()
+        response = c.get(reverse('document_detail', args=(str(d.id),)))
+        self.assertEquals(response.status_code, 200)
 
     def test_access_document_upload_form(self):
         """Test the form page is returned correctly via GET request /documents/upload"""
-        if settings.DOCUMENTS_APP:
-            c = Client()
-            log = c.login(username='bobby', password='bob')
-            self.assertTrue(log)
-            response = c.get(reverse('document_upload'))
-            self.assertTrue('Upload Documents' in response.content)
-        else:
-            pass
+
+        c = Client()
+        log = c.login(username='bobby', password='bob')
+        self.assertTrue(log)
+        response = c.get(reverse('document_upload'))
+        self.assertTrue('Upload Documents' in response.content)
+
 
     def test_document_isuploaded(self):
         """/documents/upload -> Test uploading a document"""
-        if settings.DOCUMENTS_APP:
-            f = SimpleUploadedFile('test_img_file.gif', imgfile.read(), 'image/gif')
-            m = Map.objects.all()[0]        
-            c = Client()
-            
-            c.login(username='admin', password='admin')
-            response = c.post(reverse('document_upload'), data={'file': f, 'title': 'uploaded_document', 'q': m.id, 'type': 'map', 
-                'permissions': '{"anonymous":"document_readonly","users":[]}'},
-                              follow=True)
-            self.assertEquals(response.status_code, 200)
-        else:
-            pass
+
+        f = SimpleUploadedFile('test_img_file.gif', imgfile.read(), 'image/gif')
+        m = Map.objects.all()[0]        
+        c = Client()
+        
+        c.login(username='admin', password='admin')
+        response = c.post(reverse('document_upload'), data={'file': f, 'title': 'uploaded_document', 'q': m.id, 'type': 'map', 
+            'permissions': '{"anonymous":"document_readonly","users":[]}'},
+                          follow=True)
+        self.assertEquals(response.status_code, 200)
+
         
     # Permissions Tests
 
@@ -122,8 +120,6 @@ class EventsTest(TestCase):
         f = SimpleUploadedFile('test_img_file.gif', imgfile.read(), 'image/gif')
     
         superuser = User.objects.get(pk=2)
-        document, created = Document.objects.get_or_create(id=1, doc_file=f,owner=superuser, title='theimg')
-        document.set_default_permissions()
         # Get a document to work with
         document = Document.objects.all()[0]
        
@@ -148,53 +144,51 @@ class EventsTest(TestCase):
     def test_ajax_document_permissions(self):
         """Verify that the ajax_document_permissions view is behaving as expected
         """
+    
+        # Setup some document names to work with 
+        f = SimpleUploadedFile('test_img_file.gif', imgfile.read(), 'image/gif')
+    
+        superuser = User.objects.get(pk=2)
+        document = Document.objects.create(doc_file=f,owner=superuser, title='theimg')
+        document.set_default_permissions()
+        document_id = document.id
+        invalid_document_id = 20
         
-        if settings.DOCUMENTS_APP:
-            # Setup some document names to work with 
-            f = SimpleUploadedFile('test_img_file.gif', imgfile.read(), 'image/gif')
+        c = Client()
+
+        # Test that an invalid document is handled for properly
+        response = c.post(reverse('document_permissions', args=(invalid_document_id,)), 
+                            data=json.dumps(self.perm_spec),
+                            content_type="application/json")
+        self.assertEquals(response.status_code, 404) 
+
+        # Test that GET returns permissions
+        response = c.get(reverse('document_permissions', args=(document_id,)))
+        assert('permissions' in response.content)
         
-            superuser = User.objects.get(pk=1)
-            document, created = Document.objects.get_or_create(id=1, doc_file=f,owner=superuser, title='theimg')
-            document.set_default_permissions()
-            document_id = document.id
-            invalid_document_id = 5
-            
-            c = Client()
+        # Test that a user is required to have documents.change_layer_permissions
 
-            # Test that an invalid document is handled for properly
-            response = c.post(reverse('document_permissions', args=(invalid_document_id,)), 
-                                data=json.dumps(self.perm_spec),
-                                content_type="application/json")
-            self.assertEquals(response.status_code, 404) 
+        # First test un-authenticated
+        response = c.post(reverse('document_permissions', args=(document_id,)), 
+                            data=json.dumps(self.perm_spec),
+                            content_type="application/json")
+        self.assertEquals(response.status_code, 401) 
 
-            # Test that GET returns permissions
-            response = c.get(reverse('document_permissions', args=(document_id,)))
-            assert('permissions' in response.content)
-            
-            # Test that a user is required to have documents.change_layer_permissions
+        # Next Test with a user that does NOT have the proper perms
+        logged_in = c.login(username='bobby', password='bob')
+        self.assertEquals(logged_in, True) 
+        response = c.post(reverse('document_permissions', args=(document_id,)), 
+                            data=json.dumps(self.perm_spec),
+                            content_type="application/json")
+        self.assertEquals(response.status_code, 401) 
 
-            # First test un-authenticated
-            response = c.post(reverse('document_permissions', args=(document_id,)), 
-                                data=json.dumps(self.perm_spec),
-                                content_type="application/json")
-            self.assertEquals(response.status_code, 401) 
+        # Login as a user with the proper permission and test the endpoint
+        logged_in = c.login(username='admin', password='admin')
+        self.assertEquals(logged_in, True)
+        response = c.post(reverse('document_permissions', args=(document_id,)), 
+                            data=json.dumps(self.perm_spec),
+                            content_type="application/json")
 
-            # Next Test with a user that does NOT have the proper perms
-            logged_in = c.login(username='bobby', password='bob')
-            self.assertEquals(logged_in, True) 
-            response = c.post(reverse('document_permissions', args=(document_id,)), 
-                                data=json.dumps(self.perm_spec),
-                                content_type="application/json")
-            self.assertEquals(response.status_code, 401) 
+        # Test that the method returns 200         
+        self.assertEquals(response.status_code, 200)
 
-            # Login as a user with the proper permission and test the endpoint
-            logged_in = c.login(username='admin', password='admin')
-            self.assertEquals(logged_in, True)
-            response = c.post(reverse('document_permissions', args=(document_id,)), 
-                                data=json.dumps(self.perm_spec),
-                                content_type="application/json")
-
-            # Test that the method returns 200         
-            self.assertEquals(response.status_code, 200)
-        else:
-            pass
