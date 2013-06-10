38e228e84cae1c1989e6f108f2289ffd22f7645a
capooti <pcorti@gmail.com>
2013-06-10 17:48:52 +0200

    Moved the SPATIAL_REPRESENTATION_TYPES base/enumerator to the SpatialRepresentationType model.

--- geonode/base/admin.py
+++ geonode/base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from geonode.base.models import TopicCategory, ContactRole, ResourceBase, Link
+from geonode.base.models import (TopicCategory, SpatialRepresentationType,
+    ContactRole, ResourceBase, Link)
 
 class ResourceBaseAdmin(admin.ModelAdmin):
     list_display = ('id','title', 'date', 'category')
@@ -10,6 +11,25 @@ class TopicCategoryAdmin(admin.ModelAdmin):
     model = TopicCategory
     list_display_links = ('name',)
     list_display = ('id', 'name', 'slug', 'description')
+    
+class SpatialRepresentationTypeAdmin(admin.ModelAdmin):
+    """
+    Model for spatial representation types in metadata.
+    It is initially preloaded with codes from TC211
+    See: http://www.isotc211.org/2005/resources/Codelist/gmxCodelists.xml
+    <CodeListDictionary gml:id="MD_SpatialRepresentationTypeCode">
+    """
+    model = SpatialRepresentationType
+    list_display_links = ('identifier',)
+    list_display = ('identifier', 'description', 'gn_description', 'is_choice')
+    
+    def has_add_permission(self, request):
+        # the records are from the standard TC 211 list, so no way to add
+        return False
+        
+    def has_delete_permission(self, request, obj=None):
+        # the records are from the standard TC 211 list, so no way to remove
+        return False
 
 class ContactRoleAdmin(admin.ModelAdmin):
     model = ContactRole
@@ -25,6 +45,7 @@ class LinkAdmin(admin.ModelAdmin):
     search_fields = ('name', 'resource__title',)
 
 admin.site.register(TopicCategory, TopicCategoryAdmin)
+admin.site.register(SpatialRepresentationType, SpatialRepresentationTypeAdmin)
 admin.site.register(ContactRole, ContactRoleAdmin)
 admin.site.register(ResourceBase, ResourceBaseAdmin)
 admin.site.register(Link, LinkAdmin)
