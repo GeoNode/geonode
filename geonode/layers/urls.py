26a2a03e2b010e41c465652d5e00f0a651d2ad0e
Jeffrey Johnson <jjohnson@opengeo.org>
2013-06-14 12:58:10 -0700

    Wire up layer_style_upload in layers/urls.py

--- geonode/layers/urls.py
+++ geonode/layers/urls.py
@@ -39,6 +39,7 @@ urlpatterns = patterns(
     url(r'^(?P<layername>[^/]*)/replace$', 'layer_replace',
         name="layer_replace"),
     url(r'^(?P<layername>[^/]*)/style$', 'layer_style', name="layer_style"),
+    url(r'^(?P<layername>[^/]*)/style/upload$','layer_style_upload',name='layer_style_upload'),
     url(r'^(?P<layername>[^/]*)/permissions$', 'layer_permissions',
         name='layer_permissions'),
     url(r'^(?P<layername>[^/]*)/edit-check?$', 'feature_edit_check',
