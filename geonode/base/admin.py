e4a1dcbe3f801e22d4aac0c1af456f05301b155f
Simone Dalmasso <simone.dalmasso@gmail.com>
2013-06-20 09:17:30 +0200

    move counts to base tag

--- geonode/base/admin.py
+++ geonode/base/admin.py
@@ -10,7 +10,6 @@ class TopicCategoryAdmin(admin.ModelAdmin):
     model = TopicCategory
     list_display_links = ('name',)
     list_display = ('id', 'name', 'slug', 'description')
-    exclude = ('layers_count', 'maps_count', 'documents_count')
 
 class ContactRoleAdmin(admin.ModelAdmin):
     model = ContactRole
