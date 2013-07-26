6f7918d63d48065601d77a83f771962244cc154c
Christian Spanring <cspanring@gmail.com>
2013-07-25 22:45:28 -0400

    adds DEBUG_STATIC condition for assets in base template
    default = False; for devs prefering debug-friendly non-minified
    staticfiles

--- geonode/settings.py
+++ geonode/settings.py
@@ -33,6 +33,9 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # present pretty error pages.
 DEBUG = TEMPLATE_DEBUG = True
 
+# Set to True to load non-minified versions of (static) client dependencies
+DEBUG_STATIC = False
+
 # This is needed for integration tests, they require
 # geonode to be listening for GeoServer auth requests.
 os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8000'
