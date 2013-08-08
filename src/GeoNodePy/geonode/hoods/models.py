4affbfb694e4665edc9e7424a088abb8ce6ca2bd
Matt Bertrand <mbertrand@cga.harvard.edu>
2013-08-08 07:41:55 -0400

    Add logging to hoods model Update gxp

--- src/GeoNodePy/geonode/hoods/models.py
+++ src/GeoNodePy/geonode/hoods/models.py
@@ -3,11 +3,17 @@ from django.db.models import signals
 from django.conf import settings
 from geonode.maps.models import Map
 from geonode.hoods.views import update_hood_map
+import logging
 
 # Create your models here.
 
+logger = logging.getLogger("geonode.hoods.models")
+
 def post_save_map(instance, sender, **kwargs):
     if instance.officialurl == 'boston' and settings.HOODS_TEMPLATE_ID is not None:
+        logger.info("Update hood map")
         update_hood_map()
+    else:
+        logger.info("Dont update hood map")
 
 signals.post_save.connect(post_save_map, sender=Map)
