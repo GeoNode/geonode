89d58f525e782ccf1f887f93ca60121fb616076c
Barbara <barbara@barbara-VirtualBox.(none)>
2013-08-27 13:33:57 +0200

    modify conf.py

--- newdocs/conf.py
+++ newdocs/conf.py
@@ -28,7 +28,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geonode.settings")
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.doctest','sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
