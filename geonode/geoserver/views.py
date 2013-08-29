6aedc0dfa743fb4b5fc70dd8c05287000152f1df
vdeparday <vivien.deparday@gmail.com>
2013-08-29 01:19:01 -0400

    Create /updatelayers hook, reformat output of gs_slurp to avoid code duplication

--- geonode/geoserver/views.py
+++ geonode/geoserver/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.utils import simplejson
 from helpers import get_stores
+from helpers import gs_slurp
+import traceback
+import datetime
 
 def stores(request, store_type=None):
     stores = get_stores(store_type)
     data = simplejson.dumps(stores)
     return HttpResponse(data)
+
+def updatelayers(request):
+    params = request.REQUEST
+    user = params.get('user',None)
+    workspace = params.get('workspace', None)
+    store = params.get('store',None)
+    layer = params.get('layer',None)
+
+    output = gs_slurp(ignore_errors=False, owner=user, workspace=workspace, store=store, filter=layer)
+    return HttpResponse(simplejson.dumps(output))
