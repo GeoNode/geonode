5d4ca8a6128732c8316cb7b08c1ba90c8bf21fe5
Ariel Nunez <ingenieroariel@gmail.com>
2013-11-07 11:38:45 -0500

    Added fixsitename command, refs #498

---
+++ geonode/base/management/commands/fixsitename.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.sites.models import Site
+from urlparse import urlsplit
+
+class Command(BaseCommand):
+    """Overrides the default Site object with information from
+       SITENAME and SITEURL
+    """
+    can_import_settings = True
+
+    def handle(self, *args, **options):
+        from django.conf import settings
+        name = getattr(settings, 'SITENAME', 'GeoNode')
+        url = getattr(settings, 'SITEURL')
+
+        parsed = urlsplit(url)
+
+        site = Site.objects.get_current()
+        site.name=name
+        site.domain=parsed.netloc
+        site.save()
