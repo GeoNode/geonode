804e56587b877174094de36338578ad19f77b841
Elliot Bradbury <ebradbury@gmail.com>
2013-11-12 12:10:07 -0500

    Added category_list to advanced search context for category filter

--- geonode/search/views.py
+++ geonode/search/views.py
@@ -35,6 +35,7 @@ from geonode.search.util import resolve_extension
 from geonode.search.normalizers import apply_normalizers
 from geonode.search.query import query_from_request
 from geonode.search.query import BadQuery
+from geonode.base.models import TopicCategory
 
 from datetime import datetime
 from time import time
@@ -77,8 +78,11 @@ def search_page(request, template='search/search.html', **kw):
         'facets': facets, 'query': json.dumps(query.get_query_response()), 'tags': tags}))
 
 def advanced_search(request, **kw):
+    ctx = {
+        'category_list': TopicCategory.objects.all(),
+    }
     
-    return render_to_response('search/advanced_search.html', RequestContext(request))
+    return render_to_response('search/advanced_search.html', RequestContext(request, ctx))
 
 def _get_search_context():
     cache_key = 'simple_search_context'
