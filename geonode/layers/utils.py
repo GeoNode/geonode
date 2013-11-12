062b7e17a6f2a00398c6c0950883563d103f4c61
Elliot Bradbury <ebradbury@gmail.com>
2013-11-12 12:05:54 -0500

    Assigns TopicCategory to Layer using extracted metadata.

--- geonode/layers/utils.py
+++ geonode/layers/utils.py
@@ -44,7 +44,7 @@ from geonode.people.models import Profile
 from geonode.geoserver.helpers import cascading_delete, get_sld_for, delete_from_postgis
 from geonode.layers.metadata import set_metadata
 from geonode.security.enumerations import AUTHENTICATED_USERS, ANONYMOUS_USERS
-from geonode.base.models import SpatialRepresentationType
+from geonode.base.models import SpatialRepresentationType, TopicCategory
 from geonode.utils import ogc_server_settings
 from geonode.upload.files import _clean_string
 # Geoserver functionality
@@ -501,7 +501,12 @@ def save(layer, base_file, user, overwrite=True, title=None,
         for (key, value) in vals.items():
             if key == 'spatial_representation_type':
                 value = SpatialRepresentationType(identifier=value)
-            setattr(saved_layer, key, value)
+            elif key == 'topic_category':
+                category, created = TopicCategory.objects.get_or_create(identifier=value.lower(), gn_description=value)
+
+                saved_layer.category = category
+            else:
+                setattr(saved_layer, key, value)
 
         saved_layer.save()
 
