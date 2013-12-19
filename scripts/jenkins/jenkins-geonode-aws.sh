12e5e595ab2c59bdc2930cc26a5c56c3b8bb85fd
Jeffrey Johnson <jjohnson@opengeo.org>
2013-12-19 13:58:18 -0800

    Reenable sourcing of ~/.bashrc now that the ec2 tools are configured

--- scripts/jenkins/jenkins-geonode-aws.sh
+++ scripts/jenkins/jenkins-geonode-aws.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 source ~/.bashrc
+PYENV_HOME=$HOME/.pyenv-master/
+
+# Delete previously built virtualenv
+if [ -d $PYENV_HOME ]; then
+    rm -rf $PYENV_HOME
+fi
+
+# Setup the virtualenv
+virtualenv --system-site-packages $PYENV_HOME
+source $PYENV_HOME/bin/activate
+
+# Install build & test tools
+pip install --quiet fabric
+pip install --quiet boto
+ 
 cd scripts/cloud/
 python ec2.py terminate
 rm .gnec2.cfg
