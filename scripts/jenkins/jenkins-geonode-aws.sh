ac623b9240131ee7ee69450166c83818f12bfe0d
Jeffrey Johnson <jjohnson@opengeo.org>
2013-12-20 09:34:02 -0800

    Use existing venv for aws task

--- scripts/jenkins/jenkins-geonode-aws.sh
+++ scripts/jenkins/jenkins-geonode-aws.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 source ~/.bashrc
-PYENV_HOME=$HOME/.pyenv-master/
-
-# Delete previously built virtualenv
-if [ -d $PYENV_HOME ]; then
-    rm -rf $PYENV_HOME
-fi
-
-# Setup the virtualenv
-virtualenv --system-site-packages $PYENV_HOME
+PYENV_HOME=$HOME/.pyenv-aws/
 source $PYENV_HOME/bin/activate
 
 # Install build & test tools
-pip install --quiet fabric
-pip install --quiet boto
+pip install fabric
+pip install boto
  
 cd scripts/cloud/
 python ec2.py terminate
