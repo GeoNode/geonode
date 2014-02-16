c2833323de62222acc55b34a3b8e712d63b4a4d5
Tom Kralidis <tomkralidis@gmail.com>
2014-02-16 10:41:41 -0500

    safeguard if value is None (#1260)

--- geonode/layers/metadata.py
+++ geonode/layers/metadata.py
@@ -138,16 +138,17 @@ def fgdc2dict(exml):
     if hasattr(mdata.idinfo, 'keywords'):
         if mdata.idinfo.keywords.theme:
             for theme in mdata.idinfo.keywords.theme:
-                lowered_themekt = theme['themekt'].lower()
+                if theme['themekt'] is not None:
+                    lowered_themekt = theme['themekt'].lower()
 
-                # Owslib doesn't support extracting the Topic Category
-                # from FGDC.  So we add support here.
-                # http://www.fgdc.gov/metadata/geospatial-metadata-standards
-                if all(ss in lowered_themekt for ss in ['iso', '19115', 'topic']) \
-                    and any(ss in lowered_themekt for ss in ['category', 'categories']):
-                    vals['topic_category'] = theme['themekey'][0]
+                    # Owslib doesn't support extracting the Topic Category
+                    # from FGDC.  So we add support here.
+                    # http://www.fgdc.gov/metadata/geospatial-metadata-standards
+                    if all(ss in lowered_themekt for ss in ['iso', '19115', 'topic']) \
+                        and any(ss in lowered_themekt for ss in ['category', 'categories']):
+                        vals['topic_category'] = theme['themekey'][0]
 
-                keywords.extend(theme['themekey'])
+                    keywords.extend(theme['themekey'])
 
     if hasattr(mdata.idinfo.timeperd, 'timeinfo'):
         if hasattr(mdata.idinfo.timeperd.timeinfo, 'rngdates'):
