5cca3f031f21bb32e2a4e1db9e242b86f3c7fc8d
npmiller <nicolas.pierre.miller@gmail.com>
2014-02-17 10:53:29 +0100

    Adds a first model for an analysis

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from geonode.base.models import ResourceBase
 
-# Create your models here.
+class Analysis(ResourceBase):
+  last_modified = models.DateTimeField(auto_now_add=True)  
+  # The last time the map was modified.
+  
+  popular_count = models.IntegerField(default=0)
+  
+  share_count = models.IntegerField(default=0)
+  
+  def __unicode__(self):
+    return self.title
+
