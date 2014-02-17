8684ad19e990c706c5af0f79d68683772141c69f
npmiller <nicolas.pierre.miller@gmail.com>
2014-02-17 17:54:40 +0100

    Adds a basic analysis view

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def analytic_list(request, template='analytics/analysis_list.html'):
     from geonode.search.views import search_page
@@ -11,11 +12,4 @@ def analytic_list(request, template='analytics/analysis_list.html'):
 #### NEW ANALYSES ####
 
 def new_analysis(request, template='analytics/analysis_view.html'):
-    config = new_analysis_config(request)
-    if isinstance(config, HttpResponse):
-        return config
-    else:
-        return render_to_response(template, RequestContext(request, {
-            'config': config,
-        }))
-
+    return render_to_response(template, RequestContext(request, { }))
