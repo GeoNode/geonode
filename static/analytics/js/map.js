ddeb3c6160b72d801d4e2bc96c0d0798eef7d892
jlebloas <jonathanlebloas@gmail.com>
2014-02-20 16:20:52 +0100

    Spaces and tab convention

--- static/analytics/js/map.js
+++ static/analytics/js/map.js
@@ -1,41 +1,37 @@
-
 var container = d3.select("body").append("svg")
-	.attr("width", 2000)
-	.attr("height", 2000)
-	  
-
+    .attr("width", 2000)
+    .attr("height", 2000)
 
 d3.json("continent_Europe_subunits", function (data) {
-	  
-	var zoom = d3.behavior.zoom()
-		  .translate([0, 0])
-		  .scale(1)
-		  .scaleExtent([1, 8])
-		  .on("zoom", zoomed);	
-		  
-	var drag = d3.behavior.drag()
-		.origin(function(d) {return d;})
-		.on("dragstart", dragstarted)
-		.on("drag", dragged)
-		.on("dragend", dragended);
-		  
-		  var group = container.selectAll("g")
-			  .data(data.features)
-			.enter()
-			  .append("g")
-			  .call(zoom)
-			  .call(drag)
-			  
-		  var projection = d3.geo.mercator().center([40, 60]).scale(400)
-		  var path = d3.geo.path().projection(projection);
-		  
-		  var areas = group.append("path")
-			  .attr("d", path)
-			  .attr("fill", "steelblue")
-			  .on("mouseover", color)
-			  .on("mouseout", decolor);
+  var zoom = d3.behavior.zoom()
+      .translate([0, 0])
+      .scale(1)
+      .scaleExtent([1, 8])
+      .on("zoom", zoomed);	
+
+  var drag = d3.behavior.drag()
+      .origin(function(d) {return d;})
+      .on("dragstart", dragstarted)
+      .on("drag", dragged)
+      .on("dragend", dragended);
+
+  var group = container.selectAll("g")
+      .data(data.features)
+    .enter()
+      .append("g")
+      .call(zoom)
+      .call(drag);
+
+  var projection = d3.geo.mercator().center([40, 60]).scale(400);
+  var path = d3.geo.path().projection(projection);
+
+  var areas = group.append("path")
+      .attr("d", path)
+      .attr("fill", "steelblue")
+      .on("mouseover", color)
+      .on("mouseout", decolor);
 });
-	
+      
 function zoomed() {
   container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 }
