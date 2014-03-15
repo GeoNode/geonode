9c8a11c33df63f08f2d6b1235b7e8e5915adc408
npmiller <nicolas.pierre.miller@gmail.com>
2014-03-15 12:03:28 +0100

    Save analysis in the database

--- geonode/analytics/views.py
+++ geonode/analytics/views.py
@@ -5,6 +5,7 @@ from geonode.utils import resolve_object
 from geonode.security.views import _perms_info
 from geonode.documents.models import get_related_documents
 from geonode.analytics.models import Analysis
+from django.http import HttpResponse
 
 ANALYSIS_LEV_NAMES = {
     Analysis.LEVEL_NONE  : _('No Permissions'),
@@ -53,5 +54,20 @@ def _resolve_analysis(request, id, permission='analysis.change_analysis',
                           permission_msg=msg, **kwargs)
 
 def new_analysis_json(request):
-	print 'SAVE ANALYSIS !!!'
-	return render(request, template, { })
+	if request.method == 'POST':
+		if not request.user.is_authenticated():
+			return HttpResponse(
+				'You must be logged in to save new maps',
+				mimetype="text/plain",
+				status=401
+			)
+		analysis_obj = Analysis(owner=request.user, title=request.POST.get('title'), abstract=request.POST.get('abstract'))
+		analysis_obj.save()
+		return HttpResponse(
+			json.dumps({'id':analysis_obj.id }),
+			status=200,
+			mimetype='application/json'
+		)
+	else:
+		return HttpResponse(status=405)
+
