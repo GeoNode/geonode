b8ef19d1da4a1ffc39f4e8b086645380b0062dae
Tyler Garner <garnertb@gmail.com>
2014-03-18 15:56:05 -0400

    Clean up the groups tests.
    * Explicitly import the Group and GroupInvitation classes.
    * Add a test case to make sure the token URL route works when a user is invited to a group.

--- geonode/contrib/groups/tests.py
+++ geonode/contrib/groups/tests.py
@@ -1,10 +1,8 @@
-from django.conf import settings
+from django.contrib.auth.models import User, AnonymousUser
 from django.test import TestCase
 from django.test.client import Client
-from django.contrib.auth.models import User, AnonymousUser
+from geonode.contrib.groups.models import Group, GroupInvitation
 
-import geonode.contrib.groups.models
-import geonode.contrib.groups.views
 
 class SmokeTest(TestCase):
     "Basic checks to make sure pages load, etc."
@@ -24,8 +22,6 @@ class SmokeTest(TestCase):
 
         response = c.get("/groups/group/bar/members/")
         self.assertEqual(200, response.status_code)
-
-        # TODO: test invite: url("/groups/group/bar/invite/(?P<token>[\w]{40})/")
 
         # 302 for auth failure since we redirect to login page
         response = c.get("/groups/create/")
@@ -47,7 +43,6 @@ class SmokeTest(TestCase):
         response = c.get("/groups/group/bar/invite/")
         self.assertEqual(405, response.status_code)
 
-
     def test_protected_pages_render(self):
         "Verify pages that require login load without internal error"
 
@@ -62,8 +57,6 @@ class SmokeTest(TestCase):
 
         response = c.get("/groups/group/bar/members/")
         self.assertEqual(200, response.status_code)
-
-        # TODO: test invite: url("/groups/group/bar/invite/(?P<token>[\w]{40})/")
 
         response = c.get("/groups/create/")
         self.assertEqual(200, response.status_code)
@@ -95,7 +88,7 @@ class MembershipTest(TestCase):
 
         anon = AnonymousUser()
         normal = User.objects.get(username="norman")
-        group = geonode.contrib.groups.models.Group.objects.get(slug="bar")
+        group = Group.objects.get(slug="bar")
 
         self.assert_(not group.user_is_member(anon))
         self.assert_(not group.user_is_member(normal))
@@ -105,10 +98,11 @@ class MembershipTest(TestCase):
 
         anon = AnonymousUser()
         normal = User.objects.get(username="norman")
-        group = geonode.contrib.groups.models.Group.objects.get(slug="bar")
+        group = Group.objects.get(slug="bar")
         group.join(normal)
         self.assert_(group.user_is_member(normal))
         self.assertRaises(ValueError, lambda: group.join(anon))
+
 
 class InvitationTest(TestCase):
     "Tests invitation logic in geonode.contrib.groups models"
@@ -121,16 +115,20 @@ class InvitationTest(TestCase):
         anon = AnonymousUser()
         normal = User.objects.get(username="norman")
         admin = User.objects.get(username="admin")
-        group = geonode.contrib.groups.models.Group.objects.get(slug="bar")
+        group = Group.objects.get(slug="bar")
         group.invite(normal, admin, role="member", send=False)
 
         self.assert_(
-            geonode.contrib.groups.models.GroupInvitation.objects.filter(
-                user=normal,
-                from_user=admin,
-                group=group
-            ).exists()
+            GroupInvitation.objects.filter(user=normal, from_user=admin, group=group).exists()
         )
+
+        invite = GroupInvitation.objects.get(user=normal, from_user=admin, group=group)
+
+        # Test that the user can access the token url.
+        c = Client()
+        c.login(username="norman", password="norman")
+        response = c.get("/groups/group/{group}/invite/{token}/".format(group=group, token=invite.token))
+        self.assertEqual(200, response.status_code)
 
     def test_accept_invitation(self):
         "Test accepting an invitation"
@@ -138,14 +136,10 @@ class InvitationTest(TestCase):
         anon = AnonymousUser()
         normal = User.objects.get(username="norman")
         admin = User.objects.get(username="admin")
-        group = geonode.contrib.groups.models.Group.objects.get(slug="bar")
+        group = Group.objects.get(slug="bar")
         group.invite(normal, admin, role="member", send=False)
 
-        invitation = geonode.contrib.groups.models.GroupInvitation.objects.get(
-            user = normal,
-            from_user = admin,
-            group = group
-        )
+        invitation = GroupInvitation.objects.get(user=normal, from_user=admin, group=group)
 
         self.assertRaises(ValueError, lambda: invitation.accept(anon))
         self.assertRaises(ValueError, lambda: invitation.accept(admin))
@@ -160,14 +154,10 @@ class InvitationTest(TestCase):
         anon = AnonymousUser()
         normal = User.objects.get(username="norman")
         admin = User.objects.get(username="admin")
-        group = geonode.contrib.groups.models.Group.objects.get(slug="bar")
+        group = Group.objects.get(slug="bar")
         group.invite(normal, admin, role="member", send=False)
 
-        invitation = geonode.contrib.groups.models.GroupInvitation.objects.get(
-            user = normal,
-            from_user = admin,
-            group = group
-        )
+        invitation = GroupInvitation.objects.get(user=normal, from_user=admin, group=group)
 
         self.assertRaises(ValueError, lambda: invitation.decline(anon))
         self.assertRaises(ValueError, lambda: invitation.decline(admin))
