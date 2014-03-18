4d46281374cfdc89197383b4454039213d75a0e7
tszym <develop@tszymanski.com>
2014-03-18 09:14:40 +0100

    Fix coding style in analytics

--- geonode/analytics/tests.py
+++ geonode/analytics/tests.py
@@ -2,14 +2,14 @@ from django.test import TestCase
 from django.test.client import Client
 
 class AnalyticsTest(TestCase):
-    
+
     def setUp(self):
         self.client = Client()
 
     def test_analytics_tab(self):
-        r = self.client.get('/analytics/')
-        self.assertEqual(r.status_code, 200)
+        response = self.client.get('/analytics/')
+        self.assertEqual(response.status_code, 200)
 
     def test_new_analysis(self):
-        r = self.client.get('/analytics/new/')
-        self.assertEqual(r.status_code, 200)
+        response = self.client.get('/analytics/new/')
+        self.assertEqual(response.status_code, 200)
--- geonode/analytics/views.py
+++ geonode/analytics/views.py
@@ -34,11 +34,10 @@ def new_analysis(request, template='analytics/analysis_view.html'):
     return render(request, template, { })
 
 def analysis_detail(request, analysisid, template='analytics/analysis_detail.html'):
-    
     analysis_obj = _resolve_analysis(request, analysisid, 'analysis.view_analysis', _PERMISSION_MSG_VIEW)
     analysis_obj.popular_count += 1
     analysis_obj.save()
-    
+
     return render(request, template, {
         'analysis' : analysis_obj,
         'documents' : get_related_documents(analysis_obj),
@@ -50,24 +49,24 @@ def _resolve_analysis(request, id, permission='analysis.change_analysis',
     '''
     Resolve the Analysis by the provided typename and check the optional permission.
     '''
-    return resolve_object(request, Analysis, {'pk':id}, permission = permission,
+    return resolve_object(request, Analysis, {'pk':id}, permission=permission,
                           permission_msg=msg, **kwargs)
 
 def new_analysis_json(request):
-	if request.method == 'POST':
-		if not request.user.is_authenticated():
-			return HttpResponse(
-				'You must be logged in to save new maps',
-				mimetype="text/plain",
-				status=401
-			)
-		analysis_obj = Analysis(owner=request.user, title=request.POST.get('title'), abstract=request.POST.get('abstract'))
-		analysis_obj.save()
-		return HttpResponse(
-			json.dumps({'id':analysis_obj.id }),
-			status=200,
-			mimetype='application/json'
-		)
-	else:
-		return HttpResponse(status=405)
+    if request.method == 'POST':
+        if not request.user.is_authenticated():
+            return HttpResponse(
+                'You must be logged in to save new maps',
+                mimetype="text/plain",
+                status=401
+            )
 
+        analysis_obj = Analysis(owner=request.user, title=request.POST.get('title'), abstract=request.POST.get('abstract'))
+        analysis_obj.save()
+        return HttpResponse(
+            json.dumps({'id':analysis_obj.id}),
+            status=200,
+            mimetype='application/json'
+        )
+    else:
+        return HttpResponse(status=405)
