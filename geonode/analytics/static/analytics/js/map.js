ffe7a8d112b527cd38f39d6c71ccf0e64f318628
jlebloas <jonathanlebloas@gmail.com>
2014-03-19 14:40:57 +0100

    Add the static files

--- geonode/analytics/static/analytics/js/map.js
+++ geonode/analytics/static/analytics/js/map.js
@@ -1,59 +1,184 @@
-var container = d3.select('#map-pane').append('svg')
-    .attr('height', 660);
-    console.log(container);
+var width = $('#map-pane').width(),
+//height = $('#map-pane').height();
+height = 700;
 
-d3.json("http://127.0.0.1:8000/static/analytics/data/continent_Europe_subunits.json", function (data) {
-  var zoom = d3.behavior.zoom()
-      .translate([0, 0])
-      .scale(1)
-      .scaleExtent([1, 8])
-      .on("zoom", zoomed);
+d3.json("http://127.0.0.1:8000/static/analytics/data/europe.topo.json", function (geographicalData) {
+  d3.json("http://127.0.0.1:8000/static/analytics/data/donnees.json", function (biData) {
+    //Will be modified in the future
+    var active;    
+    
+    // Begin of the class
 
-  var drag = d3.behavior.drag()
-      .origin(function(d) {return d;})
-      .on("dragstart", dragstarted)
-      .on("drag", dragged)
-      .on("dragend", dragended);
+    // class attribute
+    var geoData = null;
+    var areas = null;
 
-  var group = container.selectAll("g")
-      .data(data.features)
-    .enter()
-      .append("g")
-      .call(zoom)
-      .call(drag);
 
-  var projection = d3.geo.mercator().center([40, 60]).scale(400);
-  var path = d3.geo.path().projection(projection);
+    var svg = d3.select("#map-pane").append("svg")
+	.attr("width", width)
+	.attr("height", height);
 
-  var areas = group.append("path")
-      .attr("d", path)
-      .attr("fill", "steelblue")
-      .on("mouseover", color)
-      .on("mouseout", decolor);
+    var g = svg.append("g");
+
+    g.append("rect")
+	.attr("class", "background")
+	.attr("width", width)
+	.attr("height", height)
+	.on("click", reset);
+
+    var europe = g.append("g")
+	.attr("id", "Europe");
+
+    // Create a unit projection.
+    var projection = d3.geo.mercator()
+	.scale(1)
+	.translate([0, 0]);
+
+    // Create a path generator.
+    var path = d3.geo.path()
+	.projection(projection);
+
+    function setGeoData(geoData,levelID){
+      var b = path.bounds(topojson.feature(geoData, geoData.objects.europe));
+      var s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
+      var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+      var center = d3.geo.centroid(topojson.feature(geoData, geoData.objects.europe));
+
+      var t = [width/2, height/2];
+
+      // Update the projection to center wuth the computed scale & center & translate.
+      projection
+	  .scale(s)
+	  .center(center)
+	  .translate(t);
+
+      areas = europe.selectAll("path")
+	  .data(topojson.feature(geoData, geoData.objects.europe).features)
+	.enter().append("path")
+	  .attr("d", path)
+	  .on("click", clicked);
+
+      function clicked(d) {
+	console.log(d.id);
+	if (active === d) return reset();
+	g.selectAll(".active").classed("active", false);
+	d3.select(this).classed("active", active = d);
+
+	var b = path.bounds(d);
+	var s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
+	var t = [-(b[1][0] + b[0][0]) / 2 , -(b[1][1] + b[0][1]) / 2];
+
+	g.transition().duration(750).attr("transform",
+	    "translate(" + projection.translate() + ")"
+	    + "scale(" + s + ")"
+	    + "translate(" + t + ")");
+      }
+    }
+
+    function reset() {
+      g.selectAll(".active").classed("active", active = false);
+
+      var b = path.bounds(geoData);
+      var s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
+      var t = [-(b[1][0] + b[0][0]) / 2 , -(b[1][1] + b[0][1]) / 2];
+
+      g.transition().duration(750).attr("transform",
+	  "translate(" + projection.translate() + ")"
+	  + "scale(" + s + ")"
+	  + "translate(" + t + ")");
+    }
+
+    // To do : before : one assert geoData was make
+    function setData(data, levelID){
+      var minMax = getMinMax(data);
+      var measures = data.map(function (d){ return d.measure;});
+
+      // Function that will translate value to colorClass
+      var quantize = d3.scale.quantize()
+          .domain(minMax)
+          .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+
+      areas.attr("class", function(d){ return idToColorClass(d.id); });
+
+      function idToColorClass(id){
+	var names = data.map(function(d){return d.name});
+
+	return quantize(measures[indexOfInArrayOfArray(names,id)]);
+      }
+    }
+
+    // tanks to indexOf that doesn't work on array of array we need this
+    function indexOfInArrayOfArray(array,elmt){
+      var i=0;
+      var more = true;
+      while((i<array.length) && more){
+	if(equalsArrays(array[i],elmt)){
+	  more = false;
+	}
+	i++;
+      }
+      if(more==false){
+        return i-1;
+      }else{
+	return undefined;
+      }
+    }
+
+    function equalsArrays(array1, array2) {
+      if (array1.length == array2.length){
+	var result=true;
+	array1.forEach(function(d,i){
+	  result = result && (d == array2[i])
+	});
+	return result;
+      }else{
+	return false;
+      }
+    }
+
+
+    // Function that gives min and max of our data
+    function getMinMax(data){
+      var mesures=data.map(function(d) {return d.measure;});
+
+      var result=[];
+      result[0]=d3.min(mesures);
+      result[1]=d3.max(mesures);
+      return result;
+    }
+
+//     var t1 = ["Europe", "Pays1"];
+//     var t2 = ["Europe", "Pays1"];
+//     var t3 = ["Europe", "Pays2"];
+//     var t4 = ["Europe", "Pays2", "test"];
+//     var t5 = ["Europe", "Pays2", "test"];
+//     console.log("test must be true : "+equalsArrays(t1,t2));
+//     console.log("test must be false : "+equalsArrays(t1,t3));
+//     console.log("test must be false : "+equalsArrays(t1,t4));
+//     console.log("test must be true : "+equalsArrays(t5,t4));
+    
+//     var array1 = [
+// 	      ["Europe", "Pays0"],
+// 	      ["Europe", "Pays1"],
+// 	      ["Europe", "Pays2"],
+// 	      ["Europe", "Pays3"],
+// 	      ["Europe", "Pays4"],
+// 	      ["Europe", "Pays5"]];
+//     
+//     var t1 = ["Europe", "Pays32"];
+//     var t2 = ["Europe", "Pays1"];
+//     var t3 = ["Europe", "Pays5"];
+//     var t4 = ["Asia", "Pays1"];
+//     
+//     console.log(biData);
+//     console.log("test must be true : "+(indexOfInArrayOfArray(array1,t2)==1));
+//     console.log("test must be true : "+(indexOfInArrayOfArray(array1,t1)==undefined));
+//     console.log("test must be true : "+(indexOfInArrayOfArray(array1,t3)==5));
+//     console.log("test must be true : "+(indexOfInArrayOfArray(array1,t4)==undefined));
+
+    setGeoData(geographicalData);
+    setData(biData);
+
+  });
 });
-      
-function zoomed() {
-  container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-}
-	
-function dragstarted(d) {
-  d3.event.sourceEvent.stopPropagation();
-  d3.select(this).classed("dragging", true);
-}
-
-function dragged(d) {
-}
-
-function dragended(d) {
-  d3.select(this).classed("dragging", false);
-}
-
-function color(d) {
-  d3.select(this).classed("rouge",true);
-  d3.select(this).classed("area",false);
-}
-
-function decolor(d) {
-  d3.select(this).classed("area",true);
-  d3.select(this).classed("rouge",false);
-} 
