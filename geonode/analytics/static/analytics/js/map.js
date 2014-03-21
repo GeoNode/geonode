7ca7134fdab89ae7f69bee5d6f8ce3871b0a9776
jlebloas <jonathanlebloas@gmail.com>
2014-03-21 12:13:00 +0100

    Refactoring and update map with the new specs of the json result

--- geonode/analytics/static/analytics/js/map.js
+++ geonode/analytics/static/analytics/js/map.js
@@ -3,10 +3,10 @@ var width = $('#map-pane').width(),
 height = 700;
 
 d3.json("http://127.0.0.1:8000/static/analytics/data/europe.topo.json", function (geographicalData) {
-  d3.json("http://127.0.0.1:8000/static/analytics/data/donnees.json", function (biData) {
+  d3.json("http://127.0.0.1:8000/static/analytics/data/data.json", function (biData) {
     //Will be modified in the future
-    var active;    
-    
+    var active;
+
     // Begin of the class
 
     // class attribute
@@ -101,18 +101,18 @@ d3.json("http://127.0.0.1:8000/static/analytics/data/europe.topo.json", function
       areas.attr("class", function(d){ return idToColorClass(d.id); });
 
       function idToColorClass(id){
-	var names = data.map(function(d){return d.name});
+	var names = data.map(function(d){return d.id});
 
 	return quantize(measures[indexOfInArrayOfArray(names,id)]);
       }
     }
 
     // tanks to indexOf that doesn't work on array of array we need this
-    function indexOfInArrayOfArray(array,elmt){
+    function indexOfInArrayOfArray(array,idElmt){
       var i=0;
       var more = true;
       while((i<array.length) && more){
-	if(equalsArrays(array[i],elmt)){
+	if(array[i]==idElmt){
 	  more = false;
 	}
 	i++;
@@ -124,19 +124,6 @@ d3.json("http://127.0.0.1:8000/static/analytics/data/europe.topo.json", function
       }
     }
 
-    function equalsArrays(array1, array2) {
-      if (array1.length == array2.length){
-	var result=true;
-	array1.forEach(function(d,i){
-	  result = result && (d == array2[i])
-	});
-	return result;
-      }else{
-	return false;
-      }
-    }
-
-
     // Function that gives min and max of our data
     function getMinMax(data){
       var mesures=data.map(function(d) {return d.measure;});
@@ -146,35 +133,6 @@ d3.json("http://127.0.0.1:8000/static/analytics/data/europe.topo.json", function
       result[1]=d3.max(mesures);
       return result;
     }
-
-//     var t1 = ["Europe", "Pays1"];
-//     var t2 = ["Europe", "Pays1"];
-//     var t3 = ["Europe", "Pays2"];
-//     var t4 = ["Europe", "Pays2", "test"];
-//     var t5 = ["Europe", "Pays2", "test"];
-//     console.log("test must be true : "+equalsArrays(t1,t2));
-//     console.log("test must be false : "+equalsArrays(t1,t3));
-//     console.log("test must be false : "+equalsArrays(t1,t4));
-//     console.log("test must be true : "+equalsArrays(t5,t4));
-    
-//     var array1 = [
-// 	      ["Europe", "Pays0"],
-// 	      ["Europe", "Pays1"],
-// 	      ["Europe", "Pays2"],
-// 	      ["Europe", "Pays3"],
-// 	      ["Europe", "Pays4"],
-// 	      ["Europe", "Pays5"]];
-//     
-//     var t1 = ["Europe", "Pays32"];
-//     var t2 = ["Europe", "Pays1"];
-//     var t3 = ["Europe", "Pays5"];
-//     var t4 = ["Asia", "Pays1"];
-//     
-//     console.log(biData);
-//     console.log("test must be true : "+(indexOfInArrayOfArray(array1,t2)==1));
-//     console.log("test must be true : "+(indexOfInArrayOfArray(array1,t1)==undefined));
-//     console.log("test must be true : "+(indexOfInArrayOfArray(array1,t3)==5));
-//     console.log("test must be true : "+(indexOfInArrayOfArray(array1,t4)==undefined));
 
     setGeoData(geographicalData);
     setData(biData);
