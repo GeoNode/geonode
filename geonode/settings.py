8b36759f050129ee163dd44e1fde0e190cd5dff2
npmiller <nicolas.pierre.miller@gmail.com>
2014-03-22 08:00:33 +0100

    Change some settings back to default

--- geonode/settings.py
+++ geonode/settings.py
@@ -46,7 +46,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(PROJECT_ROOT, 'development.db'),
-        'TEST_NAME' : os.path.join(PROJECT_ROOT, 'development-test.db'),
     },
 
     # vector datastore for uploads
