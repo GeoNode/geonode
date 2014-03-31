ed7d4cf99be8af9002d5bb2c9e4a30c5fb3dac64
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-03-31 15:59:38 +0200

    make authorization apply the permissions based on the object type

--- geonode/api/authorization.py
+++ geonode/api/authorization.py
@@ -1,37 +1,52 @@
 from tastypie.authorization import DjangoAuthorization
 from tastypie.exceptions import Unauthorized
 
+perms = {
+    'Layer': {
+        'view': 'layers.view_layer',
+        'add': 'layers.add_layer',
+        'change': 'layers.change_layer',
+        'delete': 'layers.delete_layer',
+    },
+    'Map': {
+        'view': 'maps.view_map',
+        'add': 'maps.add_map',
+        'change': 'maps.change_map',
+        'delete': 'maps.delete_map',
+    },
+    'Document': {
+        'view': 'documents.view_document',
+        'add': 'documents.add_document',
+        'change': 'documents.change_document',
+        'delete': 'documents.delete_document',
+    }
+}
+
 
 class GeoNodeAuthorization(DjangoAuthorization):
     """Object level API authorization based on GeoNode granular permission system"""
 
-    def __init__(self, *args, **kwargs):
-        self.view_perm = kwargs.pop('view_perm')
-        self.create_perm = kwargs.pop('create_perm')
-        self.update_perm = kwargs.pop('update_perm')
-        self.delete_perm = kwargs.pop('delete_perm')
-        super(GeoNodeAuthorization, self).__init__(**kwargs)
-
     def read_list(self, object_list, bundle):
-        return [i for i in object_list if bundle.request.user.has_perm(self.view_perm, i)]
+        return [i for i in object_list if bundle.request.user.has_perm(
+            perms[bundle.obj.class_name]['view'], i)]
 
     def read_detail(self, object_list, bundle):
-        return bundle.request.user.has_perm(self.view_perm, bundle.obj)
+        return bundle.request.user.has_perm(perms[bundle.obj.class_name]['view'], bundle.obj)
 
     def create_list(self, object_list, bundle):
         raise Unauthorized()
 
     def create_detail(self, object_list, bundle):
-        return bundle.request.user.has_perm(self.create_perm, bundle.obj)
+        return bundle.request.user.has_perm(perms[bundle.obj.class_name]['add'], bundle.obj)
 
     def update_list(self, object_list, bundle):
         raise Unauthorized()
 
     def update_detail(self, object_list, bundle):
-        return bundle.request.user.has_perm(self.update_perm, bundle.obj)
+        return bundle.request.user.has_perm(perms[bundle.obj.class_name]['change'], bundle.obj)
 
     def delete_list(self, object_list, bundle):
         raise Unauthorized()
 
     def delete_detail(self, object_list, bundle):
-        return bundle.request.user.has_perm(self.delete_perm, bundle.obj)
+        return bundle.request.user.has_perm(perms[bundle.obj.class_name]['delete'], bundle.obj)
