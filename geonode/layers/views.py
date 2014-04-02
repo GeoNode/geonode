8d8c920e94e936b8728bdeb4a30133f26bf2136a
capooti <pcorti@gmail.com>
2014-04-02 16:58:55 +0200

    Removed service relation in layer, now we have a m-m relation via ServiceLayer.

--- geonode/layers/views.py
+++ geonode/layers/views.py
@@ -176,10 +176,12 @@ def layer_detail(request, layername, template='layers/layer_detail.html'):
     layer = _resolve_layer(request, layername, 'layers.view_layer', _PERMISSION_MSG_VIEW)
 
     config = layer.attribute_config()
-    if layer.service == None:
+    # TODO we need to remove this dependency some way
+    if layer.service_set == None:
         maplayer = GXPLayer(name = layer.typename, ows_url = layer.ows_url, layer_params=json.dumps( config))
     else:
-        source_params = {"ptype":layer.service.ptype, "remote": True, "url": layer.service.base_url, "name": layer.service.name}
+        service = layer.service_set.all()[0]
+        source_params = {"ptype":service.ptype, "remote": True, "url": service.base_url, "name": service.name}
         maplayer = GXPLayer(name = layer.typename, ows_url = layer.ows_url, layer_params=json.dumps( config), source_params=json.dumps(source_params))
 
     layer.srid_url = "http://www.spatialreference.org/ref/" + layer.srid.replace(':','/').lower() + "/"
