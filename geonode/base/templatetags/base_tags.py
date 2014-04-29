7c79345cd4120c25946c2f3b4dc908117dd21393
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-04-29 10:03:52 +0200

    refactor and add categories counts

--- geonode/base/templatetags/base_tags.py
+++ geonode/base/templatetags/base_tags.py
@@ -5,8 +5,6 @@ from django.db.models import Count
 from agon_ratings.models import Rating
 from django.contrib.contenttypes.models import ContentType
 
-from geonode.base.models import TopicCategory
-
 register = template.Library()
 
 counts = ('map','layer','document')
@@ -18,11 +16,4 @@ def num_ratings(obj):
                 object_id = obj.pk,
                 content_type = ct
     ))
-
-@register.assignment_tag
-def categories():
-    topics = TopicCategory.objects.all()
-    for c in counts:
-        topics = topics.annotate(**{ '%s_count' % c : Count('resourcebase__%s__category' % c)})
-    return topics
     
--- geonode/static/geonode/js/search/search.js
+++ geonode/static/geonode/js/search/search.js
@@ -4,9 +4,10 @@
 
   var module = angular.module('main_search', []);
 
-  module.controller('MainSearch', function($scope, $http, Configs){
+  module.controller('MainSearch', function($scope, $rootScope, $http, Configs){
     $http.get(Configs.url).success(function(data){
       $scope.results = data.objects;
+      $rootScope.results_meta = data.meta;
     });
   });
 })();
