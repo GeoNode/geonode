db03909243ed77cc4a1729defc014d6d62ee39e0
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-05-06 11:20:54 +0200

    just facet the needed type

--- geonode/base/templatetags/base_tags.py
+++ geonode/base/templatetags/base_tags.py
@@ -23,26 +23,30 @@ def num_ratings(obj):
 @register.assignment_tag(takes_context=True)
 def facets(context):
     request = context['request']
+    path = request.path
     facets = {
         'raster': 0,
         'vector': 0,
         'map': 0,
         'document': 0,
     }
-    for layer in Layer.objects.all():
-        if request.user.has_perm('layers.view_layer', layer):
-            if layer.storeType == 'coverageStore':
-                facets['raster'] += 1
-            else:
-                facets['vector'] +=1
+    
+    if 'layers' in path or 'search' in path:
+        for layer in Layer.objects.all():
+            if request.user.has_perm('layers.view_layer', layer):
+                if layer.storeType == 'coverageStore':
+                    facets['raster'] += 1
+                else:
+                    facets['vector'] +=1
 
-    for the_map in Map.objects.all():
-        if request.user.has_perm('maps.view_map', the_map):
-            facets['map'] +=1
+    if 'search' in path:                
+        for the_map in Map.objects.all():
+            if request.user.has_perm('maps.view_map', the_map):
+                facets['map'] +=1
 
-    for doc in Document.objects.all():
-        if request.user.has_perm('document.view_document', doc):
-            facets['document'] += 1
+        for doc in Document.objects.all():
+            if request.user.has_perm('document.view_document', doc):
+                facets['document'] += 1        
 
     return facets
 
