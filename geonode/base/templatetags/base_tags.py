4fdf1bad4bb2bfe0e31197f9a8c07141a1ecaec3
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-05-06 11:10:02 +0200

    add server side resources faceting with security

--- geonode/base/templatetags/base_tags.py
+++ geonode/base/templatetags/base_tags.py
@@ -5,9 +5,11 @@ from django.db.models import Count
 from agon_ratings.models import Rating
 from django.contrib.contenttypes.models import ContentType
 
-register = template.Library()
+from geonode.layers.models import Layer
+from geonode.maps.models import Map
+from geonode.documents.models import Document
 
-counts = ('map','layer','document')
+register = template.Library()
 
 @register.assignment_tag
 def num_ratings(obj):
@@ -17,3 +19,32 @@ def num_ratings(obj):
                 content_type = ct
     ))
     
+
+@register.assignment_tag(takes_context=True)
+def facets(context):
+    request = context['request']
+    facets = {
+        'raster': 0,
+        'vector': 0,
+        'map': 0,
+        'document': 0,
+    }
+    for layer in Layer.objects.all():
+        if request.user.has_perm('layers.view_layer', layer):
+            if layer.storeType == 'coverageStore':
+                facets['raster'] += 1
+            else:
+                facets['vector'] +=1
+
+    for the_map in Map.objects.all():
+        if request.user.has_perm('maps.view_map', the_map):
+            facets['map'] +=1
+
+    for doc in Document.objects.all():
+        if request.user.has_perm('document.view_document', doc):
+            facets['document'] += 1
+
+    return facets
+
+
+
