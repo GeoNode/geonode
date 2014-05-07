67c8683be243f598a66e6935487cd7d4f6eb3543
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-05-07 08:53:53 +0200

    add sorting

--- geonode/static/geonode/js/search/search.js
+++ geonode/static/geonode/js/search/search.js
@@ -9,6 +9,10 @@
       angular.element("a").prop("target", "_self");
     });
 
+
+  /*
+  * Load categories and keywords
+  */
   module.run(function($http, $rootScope){
     var params = typeof FILTER_TYPE == 'undefined' ? {} : {'type': FILTER_TYPE};
     $http.get(CATEGORIES_ENDPOINT, {params: params}).success(function(data){
@@ -20,6 +24,11 @@
     });
   });
 
+  /*
+  * Main search controller 
+  * Load data from api and defines the multiple and single choice handlers
+  * Syncs the browser url with the selections
+  */
   module.controller('MainController', function($scope, $location, $http, Configs){
     $scope.query = $location.search();
     
@@ -85,6 +94,33 @@
       }
       query_api($scope.query);
     }
+
+    $scope.single_choice_listener = function($event){
+      var element = $($event.target);
+      var query_entry = [];
+      var data_filter = element.attr('data-filter');
+      var value = element.attr('data-value');
+
+      // If the query object has the record then grab it 
+      if ($scope.query.hasOwnProperty(data_filter)){
+        query_entry = $scope.query[data_filter];
+      }
+
+      if(!element.hasClass('selected')){
+        // Add the entry in the correct query
+        query_entry = value;
+
+        // clear the active class from it
+        element.parents('ul').find('a').removeClass('selected');
+
+        element.addClass('selected');
+
+        //save back the new query entry to the scope query
+        $scope.query[data_filter] = query_entry;
+
+        query_api($scope.query);
+      }     
+    }
+
   });
-  
 })();
