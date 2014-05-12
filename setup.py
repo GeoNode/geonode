752d46e58c4843af03ea066403bf2a5cc40570d0
Ariel Nunez <ingenieroariel@gmail.com>
2014-05-12 17:27:54 +0200

    Pegging pinax-theme-bootstrap-account to the latest pypi release

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(name='GeoNode',
         "gsimporter==0.1",
         # Django Apps
         "pinax-theme-bootstrap==3.0a11",
-        "pinax-theme-bootstrap-account==1.0b3",
+        "pinax-theme-bootstrap-account==1.0b2",
         "django-user-accounts==1.0b14",
         "django-forms-bootstrap==2.0.3.post1",
         "django-pagination==1.0.7",
