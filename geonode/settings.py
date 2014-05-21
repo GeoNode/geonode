eabadd87701087fd2430120de936d25f6a7d6ee9
Ariel Nunez <ingenieroariel@gmail.com>
2014-05-21 15:03:26 +0200

    Added transparency to overlay in leaflet

--- geonode/settings.py
+++ geonode/settings.py
@@ -644,6 +644,7 @@ LEAFLET_CONFIG = {
     # http://leaflet-extras.github.io/leaflet-providers/preview/
 
     # Stamen toner lite.
+    ('Watercolor', 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
     ('Toner Lite', 'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
 ]
 }
