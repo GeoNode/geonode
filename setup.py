7f94d447605d50508549b7acc0236da5e454d83c
Paolo Corti <pcorti@gmail.com>
2014-05-29 18:18:54 +0200

    Moved django-geoexplorer 4.0.3 with classify

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ setup(name='GeoNode',
         "django-leaflet==0.13.2",
 
         # GeoNode org maintained apps.
-        "django-geoexplorer==4.0.2",
+        "django-geoexplorer==4.0.3",
         "geonode-user-messages==0.1",
         "geonode-avatar==2.1.2",
         "geonode-announcements==1.0.2",
