d053803a70e76b2f2092af6a0e874b3274f75b10
Tyler Garner <garnertb@gmail.com>
2014-06-02 12:20:56 -0400

    Remove multiple if statements in the geoserver cascading delete logic.

--- geonode/geoserver/helpers.py
+++ geonode/geoserver/helpers.py
@@ -262,17 +262,13 @@ def cascading_delete(cat, layer_name):
             
         if store.resource_type == 'dataStore' and 'dbtype' in store.connection_parameters and store.connection_parameters['dbtype'] == 'postgis':
             delete_from_postgis(resource_name)
-
-        # Prevent the entire store from being removed when the store is a GeoGIT repository.
-        if store.type and store.type.lower() == 'geogit':
+        elif store.type and store.type.lower() == 'geogit':
+            # Prevent the entire store from being removed when the store is a GeoGIT repository.
             return
-
         else:
             try:
                 if not store.get_resources():
                     cat.delete(store, recurse=True)
-                else:
-                    return
             except FailedRequestError as e:
                 # Catch the exception and log it.
                 logger.debug(e) 
