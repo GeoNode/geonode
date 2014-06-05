0d6f73e286ef9badeb9ee70d6926f32d751d4bb0
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-06-05 09:52:46 +0200

    use the guardian get_objects_for_user to filter permissions

--- geonode/api/authorization.py
+++ geonode/api/authorization.py
@@ -1,17 +1,20 @@
 from tastypie.authorization import DjangoAuthorization
 from tastypie.exceptions import Unauthorized
+from guardian.shortcuts import get_objects_for_user
 
 
 class GeoNodeAuthorization(DjangoAuthorization):
     """Object level API authorization based on GeoNode granular permission system"""
 
     def read_list(self, object_list, bundle):
-        # this applies permissions preserving the queryset for future use (faceting)
-        for obj in object_list:
-            if not bundle.request.user.has_perm('base.view_resourcebase', obj.resourcebase_ptr):
-                object_list = object_list.exclude(id__exact=obj.id)
+        # uses the django guardian to check permissions, then uses the id's to filter the queryset
+        # The permissions cannot be checked directly on the queryset because it's polymorphic and
+        # guardian does not allow to mix permissions and models
+
+        # TODO: improve this
+        perm_id_list = [val[0] for val in get_objects_for_user(bundle.request.user,'base.view_resourcebase').values_list('id')]
         
-        return object_list
+        return object_list.filter(id__in=perm_id_list)
 
     def read_detail(self, object_list, bundle):
         return bundle.request.user.has_perm('base.view_resourcebase', bundle.obj.resourcebase_ptr)
