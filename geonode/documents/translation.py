55d97fd685bce734a99ebafa63fc046f5e5b4c46
Jeffrey Johnson <ortelius@gmail.com>
2014-06-11 14:14:19 -0700

    Remove extraneous Media class from DocumentTranslationOptions

--- geonode/documents/translation.py
+++ geonode/documents/translation.py
@@ -4,14 +4,4 @@ from geonode.documents.models import Document
 class DocumentTranslationOptions(TranslationOptions):
     fields = ('title', 'abstract', 'purpose', 'constraints_other', 'supplemental_information', 'distribution_description', 'data_quality_statement', )
 
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
-
 translator.register(Document, DocumentTranslationOptions)
