1eae25c0af53506f884f38775087b38061453212
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-06-13 14:04:41 +0200

    remove the middleware as not valid and fix permissions check in templates

--- geonode/middleware.py
+++
@@ -1,7 +0,0 @@
-from guardian.shortcuts import get_anonymous_user
-
-class AnonymousUserMiddleware(object):
-
-    def process_request(self, request):
-        if not request.user.is_authenticated():
-            request.user = get_anonymous_user()
