6658efc8c1b40009e40b848239cf229190e45d26
Simone Dalmasso <simone.dalmasso@gmail.com>
2014-06-16 10:51:06 +0200

    fix autocomplete for profiles

--- geonode/people/autocomplete_light_registry.py
+++ geonode/people/autocomplete_light_registry.py
@@ -2,12 +2,7 @@ import autocomplete_light
 from models import Profile 
 from django.contrib.auth.models import User
 
-autocomplete_light.register(User,
-    search_fields=['^username','^email'],
-    autocomplete_js_attributes={'placeholder': 'name or email..',},
-)
-
 autocomplete_light.register(Profile,
-    search_fields=['^name',  '^email', '^user__username', '^user__email'],
+    search_fields=['^first_name',  '^email', '^username'],
     autocomplete_js_attributes={'placeholder': 'name or email..',},
 )
