74d696e621571e6e9495ed4c6e713379de90d674
Tyler Garner <garnertb@gmail.com>
2014-07-18 16:41:09 -0400

    Move activity.less to base.less and add activity styling.  Fixes #1572.

--- geonode/static/gruntfile.js
+++ geonode/static/gruntfile.js
@@ -26,8 +26,7 @@ module.exports = function(grunt) {
         files: [
           {
             // includes bootstrap.css
-            'geonode/css/base.css': 'geonode/less/base.less',
-            'geonode/css/activity.css': 'geonode/less/activity.less'
+            'geonode/css/base.css': 'geonode/less/base.less'
           }
         ]
       },
@@ -39,8 +38,7 @@ module.exports = function(grunt) {
         files: [
           {
             // includes bootstrap.css
-            'geonode/css/base.css': 'geonode/less/base.less',
-            'geonode/css/activity.css': 'geonode/less/activity.less'
+            'geonode/css/base.css': 'geonode/less/base.less'
           }
         ]
       }
