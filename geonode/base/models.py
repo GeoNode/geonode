6842efc40ecb7989039c101c3c6a79771648d7be
Micah Wengren <micah.wengren@gmail.com>
2014-07-29 15:18:38 -0400

    Changed the Thumbnail delete logic and fix tests

--- geonode/base/models.py
+++ geonode/base/models.py
@@ -354,8 +354,8 @@ class ResourceBase(PolymorphicModel, PermissionLevelMixin):
     rating = models.IntegerField(default=0, null=True)
 
     def delete(self, *args, **kwargs):
+        resourcebase_pre_delete(self)
         super(ResourceBase, self).delete(*args, **kwargs)
-        resourcebase_post_delete(self)
 
     def __unicode__(self):
         return self.title
@@ -663,10 +663,9 @@ class Link(models.Model):
     objects = LinkManager()
 
 
-def resourcebase_post_delete(instance):
+def resourcebase_pre_delete(instance):
     if instance.thumbnail_set.exists():
         instance.thumbnail_set.get().thumb_file.delete()
-        instance.thumbnail_set.get().delete()
 
 
 def resourcebase_post_save(instance, *args, **kwargs):
