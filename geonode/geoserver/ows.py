f8e3e010dabea5c877c7ed9f54d1bb5f0cb35b3c
DHohls <dhohls@csir.co.za>
2014-07-30 13:01:48 +0200

    Upgraded geoserver/ows to handle processing of SOS data

--- geonode/geoserver/ows.py
+++ geonode/geoserver/ows.py
@@ -26,6 +26,12 @@ import urllib
 from geonode import GeoNodeException
 from re import sub
 
+# SOS-related imports
+from owslib.sos import SensorObservationService
+from owslib.util import nspath_eval
+from owslib.swe.observation.sos100 import namespaces
+from owslib.namespaces import Namespaces
+
 logger = logging.getLogger(__name__)
 
 DEFAULT_EXCLUDE_FORMATS = ['PNG', 'JPEG', 'GIF', 'TIFF']
@@ -138,3 +144,109 @@ def wms_links(wms_url, identifier, bbox, srid, height, width):
         url = _wms_link(wms_url, identifier, mime, height, width, srid, bbox)
         output.append((ext, name, mime, url))
     return output
+
+### SOS functionality ###
+
+
+def sos_swe_data_list(response, constants=[], show_headers=True):
+    """Return data values from SOS XML <swe:value> tag as a list of lists.
+
+    Parameters
+    ----------
+    constants : list
+        Fixed values appended to each nested list
+    show_headers : boolean
+        if True, inserts list of headers as first nested list
+    """
+    from lxml import etree
+    result = []
+    headers = []
+    _tree = etree.fromstring(response)
+    # TEMPORARY PATCH; waiting for https://github.com/geopython/OWSLib/pull/170
+    if 'om' not in namespaces.keys():
+        ns = Namespaces()
+        namespaces['om'] = ns.get_namespace('om')
+    data = _tree.findall(
+        nspath_eval('om:member/om:Observation/om:result/swe:DataArray',
+        namespaces))
+    for datum in data:
+        encoding = datum.find(
+            nspath_eval('swe:encoding/swe:TextBlock', namespaces))
+        separators = (encoding.attrib['decimalSeparator'],
+                      encoding.attrib['tokenSeparator'],
+                      encoding.attrib['blockSeparator'])
+
+        if show_headers and not headers:  # first dataset only
+            fields = datum.findall(
+                nspath_eval('swe:elementType/swe:DataRecord/swe:field', namespaces))
+            for field in fields:
+                headers.append(field.attrib['name'])
+            if headers:
+                result.append(headers)
+
+        values = datum.find(nspath_eval('swe:values', namespaces))
+        lines = values.text.split(separators[2])
+        for line in lines:
+            items = line.split(separators[1])
+            if items:
+                if constants:
+                    items += constants
+                result.append(items)
+    return result
+
+
+def sos_observation_xml(url, version='1.0.0', xml=None, offerings=[],
+                        responseFormat=None, observedProperties=[],
+                        eventTime=None, feature=None, allProperties=False):
+    """Return the XML from a SOS GetObservation request.
+
+    Parameters
+    ----------
+    url : string
+        Full HTTP address of SOS
+    version: string
+        Version number of SOS (e.g. 1.0.0)
+    offerings : list
+        selected offerings from SOS; defaults to all available
+    responseFormat : string
+        desire format for result data
+    observedProperties : list
+        filters results for selected properties from SOS; defaults to first one
+        (unless allProperties is True)
+    eventTime : string
+        filters results for a specified instant or period.
+        Use ISO format YYYY-MM-DDTHH:mm:ss+-HH  Periods of time (start and end)
+        are separated by "/"; e.g. 2009-06-26T10:00:00+01/2009-06-26T11:00:00+01
+    feature : string
+        filters results for the ID of a feature_of_interest
+    allProperties : boolean
+        if allProperties is True, filters results for all properties (and
+        ignores any items in the observedProperties)
+    """
+    # GetCapabilites of SOS
+    _sos = SensorObservationService(url, version=version or '1.0.0', xml=xml or None)
+    # Process supplied offerings
+    if offerings:
+        for off in _sos.offerings:  # look for matching IDs
+            _offerings = [off for off in _sos.offerings if off.id in offerings]
+    else:
+        _offerings = []
+    # Get offering IDs to be used
+    offerings_objs = _offerings or  _sos.offerings
+    sos_offerings = [off.id for off in offerings_objs]
+    responseFormat = responseFormat or offerings_objs[0].response_formats[0]
+    if not allProperties:
+        observedProperties = observedProperties or [offerings_objs[0].observed_properties[0]]
+    else:
+        observedProperties = offerings_objs[0].observed_properties
+    eventTime = eventTime
+
+    if feature:
+        return _sos.get_observation(
+            offerings=sos_offerings, responseFormat=responseFormat,
+            observedProperties=observedProperties, eventTime=eventTime,
+            FEATUREOFINTEREST=feature)
+    else:
+        return _sos.get_observation(
+            offerings=sos_offerings, responseFormat=responseFormat,
+            observedProperties=observedProperties, eventTime=eventTime)
