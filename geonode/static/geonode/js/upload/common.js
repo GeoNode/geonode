55b19bc05f2768676489472ae385d1181b3a2d93
Ian Schneider <planablediglet@gmail.com>
2014-09-04 16:15:15 -0600

    uploader client-side fixes
    - fix for 'Mismatched anonymous define' error in time page by relocating
      script load order
    - refactor logStatus and logError to common module, support more options
      and fix various broken use of `self.markError`
    - refactor doUpload to time module
    - minor style/html cleanup on time page
    - fix incorrect form field name on time page
    - cleanup various dangling commas
    - don't probe geogit stores unless enabled

--- geonode/static/geonode/js/upload/common.js
+++ geonode/static/geonode/js/upload/common.js
@@ -2,7 +2,14 @@
 'use strict';
 
 define(function (require, exports) {
-    var make_request; 
+    var make_request,
+        logStatus,
+        logError;
+
+    function progressTemplate(options) {
+        var template =  _.template($('#progressTemplate').html());
+        return template(options);
+    };
 
     /** We have a different notion of success and failure for GeoNode's
      * urls this function allows the user to define two functions, success
@@ -29,4 +36,63 @@ define(function (require, exports) {
             }).fail(failure);
         };
     }());
+
+    /** Function to log to the upload-status element
+     *
+     * options required:
+     * message - the message
+     * alertLevel - class to inject
+     *
+     * optional options:
+     * empty - clear the status
+     * element - us this element instead of the default
+     *
+     *  @params {options} options
+     */
+    logStatus = function (options) {
+        var status = options.element || $('#upload-status'),
+            empty = options.empty;
+
+        if (empty) {
+            status.empty();
+        }
+        status.append(progressTemplate({
+            message: options.msg,
+            alertLevel: options.level
+        }));
+    };
+
+    /** Function to mark errors in the the status
+     *
+     *  @params {error} string or response object
+     *  @params {element} optional element to append to
+     */
+    logError = function (error, element) {
+        if (error.responseText) {
+            var parsed_errors = null;
+            var error_message = 'No error message supplied';
+            try {
+                parsed_errors = $.parseJSON(error.responseText);
+            } catch (exception) {
+                // in case the response is not json
+                error_message = error.responseText;
+            }
+
+            // Support the two different syntax used in GeoNode.
+            // TODO(Ariel): Agree on one of those server side and
+            // simplify this code. It can be either 'errormsgs' or 'error'.
+            if (parsed_errors !== null) {
+                if(parsed_errors.hasOwnProperty("errormsgs")){
+                    error_message = parsed_errors.errormsgs;
+                } else {
+                    error_message = parsed_errors.errors;
+                }
+            }
+            error = error_message;
+        }
+        logStatus({msg: error, level: 'alert-warning', empty:true, element:element});
+    };
+
+    exports.logStatus = logStatus;
+    exports.logError = logError;
 });
