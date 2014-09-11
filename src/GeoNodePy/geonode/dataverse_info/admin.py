618498df00650c75a681801aabc9aaa591fc6719
Raman Prasad <raman_prasad@harvard.edu>
2014-09-11 13:40:25 -0400

    basic dataverse info object + admin

---
+++ src/GeoNodePy/geonode/dataverse_info/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from geonode.dataverse_info.models import DataverseInfo
+
+
+class DataverseInfoAdmin(admin.ModelAdmin):
+    save_on_top = True
+    search_fields = ('map_layer__name',  'dataverse_name', 'dataset_name', 'datafile_label', 'dataset_description')
+    list_display = ('map_layer', 'dataset_name', 'doi', 'datafile_label', 'dataverse_name', )
+    list_filter = ('dataverse_name', )   
+    readonly_fields = ('modified', 'created')
+admin.site.register(DataverseInfo, DataverseInfoAdmin)
+
---
+++ src/GeoNodePy/geonode/dataverse_info/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from geonode.classification.models import TimeStampedModel
+from geonode.maps.models import Layer
+
+class DataverseInfo(models.Model):
+    """
+    If a map layer is created using a dataverse file, 
+    this objects contains Dataverse specific info on the file.
+    
+    In addition to supplying metadata, it can also be used to identify 
+    which Layers were created using Dataverse files.
+    """
+    map_layer = models.ForeignKey(Layer, on_delete=models.CASCADE)  # if the Layer is deleted, remove this DataverseInfo object
+    
+    # dv user who created layer
+    dv_username = models.CharField(max_length=255, db_index=True)
+    dv_user_email = models.EmailField(db_index=True)
+
+    # dataset url
+    dataset_url = models.URLField(max_length=255, blank=True)
+    
+    # dataverse info
+    dataverse_name = models.CharField(max_length=255, db_index=True)
+    dataverse_description = models.TextField(blank=True) 
+    
+    # dataset info
+    doi = models.CharField('DOI', max_length=255, blank=True)
+    dataset_id = models.IntegerField(default=-1)  # for API calls.  
+    dataset_version_id = models.IntegerField(default=-1)  # for API calls.
+    dataset_name = models.CharField(max_length=255, blank=True)  # for display
+    dataset_description = models.TextField(blank=True) 
+
+    # datafile info
+    datafile_label = models.CharField(max_length=255)  
+    datafile_description = models.TextField(blank=True)   
+    
+    
+    # timestamps
+    created = models.DateTimeField(auto_now_add=True) 
+    modified = models.DateTimeField(auto_now=True)
+    
+    
+    def __unicode__(self):
+        return '%s (%s -> %s)' % (self.datafile_label, self.dataverse_name, self.dataset_name)
+        
+    class Meta:
+        ordering = ('-modified',  )
+        verbose_name_plural = 'Dataverse Info'
+    
