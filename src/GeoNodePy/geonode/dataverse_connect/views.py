3c92e3d356fd4a32e89f28efd82851ea9d7056c6
Raman Prasad <raman_prasad@harvard.edu>
2014-11-19 15:03:23 -0500

    debug stmt

--- src/GeoNodePy/geonode/dataverse_connect/views.py
+++ src/GeoNodePy/geonode/dataverse_connect/views.py
@@ -26,9 +26,12 @@ logger = logging.getLogger("geonode.dataverse_connect.views")
 
 @csrf_exempt
 def view_add_worldmap_shapefile(request):
+    
+    print '----- view_add_worldmap_shapefile -----'
     if not has_proper_auth(request):
         json_msg = MessageHelperJSON.get_json_msg(success=False, msg="Authentication failed.")
         return HttpResponse(status=401, content=json_msg, content_type="application/json")
+    
     
     if request.POST:
         
