d8b9740afc772097c5a92f96b963b04ab50be5f2
Raman Prasad <raman_prasad@harvard.edu>
2014-11-20 12:09:06 -0500

    trace for auth

--- src/GeoNodePy/geonode/dataverse_connect/dataverse_auth.py
+++ src/GeoNodePy/geonode/dataverse_connect/dataverse_auth.py
@@ -2,29 +2,39 @@ import hashlib
 
 from django.conf import settings
 from django.http import QueryDict
-
+import logging
 TOKEN_KEY_NAME = 'geoconnect_token'
 
 #def has_proper_auth(request):
 #    hash_object = hashlib.sha256(b'Hello World')
 
+logger = logging.getLogger("geonode.dataverse_connect.dataverse_auth")
 
 def has_proper_auth(request):
     """For now, check for DV_TOKEN.
     Future: IP + DV_TOKEN
     Future: oauth
     """
+    logger.info('----- has_proper_auth? -----')
+
     if not request:
+        logger.info('----- not request -----')
+
         return False
     
     # Find the token
     if request.POST:
         dv_token = request.POST.get(TOKEN_KEY_NAME, None)
+        logger.info('request.POST.  token: %s' % dv_token )
     elif request.GET:
         dv_token = request.GET.get(TOKEN_KEY_NAME, None)
+        logger.info('request.GET.  token: %s' % dv_token )
     else:
+        logger.info('no POST or GET')
         return False
-    
+
+    logger.info('req: "%s"  equal to wm: "%s"' % (dv_token, settings.WORLDMAP_TOKEN_FOR_DATAVERSE) )
+
     if not dv_token == settings.WORLDMAP_TOKEN_FOR_DATAVERSE:
         return False
 
