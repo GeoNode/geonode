b1e95a92118f15e15d35bf1d2e24ebc59daa9773
Tyler Garner <garnertb@gmail.com>
2014-12-12 17:54:10 -0500

    Fix autopep8 shenanigans in the url routes.

--- geonode/urls.py
+++ geonode/urls.py
@@ -48,19 +48,10 @@ sitemaps = {
 urlpatterns = patterns('',
 
                        # Static pages
-                       url(r'^/?$',
-                           TemplateView.as_view(template_name='index.html'),
-                           name='home'),
-                       url(r'^help/$',
-                           TemplateView.as_view(template_name='help.html'),
-                           name='help'),
-                       url(r'^developer/$',
-                           TemplateView.as_view(
-                               template_name='developer.html'),
-                           name='developer'),
-                       url(r'^about/$',
-                           TemplateView.as_view(template_name='about.html'),
-                           name='about'),
+                       url(r'^/?$', TemplateView.as_view(template_name='index.html'), name='home'),
+                       url(r'^help/$', TemplateView.as_view(template_name='help.html'), name='help'),
+                       url(r'^developer/$', TemplateView.as_view(template_name='developer.html'), name='developer'),
+                       url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
                        # Layer views
                        (r'^layers/', include('geonode.layers.urls')),
@@ -72,10 +63,7 @@ urlpatterns = patterns('',
                        (r'^catalogue/', include('geonode.catalogue.urls')),
 
                        # Search views
-                       url(r'^search/$',
-                           TemplateView.as_view(
-                               template_name='search/search.html'),
-                           name='search'),
+                       url(r'^search/$', TemplateView.as_view(template_name='search/search.html'), name='search'),
 
                        # Social views
                        (r"^account/", include("account.urls")),
@@ -89,21 +77,19 @@ urlpatterns = patterns('',
                        (r'^messages/', include('user_messages.urls')),
                        (r'^social/', include('geonode.social.urls')),
                        (r'^security/', include('geonode.security.urls')),
+
                        # Accounts
-                       url(r'^account/ajax_login$', 'geonode.views.ajax_login',
-                           name='account_ajax_login'),
-                       url(r'^account/ajax_lookup$', 'geonode.views.ajax_lookup',
-                           name='account_ajax_lookup'),
+                       url(r'^account/ajax_login$', 'geonode.views.ajax_login', name='account_ajax_login'),
+                       url(r'^account/ajax_lookup$', 'geonode.views.ajax_lookup', name='account_ajax_lookup'),
 
                        # Meta
-                       url(r'^lang\.js$',
-                           TemplateView.as_view(template_name='lang.js',
-                                                content_type='text/javascript'),
+                       url(r'^lang\.js$', TemplateView.as_view(template_name='lang.js', content_type='text/javascript'),
                            name='lang'),
-                       url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog',
-                           js_info_dict, name='jscat'),
-                       url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
-                           {'sitemaps': sitemaps}, name='sitemap'),
+
+                       url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict, name='jscat'),
+                       url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps},
+                           name='sitemap'),
+
                        (r'^i18n/', include('django.conf.urls.i18n')),
                        (r'^autocomplete/', include('autocomplete_light.urls')),
                        (r'^admin/', include(admin.site.urls)),
@@ -115,8 +101,7 @@ urlpatterns = patterns('',
 
 if "geonode.contrib.dynamic" in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
-                            (r'^dynamic/',
-                             include('geonode.contrib.dynamic.urls')),
+                            (r'^dynamic/', include('geonode.contrib.dynamic.urls')),
                             )
 
 if 'geonode.geoserver' in settings.INSTALLED_APPS:
@@ -137,8 +122,6 @@ handler403 = 'geonode.views.err403'
 
 # Featured Maps Pattens
 urlpatterns += patterns('',
-                        (r'^(?P<site>[A-Za-z0-9_\-]+)/$',
-                         'geonode.maps.views.featured_map'),
-                        (r'^(?P<site>[A-Za-z0-9_\-]+)/info$',
-                            'geonode.maps.views.featured_map_info'),
+                        (r'^(?P<site>[A-Za-z0-9_\-]+)/$', 'geonode.maps.views.featured_map'),
+                        (r'^(?P<site>[A-Za-z0-9_\-]+)/info$', 'geonode.maps.views.featured_map_info'),
                         )
