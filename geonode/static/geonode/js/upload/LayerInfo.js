c2b849d03580f09e6c9ecea8f7d8502f5e0f662e
Tyler Garner <garnertb@gmail.com>
2014-12-15 12:44:29 -0500

    Improve upload button styles.

--- geonode/static/geonode/js/upload/LayerInfo.js
+++ geonode/static/geonode/js/upload/LayerInfo.js
@@ -228,9 +228,9 @@ define(function (require, exports) {
 
     LayerInfo.prototype.displayUploadedLayerLinks = function(resp) {
         var self = this;
-        var a = '<a href="' + resp.url + '" class="btn">' + gettext('Layer Info') + '</a>';
-        var b = '<a href="' + resp.url + '/metadata" class="btn">' + gettext('Edit Metadata') + '</a>';
-        var c = '<a href="' + resp.url + '/style/manage" class="btn">' + gettext('Manage Styles') + '</a>';
+        var a = '<a href="' + resp.url + '" class="btn btn-success">' + gettext('Layer Info') + '</a>';
+        var b = '<a href="' + resp.url + '/metadata" class="btn btn-warning">' + gettext('Edit Metadata') + '</a>';
+        var c = '<a href="' + resp.url + '/style/manage" class="btn btn-warning">' + gettext('Manage Styles') + '</a>';
         self.logStatus({
             msg: '<p>' + gettext('Your layer was successfully uploaded') + '<br/><br/>' + a + '&nbsp;&nbsp;&nbsp;' + b + '&nbsp;&nbsp;&nbsp;' + c + '</p>',
             level: 'alert-success',
