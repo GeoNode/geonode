8bb675e717cb4895d80de347ebc7afed50e0d4ef
Tyler Garner <garnertb@gmail.com>
2015-01-15 08:16:10 -0500

    Bump Django to 1.6.10.

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(name='GeoNode',
         "pillow", # python-pillow
         "lxml", # python-lxml
         # "psycopg2==2.4.5", # python-psycopg2
-        "Django >=1.6.1, <=1.6.5", # python-django
+        "Django==1.6.10", # python-django
 
         # Other
         "beautifulsoup4==4.2.1", # python-bs4
