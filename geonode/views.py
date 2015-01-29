6225ae857b98777b9f872c9d8882a36caed1c2ec
Geode Team <team@opengeode.be>
2015-01-29 10:30:12 +0100

    research user-group insensitive and exclude AnonymousUser

--- geonode/views.py
+++ geonode/views.py
@@ -79,11 +79,12 @@ def ajax_lookup(request):
             content='use a field named "query" to specify a prefix to filter usernames',
             mimetype='text/plain')
     keyword = request.POST['query']
-    users = get_user_model().objects.filter(Q(username__startswith=keyword) |
-                                            Q(first_name__contains=keyword) |
-                                            Q(organization__contains=keyword))
-    groups = GroupProfile.objects.filter(Q(title__startswith=keyword) |
-                                         Q(description__contains=keyword))
+    users = get_user_model().objects.filter(Q(username__istartswith=keyword) |
+                                            Q(first_name__icontains=keyword) |
+                                            Q(organization__icontains=keyword)).exclude(
+                                            username='AnonymousUser')
+    groups = GroupProfile.objects.filter(Q(title__istartswith=keyword) |
+                                         Q(description__icontains=keyword))
     json_dict = {
         'users': [({'username': u.username}) for u in users],
         'count': users.count(),
