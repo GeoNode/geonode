9c1c217c99b18d6441a1516287f5644e2d99ee3f
state-hiu <HIU_INFO@state.gov>
2015-02-03 21:02:07 -0500

    fix typo

--- geonode/utils.py
+++ geonode/utils.py
@@ -594,7 +594,7 @@ def build_social_links(request, resourcebase):
         host=request.get_host(),
         path=request.get_full_path())
     date = datetime.datetime.strftime(resourcebase.date, "%m/%d/%Y") if resourcebase.date else None
-    abstract = build_abstract(resourcebase, url=social_url, includeURL=True),
+    abstract = build_abstract(resourcebase, url=social_url, includeURL=True)
     caveats = build_caveats(resourcebase)
     return format_urls(
         settings.SOCIAL_ORIGINS,
