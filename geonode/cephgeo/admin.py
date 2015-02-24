978b67cce3b39ca89279b943e5ed62f42a978ea7
Ken Salanio <kssalanio@gmail.com>
2015-02-24 10:08:31 +0800

    Added cephgeo models and registered them to admin interface

--- geonode/cephgeo/admin.py
+++ geonode/cephgeo/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from geonode.cephgeo.models import CephDataObject, LayerToCephObjectMap
 
-# Register your models here.
+class CephDataObjectAdmin(admin.ModelAdmin):
+    model = CephDataObject
+    list_display_links = ('id',)
+    list_display = (
+        'id',
+        'name',
+        'file_ext',
+        'grid_ref',)
+    list_filter = ('file_ext', 'grid_ref')
+    search_fields = ('name', 'file_ext', 'grid_ref',)
+
+class LayerToCephObjectMapAdmin(admin.ModelAdmin):
+    model = LayerToCephObjectMap
+    list_display_links = ('id',)
+    list_display = (
+        'id',
+        'shapefile',
+        'ceph_data_obj',)
+    list_filter = ('shapefile', 'ceph_data_obj')
+    search_fields = ('shapefile', 'ceph_data_obj')
