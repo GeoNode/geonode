33d998547adb349a31402bd3523e5ebcdc669fd0
Carlo Santos <casantos@dream.upd.edu.ph>
2015-03-06 14:41:48 +0800

    added contents for maptiles.views, added templates for maptiles app, updated geonode.urls and geogeonode.settings to include maptiles

---
+++ geonode/maptiles/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+						url(r'^/?$',TemplateView.as_view(template_name='maptiles_base.html')),
+						)
