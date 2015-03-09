007ea9403d123ec4cdc6e2c3b58f2a0ed4478caa
geonode <no@email.com>
2015-03-09 15:19:32 +0800

    enabled logging for geonode django

--- geonode/settings.py
+++ geonode/settings.py
@@ -325,7 +325,7 @@ INSTALLED_APPS = (
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
