e7e59aa989902db59505c387a4b71b12896b2117
Tom Kralidis <tomkralidis@gmail.com>
2015-03-19 08:15:49 -0400

    fix flake8 error

--- geonode/catalogue/backends/pycsw_local.py
+++ geonode/catalogue/backends/pycsw_local.py
@@ -112,7 +112,7 @@ class CatalogueBackend(GenericCatalogueBackend):
         # serialize pycsw settings into SafeConfigParser
         # object for interaction with pycsw
         mdict = dict(settings.PYCSW['CONFIGURATION'], **CONFIGURATION)
-        if settings.PYCSW['CONFIGURATION'].has_key('server'):
+        if 'server' in settings.PYCSW['CONFIGURATION']:
             # override server system defaults with user specified directives
             mdict['server'].update(settings.PYCSW['CONFIGURATION']['server'])
         config = SafeConfigParser()
