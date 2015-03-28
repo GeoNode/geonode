1c86c03bc1c4cecfa45d08e2ddb25198e2c40e12
Ken Salanio <kssalanio@gmail.com>
2015-03-28 10:20:40 +0800

    [CEPHGEO] Added admin models for cart and items

--- geonode/cephgeo/admin.py
+++ geonode/cephgeo/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from geonode.cephgeo.models import CephDataObject, LayerToCephObjectMap
+from changuito.models import Cart, Item
 
 # Register your models here.
+class CartAdmin(admin.ModelAdmin):
+    model = Cart
+    list_display_links = ('id',)
+    list_display = (
+        'id',
+        'user',
+        'creation_date',
+        'checked_out',
+        'item_set')
+
+class ItemAdmin(admin.ModelAdmin):
+    model = Item
+    list_display_links = ('id',)
+    list_display = (
+        'id',
+        'cart',
+        'quantity',
+        'unit_price',
+        'content_type',
+        'object_id',)
+
 class CephDataObjectAdmin(admin.ModelAdmin):
     model = CephDataObject
     list_display_links = ('id',)
@@ -14,7 +36,7 @@ class CephDataObjectAdmin(admin.ModelAdmin):
         'geo_type',
         'grid_ref',
         'size_in_bytes',)
-    list_filter = ('geo_type', 'grid_ref')
+    list_filter = ('content_type', 'grid_ref')
     search_fields = ('name', 'content_type', 'grid_ref',)
 
 class LayerToCephObjectMapAdmin(admin.ModelAdmin):
@@ -27,6 +49,7 @@ class LayerToCephObjectMapAdmin(admin.ModelAdmin):
     list_filter = ('shapefile', 'ceph_data_obj')
     search_fields = ('shapefile', 'ceph_data_obj')
 
+admin.site.register(Cart, CartAdmin)
 admin.site.register(CephDataObject, CephDataObjectAdmin)
-admin.site.register(LayerToCephObjectMap, LayerToCephObjectMapAdmin)
+#admin.site.register(LayerToCephObjectMap, LayerToCephObjectMapAdmin)
 
