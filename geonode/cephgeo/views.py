32f80daf40a23fbebe4ab2a0ee9e56ceb4653d76
Ken Salanio <kssalanio@gmail.com>
2015-04-21 15:37:26 +0800

    [CEPHGEO] added RequestDataClassForm to get_cart view

--- geonode/cephgeo/views.py
+++ geonode/cephgeo/views.py
@@ -10,7 +10,7 @@ from django.template import RequestContext
 
 from pprint import pprint
 
-from geonode.cephgeo.forms import DataInputForm
+from geonode.cephgeo.forms import DataInputForm, RequestDataClassForm
 from geonode.cephgeo.models import CephDataObject, FTPRequest, FTPStatus, FTPRequestToObjectIndex
 from geonode.tasks.ftp import process_ftp_request
 
@@ -188,8 +188,9 @@ def error(request):
 
 @login_required
 def get_cart(request):
+    form = RequestDataClassForm()
     return render_to_response('cart.html', 
-                                dict(cart=CartProxy(request)),
+                                dict(cart=CartProxy(request),data_class_form=form}),
                                 context_instance=RequestContext(request))
 
 @login_required
