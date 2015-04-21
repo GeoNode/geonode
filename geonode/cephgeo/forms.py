a714fceba986dfe066f583da10db0ed50a1bfbf5
Ken Salanio <kssalanio@gmail.com>
2015-04-21 15:36:38 +0800

    [CEPHGEO] added form for selecting which class of data to download

--- geonode/cephgeo/forms.py
+++ geonode/cephgeo/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.models import DataClassification
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
@@ -18,6 +19,32 @@ class DataInputForm(forms.Form):
             ),
             ButtonHolder(
                 Submit('submit', 'Submit', css_class='button white')
+            )
+        )
+        super(DataInputForm, self).__init__(*args, **kwargs)
+        self.fields['pickled'].initial  = True
+
+class RequestDataClassForm(forms.Form):
+    LAZ = forms.BooleanField()
+    DEM = forms.BooleanField()
+    DSM = forms.BooleanField()
+    DTM = forms.BooleanField()
+    Orthophoto = forms.BooleanField()
+    
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+                'Choose which data to download :',
+                'LAZ',
+                'DEM',
+                'DSM',
+                'DTM',
+                'Orthophoto',
+            ),
+            FormActions(
+                Submit('submit', 'Create FTP Folder', css_class='button white')
+                Button('clear', 'Remove All Items', css_class='button white')
             )
         )
         super(DataInputForm, self).__init__(*args, **kwargs)
