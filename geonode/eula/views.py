5beb07bdd7298eec8655f87103690c6610690433
Ken Salanio <kssalanio@gmail.com>
2015-05-07 13:45:53 +0800

    transferred EULA view inside EULA app

--- geonode/eula/views.py
+++ geonode/eula/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+#@register.inclusion_tag('eula_text.html')(eula_text)
+def eula_form(request):
+    if request.method == 'POST':
+        #Handle form
+    else:
+        #Render form
+        return render(request, 'eula_form.html')
