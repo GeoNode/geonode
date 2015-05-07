256ef4ada14e20403fc20c0f9264d1c65665e235
Ken Salanio <kssalanio@gmail.com>
2015-05-07 15:39:13 +0800

    eula modal dialog added to create FTP folder button

--- geonode/eula/templatetags/eula_extras.py
+++ geonode/eula/templatetags/eula_extras.py
@@ -6,4 +6,10 @@ def show_eula(): # Only one argument.
     """Shows the EULA HTML Element"""
     return
 
+@register.inclusion_tag('eula_dialog.html')
+def eula_modal_dialog(target_url): # Only one argument.
+    """Shows the EULA HTML Element"""
+    return {'target_url' : target_url}
+
 register.filter('show_eula', show_eula)
+register.filter('eula_modal_dialog', eula_modal_dialog)
