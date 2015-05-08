cb37cc346be3a00872e864e1e2c61eb8ea073920
Ken Salanio <kssalanio@gmail.com>
2015-05-08 14:51:49 +0800

    added superuser filter for listing Ceph objects directly from Ceph Object Storage

--- geonode/cephgeo/views.py
+++ geonode/cephgeo/views.py
@@ -22,39 +22,41 @@ import client, utils, local_settings, cPickle, unicodedata, time, operator, json
 # Create your views here.
 @login_required
 def file_list_ceph(request, sort=None):
-    
-    if sort not in utils.SORT_TYPES and sort != None:
-        return HttpResponse(status=404)
+    if request.user.is_superuser:
+        if sort not in utils.SORT_TYPES and sort != None:
+            return HttpResponse(status=404)
+            
+        cephclient = client.CephStorageClient(local_settings.CEPH_USER,local_settings.CEPH_KEY,local_settings.CEPH_URL) 
+        object_list = cephclient.list_files(container_name=local_settings.DEFAULT_CONTAINER)
+        sorted_list = []
         
-    cephclient = client.CephStorageClient(local_settings.CEPH_USER,local_settings.CEPH_KEY,local_settings.CEPH_URL) 
-    object_list = cephclient.list_files(container_name=local_settings.DEFAULT_CONTAINER)
-    sorted_list = []
-    
-    for ceph_object in object_list:
-        ceph_object["type"] = utils.file_classifier(ceph_object["name"])
-        ceph_object["uploaddate"] = ceph_object["last_modified"]
-    
-    ###sorting goes here
-    if sort != None:
-        sorted_list = utils.sort_by(sort, object_list)
-        paginator = Paginator(sorted_list, 10)
+        for ceph_object in object_list:
+            ceph_object["type"] = utils.file_classifier(ceph_object["name"])
+            ceph_object["uploaddate"] = ceph_object["last_modified"]
+        
+        ###sorting goes here
+        if sort != None:
+            sorted_list = utils.sort_by(sort, object_list)
+            paginator = Paginator(sorted_list, 10)
+        else:
+            paginator = Paginator(object_list, 10)
+        
+        page = request.GET.get('page')
+        
+        try:
+            paged_objects = paginator.page(page)
+        except PageNotAnInteger:
+            paged_objects = paginator.page(1)
+        except EmptyPage:
+            paged_objects = paginator.page(paginator.num_pages)
+        
+        return render(request, "file_list.html",
+                        {"file_list"    : paged_objects, 
+                        "file_types"    : utils.TYPE_TO_IDENTIFIER_DICT , 
+                        "sort_types"    : utils.SORT_TYPES, 
+                        "sort"          : sort})
     else:
-        paginator = Paginator(object_list, 10)
-    
-    page = request.GET.get('page')
-    
-    try:
-        paged_objects = paginator.page(page)
-    except PageNotAnInteger:
-        paged_objects = paginator.page(1)
-    except EmptyPage:
-        paged_objects = paginator.page(paginator.num_pages)
-    
-    return render(request, "file_list.html",
-                    {"file_list"    : paged_objects, 
-                    "file_types"    : utils.TYPE_TO_IDENTIFIER_DICT , 
-                    "sort_types"    : utils.SORT_TYPES, 
-                    "sort"          : sort})
+        raise PermissionDenied()
 
 @login_required
 def file_list_geonode(request, sort=None, grid_ref=None):
