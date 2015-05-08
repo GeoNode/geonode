81884a165d9ece2ea7cc5b16a81e9441e7fb5d21
Ken Salanio <kssalanio@gmail.com>
2015-05-08 14:05:10 +0800

    updated to redirect to 403 page

--- geonode/cephgeo/views.py
+++ geonode/cephgeo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response, redirect
 from django.http import HttpResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils.encoding import smart_str
 from django.contrib import messages
@@ -181,7 +182,8 @@ def data_input(request):
             form = DataInputForm()
             return render(request, 'ceph_data_input.html', {'data_input_form': form})
     else:
-        return HttpResponseForbidden(u"You do not have permission to view this page!")
+        #return HttpResponseForbidden(u"You do not have permission to view this page!")
+        raise PermissionDenied()
 
 @login_required
 def error(request):
