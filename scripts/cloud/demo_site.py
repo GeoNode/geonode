b8980926c6311c2d6c257f429bb4e5df401867a6
Simone Dalmasso <simone.dalmasso@gmail.com>
2015-05-11 15:10:37 +0200

    fix errors and add single build

--- scripts/cloud/demo_site.py
+++ scripts/cloud/demo_site.py
@@ -7,20 +7,19 @@ from six.moves.urllib.request import Request
 JENKINS_IP = 'http://52.5.141.226/'
 GEONODE_DEMO_DOMAIN = 'demo.geonode.org'
 NODE_LIST = 'computer/api/json'
-GEONODE_DEMO_JOB = 'geonde-aws'
+GEONODE_DEMO_JOB = 'geonode-aws'
 
 
 class DemoGeonode(object):
     def __init__(self, username, token):
-
-        self.j = jenkins.Jenkins(JENKINS_IP, self.username, self.token)
+        self.j = jenkins.Jenkins(JENKINS_IP, username, token)
 
     def redeployDemo(self):
         nodes_data = json.loads(self.j.jenkins_open(Request(self.j.server + NODE_LIST)))
-        demo_node = getDemoNode(nodes_data)
+        demo_node = self.getDemoNode(nodes_data)
         if demo_node is not None:
-            deleteNode('demo_node')
-            buildJob(GEONODE_DEMO_JOB)
+            self.deleteNode(demo_node)
+            self.buildJob(GEONODE_DEMO_JOB)
             print 're-deploy complete!'
         else:
             print 'No demo.genode.org node found on jenkins'
@@ -29,19 +28,21 @@ class DemoGeonode(object):
     def getDemoNode(self, nodes_data):
         demo_node = None
         for node in nodes_data['computer']:
-            if NODE_LIST in node['displayName']:
+            if GEONODE_DEMO_DOMAIN in node['displayName']:
                 demo_node = node['displayName']
         return demo_node
 
 
     def deleteNode(self, node_name):
         print 'Deleting demo node'
-        self.j.delete_node('node_name')
+        self.j.delete_node(node_name)
+        print 'Deletion requested'
 
 
-    def buildJob(self):
-        print 'Building %s job' % GEONODE_DEMO_JOB
-        self.j.build_job(GEONODE_DEMO_JOB)
+    def buildJob(self, job):
+        print 'Building %s job' % job
+        self.j.build_job(job)
+        print 'Build requested'
 
 
 
@@ -55,12 +56,12 @@ if __name__ == "__main__":
     (options, args) = parser.parse_args()
     if options.username is not None and options.token is not None:
         task = sys.argv[-1]
-        if not task in ['redeploy-demo-site']:
-            print 'command not found'
+        demo = DemoGeonode(options.username, options.token)
+        if task == 'redeploy-demo-site':
+            demo.redeployDemo()
+        if task == 'build-demo-job':
+            demo.buildJob(GEONODE_DEMO_JOB)
         else:
-            demo = DemoGeonode(options.username, options.token)
-            if task == 'redeploy-demo-site':
-                demo.redeployDemo()
+            print 'Command not found'
     else:
         print 'username and access token are both required'
-    import ipdb; ipdb.set_trace()
