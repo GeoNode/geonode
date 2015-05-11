8ac51ff37b656e669060c409cecde190fd3eceaa
Ken Salanio <kssalanio@gmail.com>
2015-05-11 13:16:00 +0800

    added a custom tag for nested EULA modal dialogs

--- geonode/eula/templatetags/eula_extras.py
+++ geonode/eula/templatetags/eula_extras.py
@@ -11,5 +11,11 @@ def eula_modal_dialog(target_url): # Only one argument.
     """Shows the EULA HTML Element"""
     return {'target_url' : target_url}
 
+@register.inclusion_tag('eula_nested_modal.html')
+def eula_nested_modal_dialog(next_modal):
+    """Shows the EULA HTML Element"""
+    return {'next_modal' : next_modal}
+
 register.filter('show_eula', show_eula)
 register.filter('eula_modal_dialog', eula_modal_dialog)
+register.filter('eula_nested_modal_dialog', eula_nested_modal_dialog)
