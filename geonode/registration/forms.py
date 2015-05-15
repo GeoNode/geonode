81e2003c695609e1f81c365da34d638472dc7fa1
Ken Salanio <kssalanio@gmail.com>
2015-05-15 16:17:58 +0800

    moved registration form classes inside registration app

--- geonode/registration/forms.py
+++ geonode/registration/forms.py
@@ -5,50 +5,6 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, Button
 from crispy_forms.bootstrap import FormActions
 
-class DataInputForm(forms.Form):
-    data = forms.CharField(widget=forms.Textarea(attrs={'style' : 'resize:none; width:100%; height:60%;', 'wrap' : 'virtual'}))
-    pickled = forms.BooleanField()
-
-    def __init__(self, *args, **kwargs):
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Fieldset(
-                'Metadata output from bulk_upload.py:',
-                'data',
-                'pickled',
-            ),
-            ButtonHolder(
-                Submit('submit', 'Submit', css_class='button white')
-            )
-        )
-        super(DataInputForm, self).__init__(*args, **kwargs)
-        self.fields['pickled'].initial  = True
-
-class RequestDataClassForm(forms.Form):
-    LAZ = forms.BooleanField()
-    DEM = forms.BooleanField()
-    DSM = forms.BooleanField()
-    DTM = forms.BooleanField()
-    Orthophoto = forms.BooleanField()
-    
-    def __init__(self, *args, **kwargs):
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Fieldset(
-                'Choose which data to download :',
-                'LAZ',
-                'DEM',
-                'DSM',
-                'DTM',
-                'Orthophoto',
-            ),
-            FormActions(
-                Submit('submit', 'Create FTP Folder', css_class='button white'),
-                Button('clear', 'Remove All Items', css_class='button white')
-            )
-        )
-        super(RequestDataClassForm, self).__init__(*args, **kwargs)
-
 class UserRegistrationForm1(forms.Form):
     name_of_requestor = forms.CharField(
         label = "Name of Requestor",
