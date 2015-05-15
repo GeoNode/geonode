927504584f323614d717ab62323c1521d8cdbb72
Carlo Santos <casantos@dream.upd.edu.ph>
2015-05-15 16:04:43 +0800

    debugging validation

--- geonode/maptiles/urls.py
+++ geonode/maptiles/urls.py
@@ -6,5 +6,6 @@ import views
 urlpatterns = patterns('',
 						url(r'^/?$',views.tiled_view),
                         url(r'^test/?$',views.tiled_view2, ),
-                        url(r'^addtocart/?$',views.process_georefs)
+                        url(r'^addtocart/?$',views.process_georefs),
+                        url(r'^validate/?$',views.georefs_validation)
 						)
