741e32cba6095f59e6ec3a8cf0d0fe72da7d9948
Carlo Santos <casantos@dream.upd.edu.ph>
2015-05-19 15:48:37 +0800

    removed use of tiled_view2, maptiles.urls

--- geonode/maptiles/urls.py
+++ geonode/maptiles/urls.py
@@ -5,7 +5,7 @@ import views
 
 urlpatterns = patterns('',
 						url(r'^/?$',views.tiled_view),
-                        url(r'^test/?$',views.tiled_view2, ),
+                        url(r'^test/?$',views.tiled_view, { "overlay": settings.TILED_SHAPEFILE_TEST } ),
                         url(r'^addtocart/?$',views.process_georefs),
                         url(r'^validate/?$',views.georefs_validation)
 						)
