ef1f76c1415d5811c46e27297078423aa5fe63c8
Ken Salanio <kssalanio@gmail.com>
2015-05-19 11:11:26 +0800

    removed registration from cephgeo urls

--- geonode/cephgeo/urls.py
+++ geonode/cephgeo/urls.py
@@ -25,5 +25,4 @@ urlpatterns = patterns('',
 	url(r'^ftp/$', views.create_ftp_folder, ),
 	url(r'^ftprequests/$', views.ftp_request_list, ),
 	url(r'^ftprequests/(?P<sort>[A-Za-z]+)/$', views.ftp_request_list, ),
-    url(r'^registration/(?P<page>[A-Za-z0-9]+)/$', views.user_registration, ),
 )
