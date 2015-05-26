00dfe404e7dc75bc4edeb7b6125b4f2853db1bf9
Carlo Santos <casantos@dream.upd.edu.ph>
2015-05-26 18:23:51 +0800

    debugged MunicipalityAdmin and ProvinceAdmin attributes in registration.admin

--- geonode/registration/admin.py
+++ geonode/registration/admin.py
@@ -4,22 +4,22 @@ from geonode.registration.models import Province, Municipality
 # Register your models here.
 class ProvinceAdmin(admin.ModelAdmin):
     model = Province
-    list_display_links("province_name")
-    list_display(
+    list_display_links = ("province_name")
+    list_display = (
         "id",
         "province_name"
     )
     
 class MunicipalityAdmin(admin.ModelAdmin):
     model = Municipality
-    list_display_links("id")
-    list_display(
+    list_display_links = ("id")
+    list_display = (
         "id",
         "municipality_name",
         "province"
     )
     list_filter = ("province","municipality_name")
-    search_fields("province","municipality_name","alt_name")
+    search_fields = ("province","municipality_name","alt_name")
     
 admin.site.register(Province,ProvinceAdmin)
 admin.site.register(Municipality, MunicipalityAdmin)
