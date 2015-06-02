c0a534def391dff30989d46fbf68276b34809a18
Ken Salanio <kssalanio@gmail.com>
2015-06-02 11:28:40 +0800

    changed default timezone to Asia/Manila

--- geonode/settings.py
+++ geonode/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Manila'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
