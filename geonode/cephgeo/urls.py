1cc05688b3c4e1fb661d267cb9f07eaef623c2f4
Ken Salanio <kssalanio@gmail.com>
2015-06-03 13:52:58 +0800

    separated URLs for data management pages

--- geonode/cephgeo/urls.py
+++ geonode/cephgeo/urls.py
@@ -1,24 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 import views
 
 urlpatterns = patterns('',
     url(r'^$', login_required(TemplateView.as_view(template_name='ceph_main.html')),
                            name='ceph_main'),
-	url(r'^input/$', views.data_input ),
-	url(r'^input_old/$', views.data_input_old ),
-    url(r'^error/$', views.error ),
-	url(r'^list/$', views.file_list_geonode ),
-	url(r'^list/(?P<sort>[A-Za-z]+)/$', views.file_list_geonode ),
-	url(r'^list/(?P<sort>[A-Za-z]+)/(?P<grid_ref>[A-Za-z0-9]+)/$', views.file_list_geonode ),
-	url(r'^list_ceph/$', views.file_list_ceph ),
+	url(r'^error/$', views.error ),
 	url(r'^json_list/(?P<sort>[A-Za-z]+)/$', views.file_list_json ),
 	url(r'^json_list/(?P<sort>[A-Za-z]+)/(?P<grid_ref>[A-Za-z0-9]+)/$', views.file_list_json ),
-	url(r'^list_ceph/$', views.file_list_ceph ),
-	url(r'^list_ceph/(?P<sort>[A-Za-z]+)/$', views.file_list_ceph, ),
 	url(r'^cart/$', views.get_cart, ),
 	url(r'^cart_clear/$', views.clear_cart, ),
 	url(r'^cart_json/$', views.get_cart_json, ),
@@ -26,4 +18,15 @@ urlpatterns = patterns('',
 	url(r'^ftp/$', views.create_ftp_folder, ),
 	url(r'^ftprequests/$', views.ftp_request_list, ),
 	url(r'^ftprequests/(?P<sort>[A-Za-z]+)/$', views.ftp_request_list, ),
+    # Data Management Views
+    url(r'^datamanager/$', user_passes_test(lambda u: u.is_superuser, login_required(TemplateView.as_view(template_name='ceph_main.html')),
+                           name='ceph_manager')),
+    url(r'^datamanager/input/$', views.data_input ),
+    url(r'^datamanager/input_old/$', views.data_input_old ),
+    url(r'^datamanager/list/$', views.file_list_geonode ),
+    url(r'^datamanager/list/(?P<sort>[A-Za-z]+)/$', views.file_list_geonode ),
+    url(r'^datamanager/list/(?P<sort>[A-Za-z]+)/(?P<grid_ref>[A-Za-z0-9]+)/$', views.file_list_geonode ),
+    url(r'^datamanager/list_ceph/$', views.file_list_ceph ),
+    url(r'^datamanager/list_ceph/(?P<sort>[A-Za-z]+)/$', views.file_list_ceph, ),
+    
 )
