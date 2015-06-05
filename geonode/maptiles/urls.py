2328362930db475dac31f16770b5aad833482557
geonode-production <noaddress@dream.upd.edu.ph>
2015-06-05 14:53:23 +0800

    merge fixes

--- geonode/maptiles/urls.py
+++ geonode/maptiles/urls.py
@@ -8,7 +8,7 @@ import views
 
 
 urlpatterns = patterns('',
-						url(r'^/?$',views.tiled_view),
+						url(r'^/?$',views.tiled_view,name='maptiles_main'),
                         url(r'^test/?$',views.tiled_view, { "overlay": settings.TILED_SHAPEFILE_TEST } ),
                         url(r'^interest/(?P<interest>[^/]*)$', views.tiled_view, {"overlay": settings.TILED_SHAPEFILE_TEST}),
                         url(r'^addtocart/?$',views.process_georefs),
