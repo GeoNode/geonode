8b65d26986894a0cb2a5726e47cd398020bf07c0
Matthew Hanson <matt.a.hanson@gmail.com>
2015-07-20 18:41:33 +0200

    geosites - add GS url and project name to auto-generated site directory

--- geonode/contrib/geosites/site_template/wsgi.py
+++ geonode/contrib/geosites/site_template/wsgi.py
@@ -16,7 +16,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 curdir = os.path.basename(os.path.dirname(__file__))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "%s.settings" % curdir)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "$PROJECT_NAME.%s.settings" % curdir)
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
