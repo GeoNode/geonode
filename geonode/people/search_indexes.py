92ec0357f1a58b1e430ede0f25ad1388d47064bb
travis <travislbrundage@gmail.com>
2015-08-06 12:10:59 -0700

    Fixed users for elastic search

--- geonode/people/search_indexes.py
+++ geonode/people/search_indexes.py
@@ -1,7 +1,6 @@
 from haystack import indexes
 from geonode.people.models import Profile
 
-
 class ProfileIndex(indexes.SearchIndex, indexes.Indexable):
     id = indexes.IntegerField(model_attr='id')
     username = indexes.CharField(model_attr='username', null=True)
@@ -10,8 +9,19 @@ class ProfileIndex(indexes.SearchIndex, indexes.Indexable):
     profile = indexes.CharField(model_attr='profile', null=True)
     organization = indexes.CharField(model_attr='organization', null=True)
     position = indexes.CharField(model_attr='position', null=True)
+    # Adding these attributes so that the search page functions with elastic search
+    city = indexes.CharField(model_attr='city', null=True)
+    country = indexes.CharField(model_attr='country', null=True)
+    profile_detail_url = indexes.CharField(model_attr='get_absolute_url', null=True)
+    avatar_100 = indexes.CharField(null=True)
     text = indexes.CharField(document=True, use_template=True)
     type = indexes.CharField(faceted=True)
+
+    def prepare_avatar_100(self, obj):
+        avatar = obj.avatar_set.first()
+        if avatar:
+            return avatar.avatar_url(100)
+        return ''
 
     def get_model(self):
         return Profile
