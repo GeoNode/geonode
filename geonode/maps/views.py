24af626061856fa557c60004ebfc62a8f0327134
Clarence Davis Jr <clarence.davis@tech.geocent.com>
2015-08-13 08:33:57 -0500

    fix config to use layer.name rather than layer.typename for the layer name.

--- geonode/maps/views.py
+++ geonode/maps/views.py
@@ -511,7 +511,7 @@ def new_map_config(request):
                 else:
                     maplayer = MapLayer(
                         map=map_obj,
-                        name=layer.typename,
+                        name=layer.name,
                         ows_url=layer.ows_url,
                         layer_params=json.dumps(config),
                         visibility=True
