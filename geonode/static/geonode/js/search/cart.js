69fbd558c00506515ba57a6d70fa5e437b70876b
Simone Dalmasso <simone.dalmasso@gmail.com>
2015-09-15 16:12:26 +0200

    add bulk permissions in cart

--- geonode/static/geonode/js/search/cart.js
+++ geonode/static/geonode/js/search/cart.js
@@ -14,6 +14,45 @@
         }
         window.location = '/maps/new?' + params;
       }
+
+      $scope.bulk_perms_submit = function(){
+        var items = cart.getCart().items;
+        var permissions = permissionsString($('#permission_form'), 'base');
+        var selected_ids = $.map(items, function(item){return item.id});
+        var message = $('#bulk_perms_message');
+        if(selected_ids.length == 0){
+         message.find('.message').html('Please select at least one resource to set the permissions');
+         message.addClass('alert-danger').removeClass('alert-success alert-warning hidden');
+         return;
+        }
+        $.ajax(
+         {
+           type: "POST",
+           url: "/security/bulk-permissions",
+           data: {
+             permissions: JSON.stringify(permissions),
+             resources: selected_ids
+           },
+           success: function(data) {
+             var not_changed = $.parseJSON(data).not_changed;
+             if (not_changed.length > 0){ 
+               message.find('.message').html('Permissions correctly registered, although the following resources were'+ 
+                   ' skipped because you don\'t have the rights to edit their permissions:');
+               message.find('.extra_content').html(not_changed.join('</br>'));
+               message.addClass('alert-warning').removeClass('alert-success alert-danger hidden');
+             }
+             else{
+               message.find('.message').html('Permissions correctly registered.');
+               message.addClass('alert-success').removeClass('alert-warning alert-danger hidden');
+             }
+           },
+           error: function(data){
+             message.find('.message').html($.parseJSON(data).error);
+             message.addClass('alert-danger').removeClass('alert-success alert-warning hidden');
+           }
+         }
+        );
+      };
     })
 
     .directive('resourceCart', [function(){
