6a1761cf33c2b89f5d20086437f46c66fbf62c5d
Charles Cosse <ccosse@gmail.com>
2015-09-29 20:27:58 -0400

    1. Pass category_form from views.layer_upload to layer_upload.html 2. Require user to choose category before uploading (upload.js) 3. Pass chosen category via cookie (cookie.js, upload.js) 4. Parse category_id from cookie in POST section of views.layer_upload

--- geonode/layers/views.py
+++ geonode/layers/views.py
@@ -20,6 +20,7 @@
 
 import os
 import sys
+import string
 import logging
 import shutil
 import traceback
@@ -126,7 +127,9 @@ def layer_upload(request, template='upload/layer_upload.html'):
             'charsets': CHARSETS,
             'is_layer': True,
         }
+        category_form=CategoryForm(prefix="category_choice_field",initial=None)
         return render_to_response(template,
+                                  {"category_form":category_form},
                                   RequestContext(request, ctx))
     elif request.method == 'POST':
         form = NewLayerUploadForm(request.POST, request.FILES)
@@ -153,16 +156,40 @@ def layer_upload(request, template='upload/layer_upload.html'):
                 # exceptions when unicode characters are present.
                 # This should be followed up in upstream Django.
                 tempdir, base_file = form.write_files()
+                
+                topic_id=form.cleaned_data["category"]
+                if topic_id=="":
+                    try:
+                        //logging.debug(type(request.META.get("HTTP_COOKIE")))
+                        topic_id=request.META.get("HTTP_COOKIE")
+                        topic_id=string.split(topic_id," ")[0]
+                        topic_id=string.split(topic_id,":")[1]
+                        topic_id=string.split(topic_id,";")[0]
+                        
+                        //logging.debug("setting topid_id from cookie: %s"%topic_id)
+                    except:
+                        topic_id="1"
+                        //logging.exception("setting topic_id = 1 due to exception")
+                        
+                topic_category = TopicCategory.objects.get(
+                    id=topic_id
+                )
+                
                 saved_layer = file_upload(
                     base_file,
                     name=name,
                     user=request.user,
                     overwrite=False,
                     charset=form.cleaned_data["charset"],
+                    category=topic_category,
                     abstract=form.cleaned_data["abstract"],
                     title=form.cleaned_data["layer_title"],
                 )
-
+                
+                Layer.objects.filter(name=name).update(
+                    category=topic_category
+                )
+                
             except Exception as e:
                 exception_type, error, tb = sys.exc_info()
                 logger.exception(e)
