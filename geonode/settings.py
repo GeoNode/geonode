f2707e64e78931b31a06016a3e04f1ba76021b66
Simone Dalmasso <simone.dalmasso@gmail.com>
2015-10-07 14:03:50 +0200

    exclude bing until we figure out what changed

--- geonode/settings.py
+++ geonode/settings.py
@@ -630,13 +630,15 @@ MAP_BASELAYERS = [{
     "name": "naip",
     "group": "background",
     "visibility": False
-}, {
-    "source": {"ptype": "gxp_bingsource"},
-    "name": "AerialWithLabels",
-    "fixed": True,
-    "visibility": False,
-    "group": "background"
-}, {
+}, 
+# {    
+#     "source": {"ptype": "gxp_bingsource"},
+#     "name": "AerialWithLabels",
+#     "fixed": True,
+#     "visibility": False,
+#     "group": "background"
+# }, 
+{
     "source": {"ptype": "gxp_mapboxsource"},
 }]
 
