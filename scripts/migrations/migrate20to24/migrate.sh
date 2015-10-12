b968ebeb065aee561373d199d7d4440d4bbbd2f2
capooti <pcorti@gmail.com>
2015-10-12 15:32:21 +0200

    Some extra fixes

--- scripts/migrations/migrate20to24/migrate.sh
+++ scripts/migrations/migrate20to24/migrate.sh
@@ -13,6 +13,7 @@ python $DIR/migrate_attributes.py
 python $DIR/migrate_maps.py
 python $DIR/migrate_maplayers.py
 python $DIR/migrate_documents.py
+python $DIR/migrate_contactroles.py
 
 # things not being migrated, PR are welcome :)
 # actstream, agon_ratings, announcements, dialogos, notifications, messages
--- scripts/migrations/migrate20to24/migrate2.sh
+++ scripts/migrations/migrate20to24/migrate2.sh
@@ -1,7 +1,6 @@
 python $DIR/create_auth_group_and_update_res.py
 # here add your script to migrate other resourcebase based models
 # now contacts, tags, regions and permissions
-python $DIR/migrate_contactroles.py
 python $DIR/migrate_tags.py
 python $DIR/migrate_regions.py
 python $DIR/migrate_user_permissions.py
