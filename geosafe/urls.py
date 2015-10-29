aee09723d64ebf470d6353d10a1cdd52af325d28
Ismail Sunni <imajimatika@gmail.com>
2015-10-29 23:17:50 +0700

    Remove unused metadata things

--- geosafe/urls.py
+++ geosafe/urls.py
@@ -2,40 +2,10 @@ __author__ = 'ismailsunni'
 from django.conf.urls import patterns, url
 
 from geosafe.views import (
-    MetadataListView,
-    MetadataCreate,
-    MetadataDetailView,
-    MetadataUpdateView,
-    MetadataDeleteView,
     AnalysisCreateView)
 
 urlpatterns = patterns(
     '',
-    url(
-        r'^geosafe/metadata/$',
-        MetadataListView.as_view(),
-        name='metadata-list'
-    ),
-    url(
-        r'^geosafe/metadata/upload$',
-        MetadataCreate.as_view(),
-        name='metadata-create'
-    ),
-    url(
-        r'^geosafe/metadata/view/(?P<pk>\d+)$',
-        MetadataDetailView.as_view(),
-        name='metadata-detail'
-    ),
-    url(
-        r'^geosafe/metadata/update/(?P<pk>\d+)$',
-        MetadataUpdateView.as_view(),
-        name='metadata-update'
-    ),
-    url(
-        r'^geosafe/metadata/delete/(?P<pk>\d+)$',
-        MetadataDeleteView.as_view(),
-        name='metadata-delete'
-    ),
     url(
         r'^geosafe/analysis/create/$',
         AnalysisCreateView.as_view(),
--- geosafe/views.py
+++ geosafe/views.py
@@ -3,70 +3,7 @@ from django.views.generic import (
     DetailView, ListView, CreateView, UpdateView, DeleteView)
 
 from geosafe.models import Metadata, Analysis
-from geosafe.forms import (
-    MetadataUploadForm, MetadataUpdateForm, AnalysisCreationForm)
-
-
-# Create your views here.
-
-class MetadataListView(ListView):
-    model = Metadata
-    template_name = 'geosafe/metadata/list.html'
-    context_object_name = 'metadata_list'
-
-    def get_context_data(self, **kwargs):
-        context = super(MetadataListView, self).get_context_data(**kwargs)
-        return context
-
-
-class MetadataCreate(CreateView):
-    model = Metadata
-    form_class = MetadataUploadForm
-    template_name = 'geosafe/metadata/form.html'
-    context_object_name = 'metadata'
-
-    def get_success_url(self):
-        return reverse('metadata-list')
-
-    def get_form_kwargs(self):
-        kwargs = super(MetadataCreate, self).get_form_kwargs()
-        kwargs.update({'user': self.request.user})
-        return kwargs
-
-
-class MetadataDetailView(DetailView):
-    model = Metadata
-    template_name = 'geosafe/metadata/detail.html'
-    context_object_name = 'metadata'
-
-    def get_object(self, queryset=None):
-        obj = super(MetadataDetailView, self).get_object(queryset)
-        return obj
-
-
-class MetadataUpdateView(UpdateView):
-    model = Metadata
-    form_class = MetadataUpdateForm
-    template_name = 'geosafe/metadata/update.html'
-    context_object_name = 'metadata'
-    # fields = ['metadata_file']
-
-    def get_form_kwargs(self):
-        kwargs = super(MetadataUpdateView, self).get_form_kwargs()
-        kwargs.update({'user': self.request.user})
-        return kwargs
-
-    def get_success_url(self):
-        return reverse('metadata-detail', kwargs={'pk': self.object.layer.id})
-
-
-class MetadataDeleteView(DeleteView):
-    model = Metadata
-    template_name = 'geosafe/metadata/delete.html'
-    context_object_name = 'metadata'
-
-    def get_success_url(self):
-        return reverse('metadata-list')
+from geosafe.forms import (AnalysisCreationForm)
 
 
 class AnalysisCreateView(CreateView):
