8145db6f9e79e4802e2d09b383309718fcfb2a57
Craig Stephenson <cstephen@iarc.uaf.edu>
2015-11-19 17:14:27 -0800

    Fixed identations and made sure there are no other PEP8 violations.

--- geonode/layers/utils.py
+++ geonode/layers/utils.py
@@ -495,8 +495,8 @@ def file_upload(filename, name=None, user=None, title=None, abstract=None,
             layer.regions.add(*regions_resolved)
 
     if date is not None:
-      layer.date = date
-      layer.save()
+        layer.date = date
+        layer.save()
 
     return layer
 
