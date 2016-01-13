ee1564af1df950590a745ef4541f51792f07c4d3
Ken Salanio <kssalanio@gmail.com>
2016-01-13 15:37:24 +0800

    added Spatial Reference System (SRS) enum

--- geonode/maptiles/models.py
+++ geonode/maptiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django_enumfield import enum
 
 # Create your models here.
+class SRS(enum.Enum):
+    UTM51 = 32651
+    PRS92 = 4683
+    
+    labels = {
+        UTM51: 'UTM 51 (Default)',
+        PRS92: 'PRS 92',}
