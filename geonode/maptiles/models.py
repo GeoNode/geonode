26a52c3caefe022c6bf34cf93d3cfe76a215c8f4
Ken Salanio <kssalanio@gmail.com>
2016-01-13 16:15:18 +0800

    renamed UTM 51 to UTM 51N

--- geonode/maptiles/models.py
+++ geonode/maptiles/models.py
@@ -3,9 +3,9 @@ from django_enumfield import enum
 
 # Create your models here.
 class SRS(enum.Enum):
-    UTM51 = 32651
+    UTM51N = 32651
     PRS92 = 4683
     
     labels = {
-        UTM51: 'UTM 51 (Default)',
+        UTM51N: 'UTM 51N (Default)',
         PRS92: 'PRS 92',}
