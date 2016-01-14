a2283a572dbd2fd220e927418be79150bc171bec
Denise Leviste <drleviste@gmail.com>
2016-01-14 15:05:15 +0800

    Change password directs to SSP. No template tags yet.

---
+++ geonode/people/maptiles/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+import geonode.settings as settings
+
+import views
+
+
+urlpatterns = patterns('',
+						url(r'^/?$', views.tiled_view, name='maptiles_main'),
+                        url(r'^test/?$', views.tiled_view, { "overlay": settings.TILED_SHAPEFILE_TEST ,"test_mode":True} ),
+                        url(r'^interest=(?P<interest>[^/]*)$', views.tiled_view, {"overlay": settings.TILED_SHAPEFILE_TEST}),
+                        url(r'^addtocart/?$', views.process_georefs),
+                        url(r'^validate/?$', views.georefs_validation),
+                        url(r'^provinces/?$', views.province_lookup),
+                        url(r'^provinces/(?P<province>[^/]*)$', views.province_lookup),
+						)
