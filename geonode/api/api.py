ebd72e06c411c621e252821ce84ea546e91ed65f
Carlo Santos <casantos@dream.upd.edu.ph>
2016-01-19 15:42:55 +0800

    made changes to api.api based on http://pastebin.com/BNASvCh4 (part 1) as suggested by Renz

--- geonode/api/api.py
+++ geonode/api/api.py
@@ -394,17 +394,12 @@ class DataRequestProfileResource(ModelResource):
             return []
 """
 
-from tastypie.cache import NoCache
-
 class DataRequestProfileResource(ModelResource):
     """Data Request Profile api"""
-
+ 
     class Meta:
         authorization = GeoNodeAuthorization()
-        queryset = DataRequestProfile.objects.exclude(
-            date=None
-        ).order_by('-date')
+        queryset = DataRequestProfile.objects.all().order_by('-date')
         resource_name = 'data_requests'
         allowed_methods = ['get']
         ordering = ['date', ]
-        cache = NoCache()
