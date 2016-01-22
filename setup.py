1ecd98c3c20c73e905ed03bf26598453979e79bb
Carlo Santos <casantos@dream.upd.edu.ph>
2016-01-22 21:22:37 +0800

    updated setup.py to include django-auth-ldap

--- setup.py
+++ setup.py
@@ -157,6 +157,7 @@ setup(name='GeoNode',
         "django-model-utils==2.3.1",
         "django-recaptcha==1.0.4",
         "django-braces==1.8.1",
+        "django-auth-ldap==1.2.6",
 
         ],
       zip_safe=False,
