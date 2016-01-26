e144da9cf38cf51709365b99e62b31bf23b9aeb4
Carlo Santos <casantos@dream.upd.edu.ph>
2016-01-26 14:56:25 +0800

    added LDAP account creation in datarequests.utils, also checks if username is already present in LDAP, username and password generation is now done hidden from the user

--- geonode/datarequests/views.py
+++ geonode/datarequests/views.py
@@ -380,14 +380,13 @@ def data_request_profile_approve(request, pk):
         if not request_profile.date:
             raise Http404
         try:
-            request_profile.ftp_folder =str(request.POST[u'ftp-directory'])
-            request_profile.username = str(request.POST[u'username'])
+            #request_profile.ftp_folder =str(request.POST[u'ftp-directory'])
+            #request_profile.username = str(request.POST[u'username'])
             request_profile.request_status = 'approved'
             request_profile.save()
-            username = str(request.POST[u'username'])
-            password = str(request.POST[u'password'])
-            request_profile.create_account(username,password, request_profile.ftp_folder)
-            request_profile.save()
+           # username = str(request.POST[u'username'])
+            #password = str(request.POST[u'password'])
+            request_profile.create_account()
             return HttpResponseRedirect(request_profile.get_absolute_url())
         except:
             message = _('An unexpected error was encountered during the creation of the account.')
