4bede648534f72f50835e8f6c5b73186ee1fb434
Ken Salanio <kssalanio@gmail.com>
2016-01-28 12:58:34 +0800

    Identified django-guardian shortcuts slowing down the system

--- geonode/security/models.py
+++ geonode/security/models.py
@@ -49,7 +49,10 @@ class PermissionLevelMixin(object):
     user has exactly one assigned role with respect to
     an object representing an "access level"
     """
-
+    """
+    NOTE: django-guardian's get_users_with_perms and get_groups_with_perms
+          is VERY SLOW and needs to be replaced with something else  
+    """
     def get_all_level_info(self):
 
         resource = self.get_self_resource()
