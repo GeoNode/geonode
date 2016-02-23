3c053ea03840cb39773b3c38cd859b4fbcdb7447
Simone Dalmasso <simone.dalmasso@gmail.com>
2016-02-23 12:32:56 +0100

    enable abstract search

--- src/geonode-client/app/static/script/app/GeoNode/ogp/ogpConfig.js
+++ src/geonode-client/app/static/script/app/GeoNode/ogp/ogpConfig.js
@@ -5,7 +5,7 @@ Ext.onReady(function(){
   GeoNode.queryTerms = {
     intx: "product(max(0,sub(min(180,MaxX),max(-180,MinX))),max(0,sub(min(90,MaxY),max(-90,MinY))))",
     sort: "score desc",
-    qf: "LayerDisplayNameSynonyms^0.2 ThemeKeywordsSynonymsIso^0.1 ThemeKeywordsSynonymsLcsh^0.1 PlaceKeywordsSynonyms^0.1 Publisher^0.1 Originator^0.1",
+    qf: "LayerDisplayNameSynonyms^0.2 ThemeKeywordsSynonymsIso^0.1 ThemeKeywordsSynonymsLcsh^0.1 PlaceKeywordsSynonyms^0.1 Publisher^0.1 Originator^0.1 Abstract^0.2",
     wt: "json",
     defType: "edismax",
     fl: "Name,Institution,Access,DataType,LayerDisplayName,Publisher,GeoReferenced,Originator,Location,MinX,MaxX,MinY,MaxY,ContentDate,LayerId,score,WorkspaceName,CollectionId,ServiceType,Availability,Abstract",
