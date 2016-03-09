f819e4c129cd5c0aeb5e62cd14e9f2a0f879240e
Simone Dalmasso <simone.dalmasso@gmail.com>
2016-03-09 10:28:27 +0100

    simplify js guessing function, make slug unique and some more readme

--- geonode/contrib/collections/models.py
+++ geonode/contrib/collections/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from geonode.base.models import ResourceBase
 from geonode.groups.models import GroupProfile
 
+
 class Collection(models.Model):
     """
     A collection is a set of resouces linked to a GeoNode group
@@ -10,7 +11,7 @@ class Collection(models.Model):
     group = models.ForeignKey(GroupProfile, related_name='group_collections')
     resources = models.ManyToManyField(ResourceBase, related_name='resource_collections', blank=True, null=True)
     name = models.CharField(max_length=128)
-    slug = models.SlugField(max_length=128)
+    slug = models.SlugField(max_length=128, unique=True)
 
     def __unicode__(self):
         return self.name
