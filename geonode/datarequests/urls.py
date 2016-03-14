d5441da5153751e28985bd36081a3945c1bf528b
Carlo Santos <casantos@dream.upd.edu.ph>
2016-03-14 19:04:16 +0800

    added reconfirmation of email for unconfirmed emails

--- geonode/datarequests/urls.py
+++ geonode/datarequests/urls.py
@@ -21,6 +21,7 @@ urlpatterns = patterns(
     url(r'^(?P<pk>\d+)/$', 'data_request_profile', name="data_request_profile"),
     url(r'^(?P<pk>\d+)/approve/$', 'data_request_profile_approve', name="data_request_profile_approve"),
     url(r'^(?P<pk>\d+)/reject/$', 'data_request_profile_reject', name="data_request_profile_reject"),
+    url(r'^(?P<pk>\d+)/reconfirm/$', 'data_request_profile_reconfirm', name="data_request_profile_reconfirm"), 
     url(r'^~count_facets/$', 'data_request_facet_count', name="data_request_facet_count"),
 
 )
