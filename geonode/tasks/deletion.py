c8bd3127766a0b1c7f7bf0ac45bf4d78e3bcd525
travis <travislbrundage@gmail.com>
2016-04-28 11:42:22 -0700

    Allow MapStory objects to be removed

--- geonode/tasks/deletion.py
+++ geonode/tasks/deletion.py
@@ -1,7 +1,6 @@
 from geonode.layers.models import Layer
-from geonode.maps.models import Map
+from geonode.maps.models import Map, MapStory
 from celery.task import task
-
 
 @task(name='geonode.tasks.deletion.delete_layer', queue='cleanup')
 def delete_layer(object_id):
@@ -28,4 +27,22 @@ def delete_map(object_id):
         return
 
     map_obj.layer_set.all().delete()
+    map_obj.delete()
+
+@task(name='geonode.tasks.deletion.delete_mapstory', queue='cleanup')
+def delete_mapstory(object_id):
+    """
+    Deletes a mapstory and the associated maps and the associated map layers.
+    """
+
+    try:
+        map_obj = MapStory.objects.get(id=object_id)
+    except MapStory.DoesNotExist:
+        return
+
+    chapters = map_obj.chapters
+    for chapter in chapters:
+        chapter.layer_set.all().delete()
+        chapter.delete()
+
     map_obj.delete()
