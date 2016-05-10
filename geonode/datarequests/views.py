edfdd23ef9faa6f8fe97bf4eedb963c30db1a306
Carlo Santos <casantos@dream.upd.edu.ph>
2016-05-10 11:14:07 +0800

    pre-existing jurisdiction is deleted if approved request has no jurisdiction shapefile

--- geonode/datarequests/views.py
+++ geonode/datarequests/views.py
@@ -1,4 +1,4 @@
-import os
+appimport os
 import sys
 import shutil
 import traceback
@@ -600,8 +600,7 @@ def data_request_profile_approve(request, pk):
             else:
                 try:
                     uj = UserJurisdiction.objects.get(user=request_profile.profile)
-                    uj.jurisdiction_shapefile = None
-                    uj.save()
+                    uj.delete()
                 except ObjectDoesNotExist as e:
                     pprint("Jurisdiction Shapefile not found, nothing to delete. Carry on")
             
