88dde0d009a5326a6237f5e0d28d35a12d338b9b
Ismail Sunni <imajimatika@gmail.com>
2016-05-15 17:38:44 +0700

    Better URL management.

--- geonode/maps/urls.py
+++ geonode/maps/urls.py
@@ -28,72 +28,45 @@ js_info_dict = {
     'packages': ('geonode.maps',),
 }
 
+new_map_view = 'new_map'
 if 'geonode.geoserver' in settings.INSTALLED_APPS:
-    urlpatterns = patterns('geonode.maps.views',
-                           url(r'^$', TemplateView.as_view(
-                               template_name='maps/map_list.html'),
-                               name='maps_browse'),
-                           url(r'^new$', 'new_map', name="new_map"),
-                           url(r'^new/data$', 'new_map_json',
-                               name='new_map_json'),
-                           url(r'^checkurl/?$', 'ajax_url_lookup'),
-                           url(r'^snapshot/create/?$', 'snapshot_create'),
-                           url(r'^(?P<mapid>[^/]+)$', 'map_detail',
-                               name='map_detail'),
-                           url(r'^(?P<mapid>[^/]+)/view$', 'map_view',
-                               name='map_view'),
-                           url(r'^(?P<mapid>[^/]+)/data$', 'map_json',
-                               name='map_json'),
-                           url(r'^(?P<mapid>[^/]+)/download$', 'map_download',
-                               name='map_download'),
-                           url(r'^(?P<mapid>[^/]+)/wmc$', 'map_wmc',
-                               name='map_wmc'),
-                           url(r'^(?P<mapid>[^/]+)/wms$', 'map_wms',
-                               name='map_wms'),
-                           url(r'^(?P<mapid>[^/]+)/remove$', 'map_remove',
-                               name='map_remove'),
-                           url(r'^(?P<mapid>[^/]+)/metadata$', 'map_metadata',
-                               name='map_metadata'),
-                           url(r'^(?P<mapid>[^/]+)/embed$', 'map_embed',
-                               name='map_embed'),
-                           url(r'^(?P<mapid>[^/]+)/history$',
-                               'ajax_snapshot_history'),
-                           url(r'^(?P<mapid>\d+)/thumbnail$', 'map_thumbnail',
-                               name='map_thumbnail'),
-                           url(
-                               r'^(?P<mapid>[^/]+)/(?P<snapshot>[A-Za-z0-9_\-]+)/view$',
-                               'map_view'),
-                           url(
-                               r'^(?P<mapid>[^/]+)/(?P<snapshot>[A-Za-z0-9_\-]+)/info$',
-                               'map_detail'),
-                           url(
-                               r'^(?P<mapid>[^/]+)/(?P<snapshot>[A-Za-z0-9_\-]+)/embed/?$',
-                               'map_embed'),
-                           url(
-                               r'^(?P<mapid>[^/]+)/(?P<snapshot>[A-Za-z0-9_\-]+)/data$',
-                               'map_json', name='map_json'),
-                           url(r'^check/$', 'map_download_check',
-                               name='map_download_check'),
-                           url(r'^embed/$', 'map_embed', name='map_embed'),
-                           url(r'^(?P<layername>[^/]*)/attributes',
-                               'maplayer_attributes',
-                               name='maplayer_attributes'),
-                           # url(r'^change-poc/(?P<ids>\w+)$', 'change_poc', name='maps_change_poc'),
-                           )
+    new_map_view = 'new_map'
 
 elif 'geonode.qgis_server' in settings.INSTALLED_APPS:
-    urlpatterns = patterns(
-        '',
-        url(
-            r'^new$',
-            MapCreateView.as_view(),
-            name='new_map'
-        ),
-        url(r'^$',
-            TemplateView.as_view(
-            template_name='new_maps/map_list.html'),
-            name='maps_browse'),
-    )
+    new_map_view = MapCreateView.as_view()
 
-
-
+urlpatterns = patterns(
+    'geonode.maps.views',
+    url(r'^$',
+        TemplateView.as_view(template_name='maps/map_list.html'),
+        name='maps_browse'),
+    url(r'^new$', new_map_view, name="new_map"),
+    url(r'^new/data$', 'new_map_json', name='new_map_json'),
+    url(r'^checkurl/?$', 'ajax_url_lookup'),
+    url(r'^snapshot/create/?$', 'snapshot_create'),
+    url(r'^(?P<mapid>[^/]+)$', 'map_detail', name='map_detail'),
+    url(r'^(?P<mapid>[^/]+)/view$', 'map_view', name='map_view'),
+    url(r'^(?P<mapid>[^/]+)/data$', 'map_json', name='map_json'),
+    url(r'^(?P<mapid>[^/]+)/download$', 'map_download', name='map_download'),
+    url(r'^(?P<mapid>[^/]+)/wmc$', 'map_wmc', name='map_wmc'),
+    url(r'^(?P<mapid>[^/]+)/wms$', 'map_wms', name='map_wms'),
+    url(r'^(?P<mapid>[^/]+)/remove$', 'map_remove', name='map_remove'),
+    url(r'^(?P<mapid>[^/]+)/metadata$', 'map_metadata', name='map_metadata'),
+    url(r'^(?P<mapid>[^/]+)/embed$', 'map_embed', name='map_embed'),
+    url(r'^(?P<mapid>[^/]+)/history$', 'ajax_snapshot_history'),
+    url(r'^(?P<mapid>\d+)/thumbnail$', 'map_thumbnail', name='map_thumbnail'),
+    url(r'^(?P<mapid>[^/]+)/(?P<snapshot>[A-Za-z0-9_\-]+)/view$', 'map_view'),
+    url(r'^(?P<mapid>[^/]+)/(?P<snapshot>[A-Za-z0-9_\-]+)/info$',
+        'map_detail'),
+    url(r'^(?P<mapid>[^/]+)/(?P<snapshot>[A-Za-z0-9_\-]+)/embed/?$',
+        'map_embed'),
+    url(r'^(?P<mapid>[^/]+)/(?P<snapshot>[A-Za-z0-9_\-]+)/data$',
+        'map_json',
+        name='map_json'),
+    url(r'^check/$', 'map_download_check', name='map_download_check'),
+    url(r'^embed/$', 'map_embed', name='map_embed'),
+    url(r'^(?P<layername>[^/]*)/attributes',
+        'maplayer_attributes',
+        name='maplayer_attributes'),
+    # url(r'^change-poc/(?P<ids>\w+)$', 'change_poc', name='maps_change_poc'),
+)
