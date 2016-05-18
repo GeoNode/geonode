ddcca77ad13324d8dcaf1101ff5104787c2d3de4
Etienne Trimaille <etienne.trimaille@gmail.com>
2016-05-18 18:02:19 +0700

    set the attributes for a vector layer using WFS

--- geonode/qgis_server/gis_tools.py
+++ geonode/qgis_server/gis_tools.py
@@ -1,6 +1,115 @@
 # -*- coding: utf-8 -*-
 
+import logging
+from urllib import urlencode, urlretrieve
+from os.path import splitext
 from math import atan, degrees, sinh, pi
+from lxml import etree
+
+from geonode import settings as geonode_config
+from geonode.layers.models import Attribute
+from geonode.qgis_server.models import QGISServerLayer
+
+logger = logging.getLogger(__name__)
+
+
+def set_attributes(layer, overwrite=False):
+    """Retrieve layer attribute names & types from QGIS Server using
+    the WFS/WMS/WCS, then store in GeoNode database using Attribute model.
+
+    This function is copied/adapted from set_attributes in geoserver/helper.py.
+
+    :param layer: The layer where we want to add/update the attributes.
+    :type layer: QGISServerLayer
+
+    :param overwrite: If we should overwrite or not in the database.
+    :type overwrite: bool
+    """
+    if layer.storeType in ['dataStore']:
+        layer_name = layer.typename.encode('utf-8')
+        qgis_layer = QGISServerLayer.objects.get(layer=layer)
+
+        basename, _ = splitext(qgis_layer.base_layer_path)
+        qgis_server = geonode_config.QGIS_SERVER_CONFIG['qgis_server_url']
+        dft_url = qgis_server + '?' + urlencode({
+            'MAP': basename + '.qgs',
+            'SERVICE': 'WFS',
+            'VERSION': '1.0.0',
+            'REQUEST': 'DescribeFeatureType',
+            'LAYER': layer_name
+        })
+
+        # noinspection PyBroadException
+        try:
+            temp_file = urlretrieve(dft_url)[0]
+            with open (temp_file, 'r') as wfs_file:
+                doc = etree.fromstring(wfs_file.read())
+
+            path = './/{xsd}extension/{xsd}sequence/{xsd}element'.format(
+                xsd='{http://www.w3.org/2001/XMLSchema}')
+
+            attribute_map = [
+                [n.attrib['name'], n.attrib['type']] for n in doc.findall(
+                    path) if n.attrib.get('name') and n.attrib.get('type')]
+
+        except:
+            attribute_map = []
+    else:
+        attribute_map = []
+
+    # We need 3 more items for description, attribute_label and display_order
+    attribute_map_dict = {
+        'field': 0,
+        'ftype': 1,
+        'description': 2,
+        'label': 3,
+        'display_order': 4,
+    }
+    for attribute in attribute_map:
+        attribute.extend((None, None, 0))
+
+    attributes = layer.attribute_set.all()
+
+    # Delete existing attributes if they no longer exist in an updated layer.
+    for la in attributes:
+        lafound = False
+        for attribute in attribute_map:
+            field, ftype, description, label, display_order = attribute
+            if field == la.attribute:
+                lafound = True
+                # store description and attribute_label in attribute_map
+                attribute[attribute_map_dict['description']] = la.description
+                attribute[attribute_map_dict['label']] = la.attribute_label
+                attribute[attribute_map_dict['display_order']] = la.display_order
+
+        if overwrite or not lafound:
+            logger.debug(
+                'Going to delete [%s] for [%s]',
+                la.attribute,
+                layer.name.encode('utf-8'))
+            la.delete()
+
+    # Add new layer attributes if they don't already exist.
+    if attribute_map is not None:
+        iter = len(Attribute.objects.filter(layer=layer)) + 1
+        for attribute in attribute_map:
+            field, ftype, description, label, display_order = attribute
+            if field is not None:
+                la, created = Attribute.objects.get_or_create(
+                    layer=layer, attribute=field, attribute_type=ftype,
+                    description=description, attribute_label=label,
+                    display_order=display_order)
+                if created:
+                    la.visible = ftype.find('gml:') != 0
+                    la.display_order = iter
+                    la.save()
+                    iter += 1
+                    logger.debug(
+                        'Created [%s] attribute for [%s]',
+                        field,
+                        layer.name.encode('utf-8'))
+    else:
+        logger.debug('No attributes found')
 
 
 def num2deg(x_tile, y_tile, zoom):
