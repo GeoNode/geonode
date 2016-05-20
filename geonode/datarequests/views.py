33b9b3a1cfaa189e7c6b614869c5aaf5809d5ee8
Carlo Santos <casantos@dream.upd.edu.ph>
2016-05-20 19:28:59 +0800

    bug fixing data requests by authenticated users

--- geonode/datarequests/views.py
+++ geonode/datarequests/views.py
@@ -102,18 +102,17 @@ def registration_part_one(request):
                 form = DataRequestProfileForm(initial = initial)
     elif request.method == 'POST':
         if request.user.is_authenticated():
-            if not request_object:
-                request_object = DataRequestProfile(
-                    profile = request.user,
-                    first_name = request.user.first_name,
-                    middle_name = request.user.middle_name,
-                    last_name = request.user.last_name,
-                    organization = request.user.organization,
-                    email = request.user.email,
-                    contact_number = request.user.voice,
-                    request_status = 'pending'
-                )
-                request.session['request_object']=request_object
+            request_object = DataRequestProfile(
+                profile = request.user,
+                first_name = request.user.first_name,
+                middle_name = request.user.middle_name,
+                last_name = request.user.last_name,
+                organization = request.user.organization,
+                email = request.user.email,
+                contact_number = request.user.voice,
+                request_status = 'pending'
+            )
+            request.session['request_object']=request_object
             return HttpResponseRedirect(
                 reverse('datarequests:registration_part_two')
             )
