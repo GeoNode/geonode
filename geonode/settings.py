731a14bdc55887eb3ae296dc62d9871e84ef4a98
Carlo Santos <casantos@dream.upd.edu.ph>
2016-06-16 14:34:19 +0800

    modified sign in link

--- geonode/settings.py
+++ geonode/settings.py
@@ -427,7 +427,6 @@ MIDDLEWARE_CLASSES = (
 
 # Replacement of default authentication backend in order to support
 # permissions per object.
-<<<<<<< HEAD
 AUTHENTICATION_BACKENDS = (#y'django_auth_ldap.backend.LDAPBackend',
                            #'geonode.security.auth.GranularBackend',
                            'django.contrib.auth.backends.ModelBackend',
