96df4e3b6085e0561d965cd9fc7eab8b10c5b8fa
Ariel Nuñez <ingenieroariel@gmail.com>
2016-08-25 18:00:25 -0500

    Update migrations

---
+++ geonode/services/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('services', '0003_auto_20160824_0245'),
+        ('services', '0003_auto_20160822_1919'),
+    ]
+
+    operations = [
+    ]
