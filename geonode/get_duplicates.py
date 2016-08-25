ea1732f1fc242d67cd89f4e594e57a683613eb3f
Mapavia <mapavia@dream.upd.edu.ph>
2016-08-25 15:14:23 +0800

    added geonode.get_duplicates.py, it will list the typename of Layers with the same typenames in duplicatelayers.txt

---
+++ geonode/get_duplicates.py
@@ -0,0 +1,10 @@
+from geonode.layers.models import Layer
+
+def list_duplicate_layer():
+    dups = []
+    for x in Layer.objects.filter(name__icontains='fh'):
+        if x.typename.split('_')[-1] is int:
+            dups.append(x)
+    with open(filename,'w') as f:
+        f.write('\n'.join(dups))
+    return dups
