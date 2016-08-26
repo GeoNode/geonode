6f2a743d7eeeabfc1c80e87fbe15ad9baf80008e
Carlo Santos <casantos@dream.upd.edu.ph>
2016-08-26 13:53:53 +0800

    added more logging

--- django_cas_ng/backends.py
+++ django_cas_ng/backends.py
@@ -21,6 +21,7 @@ class CASBackend(ModelBackend):
         """Verifies CAS ticket and gets or creates User object"""
         client = get_cas_client(service_url=service)
         username, attributes, pgtiou = client.verify_ticket(ticket)
+        pprint("username: "+username)
         if attributes:
             request.session['attributes'] = attributes
         if not username:
