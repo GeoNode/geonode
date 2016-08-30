aff43c0ab60128abee22a50acd692c049ac55a31
Ariel Nuñez <ingenieroariel@gmail.com>
2016-08-30 18:48:08 -0500

    Use a pattern for adding package data, fixes missing fixtures on packages

--- setup.py
+++ setup.py
@@ -20,46 +20,8 @@
 
 from distutils.core import setup
 from distutils.command.install import INSTALL_SCHEMES
+from setuptools import find_packages
 import os
-
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils not to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-geonode_dir = 'geonode'
-
-for dirpath, dirnames, filenames in os.walk(geonode_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 
 setup(name='GeoNode',
@@ -73,8 +35,7 @@ setup(name='GeoNode',
       author_email='dev@geonode.org',
       url='http://geonode.org',
       license='GPL',
-      packages=packages,
-      data_files=data_files,
+      packages=find_packages(),
       include_package_data=True,
       install_requires=[
         # # The commented name next to the package
