9b3c666b108c665f95b7f5b7ce6710c5c5246e98
Carlo Santos <casantos@dream.upd.edu.ph>
2016-09-16 11:52:00 +0800

    reduced timeout from 600000ms to 300000ms

--- geonode/static/geonode/js/upload/LayerInfo.js
+++ geonode/static/geonode/js/upload/LayerInfo.js
@@ -388,7 +388,7 @@ define(function (require, exports) {
             data: form_data,
             processData: false,
             contentType: false,
-            timeout: 600000,
+            timeout: 300000,
             xhr: function() {
                 var req = $.ajaxSettings.xhr();
                 if (req) {
