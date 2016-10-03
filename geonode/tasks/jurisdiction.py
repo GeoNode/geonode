e5d932147abef4a12cd00d5200c250ad615f295c
Carlo Santos <casantos@dream.upd.edu.ph>
2016-10-03 17:29:37 +0800

    moved to another tasks script

--- geonode/tasks/jurisdiction.py
+++ geonode/tasks/jurisdiction.py
@@ -39,28 +39,6 @@ def jurisdiction_style(saved_layer):
     except Exception as e:
         exception_type, error, tb = sys.exc_info()
         print traceback.format_exc()
-        
-@task(name="geonode.tasks.jurisdiction.jurisdiction_gridrefs", queue="jurisdiction")
-def  jurisdiction_gridrefs(user):
-    assign_grid_refs(user)
-
-@task(name='geonode.tasks.jurisdiction.compute_size_update', queue='jurisdiction')
-def compute_size_update(requests_query_list, area_compute = True, data_size = True, save=True):
-    pprint("Updating requests data size and area coverage")
-    if len(requests_query_list) < 1:
-        pprint("Requests for update is empty")
-    else:
-        for r in requests_query_list:
-            pprint("Updating request id:{0}".format(r.pk))
-            shapefile = get_shp_ogr(r.jurisdiction_shapefile.name)
-            if shapefile:
-                if area_compute:
-                    r.area_coverage = get_area_coverage(shapefile)
-                if data_size:
-                    r.juris_data_size = get_juris_data_size(shapefile)
-                
-                if save:
-                    r.save()
 
 @task(name='geonode.tasks.jurisdiction.place_name_update', queue='jurisdiction')
 def  place_name_update(requests_query_list, save=True):
---
+++ geonode/tasks/jurisdiction2.py
@@ -0,0 +1,105 @@
+import math
+import sys
+import traceback
+from pprint import pprint
+from celery.task import task
+from osgeo import ogr
+
+from shapely.wkb import loads
+from shapely.geometry import Polygon
+from shapely.ops import cascaded_union
+
+import geonode.settings as settings
+
+from geonode.geoserver.helpers import ogc_server_settings
+from geoserver.catalog import Catalog
+from geonode.cephgeo.models import CephDataObject, UserJurisdiction, UserTiles
+from geonode.datarequests.models import DataRequestProfile
+from geonode.datarequests.utils2 import get_shp_ogr, assign_grid_refs, get_juris_data_size
+from geonode.datarequests.utils import  get_place_name, get_area_coverage
+
+
+@task(name='geonode.tasks.jurisdiction.compute_size_update', queue='jurisdiction')
+def compute_size_update(requests_query_list, area_compute = True, data_size = True, save=True):
+    pprint("Updating requests data size and area coverage")
+    if len(requests_query_list) < 1:
+        pprint("Requests for update is empty")
+    else:
+        for r in requests_query_list:
+            pprint("Updating request id:{0}".format(r.pk))
+            shapefile = get_shp_ogr(r.jurisdiction_shapefile.name)
+            if shapefile:
+                if area_compute:
+                    r.area_coverage = get_area_coverage(shapefile)
+                if data_size:
+                    r.juris_data_size = get_juris_data_size(shapefile)
+                
+                if save:
+                    r.save()
+
+def get_juris_tiles(juris_shp):
+    total_data_size = 0
+    min_x =  int(math.floor(juris_shp.bbox[0] / float(settings._TILE_SIZE)) * settings._TILE_SIZE)
+    max_x =  int(math.ceil(juris_shp.bbox[2] / float(settings._TILE_SIZE)) * settings._TILE_SIZE)
+    min_y =  int(math.floor(juris_shp.bbox[1] / float(settings._TILE_SIZE)) * settings._TILE_SIZE)
+    max_y =  int(math.ceil(juris_shp.bbox[3] / float(settings._TILE_SIZE)) * settings._TILE_SIZE)
+    tile_list = []
+    for tile_y in xrange(min_y+settings._TILE_SIZE, max_y+settings._TILE_SIZE, settings._TILE_SIZE):
+        for tile_x in xrange(min_x, max_x, settings._TILE_SIZE):
+            tile_ulp = (tile_x, tile_y)
+            tile_dlp = (tile_x, tile_y - settings._TILE_SIZE)
+            tile_drp = (tile_x + settings._TILE_SIZE, tile_y - settings._TILE_SIZE)
+            tile_urp = (tile_x + settings._TILE_SIZE, tile_y)
+            tile = Polygon([tile_ulp, tile_dlp, tile_drp, tile_urp])
+            
+            if not tile.intersection(juris_shp).is_empty:
+                tile_list.append(tile)
+                
+    return tile_list
+
+def get_juris_data_size(juris_shp):
+    tile_list = get_juris_tiles(juris_shp)
+    total_data_size = 0
+    
+    for tile in tile_list:
+        (minx, miny, maxx, maxy) = tile.bounds
+        gridref = "E{0}N{1}".format(minx / settings._TILE_SIZE, maxy / settings._TILE_SIZE,)
+        georef_query = CephDataObject.objects.filter(name__startswith=gridref)
+        total_size = 0
+        for georef_query_objects in georef_query:
+            total_size += georef_query_objects.size_in_bytes
+        total_data_size += total_size
+        
+    return total_data_size
+    
+def assign_grid_refs(user):
+    shapefile = UserJurisdiction.objects.get(user=user).jurisdiction_shapefile
+    gridref_list = []
+    
+    for tile in get_juris_tiles(shapefile):
+        (minx, miny, maxx, maxy) = tile.bounds
+        gridref = '"E{0}N{1}"'.format(minx / settings._TILE_SIZE, maxy / settings._TILE_SIZE,)
+        gridref_list .append(gridref)
+    
+    gridref_jquery = json.dumps(gridref_list)
+    
+    try:
+        tile_list_obj = UserTiles.objects.get(user=user)
+        tile_list_obj.gridref_list = gridref_jquery
+    except ObjectDoesNotExist as e:
+        tile_list_obj = UserTiles(user=user, gridref_list=gridref_jquery)
+    finally:
+        tile_list_obj.save()
+
+def get_shp_ogr(juris_shp_name):
+    source = ogr.Open(("PG:host={0} dbname={1} user={2} password={3}".format(settings.DATABASE_HOST,settings.GIS_DATABASE_NAME,settings.DATABASE_USER,settings.DATABASE_PASSWORD)))
+    data = source.ExecuteSQL("select the_geom from "+str(juris_shp_name))
+    shplist = []
+    if data:
+        for i in range(data.GetFeatureCount()):
+            feature = data.GetNextFeature()
+            shplist.append(loads(feature.GetGeometryRef().ExportToWkb()))
+        juris_shp = cascaded_union(shplist)
+        return juris_shp
+    else:
+        return None
