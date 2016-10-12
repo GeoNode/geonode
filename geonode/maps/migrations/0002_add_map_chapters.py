10298ae2d9f849b91020ecc397e830f3ef6810e8
emilyashley <ohemilyashley@gmail.com>
2016-10-12 12:52:35 -0500

    Map migration files for viewer playback modes.

---
+++ geonode/maps/migrations/0002_add_map_chapters.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0003_auto_20160809_1434'),
+        ('maps', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='MapStory',
+            fields=[
+                ('resourcebase_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='base.ResourceBase')),
+            ],
+            options={
+                'abstract': False,
+                'db_table': 'maps_mapstory',
+                'verbose_name_plural': 'MapStories',
+            },
+            bases=('base.resourcebase',),
+        ),
+        migrations.AlterModelOptions(
+            name='map',
+            options={'verbose_name_plural': 'MapStory Chapters'},
+        ),
+        migrations.AddField(
+            model_name='map',
+            name='chapter_index',
+            field=models.IntegerField(null=True, verbose_name='chapter index', blank=True),
+        ),
+        migrations.AddField(
+            model_name='map',
+            name='story',
+            field=models.ForeignKey(related_name='chapter_list', blank=True, to='maps.MapStory', null=True),
+        ),
+    ]
---
+++ geonode/maps/migrations/0003_map_viewer_playbackmode.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('maps', '0002_auto_20160809_1434'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='map',
+            name='viewer_playbackmode',
+            field=models.CharField(max_length=32, null=True, verbose_name='Viewer Playback', blank=True),
+        ),
+    ]
