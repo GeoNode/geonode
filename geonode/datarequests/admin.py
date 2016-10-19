e3a05ab442afa2e64c39c09fa9fe6dbde5a265b5
Carlo Santos <casantos@dream.upd.edu.ph>
2016-10-19 13:24:58 +0800

    added search fields for request admin

--- geonode/datarequests/admin.py
+++ geonode/datarequests/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 
 from geonode.datarequests.models import DataRequestProfile, RequestRejectionReason, DataRequest, ProfileRequest
 
-admin.site.register(ProfileRequest)
-admin.site.register(DataRequest)
+class ProfileRequestAdmin(admin.ModelAdmin):
+    model = ProfileRequest
+    search_fields=('id', 'last_name','organization','status','administrator',)
+
+class DataRequestAdmin(admin.ModelAdmin):
+    model = DataRequest
+    search_fields=('id', 'status','profile__last_name','profile_request__last_name','request_letter__title','administrator__username','profile__username')
+
+admin.site.register(ProfileRequest,ProfileRequestAdmin)
+admin.site.register(DataRequest,DataRequestAdmin)
 admin.site.register(DataRequestProfile)
 admin.site.register(RequestRejectionReason)
