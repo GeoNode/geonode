1c644614d7ee5d7ce419d92a268994fc95a73fd5
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2016-10-19 18:14:56 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,15 +12,6 @@
 # serve to show the default.
 
 import sys, os
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return Mock()
-
-MOCK_MODULES = ['Shapely', 'pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
