3ba1cccb8822b3ccd6a60f8f58aba10fd668d409
Carlo Santos <casantos@dream.upd.edu.ph>
2016-11-03 15:21:45 +0800

    restored models

---
+++ geonode/datarequests/models/email_msg_templates.py
@@ -0,0 +1,23 @@
+VERIFICATION_EMAIL_TEXT="""
+     Dear <strong>{}</strong>,
+
+    Please paste the following URL in your browser to verify your email and complete your Data Request Registration.
+    {}
+
+    For inquiries, you can contact us as at {}.
+
+    Regards,
+    LiPAD Team
+     """
+     
+VERIFICATION_EMAIL_HTML= """
+    <p>Dear <strong>{}</strong>,</p>
+
+   <p>Please click on the following link to verify your email and complete your Data Request Registration.</p>
+   <p><a rel="nofollow" target="_blank" href="{}">{}</a></p>
+   <p>For inquiries, you can contact us as at <a href="mailto:{}" target="_top">{}</a></p>
+   </br>
+    <p>Regards,</p>
+    <p>LiPAD Team</p>
+    """
+
