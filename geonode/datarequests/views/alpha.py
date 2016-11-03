1af71948ab2e00a9e9e861b936625a4d60c3b90f
Carlo Santos <casantos@dream.upd.edu.ph>
2016-11-03 15:09:48 +0800

    restored datarequests module

---
+++ geonode/datarequests/views/alpha.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import shutil
+import traceback
+import datetime
+import time
+import csv
+from urlparse import parse_qs
+
+from crispy_forms.utils import render_crispy_form
+
+from django.conf import settings
+from django.contrib import messages
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
+from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import (
+    redirect, get_object_or_404, render, render_to_response)
+from django.template import RequestContext
+from django.template.defaultfilters import slugify
+from django.template.response import TemplateResponse
+from django.utils import dateformat
+from django.utils import timezone
+from django.utils import simplejson as json
+from django.utils.html import escape
+from django.utils.translation import ugettext as _
+from django.views.decorators.http import require_POST
+from django.views.generic import TemplateView
+
+from geonode.base.enumerations import CHARSETS
+from geonode.cephgeo.models import UserJurisdiction
+from geonode.documents.models import get_related_documents
+from geonode.documents.models import Document
+from geonode.layers.models import UploadSession, Style
+from geonode.layers.utils import file_upload
+from geonode.people.models import Profile
+from geonode.people.views import profile_detail
+from geonode.security.views import _perms_info_json
+from geonode.tasks.jurisdiction import place_name_update, jurisdiction_style
+from geonode.tasks.jurisdiction2 import compute_size_update, assign_grid_refs, assign_grid_refs_all
+from geonode.utils import default_map_config
+from geonode.utils import GXPLayer
+from geonode.utils import GXPMap
+from geonode.utils import llbbox_to_mercator
+from geonode.utils import build_social_links
+
+from geoserver.catalog import Catalog
+
+from unidecode import unidecode
+from pprint import pprint
+
+
+from braces.views import (
+    SuperuserRequiredMixin, LoginRequiredMixin,
+)
+
+from geonode.datarequests.forms import (
+    ProfileRequestForm, DataRequestShapefileForm,
+    RejectionForm, DataRequestForm)
+
+from geonode.datarequests.models import DataRequestProfile, DataRequest, ProfileRequest
+
+from geonode.datarequests.utils import (
+    get_place_name, get_area_coverage)
+
+@login_required
+def requests_landing(request):
+    if request.user.is_superuser:
+        return TemplateResponse(request, 'datarequests/requests_landing.html',{}, status=200).render()
+    else:
+        return HttpResponseRedirect(reverse('datarequests:data_request_browse'))
