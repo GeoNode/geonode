eac858b6e8178256576c4ccd1520fb4ad24b1016
Carlo Santos <casantos@dream.upd.edu.ph>
2016-11-03 11:48:52 +0800

    added new function for notifying admins of new requests, created a base request admin page

--- geonode/datarequests/admin.py
+++ geonode/datarequests/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from geonode.datarequests.models import DataRequestProfile, RequestRejectionReason, DataRequest, ProfileRequest
+from geonode.datarequests.models import DataRequestProfile, RequestRejectionReason, DataRequest, ProfileRequest, BaseRequest
+
+class BaseRequestAdmin(admin.ModelAdmin):
+    model = BaseRequest
+    search_fields = ('id','status','created')
 
 class ProfileRequestAdmin(admin.ModelAdmin):
     model = ProfileRequest
@@ -10,6 +14,7 @@ class DataRequestAdmin(admin.ModelAdmin):
     model = DataRequest
     search_fields=('id', 'status','request_letter__title')
 
+admin.site.register(BaseRequest,BaseRequestAdmin)
 admin.site.register(ProfileRequest,ProfileRequestAdmin)
 admin.site.register(DataRequest,DataRequestAdmin)
 admin.site.register(DataRequestProfile)
