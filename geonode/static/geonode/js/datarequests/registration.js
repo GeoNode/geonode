24f6eaf76872953fc46cbaebab4b1a309573dd6f
Mapavia <mapavia@dream.upd.edu.ph>
2016-11-24 16:50:32 +0800

    merged test to master, includes place_name fix and organization_other addition and turning organization_type to a radio button geonode/datarequests geonode/static/geonode/js/datarequests/registration.js

--- geonode/static/geonode/js/datarequests/registration.js
+++ geonode/static/geonode/js/datarequests/registration.js
@@ -1,25 +1,35 @@
 $(function() {
 
     var $intended_use = $("#id_intended_use_of_dataset");
-    var $org_type = $("#id_organization_type")
+    // var $org_type = $("#id_organization_type")
+    var $org_type = $('input[name="organization_type"]');
+    var $org_type_checked = $('input[name="organization_type"]:checked');
     var $form = $intended_use.closest('form');
     var $form2 = $org_type.closest('form');
     var $noncommercial = $form.find('fieldset.noncommercial-fieldset');
     var $academe = $form2.find('fieldset.academe-fieldset');
+    var $other = $form2.find('div#div_id_organization_other');
 
     // Initial values
-
-    if ($org_type.val() != '3') {
-        $academe.toggle(false);
-    } else {
-        $academe.toggle(true);
+    $academe.toggle(false);
+    $other.toggle(false);
+    if ($org_type_checked.val() == '3') {
+      $academe.toggle(true);
+    }else if ($org_type_checked.val() == '7') {
+      $other.toggle(true);
     }
-    $org_type.on('change', function() {
-
-        if ($(this).val() == '3'){
+    // $org_type.on('change', function() {
+    $org_type.change( function() {
+        // if ($(this).val() == '3'){
+        if ($(this).is(':checked') && $(this).val() == '3'){
             $academe.slideDown();
         } else {
             $academe.slideUp();
+        }
+        if ($(this).is(':checked') && $(this).val() == '7'){
+            $other.slideDown();
+        } else {
+            $other.slideUp();
         }
     });
 
