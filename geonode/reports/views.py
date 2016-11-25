e12de458800fbade71d744940f03359763c7816a
Mapavia <mapavia@dream.upd.edu.ph>
2016-11-25 12:53:25 +0800

    geonode.scripts.utils.reports added monthly_dl_count.csv and old_date_to_db.py geonode.reports.models.py removed auto_now=True in date geonode.reports.views.py	edited chart

--- geonode/reports/views.py
+++ geonode/reports/views.py
@@ -32,78 +32,18 @@ from pprint import pprint
 from actstream.models import Action
 from geonode.eula.models import AnonDownloader
 
-def report_layer(request, template='reports/report_layers.html'):
-    layer_count = {}
-    auth_list = Action.objects.filter(verb='downloaded').order_by('timestamp')
-    for auth in auth_list:
-        if auth.timestamp.strftime('%b') not in layer_count:
-            layer_count[auth.timestamp.strftime('%b')] = {
-                "cov": 0,
-                "doc": 0,
-                "fhm": 0,
-                "dtm": 0,
-                "dsm": 0,
-                "laz": 0,
-                "ortho": 0,
-                "sar": 0,
-                "others": 0,
-            }
-        if auth.action_object.csw_type == 'document':
-            layer_count[auth.timestamp.strftime('%b')]['doc'] += 1
-        else:
-            if 'coverage' in auth.action_object.typename:
-                layer_count[auth.timestamp.strftime('%b')]['cov'] += 1
-            elif 'fh' in auth.action_object.typename:
-                layer_count[auth.timestamp.strftime('%b')]['fhm'] += 1
-            elif 'dtm' in auth.action_object.typename:
-                layer_count[auth.timestamp.strftime('%b')]['dtm'] += 1
-            elif 'dsm' in auth.action_object.typename:
-                layer_count[auth.timestamp.strftime('%b')]['dsm'] += 1
-            elif 'laz' in auth.action_object.typename:
-                layer_count[auth.timestamp.strftime('%b')]['laz'] += 1
-            elif 'ortho' in auth.action_object.typename:
-                layer_count[auth.timestamp.strftime('%b')]['ortho'] += 1
-            elif 'sar' in auth.action_object.typename:
-                layer_count[auth.timestamp.strftime('%b')]['sar'] += 1
-            else:
-                layer_count[auth.timestamp.strftime('%b')]['others'] += 1
+from geonode.reports.models import DownloadCount
 
-    anon_list = AnonDownloader.objects.all().order_by('date')
-    for anon in anon_list:
-        if anon.date.strftime('%b') not in layer_count:
-            layer_count[anon.date.strftime('%b')] = {
-                "cov": 0,
-                "doc": 0,
-                "fhm": 0,
-                "dtm": 0,
-                "dsm": 0,
-                "laz": 0,
-                "ortho": 0,
-                "sar": 0,
-                "others": 0,
-            }
-        if anon.anon_document:
-            layer_count[anon.date.strftime('%b')]['doc'] += 1
-        else:
-            if 'coverage' in anon.anon_layer.typename:
-                layer_count[anon.date.strftime('%b')]['cov'] += 1
-            elif 'fh' in anon.anon_layer.typename:
-                layer_count[anon.date.strftime('%b')]['fhm'] += 1
-            elif 'dtm' in anon.anon_layer.typename:
-                layer_count[anon.date.strftime('%b')]['dtm'] += 1
-            elif 'dsm' in anon.anon_layer.typename:
-                layer_count[anon.date.strftime('%b')]['dsm'] += 1
-            elif 'laz' in anon.anon_layer.typename:
-                layer_count[anon.date.strftime('%b')]['laz'] += 1
-            elif 'ortho' in anon.anon_layer.typename:
-                layer_count[anon.date.strftime('%b')]['ortho'] += 1
-            elif 'sar' in anon.anon_layer.typename:
-                layer_count[anon.date.strftime('%b')]['sar'] += 1
-            else:
-                layer_count[anon.date.strftime('%b')]['others'] += 1
-    pprint(layer_count)
+def report_layer(request, template='reports/report_layers.html'):
+    monthly_count = {}
+    monthly_list = DownloadCount.objects.filter(chart_group='monthly').order_by('date')
+    for eachinlist in monthly_list:
+        if eachinlist.date.strftime('%b') not in monthly_count:
+            monthly_count [eachinlist.date.strftime('%b')] = {}
+        monthly_count[eachinlist.date.strftime('%b')][eachinlist.download_type] = eachinlist.count
+    pprint(monthly_count)
 
     context_dict = {
-        "layer_count": layer_count,
+        "monthly_count": monthly_count,
     }
     return render_to_response(template, RequestContext(request, context_dict))
