26bb82a06cce773fb6dfaa426f26f718039873c2
geonode-prod-user <noaddress@dream.upd.edu.ph>
2016-11-28 19:32:17 +0800

    hotfix: added missing search fields for eula and datarequests admin -Ken

--- geonode/datarequests/admin.py
+++ geonode/datarequests/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 
 from geonode.datarequests.models import DataRequestProfile, RequestRejectionReason
 
+class DataRequestProfileAdmin(admin.ModelAdmin):
+    model = DataRequestProfile
+    list_display_links = ('id','username')
 
-admin.site.register(DataRequestProfile)
+    list_display = (
+        'id',
+        'request_status',
+        'username',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'organization',
+        'organization_type',)
+    list_filter = ('request_status', 'organization_type')
+    search_fields = ('username', 'first_name', 'middle_name', 'last_name', 'organization','organization_type', )
+
+
+admin.site.register(DataRequestProfile, DataRequestProfileAdmin)
 admin.site.register(RequestRejectionReason)
