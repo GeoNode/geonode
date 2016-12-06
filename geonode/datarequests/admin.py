f2f11d7d55991b83bede442c81e3df814dfea3d1
geonode-prod-user <noaddress@dream.upd.edu.ph>
2016-12-06 10:55:23 +0800

    admin search filters for datarequests and ftp requests

--- geonode/datarequests/admin.py
+++ geonode/datarequests/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 from geonode.datarequests.models import DataRequestProfile, RequestRejectionReason
 
 
-admin.site.register(DataRequestProfile)
+class DataRequestProfileAdmin(admin.ModelAdmin):
+    model = DataRequestProfile
+    list_display_links = ('id','username')
+
+    list_display = (
+        'id',
+        'request_status',
+        'username',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'organization',
+        'organization_type',)
+    list_filter = ('request_status', 'organization_type')
+    search_fields = ('username', 'first_name', 'middle_name', 'last_name', 'organization','organization_type', )
+
+
+admin.site.register(DataRequestProfile, DataRequestProfileAdmin)
 admin.site.register(RequestRejectionReason)
