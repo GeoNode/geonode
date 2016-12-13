1b46d81f7bd11cecdc61952e7818123c359c2ac1
afabiani <alessio.fabiani@geo-solutions.it>
2016-12-13 14:21:12 +0100

    - A&A initial data fixtures and pavements scripts

--- pavement.py
+++ pavement.py
@@ -224,6 +224,7 @@ def sync(options):
     except:
         pass
     sh("python manage.py loaddata sample_admin.json")
+    sh("python manage.py loaddata geonode/base/fixtures/default_oauth_apps.json")
     sh("python manage.py loaddata geonode/base/fixtures/initial_data.json")
 
 
