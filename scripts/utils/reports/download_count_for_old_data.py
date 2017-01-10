26c52ced1f6e385241c8e2460dfb1f3d59438de4
Mapavia <mapavia@dream.upd.edu.ph>
2017-01-10 17:10:56 +0800

    added daily download count changed download_count_for_old_data.py into daily

--- scripts/utils/reports/download_count_for_old_data.py
+++ scripts/utils/reports/download_count_for_old_data.py
@@ -65,14 +65,13 @@ def add_to_monthlyc(category):
         }
     layer_count[category]['Document'] += 1
 
-for n in range(52):
+for m in range(365):
     layer_count = {}
-    m = 7*n - 3
-    datetoappend = datetime.strptime((datetime.now()-timedelta(days=m)).strftime('%U-%Y')+'-3','%U-%Y-%w') #timedelta to start week count days from sunday; days=3 meaning week count if from wednesday to tuesday
+    datetoappend = datetime.strptime((datetime.now()-timedelta(days=m)).strftime('%d-%m-%Y'),'%d-%m-%Y') #timedelta to start week count days from sunday; days=3 meaning week count if from wednesday to tuesday
     # auth_list = Action.objects.filter(verb='downloaded').order_by('timestamp')
     auth_list = DownloadTracker.objects.order_by('timestamp')
     for auth in auth_list:
-        if datetoappend == datetime.strptime(auth.timestamp.strftime('%U-%Y')+'-3','%U-%Y-%w') and not auth.resource_type == 'document':#if datenow is timestamp
+        if datetoappend == datetime.strptime(auth.timestamp.strftime('%d-%m-%Y'),'%d-%m-%Y') and not auth.resource_type == 'document':#if datenow is timestamp
             luzvimin = get_luzvimin({
                 "timestamp": auth.timestamp,
                 "typename": auth.title,
@@ -80,7 +79,7 @@ for n in range(52):
             add_to_count(luzvimin, auth.title)
     anon_list = AnonDownloader.objects.all().order_by('date')
     for anon in anon_list:
-        if datetoappend == datetime.strptime(anon.date.strftime('%U-%Y')+'-3','%U-%Y-%w') and not anon.anon_document:#if datenow is timestamp
+        if datetoappend == datetime.strptime(anon.date.strftime('%d-%m-%Y'),'%d-%m-%Y') and not anon.anon_document:#if datenow is timestamp
             luzvimin = get_luzvimin({
                 "timestamp": anon.date,
                 "typename": anon.anon_layer.typename,
