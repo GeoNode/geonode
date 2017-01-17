5430c09f3a18f4ed1f59ec3afceb8f554d607806
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-01-17 15:06:01 +0100

    Fix conflict account migrations by merging

---
+++ geonode/people/migrations/0004_auto_20170117_0800.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('people', '0003_auto_20160824_0245'),
+    ]
+
+    operations = [
+        migrations.AlterModelManagers(
+            name='profile',
+            managers=[
+            ],
+        ),
+    ]
