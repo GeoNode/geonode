91ffbcce714e85fa96605a5cb924681b3e5808ff
Ariel Nuñez <ingenieroariel@gmail.com>
2017-01-19 11:42:47 -0500

    Fix uploads migration

--- geonode/upload/migrations/26_initial.py
+++ geonode/upload/migrations/26_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('layers', '26_layers'),
+        ('layers', '__first__'),
     ]
 
     operations = [
