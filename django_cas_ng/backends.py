832f63f7d302cc467a97d1a4f5b043231ed696a1
Carlo Santos <casantos@dream.upd.edu.ph>
2017-01-24 12:36:50 +0800

    added missing import

--- django_cas_ng/backends.py
+++ django_cas_ng/backends.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.conf import settings
+from django.dispatch import receiver
 
 from django_cas_ng.signals import cas_user_authenticated
 from .utils import get_cas_client
