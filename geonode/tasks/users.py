a1853b94a58a0ec829c1f605ed99629743997bcb
Carlo Santos <casantos@dream.upd.edu.ph>
2017-02-02 17:40:22 +0800

    added logging

--- geonode/tasks/users.py
+++ geonode/tasks/users.py
@@ -5,12 +5,15 @@ from celery.task import task
 
 
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import Group
 
 from geonode.groups.models import GroupProfile, GroupMember
 
 @task(name="geonode.tasks.requests.users", queue="users")
+
 def join_user_to_groups(user, group_list):
     for g in group_list:
+        pprint("Group name:"+str(g))
         group, created = GroupProfile.objects.get_or_create(
             title=str(g)
         )
