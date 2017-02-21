29af42306f1122bdd0f3eb9b035aa2a22fd1a24c
Carlo Santos <casantos@dream.upd.edu.ph>
2017-02-21 14:16:16 +0800

    initial content for admin_edit_forms

---
+++ geonode/datarequests/admin_edit_forms.py
@@ -0,0 +1,82 @@
+import os
+import traceback
+
+from django import forms
+from django.forms import widgets
+from django.utils.translation import ugettext_lazy as _
+
+from captcha.fields import ReCaptchaField
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, HTML, Div, Column, Row, Field
+from crispy_forms.bootstrap import PrependedText, InlineRadios
+from model_utils import Choices
+
+from geonode.datarequests.models import DataRequestProfile, DataRequest, ProfileRequest
+from geonode.layers.forms import NewLayerUploadForm, LayerUploadForm, JSONField
+from geonode.documents.models import Document
+from geonode.documents.forms import DocumentCreateForm
+from geonode.people.models import OrganizationType, Profile
+
+from .models import DataRequest, ProfileRequest, LipadOrgType
+
+class ProfileRequestEditForm(forms.ModelForm):
+    
+    ORG_TYPE_CHOICES = LipadOrgType.objects.values_list('val', 'val')
+    # Choices that will be used for fields
+    LOCATION_CHOICES = Choices(
+        ('local', _('Local')),
+        ('foreign', _('Foreign')),
+    )
+    
+    REQUEST_LEVEL_CHOICES = Choices(
+        ('institution', _('Academic/ Research Institution')),
+        ('faculty', _('Faculty')),
+        ('student', _('Student')),
+    )
+    
+    class Meta:
+        model = ProfileRequest
+        
+        fields = (
+            'first_name',
+            'middle_name',
+            'last_name',
+            'organization',
+            # Non-commercial requester field
+            'organization_other',
+            # Academe requester fields
+            'request_level',
+            'funding_source',
+            'is_consultant',
+
+            'location',
+            'email',
+            'contact_number',
+        )
+        
+    org_type = forms.ChoiceField(
+        label = _('Organization Type'),
+        choices = ORG_TYPE_CHOICES,
+        initial = "Other",
+        required = True
+    )
+    
+    def __init__(self, *args, **kwargs):
+        super(ProfileRequestEditForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+         self.helper.layout = Layout(
+            Fieldset('User Information',
+                Div(
+                    Field('first_name', css_class='form-control'),
+                    css_class='form-group'
+                ),
+                Div(
+                    Field('middle_name', css_class='form-control'),
+                    css_class='form-group'
+                ),
+                Div(
+                    Field('last_name', css_class='form-control'),
+                    css_class='form-group'
+                ),
+            )
+        )
