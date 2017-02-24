37a442885aefc31fb89ca060b0fb1e868a4cc9c1
Ezequiel Gonzalez Rial <gonrial@gmail.com>
2017-02-23 23:33:57 -0300

    Change Geoexplorer aboutUrl
    When accessing GeoExplorer info popup the about section shows a 404
    Needed to change default behavior to adopt geonode's location
    
    Solves https://github.com/GeoNode/geonode/issues/2892

--- geonode/utils.py
+++ geonode/utils.py
@@ -558,6 +558,7 @@ class GXPMapBase(object):
                 'title':    self.title,
                 'abstract': self.abstract
             },
+            'aboutUrl': '../about',        
             'defaultSourceType': "gxp_wmscsource",
             'sources': sources,
             'map': {
