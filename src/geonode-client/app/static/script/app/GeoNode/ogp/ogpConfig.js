3d72c3a6e72e1fcf8b8d3cd04f127f32eeb8c9fc
capooti <pcorti@gmail.com>
2017-02-27 15:53:32 -0500

    Add layer_username in query to solr

--- src/geonode-client/app/static/script/app/GeoNode/ogp/ogpConfig.js
+++ src/geonode-client/app/static/script/app/GeoNode/ogp/ogpConfig.js
@@ -6,7 +6,7 @@ Ext.onReady(function(){
     intx: "product(max(0,sub(min(180,max_x),max(-180,min_x))),max(0,sub(min(90,max_y),max(-90,min_y))))",
     sort: "score desc",
     //qf: "LayerTitleSynonyms^0.2 ThemeKeywordsSynonymsIso^0.1 ThemeKeywordsSynonymsLcsh^0.1 PlaceKeywordsSynonyms^0.1 Publisher^0.1 layer_originator^0.1 Abstract^0.2",
-    qf: "title_txt^1 abstract_txt^0.2 originator_txt",
+    qf: "title_txt^1 abstract_txt^0.2 originator_txt layer_username",
     wt: "json",
     defType: "edismax",
     q: "*",
