f907aed6e4a0c6ae3910d15a98331f18fc898082
Carlo Santos <moonseraph128@gmail.com>
2017-02-28 13:46:00 +0800

    added functions and urls for editable data request

--- geonode/datarequests/urls.py
+++ geonode/datarequests/urls.py
@@ -44,6 +44,7 @@ urlpatterns = patterns(
     url(r'^data/reverse_geocode/$','data_request_reverse_geocode_all', name='data_request_reverse_geocode_all'),
     
     url(r'^data/(?P<pk>\d+)/$', 'data_request_detail', name="data_request_detail"),
+    url(r'^data/(?P<pk>\d+)/edit/$', 'data_request_edit', name="data_request_edit"),
     url(r'^data/(?P<pk>\d+)/approve/$', 'data_request_approve', name="data_request_approve"),
     url(r'^data/(?P<pk>\d+)/reject/$', 'data_request_reject', name="data_request_reject"),
     url(r'^data/(?P<pk>\d+)/cancel/$', 'data_request_cancel', name="data_request_cancel"),
