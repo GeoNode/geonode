1bd64ec49473f58ada11387145aa1bd8ed063db9
Carlo Santos <moonseraph128@gmail.com>
2017-03-03 17:05:28 +0800

    added logging

--- geonode/datarequests/views/registration.py
+++ geonode/datarequests/views/registration.py
@@ -117,9 +117,9 @@ def data_request_view(request):
     if request.method == 'POST' :
         pprint("detected data request post")
         post_data = request.POST.copy()
+        pprint(post_data)
         post_data['permissions'] = '{"users":{"dataRegistrationUploader": ["view_resourcebase"] }}'
         details_form = DataRequestForm(post_data, request.FILES)
-        pprint(post_data)
         data_request_obj = None
         
         errormsgs = []
