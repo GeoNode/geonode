d3554646925a4a699719b576da63c7b5464a9c07
Carlo Santos <moonseraph128@gmail.com>
2017-03-03 17:28:11 +0800

    fixing data_class_requested

--- geonode/datarequests/views/registration.py
+++ geonode/datarequests/views/registration.py
@@ -120,7 +120,7 @@ def data_request_view(request):
         pprint(post_data)
         post_data['permissions'] = '{"users":{"dataRegistrationUploader": ["view_resourcebase"] }}'
         data_classes = []
-        for s in post_data.split(','):
+        for s in post_data.split['data_class_requested'](','):
             data_classes.append(s)
             
         if len(data_classes) > 0:
