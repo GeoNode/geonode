8fc3e0a9c70a1873e820d4a4430cf7aa1becfeae
Ariel Nuñez <ingenieroariel@gmail.com>
2017-03-09 14:10:50 -0500

    pep8 fixes

--- geonode/messaging/queues.py
+++ geonode/messaging/queues.py
@@ -4,7 +4,7 @@ geonode_exchange = Exchange("geonode", type="topic", durable=False)
 
 queue_all_events = Queue("broadcast", geonode_exchange, routing_key="#")
 queue_email_events = Queue("email.events", geonode_exchange, routing_key="email")
-queue_geoserver =  Queue("all.geoserver", geonode_exchange, routing_key="geoserver.#")
+queue_geoserver = Queue("all.geoserver", geonode_exchange, routing_key="geoserver.#")
 queue_geoserver_catalog = Queue("geoserver.catalog", geonode_exchange, routing_key="geoserver.catalog")
 queue_geoserver_data = Queue("geoserver.data", geonode_exchange, routing_key="geoserver.catalog")
 queue_geoserver_events = Queue("geoserver.events", geonode_exchange, routing_key="geonode.geoserver")
