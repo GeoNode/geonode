df328c947a2ec9478651efb94f50b1597dd710ee
geonode-prod-user <noaddress@dream.upd.edu.ph>
2017-03-21 10:54:11 +0800

    layers/views.py download_csv changed to org_type

--- geonode/layers/views.py
+++ geonode/layers/views.py
@@ -707,7 +707,7 @@ def layer_download_csv(request):
         lastname = unidecode(getprofile.last_name)
         email = getprofile.email
         organization = unidecode(getprofile.organization) if getprofile.organization is not None else getprofile.organization
-        orgtype = orgtypelist[getprofile.organization_type]
+        orgtype = getprofile.org_type
         #area = get_area_coverage(auth.action_object.typename)
         area = 0
         # pprint(dir(getprofile))
