fc4d02421d35d23e96b76bc8f3f4e1f689afe782
geonode-prod-user <noaddress@dream.upd.edu.ph>
2017-03-23 11:06:41 +0800

    geonode/api/views.py changed local_url to https, build_absolute_uri gives http
    Conflicts:
    	geonode/api/views.py

--- geonode/api/views.py
+++ geonode/api/views.py
@@ -60,7 +60,8 @@ def api_combined(request, apiname):
 def api_autocomplete(request):
     current_url = request.build_absolute_uri()
     apiquery = '?'.join(current_url.split('?')[1:])
-    local_url = urljoin(current_url,'../')
+    #local_url = urljoin(current_url,'../')
+    local_url = 'https://'+request.META['HTTP_HOST']+'/'
     urls_to_visit = [local_url,'https://lipad-fmc.dream.upd.edu.ph/']
     output = ''
 
