ea707b48ffd006d9b179526550a7dab27b17df71
Jonathan Doig <z8290362@ad.unsw.edu.au>
2017-05-16 18:03:14 +1000

    Add CityData User Guide

--- geonode/urls.py
+++ geonode/urls.py
@@ -54,6 +54,7 @@ urlpatterns = patterns('',
                        # Static pages
                        url(r'^/?$', TemplateView.as_view(template_name='site_index.html'), name='home'),
                        url(r'^help/$', TemplateView.as_view(template_name='help.html'), name='help'),
+                       url(r'^user_guide/$', TemplateView.as_view(template_name='user_guide.html'), name='help'),
                        url(r'^developer/$', TemplateView.as_view(template_name='developer.html'), name='developer'),
                        url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
