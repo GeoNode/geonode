2fef2a7a719c854d0a3b4787cfd39f560d07203c
Julien Acroute <julien.acroute@camptocamp.com>
2017-05-18 18:08:47 +0200

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,57 @@
-FROM geonode/django
-MAINTAINER GeoNode development team
+FROM python:2.7.9
 
-COPY requirements.txt /usr/src/app/
-RUN pip install -r requirements.txt
+# This section is borrowed from the official Django image but adds GDAL and others
+RUN apt-get update && apt-get install -y \
+        gcc \
+        gettext \
+        postgresql-client libpq-dev \
+        sqlite3 \
+        python-gdal python-psycopg2 \
+        python-imaging python-lxml \
+        python-dev libgdal-dev \
+        python-ldap \
+        libmemcached-dev libsasl2-dev zlib1g-dev \
+        python-pylibmc \
+    --no-install-recommends && rm -rf /var/lib/apt/lists/*
+
+# Install pg client
+ENV POSTGRES_VERSION 9.5
+RUN apt-get update \
+   && apt-get install -y wget \
+   && echo "deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main" > /etc/apt/sources.list.d/postgres.list \
+   && wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - \
+   && apt-get update \
+   && apt-get install -y postgresql-client-${POSTGRES_VERSION}
+
+RUN mkdir /docker-entrypoint.d
+COPY package/docker/docker-entrypoint.sh /docker-entrypoint.sh
+COPY package/docker/00-bootstrap-dbs.sh /docker-entrypoint.d/
+
+# To understand the next section (the need for requirements.txt and setup.py)
+# Please read: https://packaging.python.org/requirements/
+
+# Update pip
+RUN pip install --upgrade pip
+
+# python-gdal does not seem to work, let's install manually the version that is
+# compatible with the provided libgdal-dev
+RUN pip install GDAL==1.10 --global-option=build_ext --global-option="-I/usr/include/gdal"
+
+# Install geonode code and dependencies
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY . /usr/src/app/
+RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir --no-deps -e /usr/src/app/
+
+# Install geonode configuration in named volume
+VOLUME ["/mnt/geonode_config", "/mnt/geonode_data"]
+RUN mkdir -p /mnt/geonode_data/uploaded /mnt/geonode_data/static /mnt/geonode_config \
+  && cd /usr/src/app/geonode/ \
+  && mv local_settings.py.docker /mnt/geonode_config/ \
+  && ln -s /mnt/geonode_config/local_settings.py
 
 EXPOSE 8000
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
---
+++ package/docker/00-bootstrap-dbs.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# This script should receive super user credentials for database access in order to create database for geonode and
+# geoserver. So $DB_ADMIN_USER and $DB_ADMIN_PASSWORD should correspond to user that can create database.
+# This script will create following databases: $GEONODE_DB_NAME and $GEOSERVER_DB_NAME if they not exists.
+
+# Create .pgpass file based on env vars
+echo "$DB_HOST:$DB_PORT:*:$DB_ADMIN_USER:$DB_ADMIN_PASSWORD" > ~/.pgpass
+chmod 0600 ~/.pgpass
+
+# Configure postgres access
+export PGHOST=$DB_HOST
+export PGPORT=$DB_PORT
+export PGUSER=$DB_ADMIN_USER
+
+# Check if DB is available and wait for it if necessary
+until psql --no-password postgres -c "SELECT 1" > /dev/null 2>&1; do
+  echo "Waiting for postgres server..."
+  sleep 1
+done
+
+# Check if geonode database and user was already bootstraped
+# Database is considered bootstraped if it already exists
+if psql --no-password -lqt postgres | cut -d \| -f 1 | grep -qw $DB_GEONODE_NAME; then
+    echo "Geonode database exists, skipping bootstrap"
+else
+    echo "Bootstraping Geonode database..."
+    echo "--------------------------------"
+
+    echo -n "Creating new role $DB_GEONODE_USER..."
+    psql --no-password postgres -q -c "CREATE ROLE $DB_GEONODE_USER WITH LOGIN UNENCRYPTED PASSWORD '$DB_GEONODE_PASSWORD';"
+    echo "OK"
+
+    echo -n "Creating new database $DB_NAME..."
+    psql --no-password postgres -q -c "CREATE DATABASE $DB_GEONODE_NAME OWNER $DB_GEONODE_USER"
+    echo "OK"
+
+    echo "Launch genode DB bootstrap procedure..."
+    # python manage.py makemigrations
+    python manage.py migrate account --noinput
+    python manage.py migrate --noinput
+
+    # load sample data and admin account conditionally
+    if [ "$IGNORE_DATA" != "TRUE" ]; then
+        echo "Loading sample data and admin:admin account"
+        python manage.py loaddata sample_admin
+        python manage.py loaddata geonode/base/fixtures/default_oauth_apps_docker.json
+        python manage.py loaddata geonode/base/fixtures/initial_data.json
+    fi
+
+fi
+
+# Check if geoserver database exists
+if psql --no-password -lqt postgres | cut -d \| -f 1 | grep -qw $DB_GEOSERVER_NAME; then
+    echo "Geoserver database exists, skipping bootstrap"
+else
+    echo "Bootstraping Geoserver database..."
+    echo "----------------------------------"
+
+    echo -n "Creating new role $DB_GEOSERVER_USER..."
+    psql --no-password postgres -q -c "CREATE ROLE $DB_GEOSERVER_USER WITH LOGIN UNENCRYPTED PASSWORD '$DB_GEOSERVER_PASSWORD'"
+    echo "OK"
+
+    echo -n "Creating new database $DB_NAME..."
+    psql --no-password postgres -q -c "CREATE DATABASE $DB_GEOSERVER_NAME OWNER $DB_GEOSERVER_USER"
+    echo "OK"
+
+    echo -n "Adding postgis extension..."
+    psql --no-password $DB_GEOSERVER_NAME -q -c "CREATE EXTENSION postgis"
+    echo "OK"
+fi
