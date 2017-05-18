e04ae3c962bf4e293a0a6e7349bb8bb3354a8f25
Mapavia <mapavia@dream.upd.edu.ph>
2017-05-18 14:57:29 +0800

    eula/templates 	renamed eula_text.html to eula_text_dream.html 		added eula_text_phillidar1.html eula/templatetags/eula_extras.py	added show_eula_phillidar1, edited show_eula to show_eula_dream layers/views.py	added phillidar1keyword = "UPD" layers/templates/layers/layer_eula_nested_dialog.html	moved if statement for dream,pl1, and pl2 from layer_detail.html layers/templates/layers/layer_detail.html		moved if statement for dream,pl1 and pl2 to layer_eula_nested_dialog.html

--- geonode/eula/templatetags/eula_extras.py
+++ geonode/eula/templatetags/eula_extras.py
@@ -1,8 +1,12 @@
 from django import template
 register = template.Library()
 
-@register.inclusion_tag('eula_text.html')
-def show_eula(): # Only one argument.
+@register.inclusion_tag('eula_text_dream.html')
+def show_eula_dream(): # Only one argument.
+    """Shows the EULA HTML Element"""
+    return
+@register.inclusion_tag('eula_text_phillidar1.html')
+def show_eula_phillidar1(): # Only one argument.
     """Shows the EULA HTML Element"""
     return
 @register.inclusion_tag('eula_text_phillidar2.html')
@@ -20,7 +24,8 @@ def eula_nested_modal_dialog(next_modal):
     """Shows the EULA HTML Element"""
     return {'next_modal' : next_modal}
 
-register.filter('show_eula', show_eula)
+register.filter('show_eula_dream', show_eula_dream)
+register.filter('show_eula_phillidar1', show_eula_phillidar1)
 register.filter('show_eula_phillidar2', show_eula_phillidar2)
 register.filter('eula_modal_dialog', eula_modal_dialog)
 register.filter('eula_nested_modal_dialog', eula_nested_modal_dialog)
