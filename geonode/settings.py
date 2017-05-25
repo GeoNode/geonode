70cd6cbe04dcff1eef2d88618bdeba0c71289618
afabiani <alessio.fabiani@geo-solutions.it>
2017-05-25 13:45:07 +0200

    - sample tile layer background

--- geonode/settings.py
+++ geonode/settings.py
@@ -746,7 +746,19 @@ MAP_BASELAYERS = [{
     "visibility": False,
     "fixed": True,
     "group":"background"
-}, {
+},
+# {
+#     "source": {"ptype": "gxp_olsource"},
+#     "type": "OpenLayers.Layer.XYZ",
+#     "title": "TEST TILE",
+#     "args": ["TEST_TILE", "http://test_tiles/tiles/${z}/${x}/${y}.png"],
+#     "name": "background",
+#     "attribution": "&copy; TEST TILE",
+#     "visibility": False,
+#     "fixed": True,
+#     "group":"background"
+# },
+{
     "source": {"ptype": "gxp_osmsource"},
     "type": "OpenLayers.Layer.OSM",
     "name": "mapnik",
