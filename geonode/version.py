6063349865388b878934976a39cdebb9f35fc5cc
Ami Rahav <arahav@boundlessgeo.com>
2017-06-14 17:32:37 -0400

    add short form github commit to version
    Only add commit if it has value

--- geonode/version.py
+++ geonode/version.py
@@ -49,7 +49,27 @@ def get_version(version=None):
         mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
         sub = mapping[version[3]] + str(version[4])
 
-    return main + sub
+    commit = get_git_commit()
+    if commit:
+        return "{}{}.{}".format(main, sub, commit)
+    else:
+        return "{}{}".format(main, sub)
+
+
+def get_git_commit():
+    repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    _commit = subprocess.Popen(
+        'git rev-parse --short HEAD',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+        cwd=repo_dir,
+        universal_newlines=True
+    )
+
+    commit = _commit.communicate()[0].partition('\n')[0]
+    return commit
 
 
 def get_git_changeset():
