64593048a3724fbf24f4ec3401007038ee11d268
Goran Mekić <meka@tilda.center>
2017-06-20 09:42:10 +0200

    Unify static file paths

--- geonode/contrib/monitoring/frontend/webpack.common.js
+++ geonode/contrib/monitoring/frontend/webpack.common.js
@@ -7,9 +7,9 @@ const localIdentName = '[path]___[name]__[local]___[hash:base64:5]';
 module.exports = {
   entry: ['./src/index'],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
-    publicPath: '/static/',
+    publicPath: 'static/',
   },
   resolveLoader: {
     fallback: path.join(__dirname, 'node_modules'),
