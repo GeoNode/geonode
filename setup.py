2930f590ed2989ceff008ed7fef6b2a92c652c14
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-06-28 18:04:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(name='GeoNode',
 
         # GeoNode org maintained apps.
         "django-geoexplorer>=4.0.8",
-        "geonode-user-messages>=0.1.4",  # (0.1.3 in ppa) FIXME
+        "geonode-user-messages==0.1.6",  # (0.1.3 in ppa) FIXME
         "geonode-avatar>=2.1.6",  # (2.1.5 in ppa) FIXME
         "geonode-announcements>=1.0.8",
         "geonode-agon-ratings>=0.3.5",  # (0.3.1 in ppa) FIXME
