1fd134d3e0c61927d72050c68f4460453473d884
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2017-08-02 14:48:13 +0300

    Fixed flake8 issues

--- geonode/base/management/commands/set_all_layers_public.py
+++ geonode/base/management/commands/set_all_layers_public.py
@@ -19,8 +19,6 @@
 #########################################################################
 
 from django.core.management.base import BaseCommand
-from guardian.shortcuts import get_objects_for_user
-from geonode.people.models import Profile
 from geonode.layers.models import Layer
 from geonode.security.models import set_geofence_all
 
@@ -30,11 +28,7 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        profiles = Profile.objects.filter(is_superuser=False)
-        authorized_layers = list(get_objects_for_user(profiles[0], 'base.view_resourcebase').values('id'))
         all_layers = Layer.objects.all()
-        unprotected_layers = Layer.objects.filter(id__in=[d['id'] for d in authorized_layers])
-        protected_layers = Layer.objects.all().exclude(id__in=[d['id'] for d in authorized_layers])
 
         for index, layer in enumerate(all_layers):
             print "[%s / %s] Setting public permissions to Layer [%s] ..." % ((index + 1), len(all_layers), layer.name)
@@ -42,9 +36,13 @@ class Command(BaseCommand):
                 set_geofence_all(layer)
                 layer.set_default_permissions()
                 perm_spec = {"users": {}, "groups": {}}
-                perm_spec["users"]["admin"] = ['view_resourcebase','change_resourcebase_permissions','download_resourcebase','publish_resourcebase','change_resourcebase_metadata']
-                perm_spec["users"][str(layer.owner)] = ['view_resourcebase','change_resourcebase_permissions','download_resourcebase','publish_resourcebase','change_resourcebase_metadata']
-                perm_spec["users"]["AnonymousUser"] = ['view_resourcebase','download_resourcebase']
+                perm_spec["users"]["admin"] = ['view_resourcebase', 'change_resourcebase_permissions',
+                                               'download_resourcebase', 'publish_resourcebase',
+                                               'change_resourcebase_metadata']
+                perm_spec["users"][str(layer.owner)] = ['view_resourcebase', 'change_resourcebase_permissions',
+                                                        'download_resourcebase', 'publish_resourcebase',
+                                                        'change_resourcebase_metadata']
+                perm_spec["users"]["AnonymousUser"] = ['view_resourcebase', 'download_resourcebase']
                 layer.set_permissions(perm_spec)
             except:
                 print "[ERROR] Layer [%s] couldn't be updated" % (layer.name)
