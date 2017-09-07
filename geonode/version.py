99d82cfb338bc37e67d7dad2a0a29540d955e351
Simone Dalmasso <simone.dalmasso@gmail.com>
2017-09-07 15:49:45 +0200

    make sure get_version is happy with the unstable flag

--- geonode/version.py
+++ geonode/version.py
@@ -29,7 +29,7 @@ def get_version(version=None):
         from geonode import __version__ as version
     else:
         assert len(version) == 5
-        assert version[3] in ('alpha', 'beta', 'rc', 'final')
+        assert version[3] in ('unstable', 'beta', 'rc', 'final')
 
     # Now build the two parts of the version number:
     # main = X.Y[.Z]
@@ -40,13 +40,13 @@ def get_version(version=None):
     main = '.'.join(str(x) for x in version[:parts])
 
     sub = ''
-    if version[3] == 'alpha' and version[4] == 0:
+    if version[3] == 'unstable':
         git_changeset = get_git_changeset()
         if git_changeset:
             sub = '.dev%s' % git_changeset
 
     elif version[3] != 'final':
-        mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
+        mapping = {'beta': 'b', 'rc': 'c'}
         sub = mapping[version[3]] + str(version[4])
 
     return main + sub
