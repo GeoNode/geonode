a470d438f52750ad1a903e1152d5a5fa4189ccef
cjahangir <jahangir.cse09@gmail.com>
2017-10-09 00:42:38 +0600

    made some changes in settings.py file

--- geonode/settings.py
+++ geonode/settings.py
@@ -535,6 +535,13 @@ AGON_RATINGS_CATEGORY_CHOICES = {
 
 # Activity Stream
 ACTSTREAM_SETTINGS = {
+    'MODELS': (
+        'people.Profile',
+        'layers.layer',
+        'maps.map',
+        'dialogos.comment',
+        'documents.document',
+        'services.service'),
     'FETCH_RELATIONS': True,
     'USE_PREFETCH': False,
     'USE_JSONFIELD': True,
