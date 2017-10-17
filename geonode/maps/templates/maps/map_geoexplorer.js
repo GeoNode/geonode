fd7a69297b7c43696814281dc4820a969f13e0f1
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-10-17 02:05:23 +0200

    - Improve GeoExt base GIS viewer; adding 'wrapDateline' option and update OL2 version

--- geonode/maps/templates/maps/map_geoexplorer.js
+++ geonode/maps/templates/maps/map_geoexplorer.js
@@ -32,6 +32,49 @@ Ext.onReady(function() {
         csrfToken: "{{ csrf_token }}",
         tools: [{ptype: "gxp_getfeedfeatureinfo"}],
         listeners: {
+            "ready": function() {
+                app.mapPanel.map.getMaxExtent = function() {
+                    return new OpenLayers.Bounds(-80150033.36/2,-80150033.36/2,80150033.36/2,80150033.36/2);
+                }
+                app.mapPanel.map.getMaxResolution = function() {
+                    return 626172.135625/2;
+                }
+                l = app.selectedLayer.getLayer();
+                l.addOptions({wrapDateLine:true, displayOutsideMaxExtent: true});
+                l.addOptions({maxExtent:app.mapPanel.map.getMaxExtent(), restrictedExtent:app.mapPanel.map.getMaxExtent()});
+                for (var l in app.mapPanel.map.layers) {
+                    l = app.selectedLayer.getLayer();
+                    l.addOptions({wrapDateLine:true, displayOutsideMaxExtent: true});
+                    l.addOptions({maxExtent:app.mapPanel.map.getMaxExtent(), restrictedExtent:app.mapPanel.map.getMaxExtent()});
+                }
+
+                var map = app.mapPanel.map;
+                var layer = app.map.layers.slice(-1)[0];
+
+                // FIXME(Ariel): Zoom to extent until #1795 is fixed.
+                //map.zoomToExtent(layer.maxExtent, false)
+
+                var bbox = layer.bbox;
+                if (bbox != undefined)
+                {
+                   if (!Array.isArray(bbox) && Object.keys(layer.srs) in bbox) {
+                    bbox = bbox[Object.keys(layer.srs)].bbox;
+                   }
+
+                   var extent = OpenLayers.Bounds.fromArray(bbox);
+                   var zoomToData = function()
+                   {
+                       map.zoomToExtent(extent, false);
+                       app.mapPanel.center = map.center;
+                       app.mapPanel.zoom = map.zoom;
+                       map.events.unregister('changebaselayer', null, zoomToData);
+                   };
+                   map.events.register('changebaselayer',null,zoomToData);
+                   if(map.baseLayer){
+                    map.zoomToExtent(extent, false);
+                   }
+                }
+            },
            'save': function(obj_id) {
                createMapThumbnail(obj_id);
            }
