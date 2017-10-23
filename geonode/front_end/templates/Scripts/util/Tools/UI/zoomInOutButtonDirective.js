50ac5de0207a79f0cefde09b6a34d81e25139708
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2017-10-23 13:53:57 +0600

    refactor directive and factory

--- geonode/front_end/templates/Scripts/util/Tools/UI/zoomInOutButtonDirective.js
+++ geonode/front_end/templates/Scripts/util/Tools/UI/zoomInOutButtonDirective.js
@@ -1,16 +1,17 @@
-mapModule.directive('zoomInOutButtons', [
-    'mapTools',
-    function (mapTools) {
-        return {
-            restrict: 'EA',
-            scope: {},
-            templateUrl: '/static/Templates/Tools/Map/zoomInOutButtons.html',
-            controller: [
-                '$scope',
-                function ($scope) {
-                    angular.extend($scope, mapTools.zoomInOutTool);
-                }
-            ]
-        };
-    }
-]);
+mapModule
+    .directive('zoomInOutButtons', zoomInOutButtons);
+zoomInOutButtons.$inject = ['mapTools'];
+
+function zoomInOutButtons(mapTools) {
+    return {
+        restrict: 'EA',
+        scope: {},
+        templateUrl: '/static/Templates/Tools/Map/zoomInOutButtons.html',
+        controller: [
+            '$scope',
+            function($scope) {
+                angular.extend($scope, mapTools.zoomInOutTool);
+            }
+        ]
+    };
+}
