0f2b72b5f266657ee160a64d6b740ef9c3a8fa1d
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2017-10-23 13:53:57 +0600

    refactor directive and factory

--- geonode/front_end/templates/Scripts/util/Tools/Map/mapToolsFactory.js
+++ geonode/front_end/templates/Scripts/util/Tools/Map/mapToolsFactory.js
@@ -1,4 +1,5 @@
-﻿mapModule.factory('mapToolsFactory', [
+﻿mapModule.factory('mapToolsFactory', mapToolsFactory);
+mapToolsFactory.$inject = [
     'ol',
     'GeoLocationTool',
     'LocationSearchTool',
@@ -10,135 +11,136 @@
     'NavigationHistoryTool',
     'WmsMultiSelectFeatureTool',
     'ZoomInOutTool',
-    'ZoomToMaxExtentTool',
-    function (ol, GeoLocationTool, LocationSearchTool, ActiveLayerTool, ZoomToLayerTool, ZoomTrackerTool, BaseMapTool, AllSelectableLayerTool, NavigationHistoryTool, WmsMultiSelectFeatureTool, ZoomInOutTool, ZoomToMaxExtentTool) {
-        var _markerSource;
-        var _olMap, _olView;
-        var _gMap;
-        var _surfMap;
+    'ZoomToMaxExtentTool'
+];
 
-        function initialize(surfMap, olMap, gMap) {
-            _surfMap = surfMap;
-            _olMap = olMap;
-            _olView = _olMap.getView();
-            _gMap = gMap;
+function mapToolsFactory(ol, GeoLocationTool, LocationSearchTool, ActiveLayerTool, ZoomToLayerTool, ZoomTrackerTool, BaseMapTool, AllSelectableLayerTool, NavigationHistoryTool, WmsMultiSelectFeatureTool, ZoomInOutTool, ZoomToMaxExtentTool) {
+    var _markerSource;
+    var _olMap, _olView;
+    var _gMap;
+    var _surfMap;
+
+    function initialize(surfMap, olMap, gMap) {
+        _surfMap = surfMap;
+        _olMap = olMap;
+        _olView = _olMap.getView();
+        _gMap = gMap;
+    }
+
+    function getMarkerOverlay() {
+        if (!_markerSource) {
+
+            _markerSource = new ol.source.Vector({
+                features: new ol.Collection(),
+                useSpatialIndex: false,
+                updateWhileAnimating: true,
+                updateWhileInteracting: true
+            });
+
+            _olMap.topLayers.getLayers().push(new ol.layer.Vector({
+                source: _markerSource
+            }));
+
         }
 
-        function getMarkerOverlay() {
-            if (!_markerSource) {
+        return _markerSource;
+    }
 
-                _markerSource = new ol.source.Vector({
-                    features: new ol.Collection(),
-                    useSpatialIndex: false,
-                    updateWhileAnimating: true,
-                    updateWhileInteracting: true
-                });
+    function createGeoLocationTool() {
+        var _locationMarker = new ol.Feature();
+        var _accuracyFeature = new ol.Feature();
 
-                _olMap.topLayers.getLayers().push(new ol.layer.Vector({
-                    source: _markerSource
-                }));
+        _locationMarker.setStyle(new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 6,
+                fill: new ol.style.Fill({
+                    color: '#3399CC'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: '#FFF',
+                    width: 2
+                })
+            })
+        }));
 
+        getMarkerOverlay().addFeature(_locationMarker);
+        getMarkerOverlay().addFeature(_accuracyFeature);
+
+        var olGeolocation = new ol.Geolocation({
+            projection: _olMap.getView().getProjection(),
+            trackingOptions: {
+                enableHighAccuracy: false,
+                maximumAge: 600000,
+                timeout: 10000
             }
+        });
 
-            return _markerSource;
-        }
+        return new GeoLocationTool(_locationMarker, _accuracyFeature, _olView, olGeolocation);
+    };
 
-        function createGeoLocationTool() {
-            var _locationMarker = new ol.Feature();
-            var _accuracyFeature = new ol.Feature();
-
-            _locationMarker.setStyle(new ol.style.Style({
-                image: new ol.style.Circle({
-                    radius: 6,
-                    fill: new ol.style.Fill({
-                        color: '#3399CC'
-                    }),
-                    stroke: new ol.style.Stroke({
-                        color: '#FFF',
-                        width: 2
-                    })
+    function createLocationSearchTool() {
+        var _searchMarker = new ol.Feature();
+        _searchMarker.setStyle(
+            new ol.style.Style({
+                image: new ol.style.Icon({
+                    anchor: [0.5, 1],
+                    src: '/static/Content/assets/images/marker.png'
                 })
             }));
 
-            getMarkerOverlay().addFeature(_locationMarker);
-            getMarkerOverlay().addFeature(_accuracyFeature);
+        getMarkerOverlay().addFeature(_searchMarker);
 
-            var olGeolocation = new ol.Geolocation({
-                projection: _olMap.getView().getProjection(),
-                trackingOptions: {
-                    enableHighAccuracy: false,
-                    maximumAge: 600000,
-                    timeout: 10000
-                }
-            });
-
-            return new GeoLocationTool(_locationMarker, _accuracyFeature, _olView, olGeolocation);
-        };
-
-        function createLocationSearchTool() {
-            var _searchMarker = new ol.Feature();
-            _searchMarker.setStyle(
-                new ol.style.Style({
-                    image: new ol.style.Icon({
-                        anchor: [0.5, 1],
-                        src: '/static/Content/assets/images/marker.png'
-                    })
-                }));
-
-            getMarkerOverlay().addFeature(_searchMarker);
-
-            return new LocationSearchTool(_searchMarker, _olMap);
-        }
-
-        function createActiveLayerTool(interactionHandler) {
-            return new ActiveLayerTool(_surfMap, interactionHandler);
-        }
-
-        function createZoomToLayerTool() {
-            return new ZoomToLayerTool(_surfMap, _olMap);
-        }
-
-        function createZoomTrackerTool() {
-            return new ZoomTrackerTool(_olMap);
-        }
-
-        function createBaseMapTool() {
-            return new BaseMapTool(_olMap, _gMap);
-        }
-
-        function createAllSelectableLayerTool() {
-            return new AllSelectableLayerTool(_surfMap);
-        }
-
-        function createNavigationHistoryTool() {
-            return new NavigationHistoryTool(_olMap);
-        }
-
-        function createSelectFeatureTool() {
-            return new WmsMultiSelectFeatureTool(_olMap, _surfMap);
-        }
-
-        function createZoomInOutTool() {
-            return new ZoomInOutTool(_olView);
-        }
-
-        function createZoomToMaxExtentTool() {
-            return new ZoomToMaxExtentTool(_olView);
-        }
-
-        return {
-            initialize: initialize,
-            createGeoLocationTool: createGeoLocationTool,
-            createLocationSearchTool: createLocationSearchTool,
-            createActiveLayerTool: createActiveLayerTool,
-            createZoomToLayerTool: createZoomToLayerTool,
-            createZoomTrackerTool: createZoomTrackerTool,
-            createBaseMapTool: createBaseMapTool,
-            createAllSelectableLayerTool: createAllSelectableLayerTool,
-            createNavigationHistoryTool: createNavigationHistoryTool,
-            createSelectFeatureTool: createSelectFeatureTool,
-            createZoomInOutTool: createZoomInOutTool, 
-            createZoomToMaxExtentTool: createZoomToMaxExtentTool
-        };
+        return new LocationSearchTool(_searchMarker, _olMap);
     }
-]);
+
+    function createActiveLayerTool(interactionHandler) {
+        return new ActiveLayerTool(_surfMap, interactionHandler);
+    }
+
+    function createZoomToLayerTool() {
+        return new ZoomToLayerTool(_surfMap, _olMap);
+    }
+
+    function createZoomTrackerTool() {
+        return new ZoomTrackerTool(_olMap);
+    }
+
+    function createBaseMapTool() {
+        return new BaseMapTool(_olMap, _gMap);
+    }
+
+    function createAllSelectableLayerTool() {
+        return new AllSelectableLayerTool(_surfMap);
+    }
+
+    function createNavigationHistoryTool() {
+        return new NavigationHistoryTool(_olMap);
+    }
+
+    function createSelectFeatureTool() {
+        return new WmsMultiSelectFeatureTool(_olMap, _surfMap);
+    }
+
+    function createZoomInOutTool() {
+        return new ZoomInOutTool(_olView);
+    }
+
+    function createZoomToMaxExtentTool() {
+        return new ZoomToMaxExtentTool(_olView);
+    }
+
+    return {
+        initialize: initialize,
+        createGeoLocationTool: createGeoLocationTool,
+        createLocationSearchTool: createLocationSearchTool,
+        createActiveLayerTool: createActiveLayerTool,
+        createZoomToLayerTool: createZoomToLayerTool,
+        createZoomTrackerTool: createZoomTrackerTool,
+        createBaseMapTool: createBaseMapTool,
+        createAllSelectableLayerTool: createAllSelectableLayerTool,
+        createNavigationHistoryTool: createNavigationHistoryTool,
+        createSelectFeatureTool: createSelectFeatureTool,
+        createZoomInOutTool: createZoomInOutTool,
+        createZoomToMaxExtentTool: createZoomToMaxExtentTool
+    };
+}
--- geonode/front_end/templates/Scripts/util/Tools/Map/zoomInOutTool.js
+++ geonode/front_end/templates/Scripts/util/Tools/Map/zoomInOutTool.js
@@ -1,12 +1,14 @@
-mapModule.factory('ZoomInOutTool', [
-    function() {
-        return function ZoomInOutTool(olView) {
-            this.zoomIn = function() {
-                olView.setZoom(olView.getZoom() + 1);
-            };
-            this.zoomOut = function() {
-                olView.setZoom(olView.getZoom() - 1);
-            };
+mapModule
+    .factory('ZoomInOutTool', ZoomInOutTool);
+ZoomInOutTool.$inject = [];
+
+function ZoomInOutTool() {
+    return function ZoomInOutTool(olView) {
+        this.zoomIn = function() {
+            olView.setZoom(olView.getZoom() + 1);
         };
-    }
-]);
+        this.zoomOut = function() {
+            olView.setZoom(olView.getZoom() - 1);
+        };
+    };
+}
