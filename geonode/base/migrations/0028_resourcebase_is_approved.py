6169bc7498c447b17c4d7c22f4c95c729056727b
capooti <pcorti@gmail.com>
2017-10-31 14:27:24 -0400

    Added missing migration

---
+++ geonode/base/migrations/0028_resourcebase_is_approved.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0027_auto_20170801_1228'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='resourcebase',
+            name='is_approved',
+            field=models.BooleanField(default=False, help_text='Is this resource validated from a publisher or editor?', verbose_name='Approved'),
+        ),
+    ]
