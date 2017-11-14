8067a38c203b1c67f53dedb0d7f3dbcba1fdd758
shahjalal <shahjalal.tipu@gmail.com>
2017-11-14 11:51:24 +0600

    error reporting enabled

--- geonode/error_reporting/admin.py
+++ geonode/error_reporting/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from geonode.error_reporting.models import ErrorLoggingAndReporting
 
 # Register your models here.
-
-admin.site.register(ErrorLoggingAndReporting)
--- geonode/error_reporting/views.py
+++ geonode/error_reporting/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import ErrorLoggingAndReporting
 from django_db_logger.models import StatusLog
+from django.utils.html import format_html
 
 
 # Create your views here.
@@ -9,11 +8,63 @@ class ErrorReportingListView(ListView):
 
     template_name = 'error_reporting/error_reporting.html'
     model = StatusLog
-    paginate_by = 2
+    paginate_by = 3
 
-    """
     def get_context_data(self, **kwargs):
         context = super(ErrorReportingListView, self).get_context_data(**kwargs)
-        # import pdb;pdb.set_trace()
+
+        """
+        CRITICAL = 50
+        FATAL = CRITICAL
+        ERROR = 40
+        WARNING = 30
+        WARN = WARNING
+        INFO = 20
+        DEBUG = 10
+        NOTSET = 0
+        """
+
+        error_object = {
+            50: format_html(
+                "<span style='color:{color}; font-weight:{weight}'>{msg}</span>",
+                color='red',
+                weight='bold',
+                msg='CRITICAL'
+            ),
+            40: format_html(
+                "<span style='color:{color}; font-weight:{weight}'>{msg}</span>",
+                color='red',
+                weight='bold',
+                msg='ERROR'
+            ),
+            30: format_html(
+                "<span style='color:{color}; font-weight:{weight}'>{msg}</span>",
+                color='orange',
+                weight='bold',
+                msg='WARNING'
+            ),
+            20: format_html(
+                "<span style='color:{color}; font-weight:{weight}'>{msg}</span>",
+                color='green',
+                weight='bold',
+                msg='INFO'
+            ),
+            10: format_html(
+                "<span style='color:{color}; font-weight:{weight}'>{msg}</span>",
+                color='orange',
+                weight='bold',
+                msg='DEBUG'
+            ),
+            0: format_html(
+                "<span style='color:{color}; font-weight:{weight}'>{msg}</span>",
+                color='green',
+                weight='bold',
+                msg='NOTSET'
+            ),
+        }
+
+        if context['object_list']:
+            for error in context['object_list']:
+                error.level = error_object[int(error.level)]
+
         return context
-    """
