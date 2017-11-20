7181e05b393871da54b8aa3a3120f1bf8304b1fb
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2017-11-20 17:21:56 +0600

    restructre files

--- geonode/front_end/views.py
+++ geonode/front_end/views.py
@@ -5,11 +5,11 @@ try:
 except ImportError:
     from django.utils import simplejson as json
 
-from .class_factory import ClassFactory
 from django.conf import settings
 from django.core.files import File
 from django.http import HttpResponse
 from django.views.generic import View
+from geonode.class_factory import ClassFactory
 from geonode.layers.models import Layer
 from geonode.layers.views import _resolve_layer
 
@@ -40,14 +40,20 @@ class LayerAttributeUploadView(View):
         csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
         headers = csv_reader.next()
         headers = [h.lower().replace(' ', '_') for h in headers]
-
+        success_count, failed_count = 0, 0
         for row in csv_reader:
             row = dict(zip(headers, row))
             try:
                 obj = model_instance(**row)
                 obj.save()
+                success_count += 1
             except model_instance.DoesNotExist as ex:
                 db_logger.exception(ex)
+                failed_count += 1
         
-        return HttpResponse({'success': True})
+        return HttpResponse(
+                    json.dumps(
+                        dict(success=success_count, failed=failed_count),
+                        ensure_ascii=False), 
+                    content_type='application/javascript')
     
