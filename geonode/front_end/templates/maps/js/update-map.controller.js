5fb6a62bba60a55a7e8cdfc3468b8d8d184fffaa
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2017-11-23 16:25:13 +0600

    add map name and fix map visibility problem

--- geonode/front_end/templates/maps/js/update-map.controller.js
+++ geonode/front_end/templates/maps/js/update-map.controller.js
@@ -9,6 +9,7 @@
         var map = mapService.getMap();
         self.MapConfig = $window.mapConfig;
         console.log(self.MapConfig);
+        mapService.setMapName(self.MapConfig.about.title);
         self.MapConfig.map.layers.forEach(function(layer) {
             console.log(layer);
             var url = self.MapConfig.sources[layer.source].url;
@@ -23,16 +24,24 @@
                 //             serverType: 'geoserver'
                 //         })
                 //     }))
-                    // mapService.addVectorLayer(vector);
+                // mapService.addVectorLayer(vector);
             }
         });
 
-        function _map(layer, order){
-            if(!layer.bbox){
-                layer.bbox =  [-9818543.41779904,  5183814.6260749, -9770487.95134629,  5235883.07751104];                
+        function _uuid() {
+            function _() {
+                return Math.ceil(1 + Math.random() * 1e5).toString(16).substring(1);
+            }
+            return _() + _() + '-' + _() + '-' + _() + '-' + _() + '-' + _() + _();
+
+        }
+
+        function _map(layer, order) {
+            if (!layer.bbox) {
+                layer.bbox = [-9818543.41779904, 5183814.6260749, -9770487.95134629, 5235883.07751104];
             }
             return {
-                "LayerId": "s_31b086d815f5492a9da932bbf9af7333",
+                "LayerId": _uuid(),
                 "Name": layer.name,
                 "SortOrder": order || 0,
                 // "LastUpdateOn": "2017-10-10T11:10:26.083Z",
@@ -40,61 +49,61 @@
                 "CanWrite": true,
                 // "DataId": "s_facf34ee54914605943fe987f5b3637c",
                 "ShapeType": "point",
-                 "Style": { /* not available */
-                //     "Name": "s_a5170c56d2a64a0c8ee7dff53d916e71",
-                //     "default": {
-                //         "fillPattern": null,
-                //         "textFillColor": "#222026",
-                //         "text": null,
-                //         "pixelDensity": null,
-                //         "strokeDashstyle": "solid",
-                //         "strokeWidth": 1.0,
-                //         "strokeColor": "#5EF1F2",
-                //         "strokeOpacity": null,
-                //         "fillOpacity": 0.75,
-                //         "fillColor": "#2f7979",
-                //         "pointRadius": 14.0,
-                //         "graphicName": "circle",
-                //         "textGraphicName": null,
-                //         "externalGraphic": null
-                //     },
-                //     "select": {
-                //         "fillPattern": "",
-                //         "textFillColor": "#222026",
-                //         "text": null,
-                //         "pixelDensity": null,
-                //         "strokeDashstyle": "solid",
-                //         "strokeWidth": 1.0,
-                //         "strokeColor": "#0000ff",
-                //         "strokeOpacity": 1.0,
-                //         "fillOpacity": 0.4,
-                //         "fillColor": "#0000ff",
-                //         "pointRadius": 6.0,
-                //         "graphicName": "circle",
-                //         "textGraphicName": null,
-                //         "externalGraphic": null
-                //     },
-                     "labelConfig": {
-                //         "attribute": null,
-                         "visibilityZoomLevel": 0,
-                //         "font": "Times",
-                //         "fontStyle": "normal",
-                //         "fontWeight": "normal",
-                //         "color": "#000000",
-                //         "borderColor": "#ffffff",
-                //         "showBorder": true,
-                //         "size": 10.0,
-                //         "alignment": 1.0,
-                //         "offsetX": 0.0,
-                //         "offsetY": 0.0,
-                //         "rotation": 0.0,
-                //         "followLine": false,
-                //         "repeat": false,
-                //         "repeatInterval": 5.0,
-                //         "wrap": false,
-                //         "wrapPixel": 50.0
-                     }
-                 },
+                "Style": { /* not available */
+                    //     "Name": "s_a5170c56d2a64a0c8ee7dff53d916e71",
+                    //     "default": {
+                    //         "fillPattern": null,
+                    //         "textFillColor": "#222026",
+                    //         "text": null,
+                    //         "pixelDensity": null,
+                    //         "strokeDashstyle": "solid",
+                    //         "strokeWidth": 1.0,
+                    //         "strokeColor": "#5EF1F2",
+                    //         "strokeOpacity": null,
+                    //         "fillOpacity": 0.75,
+                    //         "fillColor": "#2f7979",
+                    //         "pointRadius": 14.0,
+                    //         "graphicName": "circle",
+                    //         "textGraphicName": null,
+                    //         "externalGraphic": null
+                    //     },
+                    //     "select": {
+                    //         "fillPattern": "",
+                    //         "textFillColor": "#222026",
+                    //         "text": null,
+                    //         "pixelDensity": null,
+                    //         "strokeDashstyle": "solid",
+                    //         "strokeWidth": 1.0,
+                    //         "strokeColor": "#0000ff",
+                    //         "strokeOpacity": 1.0,
+                    //         "fillOpacity": 0.4,
+                    //         "fillColor": "#0000ff",
+                    //         "pointRadius": 6.0,
+                    //         "graphicName": "circle",
+                    //         "textGraphicName": null,
+                    //         "externalGraphic": null
+                    //     },
+                    "labelConfig": {
+                        //         "attribute": null,
+                        "visibilityZoomLevel": 0,
+                        //         "font": "Times",
+                        //         "fontStyle": "normal",
+                        //         "fontWeight": "normal",
+                        //         "color": "#000000",
+                        //         "borderColor": "#ffffff",
+                        //         "showBorder": true,
+                        //         "size": 10.0,
+                        //         "alignment": 1.0,
+                        //         "offsetX": 0.0,
+                        //         "offsetY": 0.0,
+                        //         "rotation": 0.0,
+                        //         "followLine": false,
+                        //         "repeat": false,
+                        //         "repeatInterval": 5.0,
+                        //         "wrap": false,
+                        //         "wrapPixel": 50.0
+                    }
+                },
                 "VisualizationSettings": null,
                 "IsVisible": layer.visibility,
                 "Filters": [],
