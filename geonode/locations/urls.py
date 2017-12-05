e66edbbcf03f0d5ce15ad7c137f17b05ac94f762
shahjalalh <shahjalal.tipu@gmail.com>
2017-12-05 18:07:26 +0600

    locations search

--- geonode/locations/urls.py
+++ geonode/locations/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 
-from .views import LocationView
+from .views import LocationView, LayersAttributesAPIView
 
 urlpatterns = [
+
+    url(r'^api/layers/attributes/', LayersAttributesAPIView.as_view()),
 
     url(r'^', LocationView.as_view(), name='locations'),
 
--- geonode/locations/views.py
+++ geonode/locations/views.py
@@ -3,18 +3,53 @@ from django.views.generic.base import TemplateView
 from geonode.layers.models import Layer
 from geonode.layers.views import _resolve_layer
 from geonode.class_factory import ClassFactory
+from django.db import connection
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework import status
+import json
 
 
 class LocationView(TemplateView):
 
     template_name = "locations/locations.html"
 
-    def get_context_data(self, **kwargs):
-        context = super(LocationView, self).get_context_data(**kwargs)
+
+class LayersAttributesAPIView(APIView):
+
+    # permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
         # context['latest_articles'] = Article.objects.all()[:5]
 
-        layers = Layer.objects.all()
+        layers = Layer.objects.values('name')
 
-        context['layers'] = layers
+        layers_attributes = dict()
 
-        return context
+        for layer_name in layers:
+            cursor = connection.cursor()
+            try:
+
+                cursor.execute("select column_name from INFORMATION_SCHEMA.COLUMNS where table_name =  %s",
+                               [layer_name['name']])
+                result = cursor.fetchall()
+                result = [str(r[0]) for r in result]
+                layers_attributes[str(layer_name['name'])] = result
+
+            finally:
+                cursor.close()
+
+        # context['layers'] = layers
+        # context['layers_attributes'] = layers_attributes
+
+        # import pdb;pdb.set_trace()
+        return JsonResponse(layers_attributes)
+
+
+class LayersAttributesSearchAPIView(APIView):
+
+    # permission_classes = (IsAuthenticated,)
+
+    def post(self, request, format=None):
+        pass
