b5ccba41e1966a9683df1150ec5d045dee1621ec
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-12-06 17:30:01 +0100

    Add configuration files for pytest

---
+++ geonode/tests/bdd/e2e/conftest.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#########################################################################
+#
+# Copyright (C) 2016 OSGeo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#########################################################################
+
+import os
+
+import pytest
+
+from .fixtures.auth import *
+from .fixtures.browser import *
+
+
+@pytest.fixture(scope='session')
+def pytestbdd_selenium_speed():
+    return 0.5
+
+
+@pytest.fixture
+def pytestbdd_feature_base_dir():
+    """Feature files base directory."""
+    return os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            '..',
+            'features',
+        )
+    )
