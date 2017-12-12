d7273b3fd106e9b4aeb89479b7767d9818a6a701
Sharif <design.can.talk@gmail.com>
2017-12-12 04:56:58 -0800

    icon fix

--- geonode/local_settings.py
+++ geonode/local_settings.py
@@ -22,7 +22,7 @@ import os
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-SITEURL = "http://103.48.16.135:8000/"
+SITEURL = "http://localhost:8000/"
 
 REMOVE_ANONYMOUS_USER = False
 
@@ -31,16 +31,18 @@ DATABASES = {
     'default': {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
          'NAME': 'geodash_dev',
-         'USER': 'geodash_dev',
-         'PASSWORD': 'geodash_dev',
+         'USER': 'postgres',
+         'PASSWORD': '12345',
+	 'HOST' : 'localhost',
+         'PORT' : '5432',
      },
     # vector datastore for uploads
     'datastore' : {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         #'ENGINE': '', # Empty ENGINE name disables
         'NAME': 'geodash_dev-imports',
-        'USER' : 'geodash_dev',
-        'PASSWORD' : 'geodash_dev',
+        'USER' : 'postgres',
+        'PASSWORD' : '12345',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
@@ -50,7 +52,7 @@ GEOSERVER_LOCATION = os.getenv(
     'GEOSERVER_LOCATION', 'http://localhost:8080/geoserver/'
 )
 GEOSERVER_PUBLIC_LOCATION = os.getenv(
-    'GEOSERVER_PUBLIC_LOCATION', 'http://103.48.16.135:8080/geoserver/'
+    'GEOSERVER_PUBLIC_LOCATION', 'http://localhost:8080/geoserver/'
 )
 
 # OGC (WMS/WFS/WCS) Server Settings
