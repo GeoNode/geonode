559478289aeeb5b9e447a145d3b4936561c49b6d
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-12-20 19:00:41 +0100

    Remove symbolic link which fails

--- scripts/misc/bdd_setup.sh
+++ scripts/misc/bdd_setup.sh
@@ -9,7 +9,7 @@ case $1 in
 		sudo apt-get install build-essential chrpath libssl-dev libxft-dev libfreetype6-dev libfreetype6 libfontconfig1-dev libfontconfig1 -y
 		sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 		sudo tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
-		sudo ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
+		# sudo ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
 		;;
 	"before_script")
 		echo "Setting up PyTest suite"
