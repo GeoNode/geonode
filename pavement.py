4df41556785e6db8e4258deb1086935eb5d0db91
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2017-12-21 19:52:18 +0100

    Remove commented function

--- pavement.py
+++ pavement.py
@@ -940,13 +940,3 @@ def justcopy(origin, target):
         if not os.path.exists(target):
             os.makedirs(target)
         shutil.copy(origin, target)
-
-#
-# def _copytree(src, dst, symlinks=False, ignore=None):
-#     for item in os.listdir(src):
-#         s = os.path.join(src, item)
-#         d = os.path.join(dst, item)
-#         if os.path.isdir(s):
-#             shutil.copytree(s, d, symlinks, ignore)
-#         else:
-#             shutil.copy2(s, d)
