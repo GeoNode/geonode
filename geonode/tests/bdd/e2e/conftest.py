64d4825b7a46339ea19f194c6ba2a8261800e4ce
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-01-08 17:03:09 +0100

    - Tentative fix bdd tests

--- geonode/tests/bdd/e2e/conftest.py
+++ geonode/tests/bdd/e2e/conftest.py
@@ -42,7 +42,14 @@ def en_browser(browser, bdd_server):
     en_browser = browser
     en_browser.visit(urljoin(bdd_server.url, gn_settings.LOGIN_URL))
     yield en_browser
-    en_browser.quit()
+    try:
+        en_browser.service.process.send_signal(signal.SIGTERM) # kill the specific phantomjs child proc
+    except:
+        pass
+    try:
+        en_browser.quit()                                      # quit the node proc
+    except:
+        pass
 
 
 @pytest.fixture
