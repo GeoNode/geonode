94cdce53603bfc83b9911ec7b1486fec0faf286c
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-01-09 11:01:55 +0100

    [Fwport] - 26 to 27+ migration issues  (#3549)
    *  - Fix GeoNode 2.6.3 to 2.7.x+ Migration
    
    *  - BDD tests fixes and headers update

--- geonode/base/management/commands/set_all_layers_alternate.py
+++ geonode/base/management/commands/set_all_layers_alternate.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #########################################################################
 #
-# Copyright (C) 2018 OSGeo
+# Copyright (C) 2017 OSGeo
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,3 +17,23 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
+
+from django.core.management.base import BaseCommand
+from geonode.layers.models import Layer
+
+
+class Command(BaseCommand):
+    """Resets Permissions to Public for All Layers
+    """
+
+    def handle(self, *args, **options):
+        all_layers = Layer.objects.all()
+
+        for index, layer in enumerate(all_layers):
+            print "[%s / %s] Checking 'alternate' of Layer [%s] ..." % ((index + 1), len(all_layers), layer.name)
+            try:
+                if not layer.alternate:
+                    layer.alternate = layer.typename
+                    layer.save()
+            except:
+                print "[ERROR] Layer [%s] couldn't be updated" % (layer.name)
