0692010207cc3b9eafb30aa57a9c4ab026415ef4
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-02-14 18:40:26 +0100

    [Fixes #3588] [Monitoring] Protect GeoServer REST endpoint and make requests pass through GeoNode Proxy

--- geonode/contrib/monitoring/service_handlers.py
+++ geonode/contrib/monitoring/service_handlers.py
@@ -126,7 +126,7 @@ class BaseServiceHandler(object):
             if last_check + self.service.check_interval > now:
                 log.warning("Next check too soon")
                 return
-        _collected = self._collect(since, until, **kwargs)
+        _collected = self._collect(since.astimezone(utc), until.astimezone(utc), **kwargs)
         return self.handle_collected(_collected)
 
     def _collect(self, since, until, *args, **kwargs):
