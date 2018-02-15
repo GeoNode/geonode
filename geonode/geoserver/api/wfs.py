3652834867b4ebed2a629312d0aa4a682159c3f6
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2018-02-15 19:14:30 +0600

    update geoserver wfs api to create layer with wkt in 4326 proj

--- geonode/geoserver/api/wfs.py
+++ geonode/geoserver/api/wfs.py
@@ -1,5 +1,6 @@
-import csv, StringIO, urllib
+import csv, StringIO, urllib, requests, json 
 from geonode.geoserver.mixins import GeoServerMixin
+from osgeo import ogr, osr
 from rest_framework import status
 from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.response import Response
@@ -29,14 +30,58 @@ class GeoserverCreateLayerByFeature(GeoServerMixin, CreateAPIView):
     """
     This api will serve create layer
     """
-    def create_csv(self, result):
-        from shapely.geometry import shape
 
+    def _get_param(self, geom_type, layer_title,organizationId, categoryId, permissions = None):      
+        param =  {
+                'permissions': json.dumps({} if permissions is None else permissions),
+                'charset': 'UTF-8',
+                'layer_title': layer_title,
+                'category': categoryId,
+                'organization': organizationId,
+                'layer_type': 'csv',
+            }
+        if geom_type == 'Point':
+            res = param.copy()
+            res.update({
+                'csv_layer_type': 'latlon',
+                'longitude': 'longitude',
+                'lattitude': 'latitude',
+            })
+            return res
+        else:
+            res = param.copy()
+            res.update({
+                'csv_layer_type': 'the_geom',
+                'the_geom': 'geom',
+            })
+            return res
+
+
+
+    def create_csv(self, result):
+        geom_type = None
         for feature in result.get('features'):
             if 'geometry' in feature and 'properties' in feature:
-                feature['properties'].update(dict(geom=shape(feature.get('geometry')).wkt))
-         
+                inSpatialRef = osr.SpatialReference()
+                inSpatialRef.ImportFromEPSG(3857)
+
+                outSpatialRef = osr.SpatialReference()
+                outSpatialRef.ImportFromEPSG(4326)
+                coordTransform = osr.CoordinateTransformation(inSpatialRef, outSpatialRef)
+                geometry = feature.get('geometry')
+                if geometry['type'] == 'Point':
+                    geom_type =  geometry['type']
+                    point =  ogr.CreateGeometryFromJson(json.dumps(geometry))
+                    point.Transform(coordTransform)
+                    feature['properties'].update(dict(longitude=point.GetX(), latitude=point.GetY()))
+                else:
+                    geom_type =  'Others'
+                    geom = ogr.CreateGeometryFromJson(json.dumps(geometry))
+                    geom.Transform(coordTransform)                    
+                    feature['properties'].update(dict(geom=geom.ExportToWkt()))
+
         mem_file = StringIO.StringIO()
+
         csv_writer = csv.writer(mem_file, quoting=csv.QUOTE_ALL)
 
         features = [r.get('properties') for r in result.get('features')]
@@ -44,27 +89,16 @@ class GeoserverCreateLayerByFeature(GeoServerMixin, CreateAPIView):
         values = [[v for k, v in f.items()] for f in features]
         csv_writer.writerows([keys] + values)
 
-        return mem_file
+        return mem_file, geom_type
 
-    def create_layer(self, csv, layer_title, organizationId, categoryId, host, scheme='http', cookies = None):
-        import requests, json   
-
+    def create_layer(self, csv, geom_type, layer_title, organizationId, categoryId, host, scheme='http', cookies = None):
         if not cookies:
             cookies = dict()
 
         files = {'base_file': ('base_file.csv', csv.getvalue(), 'application/octet-stream', {'Expires': '0'})}
 
-        data = {
-            'permissions': json.dumps({}),
-            'charset': 'UTF-8',
-            'layer_title': layer_title,
-            'category': categoryId,
-            'organization': organizationId,
-            'csv_layer_type': 'the_geom',
-            'the_geom': 'geom',
-            'layer_type': 'csv',
-        }
-
+        data = self._get_param(layer_title=layer_title, geom_type=geom_type, organizationId=organizationId,categoryId=categoryId)
+        # import pdb;pdb.set_trace()
         url = "{0}://{1}/layers/upload".format(scheme, host)
         client = requests.get("{0}://{1}".format(scheme, host))
 
@@ -97,9 +131,10 @@ class GeoserverCreateLayerByFeature(GeoServerMixin, CreateAPIView):
         query.update(dict(SERVICE='WFS'))
 
         result = self.get_response_from_geoserver('wfs', query)
-        mem_file = self.create_csv(result)
+        mem_file, geom_type = self.create_csv(result)
         
-        response = self.create_layer(csv=mem_file, 
+        response = self.create_layer(csv=mem_file,
+                                    geom_type=geom_type,
                                     layer_title=layer_title, 
                                     categoryId=categoryId,
                                     organizationId=organizationId,
