74787018f43ad11242991ddf409c057b09cb9cc3
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-02-26 21:49:37 +0100

    Add more variables to stdout

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,13 @@ set -e
 
 /usr/local/bin/invoke update >> /usr/src/app/invoke.log
 
-source /root/.override_env
+source $HOME/.override_env
 
 echo DATABASE_URL=$DATABASE_URL
 echo GEODATABASE_URL=$GEODATABASE_URL
+echo SITEURL=$SITEURL
+echo ALLOWED_HOSTS=$ALLOWED_HOSTS
+echo GEOSERVER_PUBLIC_LOCATION=$GEOSERVER_PUBLIC_LOCATION
 
 /usr/local/bin/invoke waitfordbs >> /usr/src/app/invoke.log
 
