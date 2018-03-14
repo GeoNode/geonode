49a7575eb0a2e9930a17d1be897ddff76c3db811
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2018-03-14 13:00:34 +0600

    update pagination

--- geonode/error_reporting/api/views.py
+++ geonode/error_reporting/api/views.py
@@ -2,10 +2,12 @@ from django_db_logger.models import StatusLog
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .serializers import LogsSerializer
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 
 class LogsListAPIView(ListAPIView):
     queryset = StatusLog.objects.all()
     permission_classes = (IsAuthenticated, IsAdminUser)
     serializer_class = LogsSerializer
+    pagination_class = LimitOffsetPagination
     
