16fd4fde1b02eb307181653ba2c16ac4056ed9da
Vasu Babu Kandimalla <vasuse7en@gmail.com>
2018-03-22 15:10:34 +0530

    remove duplicate logs. This will resolve kartoza/docker-geosafe#98

--- geonode/settings.py
+++ geonode/settings.py
@@ -420,8 +420,6 @@ LOGGING = {
             "handlers": ["console"], "level": "ERROR", },
         "geonode": {
             "handlers": ["console"], "level": "ERROR", },
-        "geonode.qgis_server": {
-            "handlers": ["console"], "level": "DEBUG", },
         "gsconfig.catalog": {
             "handlers": ["console"], "level": "ERROR", },
         "owslib": {
