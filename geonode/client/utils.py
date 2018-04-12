7b5f71ad48715522d309b85e4d10e70279794cc4
afabiani <alessio.fabiani@geo-solutions.it>
2018-04-12 14:25:44 +0200

    [Closes #3743] [GNIP] GeoNode UI customization from admin - add partners management

--- geonode/client/utils.py
+++ geonode/client/utils.py
@@ -147,6 +147,9 @@ def activate_theme(theme):
         theme_html = os.path.join(os.path.dirname(geonode_base_html[0]), "%s.html" % theme.theme_uuid)
         t = loader.get_template('admin/themes/custom_theme_html.txt')
 
+        custom_theme_base_header_template = find_all_templates(pattern="custom_theme_base_header.txt")
+        partners_header_template = find_all_templates(pattern="custom_theme_partners_header.txt")
+        partners_footer_template = find_all_templates(pattern="custom_theme_partners_footer.txt")
         contactus_header_template = find_all_templates(pattern="custom_theme_contactus_header.txt")
         contactus_footer_template = find_all_templates(pattern="custom_theme_contactus_footer.txt")
         footer_header_template = find_all_templates(pattern="custom_theme_footer_header.txt")
@@ -161,6 +164,9 @@ def activate_theme(theme):
             else:
                 return ''
 
+        custom_theme_base_header = _read_template(custom_theme_base_header_template)
+        partners_header = _read_template(partners_header_template)
+        partners_footer = _read_template(partners_footer_template)
         contactus_header = _read_template(contactus_header_template)
         contactus_footer = _read_template(contactus_footer_template)
         footer_header = _read_template(footer_header_template)
@@ -169,6 +175,9 @@ def activate_theme(theme):
         theme_html_content = t.render(
             {
                 'theme': theme,
+                'custom_base_header': custom_theme_base_header,
+                'partners_header': partners_header,
+                'partners_footer': partners_footer,
                 'contactus_header': contactus_header,
                 'contactus_footer': contactus_footer,
                 'footer_header': footer_header,
