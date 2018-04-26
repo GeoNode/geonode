5a8c785a678b1d47fa3286b6cf411faec1cea681
capooti <pcorti@gmail.com>
2018-04-26 17:49:51 -0400

    Re-enable thumbnails for layers. Fixes #351

--- geonode/contrib/worldmap/wm_extra/utils.py
+++ geonode/contrib/worldmap/wm_extra/utils.py
@@ -1,5 +1,12 @@
+from geonode.geoserver.helpers import create_gs_thumbnail_geonode
+from geonode.layers.models import Layer
+
 def create_wm_thumbnail(instance, overwrite=False):
     """
     WorldMap doesn't use thumbnails from GeoServer, but just the ones from html.
     """
-    return None
+    # we use the standard way just for layers, not for maps
+    if isinstance(instance, Layer):
+        create_gs_thumbnail_geonode(instance)
+    else:
+        return None
