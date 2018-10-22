497fb6d01a9012f4fb907bce8be0bdb4d0bc61ec
kappu <kappu72gmail.com>
2018-10-22 17:26:48 +0200

    Fixing thumb generation

--- geonode/utils.py
+++ geonode/utils.py
@@ -193,6 +193,8 @@ def bbox_to_wkt(x0, x1, y0, y1, srid="4326"):
 
 
 def _v(coord, x, source_srid=4326, target_srid=3857):
+    if source_srid == 4326 and x:
+        coord = coord - (round(coord / 360.0) * 360.0)
     if source_srid == 4326 and target_srid != 4326:
         if x and coord >= 180.0:
             return 179.0
