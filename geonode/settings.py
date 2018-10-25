5a731ed016d0d0757e9bc3795eebc5b8b73997a9
capooti <pcorti@gmail.com>
2018-10-25 09:59:39 -0400

    we use geoext client by default

--- geonode/settings.py
+++ geonode/settings.py
@@ -1235,10 +1235,12 @@ CORS_ORIGIN_WHITELIST = (
 """
 
 # To enable the WorldMap based Client enable those
+"""
 GEONODE_CLIENT_HOOKSET = "geonode.client.hooksets.WorldMapHookSet"
 CORS_ORIGIN_WHITELIST = (
     HOSTNAME
 )
+"""
 
 SERVICE_UPDATE_INTERVAL = 0
 
