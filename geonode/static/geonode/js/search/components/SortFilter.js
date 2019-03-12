7ccf51669aaf91aeb0a3720253c29a9873a861cd
Darin Acosta <darinjacosta@gmail.com>
2019-03-12 15:35:01 -0400

    More SortFilter refactor.

--- geonode/static/geonode/js/search/components/SortFilter.js
+++ geonode/static/geonode/js/search/components/SortFilter.js
@@ -1,53 +1,30 @@
 import React from "react";
-import searcher from "app/search/searcher";
-import PubSub from "app/utils/pubsub";
 
 const facetNodes = {
   default: (
     <ul className="dropdown-menu dropdown-menu-right" id="sort">
       <li>
-        <a
-          data-value="-date"
-          data-filter="order_by"
-          className="selected"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="-date" data-filter="order_by" className="selected">
           {window.gettext("Most recent")}
         </a>
       </li>
       <li>
-        <a
-          data-value="date"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="date" data-filter="order_by">
           {window.gettext("Less recent")}
         </a>
       </li>
       <li>
-        <a
-          data-value="title"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="title" data-filter="order_by">
           {window.gettext("A - Z")}
         </a>
       </li>
       <li>
-        <a
-          data-value="-title"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="-title" data-filter="order_by">
           {window.gettext("Z - A")}
         </a>
       </li>
       <li>
-        <a
-          data-value="-popular_count"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="-popular_count" data-filter="order_by">
           {window.gettext("Most popular")}
         </a>
       </li>
@@ -60,35 +37,22 @@ const facetNodes = {
           data-value="-last_modified"
           data-filter="order_by"
           className="selected"
-          ng-click="single_choice_listener($event)"
         >
           {window.gettext("Most recent")}
         </a>
       </li>
       <li>
-        <a
-          data-value="last_modified"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="last_modified" data-filter="order_by">
           {window.gettext("Less recent")}
         </a>
       </li>
       <li>
-        <a
-          data-value="title"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="title" data-filter="order_by">
           {window.gettext("A - Z")}
         </a>
       </li>
       <li>
-        <a
-          data-value="-title"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="-title" data-filter="order_by">
           {window.gettext("Z - A")}
         </a>
       </li>
@@ -101,35 +65,22 @@ const facetNodes = {
           data-value="-date_joined"
           data-filter="order_by"
           className="selected"
-          ng-click="single_choice_listener($event)"
         >
           {window.gettext("Most recent")}
         </a>
       </li>
       <li>
-        <a
-          data-value="date_joined"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="date_joined" data-filter="order_by">
           {window.gettext("Less recent")}
         </a>
       </li>
       <li>
-        <a
-          data-value="username"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="username" data-filter="order_by">
           {window.gettext("A - Z")}
         </a>
       </li>
       <li>
-        <a
-          data-value="-username"
-          data-filter="order_by"
-          ng-click="single_choice_listener($event)"
-        >
+        <a data-value="-username" data-filter="order_by">
           {window.gettext("Z - A")}
         </a>
       </li>
@@ -143,9 +94,17 @@ export default class SortFilter extends React.Component {
     dataValue: "-date"
   };
   facetType = window.djangoSearchVars.facet_type;
+  updateDataValue = dataValue => {
+    console.log("!!!DATAVALUE", dataValue);
+    this.setState({
+      dataValue
+    });
+  };
   getStyle = dataValue => {
     if (dataValue instanceof Array) {
-      // step through dataValue array
+      for (let i = 0; i < dataValue.length; i += 1) {
+        if (this.state.dataValue === dataValue[i]) return { display: "block" };
+      }
     }
     if (this.state.dataValue === dataValue) return { display: "block" };
     return { display: "none" };
@@ -172,36 +131,36 @@ export default class SortFilter extends React.Component {
             aria-expanded="false"
           >
             <div
-              ng-if="dataValue == 'title' || dataValue == 'username'"
+              onClick={() => this.updateDataValue("title")}
+              style={this.getStyle(["title", "username"])}
               ng-cloak
             >
               <i className="fa fa-sort-alpha-asc fa-lg" />
               <i className="fa fa-angle-down fa-lg" />
             </div>
             <div
-              ng-if="dataValue == '-title' || dataValue == '-username'"
+              onClick={() => this.updateDataValue("-title")}
+              style={this.getStyle(["-title", "-username"])}
               ng-cloak
             >
               <i className="fa fa-sort-alpha-desc fa-lg" />
               <i className="fa fa-angle-down fa-lg" />
             </div>
             <div
-              ng-if="dataValue == 'date' || dataValue == 'last_modified' || dataValue == 'date_joined'"
-              ng-cloak
+              style={this.getStyle(["date", "last_modified", "date_joined"])}
             >
               <i className="fa fa-clock-o" />
               <i className="fa fa-clock-o fa-level-up" />
               <i className="fa fa-angle-down fa-lg" />
             </div>
             <div
-              ng-if="dataValue == '-date' || dataValue == '-last_modified' || dataValue == '-date_joined'"
-              ng-cloak
+              style={this.getStyle(["-date", "-last_modified", "-date_joined"])}
             >
               <i className="fa fa-clock-o" />
               <i className="fa fa-clock-o fa-level-down" />
               <i className="fa fa-angle-down fa-lg" />
             </div>
-            <div ng-if="dataValue == '-popular_count'" ng-cloak>
+            <div style={this.getStyle(["-popular_count"])} ng-cloak>
               <i className="fa fa-fire fa-lg" />
               <i className="fa fa-angle-down fa-lg" />
             </div>
