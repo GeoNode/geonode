cd13e969c00904ee9bfecafe0578c0ad90b31d86
Darin Acosta <darinjacosta@gmail.com>
2019-03-18 16:50:43 -0400

    Add Cart methods and new cookie helpers.

--- geonode/static/geonode/js/search/components/Cart.js
+++ geonode/static/geonode/js/search/components/Cart.js
@@ -1,12 +1,40 @@
 import React from "react";
-import searcher from "app/search/searcher";
-import PubSub from "app/utils/pubsub";
-import getNewMapURL from "app/search/functions/getNewMapURL";
 import getCartSession from "app/search/functions/getCartSession";
 import cookies from "app/utils/cookies";
 
 export default class Cart extends React.Component {
   fillCart = () => getCartSession(cookies.getAll());
+  addItem = item => {
+    if (!item.id && item.layer_identifier) {
+      item.id = item.layer_identifier;
+    }
+
+    if (this.getItemById(item.id) === null) {
+      this.getCart().items.push(item);
+      cookies.storeCookie(item["uuid"], item);
+    }
+  };
+  getItemById = itemId => {
+    const items = this.getCart().items;
+    for (let i = 0; i < items.length; i += 1) {
+      if (items[i].id === itemId) {
+        return items[i];
+      }
+    }
+    return false;
+  };
+  removeItem = item => {
+    if (this.getItemById(item.id) !== null) {
+      const cart = this.getCart();
+      for (let i = 0; i < cart.items; i += 1) {
+        if (cart.items[i].id === item.id) {
+          cart.items.splice(i, 1);
+          cookies.remove(cart.items[i]["uuid"]);
+        }
+      }
+    }
+  };
+  getCart = () => this.cart;
   componentDidMount() {
     this.cart = {
       items: this.fillCart()
--- geonode/static/geonode/js/utils/cookies.js
+++ geonode/static/geonode/js/utils/cookies.js
@@ -1,13 +1,36 @@
+function isJson(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
 export default {
   getCookie: name => {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2)
-      return parts
+    if (parts.length === 2) {
+      const cookie = parts
         .pop()
         .split(";")
         .shift();
+      if (isJson(cookie)) return JSON.parse(cookie);
+      return cookie;
+    }
     return false;
+  },
+  storeCookie: (name, value) => {
+    const cookie = [
+      name,
+      "=",
+      JSON.stringify(value),
+      "; domain=.",
+      window.location.host.toString(),
+      "; path=/;"
+    ].join("");
+    document.cookie = cookie;
   },
   getAll: () => {
     const pairs = document.cookie.split(";");
