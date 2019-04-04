fc5d0f7c56d68d11c42777de09a23fcdff12edfb
Francesco Frassinelli <francesco.frassinelli@nina.no>
2019-04-04 13:43:51 +0200

    Polish tests

--- geonode/geoserver/tests.py
+++ geonode/geoserver/tests.py
@@ -1057,20 +1057,20 @@ class SignalsTests(GeoNodeBaseTestSupport):
         self.assertFalse(_links.count() > 0, "No links have been deleted")
         # Delete resources metadata
         _layers = Layer.objects.exclude(
-            metadata_xml__isnull=True).exclude(
-            metadata_xml__exact='').exclude(
-            csw_anytext__isnull=True).exclude(
-            csw_anytext__exact=''
+            metadata_xml__isnull=True,
+            metadata_xml__exact='',
+            csw_anytext__isnull=True,
+            csw_anytext__exact='',
         )
         count = _layers.count()
         self.assertTrue(count > 0, "No layers have got metadata")
         if count:
             _layers.update(metadata_xml=None)
             _updated_layers = Layer.objects.exclude(
-                metadata_xml__isnull=True).exclude(
-                metadata_xml__exact='').exclude(
-                csw_anytext__isnull=True).exclude(
-                csw_anytext__exact=''
+                metadata_xml__isnull=True,
+                metadata_xml__exact='',
+                csw_anytext__isnull=True,
+                csw_anytext__exact='',
             )
             updated_count = _updated_layers.count()
             self.assertTrue(
@@ -1087,10 +1087,10 @@ class SignalsTests(GeoNodeBaseTestSupport):
         )
         # Check layers
         _post_migrate_layers = Layer.objects.exclude(
-            metadata_xml__isnull=True).exclude(
-            metadata_xml__exact='').exclude(
-            csw_anytext__isnull=True).exclude(
-            csw_anytext__exact=''
+            metadata_xml__isnull=True,
+            metadata_xml__exact='',
+            csw_anytext__isnull=True,
+            csw_anytext__exact='',
         )
         post_migrate_layers_count = _post_migrate_layers.count()
         self.assertTrue(
