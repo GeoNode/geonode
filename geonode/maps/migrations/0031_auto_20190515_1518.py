775bb113ffd28e57ee9515fda712fbf26aac500d
afabiani <alessio.fabiani@geo-solutions.it>
2019-05-15 17:19:14 +0200

    [Release] Preparing 2.8rc1

---
+++ geonode/maps/migrations/0031_auto_20190515_1518.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('maps', '0030_auto_20180414_2120'),
+    ]
+
+    operations = [
+        migrations.AlterModelManagers(
+            name='map',
+            managers=[
+            ],
+        ),
+    ]
