c82f8e2417d087acbbc860353823495f87f2156d
capooti <pcorti@gmail.com>
2019-09-24 11:26:28 -0400

    Fixes #4949 UnicodeEncodeError in upload.models.update_from_session

--- geonode/upload/models.py
+++ geonode/upload/models.py
@@ -97,8 +97,6 @@ class Upload(models.Model):
             self.session = None
         else:
             # Make sure we don't pickle UTF-8 chars
-            upload_session.user.first_name = u'{}'.format(upload_session.user.first_name).decode("utf-8", "replace")
-            upload_session.user.last_name = u'{}'.format(upload_session.user.last_name).decode("utf-8", "replace")
             unicode_session = pickle.dumps(upload_session)
             self.session = unicode_session.decode("utf-8", "replace")
         if self.upload_dir is None:
