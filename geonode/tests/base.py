3b69301a82304abba0be09ab32ef94e879ca194f
afabiani <alessio.fabiani@geo-solutions.it>
2019-12-10 09:27:06 +0100

    - Smoke Test Seg Fault fix

--- geonode/tests/base.py
+++ geonode/tests/base.py
@@ -78,19 +78,21 @@ class GeoNodeBaseTestSupport(TestCase):
         logging.info(" Test setUp. Creating models.")
         self.get_obj_ids = create_models(type=self.get_type)
 
-    def tearDown(self):
-        super(GeoNodeBaseTestSupport, self).tearDown()
-        from django.conf import settings
-        if settings.integration_tests:
-            logging.info(" Test tearDown. Destroying models / Cleaning up Server.")
-            remove_models(self.get_obj_ids, type=self.get_type)
-            if settings.OGC_SERVER['default'].get(
-                    "GEOFENCE_SECURITY_ENABLED", False):
-                from geonode.security.utils import purge_geofence_all
-                purge_geofence_all()
-
 
 class GeoNodeLiveTestSupport(GeoNodeBaseTestSupport,
                              LiveServerTestCase):
 
     port = 8000
+
+    def setUp(self):
+        logging.info(" Test setUp. Creating models.")
+        self.get_obj_ids = create_models(type=self.get_type)
+
+    def tearDown(self):
+        logging.info(" Test tearDown. Destroying models / Cleaning up Server.")
+        remove_models(self.get_obj_ids, type=self.get_type)
+        from django.conf import settings
+        if settings.OGC_SERVER['default'].get(
+                "GEOFENCE_SECURITY_ENABLED", False):
+            from geonode.security.utils import purge_geofence_all
+            purge_geofence_all()
