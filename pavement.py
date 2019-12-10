133ad61365ff12fea97430e305e33c83d8793723
afabiani <alessio.fabiani@geo-solutions.it>
2019-12-10 10:50:03 +0100

    - Smoke Test Seg Fault fix

--- pavement.py
+++ pavement.py
@@ -150,21 +150,21 @@ def setup_geoserver(options):
     if _backend == 'geonode.qgis_server' or 'geonode.geoserver' not in INSTALLED_APPS:
         return
 
-    download_dir = path('downloaded')
-    if not download_dir.exists():
-        download_dir.makedirs()
-
-    geoserver_dir = path('geoserver')
-
-    geoserver_bin = download_dir / \
-        os.path.basename(dev_config['GEOSERVER_URL'])
-    jetty_runner = download_dir / \
-        os.path.basename(dev_config['JETTY_RUNNER_URL'])
-
-    if _django_11 and (integration_tests or integration_csw_tests or integration_bdd_tests):
+    if _django_11 and on_travis:
         """Will make use of the docker container for the Integration Tests"""
         pass
     else:
+        download_dir = path('downloaded')
+        if not download_dir.exists():
+            download_dir.makedirs()
+
+        geoserver_dir = path('geoserver')
+
+        geoserver_bin = download_dir / \
+            os.path.basename(dev_config['GEOSERVER_URL'])
+        jetty_runner = download_dir / \
+            os.path.basename(dev_config['JETTY_RUNNER_URL'])
+
         grab(
             options.get(
                 'geoserver',
@@ -530,7 +530,7 @@ def stop_geoserver(options):
     Stop GeoServer
     """
     # we use docker-compose for integration tests
-    if integration_tests or integration_csw_tests or integration_bdd_tests:
+    if on_travis:
         return
 
     # only start if using Geoserver backend
@@ -670,7 +670,7 @@ def start_geoserver(options):
     Start GeoServer with GeoNode extensions
     """
     # we use docker-compose for integration tests
-    if integration_tests or integration_csw_tests or integration_bdd_tests:
+    if on_travis:
         return
 
     # only start if using Geoserver backend
