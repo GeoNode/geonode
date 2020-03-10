c86b433b1f3812680fa11fb23971e8258ef8539a
Piotr Dankowski <dankowski.revolve@gmail.com>
2020-03-10 12:53:59 +0100

    adjust inbox layout to gmali like style

--- geonode/base/templatetags/user_messages.py
+++ geonode/base/templatetags/user_messages.py
@@ -1,5 +1,9 @@
 from django import template
 from uuid import uuid4
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext as _
+
+from user_messages.models import Message
 
 register = template.Library()
 
@@ -14,3 +18,32 @@ def is_unread(thread, user):
 @register.simple_tag
 def random_uuid():
     return str(uuid4())
+
+
+@register.simple_tag
+def format_senders(thread, current_user):
+    User = get_user_model()
+    users = User.objects.filter(sent_messages__thread=thread).order_by('sent_messages__sent_at').distinct()
+    sender_string = ''
+    u_count = users.count()
+    if u_count < 3:
+        for user in users:
+            if user == current_user:
+                user_repr = _('me')
+            else:
+                user_repr = f'{user.first_name} {user.last_name}'
+            sender_string += f'{user_repr}, '
+        sender_string = sender_string[:-2]
+    elif u_count == 3:
+        for user in users:
+            if user == current_user:
+                user_repr = _('me')
+            else:
+                user_repr = f'{user.first_name}'
+            sender_string += f'{user_repr}, '
+        sender_string = sender_string[:-2]
+    else:
+        first_sender = thread.latest_message
+        last_sender = thread.latest_message
+        sender_string = f'{first_sender.first_name} .. {last_sender.last_name}'
+    return f'{sender_string}'
