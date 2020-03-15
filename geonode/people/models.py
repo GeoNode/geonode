71bd4678cf9eb74941876ac9f37ea4f5250ff263
afabiani <alessio.fabiani@geo-solutions.it>
2020-03-15 16:38:48 +0100

    [Hardening] People Profile Manager -> match username case sensitive

--- geonode/people/models.py
+++ geonode/people/models.py
@@ -56,7 +56,7 @@ logger = logging.getLogger(__name__)
 
 class ProfileUserManager(UserManager):
     def get_by_natural_key(self, username):
-        return self.get(username__iexact=username)
+        return self.get(username=username)
 
 
 class Profile(AbstractUser):
