7fd8417a7c249ed687767d3d7d1a398379e040fd
afabiani <alessio.fabiani@geo-solutions.it>
2020-05-13 13:25:48 +0430

    [Hardening] Allow BR model showing on Admin site for Django 1.11

--- geonode/br/admin.py
+++ geonode/br/admin.py
@@ -31,8 +31,8 @@ class RestoredBackupAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_change_permission(self, request, obj=None):
-        return False
+    # def has_change_permission(self, request, obj=None):
+    #    return False
 
     def has_delete_permission(self, request, obj=None):
         return False
