831caff48c147a37a565fc31fda49c651d4cf341
Piotr Dankowski <dankowski.revolve@gmail.com>
2020-05-15 15:15:06 +0200

    [FIX #6097]
    Abort old request when new one arrives

--- geonode/static/geonode/js/messages/message_recipients_autocomplete.js
+++ geonode/static/geonode/js/messages/message_recipients_autocomplete.js
@@ -11,21 +11,22 @@ function get_groups_data(data) {
 }
 
 class MessageRecipientsTags {
-    constructor(input, data_extract_func, url, blacklist=[]) {
+    constructor(input, data_extract_func, url, blacklist = []) {
         if (input.tagName !== 'INPUT' || input.type !== 'text') {
             throw Error('Base element should be <input type="text">');
         }
         this.input = input;
         this.tagify = null;
-        this.controller = null;
         this.data_extract_func = data_extract_func;
         this.url = url;
         this.blacklist = blacklist;
+        this.request_controller = null;
     }
 
     init() {
         this.tagify = new Tagify(this.input, {whitelist: [], blacklist: this.blacklist});
         this.tagify.on('input', this._onInputHandler.bind(this));
+        this.request_controller = new AbortController();
     }
 
 
@@ -34,8 +35,10 @@ class MessageRecipientsTags {
         this.tagify.settings.whitelist.length = 0;
 
         this.tagify.loading(true).dropdown.hide.call(this.tagify);
+        this.request_controller.abort();
+        this.request_controller = new AbortController();
 
-        fetch(this.url + value).then(response =>
+        fetch(this.url + value, {signal:this.request_controller.signal}).then(response =>
             response.json()
                 .then(this.data_extract_func)
                 .then(res => {
--- geonode/static/geonode/js/messages/message_recipients_autocomplete_es5.js
+++ geonode/static/geonode/js/messages/message_recipients_autocomplete_es5.js
@@ -1,16 +1,12 @@
 "use strict";
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
 
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
 
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
 
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -48,10 +44,10 @@ var MessageRecipientsTags = /*#__PURE__*/function () {
 
     this.input = input;
     this.tagify = null;
-    this.controller = null;
     this.data_extract_func = data_extract_func;
     this.url = url;
     this.blacklist = blacklist;
+    this.request_controller = null;
   }
 
   _createClass(MessageRecipientsTags, [{
@@ -62,6 +58,7 @@ var MessageRecipientsTags = /*#__PURE__*/function () {
         blacklist: this.blacklist
       });
       this.tagify.on('input', this._onInputHandler.bind(this));
+      this.request_controller = new AbortController();
     }
   }, {
     key: "_onInputHandler",
@@ -71,7 +68,11 @@ var MessageRecipientsTags = /*#__PURE__*/function () {
       var value = event.detail.value;
       this.tagify.settings.whitelist.length = 0;
       this.tagify.loading(true).dropdown.hide.call(this.tagify);
-      fetch(this.url + value).then(function (response) {
+      this.request_controller.abort();
+      this.request_controller = new AbortController();
+      fetch(this.url + value, {
+        signal: this.request_controller.signal
+      }).then(function (response) {
         return response.json().then(_this.data_extract_func).then(function (res) {
           var _this$tagify$settings;
 
