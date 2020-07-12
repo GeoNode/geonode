8e14bcdc132f0d198e3bd49a502e4d15c75e5c45
Toni <toni.schoenbuchner@csgis.de>
2020-07-12 15:27:11 +0000

    [Fixes #6131; Fixes: #6204] Add missing strings and French translation (#6205)

--- geonode/maps/views.py
+++ geonode/maps/views.py
@@ -96,13 +96,13 @@ DEFAULT_MAPS_SEARCH_BATCH_SIZE = 10
 MAX_MAPS_SEARCH_BATCH_SIZE = 25
 
 _PERMISSION_MSG_DELETE = _("You are not permitted to delete this map.")
-_PERMISSION_MSG_GENERIC = _('You do not have permissions for this map.')
+_PERMISSION_MSG_GENERIC = _("You do not have permissions for this map.")
 _PERMISSION_MSG_LOGIN = _("You must be logged in to save this map")
 _PERMISSION_MSG_SAVE = _("You are not permitted to save or edit this map.")
 _PERMISSION_MSG_METADATA = _(
     "You are not allowed to modify this map's metadata.")
 _PERMISSION_MSG_VIEW = _("You are not allowed to view this map.")
-_PERMISSION_MSG_UNKNOWN = _('An unknown error has occured.')
+_PERMISSION_MSG_UNKNOWN = _("An unknown error has occured.")
 
 
 def _resolve_map(request, id, permission='base.change_resourcebase',
