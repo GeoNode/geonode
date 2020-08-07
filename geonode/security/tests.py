5b644f4a118eb96d8946f71a98303931fdfda2c5
afabiani <alessio.fabiani@geo-solutions.it>
2020-08-07 16:25:32 +0200

    - Bump GeoServer to version 2.17.2

--- geonode/security/tests.py
+++ geonode/security/tests.py
@@ -698,7 +698,7 @@ class PermissionsTest(GeoNodeBaseTestSupport):
 
         from geonode.geoserver.views import get_layer_capabilities
         capab = get_layer_capabilities(saved_layer, tolerant=True)
-        self.assertIsNotNone(capab)
+
         wms_capabilities_url = reverse('capabilities_layer', args=[saved_layer.id])
         wms_capabilities_resp = self.client.get(wms_capabilities_url)
         self.assertTrue(wms_capabilities_resp.status_code, 200)
