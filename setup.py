ce7689fc6d256707c9ea7e6f3f14c1fcc21d5342
afabiani <alessio.fabiani@geo-solutions.it>
2020-08-31 10:56:16 +0200

    [Fixes #6388] Error when installing core GeoNode into virtual environment

--- setup.py
+++ setup.py
@@ -17,25 +17,6 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
-
-try:
-    # pip >=20
-    from pip._internal.network.session import PipSession
-    try:
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        # pip >=21
-        from pip._internal.req.req_file import parse_requirements
-except ImportError:
-    try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.download import PipSession
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        # pip <= 9.0.3
-        from pip.download import PipSession
-        from pip.req import parse_requirements
-
 from distutils.core import setup
 
 from setuptools import find_packages
@@ -46,18 +27,12 @@ import sys
 current_directory = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_directory)
 
-# Parse requirements.txt to get the list of dependencies
-inst_req = parse_requirements("requirements.txt", session=PipSession())
-REQUIREMENTS = [str(r.req) if hasattr(r, 'req') else r.requirement if not r.is_editable else ''
-                for r in inst_req]
-
 setup(
     name="GeoNode",
     version=__import__("geonode").get_version(),
     description="Application for serving and sharing geospatial data",
     long_description=open("README.md").read(),
     classifiers=["Development Status :: 5 - Production/Stable"],
-    python_requires=">=3.6",
     keywords="",
     author="GeoNode Developers",
     author_email="dev@geonode.org",
@@ -72,6 +47,4 @@ setup(
         "templates": ["*.*"],
     },
     include_package_data=True,
-    install_requires=REQUIREMENTS,
-    zip_safe=False,
 )
