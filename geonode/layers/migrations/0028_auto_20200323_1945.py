80a348be42e0202986b5f9e2e0a28ff5bc6b2d13
Matthew Northcott <matthewnorthcott@catalyst.net.nz>
2020-09-30 11:01:58 +1300

    Check bbox attributes in populate_polygon

--- geonode/layers/migrations/0028_auto_20200323_1945.py
+++ geonode/layers/migrations/0028_auto_20200323_1945.py
@@ -7,9 +7,10 @@ from django.contrib.gis.geos import Polygon
 def populate_polygon(apps, schema_editor):
     Layer = apps.get_model('layers', 'Layer')
     for layer in Layer.objects.all():
-        bbox_tup = (layer.bbox_x0, layer.bbox_y0, layer.bbox_x1, layer.bbox_y1)
-        layer.bbox_polygon = Polygon.from_bbox(bbox_tup)
-        layer.save()
+        bbox = [getattr(layer, key, None) for key in ('bbox_x0', 'bbox_y0', 'bbox_x1', 'bbox_y1')]
+        if all(bbox):
+            layer.bbox_polygon = Polygon.from_bbox(bbox)
+            layer.save()
 
 
 class Migration(migrations.Migration):
