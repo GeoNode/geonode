83389d11d2700d6efcb8797bcc6be187f953031c
afabiani <alessio.fabiani@geo-solutions.it>
2020-10-05 14:22:27 +0200

    Fix travis

--- geonode/br/tests/factories.py
+++ geonode/br/tests/factories.py
@@ -17,7 +17,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
-
+import factory
 import random
 import string
 import hashlib
