3adb9e77a3e0ec1e96fcf3e8486106fcb8d4ba96
afabiani <alessio.fabiani@geo-solutions.it>
2020-10-15 14:45:18 +0200

    [GeoApps] Hooking GeoApp rest v2 API serializers fixes

--- geonode/geoapps/api/serializers.py
+++ geonode/geoapps/api/serializers.py
@@ -19,6 +19,10 @@
 #########################################################################
 import json
 
+from django.contrib.auth import get_user_model
+
+from rest_framework.serializers import ValidationError
+
 from dynamic_rest.serializers import DynamicModelSerializer
 from dynamic_rest.fields.fields import DynamicRelationField
 
@@ -48,6 +52,13 @@ class AppTypePolymorphicSerializer(DynamicModelSerializer):
             return _child.app_type
 
 
+class GeoAppDataField(DynamicRelationField):
+
+    def value_to_string(self, obj):
+        value = self.value_from_object(obj)
+        return self.get_prep_value(value)
+
+
 class GeoAppDataSerializer(DynamicModelSerializer):
 
     class Meta:
@@ -55,6 +66,9 @@ class GeoAppDataSerializer(DynamicModelSerializer):
         model = GeoAppData
         name = 'GeoAppData'
         fields = ('pk', 'blob')
+
+    def to_internal_value(self, data):
+        return data
 
     def to_representation(self, value):
         data = GeoAppData.objects.filter(resource__id=value).first()
@@ -76,6 +90,90 @@ class GeoAppSerializer(ResourceBaseSerializer):
             'urlsuffix', 'data'
         )
 
+    def to_internal_value(self, data):
+        if 'data' in data:
+            _data = data.pop('data')
+            if self.is_valid():
+                data['blob'] = _data
+
+        return data
+
+    def create(self, validated_data):
+        # Sanity checks
+        if 'name' not in validated_data or \
+        'owner' not in validated_data:
+            raise ValidationError("No valid data: 'name' and 'owner' are mandatory fields!")
+
+        if GeoApp.objects.filter(name=validated_data['name']).count():
+            raise ValidationError("A GeoApp with the same 'name' already exists!")
+
+        # Extract users' profiles
+        _user_profiles = {}
+        for _key, _value in validated_data.items():
+            if _key in ('owner', 'poc', 'metadata_owner'):
+                _user_profiles[_key] = _value
+        for _key, _value in _user_profiles.items():
+            validated_data.pop(_key)
+            _u = get_user_model().objects.filter(username=_value).first()
+            if _u:
+                validated_data[_key] = _u
+            else:
+                raise ValidationError("The specified '{}' does not exist!".format(_key))
+
+        # Extract JSON blob
+        _data = None
+        if 'blob' in validated_data:
+            _data = validated_data.pop('blob')
+
+        # Create a new instance
+        _instance = GeoApp.objects.create(**validated_data)
+
+        if _instance and _data:
+            try:
+                _geo_app, _created = GeoAppData.objects.get_or_create(resource=_instance)
+                _geo_app.blob = _data
+                _geo_app.save()
+            except Exception as e:
+                raise ValidationError(e)
+
+        return _instance
+
+    def update(self, instance, validated_data):
+
+        # Extract users' profiles
+        _user_profiles = {}
+        for _key, _value in validated_data.items():
+            if _key in ('owner', 'poc', 'metadata_owner'):
+                _user_profiles[_key] = _value
+        for _key, _value in _user_profiles.items():
+            validated_data.pop(_key)
+            _u = get_user_model().objects.filter(username=_value).first()
+            if _u:
+                validated_data[_key] = _u
+            else:
+                raise ValidationError("The specified '{}' does not exist!".format(_key))
+
+        # Extract JSON blob
+        _data = None
+        if 'blob' in validated_data:
+            _data = validated_data.pop('blob')
+
+        try:
+            GeoApp.objects.filter(pk=instance.id).update(**validated_data)
+            instance.refresh_from_db()
+        except Exception as e:
+            raise ValidationError(e)
+
+        if instance and _data:
+            try:
+                _geo_app, _created = GeoAppData.objects.get_or_create(resource=instance)
+                _geo_app.blob = _data
+                _geo_app.save()
+            except Exception as e:
+                raise ValidationError(e)
+
+        return instance
+
     app_type = DynamicRelationField(
         AppTypePolymorphicSerializer,
         source='id',
@@ -84,7 +182,7 @@ class GeoAppSerializer(ResourceBaseSerializer):
     """
      - Deferred / not Embedded --> ?include[]=data
     """
-    data = DynamicRelationField(
+    data = GeoAppDataField(
         GeoAppDataSerializer,
         source='id',
         many=False,
