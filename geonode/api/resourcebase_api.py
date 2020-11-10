437914cb9282f76fd4ec570057bb103ba2a990da
Florian Hoedt <gannebamm@gmail.com>
2020-11-10 15:20:11 +0000

    [Fixes #6534] Missing translation tags in layer_list and layer_detail (#6565)
    * updated geo limit texts and cart [#6131]
    
    * iso-categories translation msgid fix
    
    * layer detail trans tags [FIXES #6534]
    
    * adds layer list objects and layer detail iso category translations.
    
    * Revert " - Add django-grappelli==2.14.2 admin interface"
    
    This reverts commit c9f594c5
    
    * get Iso categories po texts back and updated german trans for geoapps. Defuzzied some strings
    
    * added compiled .mo file
    
    * - Revert "grappelli" removal
    
    Co-authored-by: Alessio Fabiani <alessio.fabiani@geo-solutions.it>

--- geonode/api/resourcebase_api.py
+++ geonode/api/resourcebase_api.py
@@ -67,6 +67,7 @@ from .api import (
     GroupResource,
     FILTER_TYPES)
 from .paginator import CrossSiteXHRPaginator
+from django.utils.translation import gettext as _
 
 if settings.HAYSTACK_SEARCH:
     from haystack.query import SearchQuerySet  # noqa
@@ -746,7 +747,7 @@ class LayerResource(CommonModelApi):
             formatted_obj['owner__username'] = username
             formatted_obj['owner_name'] = full_name
             if obj.category:
-                formatted_obj['category__gn_description'] = obj.category.gn_description
+                formatted_obj['category__gn_description'] = _(obj.category.gn_description)
             if obj.group:
                 formatted_obj['group'] = obj.group
                 try:
@@ -948,7 +949,7 @@ class MapResource(CommonModelApi):
             formatted_obj['owner__username'] = username
             formatted_obj['owner_name'] = full_name
             if obj.category:
-                formatted_obj['category__gn_description'] = obj.category.gn_description
+                formatted_obj['category__gn_description'] = _(obj.category.gn_description)
             if obj.group:
                 formatted_obj['group'] = obj.group
                 try:
@@ -1028,7 +1029,7 @@ class DocumentResource(CommonModelApi):
             formatted_obj['owner__username'] = username
             formatted_obj['owner_name'] = full_name
             if obj.category:
-                formatted_obj['category__gn_description'] = obj.category.gn_description
+                formatted_obj['category__gn_description'] = _(obj.category.gn_description)
             if obj.group:
                 formatted_obj['group'] = obj.group
                 try:
