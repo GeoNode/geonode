38bcc2abe1496b131718d371529adceb5aea5870
Luis Saltron <luis.saltron@skaphe.com>
2020-11-14 16:27:25 -0500

    * Created partial model for SBN module * Added initial create form for SBN * Added initial edit form for SBN * Added initial detail view for SBN * Partial tanslations for SBN module

--- geonode/waterproof_nbs_ca/forms.py
+++ geonode/waterproof_nbs_ca/forms.py
@@ -17,12 +17,20 @@ class WaterproofNbsCaForm(forms.ModelForm):
     class Meta:
         model = WaterproofNbsCa
         fields = (
-            'name', 
+            'name',
             'description',
-            
-        )    
+            'max_benefit_req_time',
+            'profit_pct_time_inter_assoc',
+            'total_profits_sbn_consec_time',
+            'unit_implementation_cost',
+            'unit_maintenance_cost',
+            'periodicity_maitenance',
+            'unit_oportunity_cost',
+            'rios_transition',
+            'land_cover_def'
+        )
 
     def save(self, *args, **kwargs):
-        
+
         obj = super(WaterproofNbsCaForm, self).save(*args, **kwargs)
         return obj
--- geonode/waterproof_nbs_ca/views.py
+++ geonode/waterproof_nbs_ca/views.py
@@ -14,14 +14,14 @@ from django.template.response import TemplateResponse
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
-from django.views.generic import CreateView, DetailView, ListView
-
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 from django_libs.views_mixins import AccessMixin
 
 from .forms import WaterproofNbsCaForm
 from .models import WaterproofNbsCa
 
 logger = logging.getLogger(__name__)
+
 
 class WaterproofNbsCaMixin(object):
     """
@@ -30,7 +30,7 @@ class WaterproofNbsCaMixin(object):
     """
 
     def post(self, request, *args, **kwargs):
-        
+
         return self.get(self, request, *args, **kwargs)
 
 
@@ -45,28 +45,23 @@ class WaterproofNbsCaListView(AccessMixin, WaterproofNbsCaMixin, ListView):
     logger.debug(template_name)
 
     def get_queryset(self):
-        
         self.queryset = super(WaterproofNbsCaListView, self).get_queryset()
         return self.queryset
 
 
-class WaterproofNbsCaView(AccessMixin, WaterproofNbsCaMixin, DetailView):
+class WaterproofNbsCaDetailView(AccessMixin, WaterproofNbsCaMixin, DetailView):
     """
     Main view to display one WaterproofNbsCa.
 
     """
-    model = WaterproofNbsCaForm
-    template_name = "waterproof_nbs_ca/entry_list.html"
+    model = WaterproofNbsCa
+    template_name = "waterproof_nbs_ca/waterproofnbsca_detail_list.html"
     access_mixin_setting_name = 'WATERPROOF_NBS_CA_ALLOW_ANONYMOUS'
-    
-    def get_object(self, **kwargs):
-        obj = super(WaterproofNbsCaView, self).get_object(**kwargs)
-        obj.save()
-        return obj
+    logger.debug(template_name)
 
-    def get_context_data(self, **kwargs):
-        context = super(WaterproofNbsCaView, self).get_context_data(**kwargs)        
-        return context
+    def get_queryset(self):
+        self.queryset = super(WaterproofNbsCaDetailView, self).get_queryset()
+        return self.queryset
 
 
 class WaterproofNbsCaCreateView(AccessMixin, CreateView):
@@ -76,14 +71,52 @@ class WaterproofNbsCaCreateView(AccessMixin, CreateView):
     """
     model = WaterproofNbsCa
     form_class = WaterproofNbsCaForm
-    access_mixin_setting_name = 'WATERPROOF_NBS_CA_ALLOW_ANONYMOUS'    
+    access_mixin_setting_name = 'WATERPROOF_NBS_CA_ALLOW_ANONYMOUS'
 
     def form_valid(self, form):
         return super(WaterproofNbsCaCreateView, self).form_valid(form)
 
     def get_form_kwargs(self):
         kwargs = super(WaterproofNbsCaCreateView, self).get_form_kwargs()
-        
+
+        return kwargs
+
+    def get_success_url(self):
+        return reverse('waterproof_nbs_ca_list')
+
+
+class WaterproofNbsUpdateView(AccessMixin, UpdateView):
+    model = WaterproofNbsCa
+    access_mixin_setting_name = 'WATERPROOF_NBS_CA_ALLOW_ANONYMOUS'
+    fields = [
+        'name',
+        'description',
+        'max_benefit_req_time',
+        'profit_pct_time_inter_assoc',
+        'total_profits_sbn_consec_time',
+        'unit_implementation_cost',
+        'unit_maintenance_cost',
+        'periodicity_maitenance',
+        'unit_oportunity_cost',
+        'rios_transition',
+        'land_cover_def'
+    ]
+
+    def get_success_url(self):
+        return reverse('waterproof_nbs_ca_list')
+
+
+class WaterproofNbsDeleteView(DeleteView):
+    model = WaterproofNbsCa
+    form_class = WaterproofNbsCaForm
+    access_mixin_setting_name = 'WATERPROOF_NBS_CA_ALLOW_ANONYMOUS'
+
+    def form_valid(self, form):
+        return super(WaterproofNbsDeleteView, self).form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super(WaterproofNbsDeleteView, self).get_form_kwargs()
+
         return kwargs
 
     def get_success_url(self):
