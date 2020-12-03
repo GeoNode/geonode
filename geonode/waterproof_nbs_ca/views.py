ba680708221e8442ac341baea263b01acb3722d8
Luis Saltron <luis.saltron@skaphe.com>
2020-12-03 16:52:18 -0500

    * Integration Waterproof NBS with Postgre * Testing geometries transformation shapefile SBN form

--- geonode/waterproof_nbs_ca/views.py
+++ geonode/waterproof_nbs_ca/views.py
@@ -16,18 +16,52 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 from django_libs.views_mixins import AccessMixin
-
+from django.shortcuts import render
 from .forms import WaterproofNbsCaForm
 from .models import WaterproofNbsCa
-
+from .models import RiosActivity
+from .models import RiosTransformation, TramsformationShapefile
+from .models import Countries
+from .models import Currency
+from django.contrib.gis.geos import Polygon,MultiPolygon,GEOSGeometry
+from django.core import serializers
+from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+import json
+from shapely.geometry import shape, Point
 logger = logging.getLogger(__name__)
 
 
+def createNbs(request):
+    if request.POST.get('action') == 'create-nbs':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        image = request.FILES.get('shapefile')  # request.FILES used for to get files
+        imageJson=json.load(image)
+        for feature in imageJson['features']:
+            polygonFile=shape(feature['geometry'])
+        print(title)
+
+        TramsformationShapefile.objects.create(
+            name=title,
+            action=description,
+            activity=description,
+            polygon=polygonFile
+        )
+
+        return render(request, 'waterproof_nbs_ca/waterproofnbsca_form.html')
+    else:
+        nbs = WaterproofNbsCa.objects.all()
+        return render(request, 'waterproof_nbs_ca/waterproofnbsca_form.html', {'nbs': nbs})
+
+
+"""
 class WaterproofNbsCaMixin(object):
-    """
+   
     Mixin to handle and arrange the entry list.
 
-    """
+    
 
     def post(self, request, *args, **kwargs):
 
@@ -35,10 +69,10 @@ class WaterproofNbsCaMixin(object):
 
 
 class WaterproofNbsCaListView(AccessMixin, WaterproofNbsCaMixin, ListView):
-    """
+   
     Main view to display all WaterproofNbsCa
 
-    """
+   
     model = WaterproofNbsCa
     template_name = "waterproof_nbs_ca/entry_list.html"
     access_mixin_setting_name = 'WATERPROOF_NBS_CA_ALLOW_ANONYMOUS'
@@ -46,14 +80,15 @@ class WaterproofNbsCaListView(AccessMixin, WaterproofNbsCaMixin, ListView):
 
     def get_queryset(self):
         self.queryset = super(WaterproofNbsCaListView, self).get_queryset()
+        # self.queryset.user=get_user_model.objects.all()
         return self.queryset
 
 
 class WaterproofNbsCaDetailView(AccessMixin, WaterproofNbsCaMixin, DetailView):
-    """
+   
     Main view to display one WaterproofNbsCa.
 
-    """
+  
     model = WaterproofNbsCa
     template_name = "waterproof_nbs_ca/waterproofnbsca_detail_list.html"
     access_mixin_setting_name = 'WATERPROOF_NBS_CA_ALLOW_ANONYMOUS'
@@ -63,32 +98,19 @@ class WaterproofNbsCaDetailView(AccessMixin, WaterproofNbsCaMixin, DetailView):
         self.queryset = super(WaterproofNbsCaDetailView, self).get_queryset()
         return self.queryset
 
+class MyCreateView(View):
 
-class WaterproofNbsCaCreateView(AccessMixin, CreateView):
-    """
-    WaterProof Nbs CA Create form view.
-
-    """
-    model = WaterproofNbsCa
-    form_class = WaterproofNbsCaForm
-    access_mixin_setting_name = 'WATERPROOF_NBS_CA_ALLOW_ANONYMOUS'
-
-    def form_valid(self, form):
-        return super(WaterproofNbsCaCreateView, self).form_valid(form)
-
-    def get_form_kwargs(self):
-        kwargs = super(WaterproofNbsCaCreateView, self).get_form_kwargs()
-
-        return kwargs
-
-    def get_success_url(self):
-        return reverse('waterproof_nbs_ca_list')
+    def post(self,request):
+        nbs = WaterproofNbsCa.objects.all().order_by('-id')
+        return render(request, 'waterproofnbsca_form', {'nbs': nbs})
 
 
 class WaterproofNbsUpdateView(AccessMixin, UpdateView):
     model = WaterproofNbsCa
     access_mixin_setting_name = 'WATERPROOF_NBS_CA_ALLOW_ANONYMOUS'
     fields = [
+        'country',
+        'currency',
         'name',
         'description',
         'max_benefit_req_time',
@@ -98,8 +120,8 @@ class WaterproofNbsUpdateView(AccessMixin, UpdateView):
         'unit_maintenance_cost',
         'periodicity_maitenance',
         'unit_oportunity_cost',
-        'rios_transition',
-        'land_cover_def'
+        'rios_transformations',
+        'added_by'
     ]
 
     def get_success_url(self):
@@ -121,3 +143,40 @@ class WaterproofNbsDeleteView(DeleteView):
 
     def get_success_url(self):
         return reverse('waterproof_nbs_ca_list')
+
+
+class WaterProofLoadActivities(ListView):
+    model = RiosActivity
+    template_name = "waterproof_nbs_ca/waterproofnsbca_activities_list_options.html"
+
+    def get_queryset(self):
+        transition = self.request.GET.get('transition')
+        self.queryset = RiosActivity.objects.filter(transition_id=transition)
+        return self.queryset
+
+
+class WaterProofLoadTransformations(ListView):
+    model = RiosTransformation
+    template_name = "waterproof_nbs_ca/waterproofnsbca_transformations_list_options.html"
+
+    def get_queryset(self):
+        activity = self.request.GET.get('activity')
+        self.queryset = RiosTransformation.objects.filter(transition_id=activity)
+        return self.queryset
+
+
+class WaterProofLoadCountryFactor(View):
+    def get(self, request):
+        country = self.request.GET.get('country')
+        countries = Countries.objects.filter(id=country)
+        countries_serialized = serializers.serialize('json', countries)
+        return JsonResponse(countries_serialized, safe=False)
+
+
+class WaterProofLoadCurrency(View):
+    def get(self, request):
+        currency = self.request.GET.get('currency')
+        currencies = Currency.objects.filter(id=currency)
+        currencies_serialized = serializers.serialize('json', currencies)
+        return JsonResponse(currencies_serialized, safe=False)
+"""
