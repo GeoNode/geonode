350c232d76c246129c06ba7fc960d08929bdbb98
afabiani <alessio.fabiani@geo-solutions.it>
2020-12-05 11:06:33 +0100

    - Set local .sh files exec perms
    (cherry picked from commit 0ce8d09441d162005a2996b8c1eaaa3b32e0f165)
    
    # Conflicts:
    #	start_django_async.sh
    #	test_api_v2.sh

--- docker-purge.sh
+++ docker-purge.sh
@@ -1,1 +1,2 @@
 docker kill $(docker ps -q); docker rm $(docker ps -a -q); docker rmi $(docker images -q); docker volume ls -qf dangling=true | xargs -r docker volume rm; docker system prune -a
+docker volume prune
