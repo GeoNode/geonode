1adb80bd56a70f9797531b5c525229378660771e
Edwin Piragauta Vargas <edwin.piragauta@gmail.com>
2020-12-16 20:45:35 -0500

    add waterproof intake module

---
+++ geonode/waterproof_intake/forms.py
@@ -0,0 +1,26 @@
+"""
+Forms for the ``django - Waterproof Intake`` application.
+
+"""
+
+from django import forms
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Intake
+
+class IntakeForm(forms.ModelForm):    
+
+    class Meta:
+        model = Intake
+        fields = ('name', 
+                'description',
+                'id_region', 
+                'id_city')
+        widgets = {'id_region': forms.HiddenInput(),
+                    'id_city': forms.HiddenInput()}
+
+    def save(self, *args, **kwargs):
+        
+        obj = super(IntakeForm, self).save(*args, **kwargs)
+        return obj
