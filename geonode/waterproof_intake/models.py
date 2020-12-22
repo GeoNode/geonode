db6efe32792e68b22533aa7a01138b57e0a65816
Luis Saltron <luis.saltron@skaphe.com>
2020-12-22 12:40:18 -0500

    First definition of model for water intake

--- geonode/waterproof_intake/models.py
+++ geonode/waterproof_intake/models.py
@@ -2,7 +2,22 @@
 
 from django.conf import settings
 from django.db import models
+from django.contrib.gis.db import models
 from django.utils.translation import ugettext_lazy as _
+from geonode.waterproof_nbs_ca.models import Countries
+
+
+class City(models.Model):
+    country = models.ForeignKey(Countries, on_delete=models.CASCADE)
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_('Name'),
+    )
+
+    def __str__(self):
+        return "%s" % self.name
+
 
 class Intake(models.Model):
     """
@@ -12,7 +27,7 @@ class Intake(models.Model):
     :descripcion: Intake Description.
 
     """
-    
+
     name = models.CharField(
         max_length=100,
         verbose_name=_('Name'),
@@ -23,14 +38,149 @@ class Intake(models.Model):
         verbose_name=_('Description'),
     )
 
-    id_region = models.CharField(
-        max_length=24,
-        verbose_name=_('Id Region'),
+    area = models.MultiPolygonField()
+
+    city = models.ForeignKey(Countries, on_delete=models.CASCADE)
+
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
     )
 
-    id_city = models.CharField(
-        max_length=24,
-        verbose_name=_('Id City'),
+
+class UserCosts(models.model):
+     name = models.CharField(
+        max_length=100,
+        verbose_name=_('Name'),
     )
 
-    
+    value = models.DecimalField(
+        decimal_places=4,
+        max_digits=14,
+        verbose_name=_('Value')
+    )
+
+class CategoryCosts(models.model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_('Name'),
+    )
+
+    description=models.CharField(
+        max_length=100,
+        verbose_name=_('Name'),
+    )
+
+class SystemCosts(models.model):
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name=_('Name'),
+    )
+
+    value = models.DecimalField(
+        decimal_places=4,
+        max_digits=14,
+        verbose_name=_('Extraction value')
+    )
+
+   
+class ElementSystem(models.model):
+    origin = models.IntegerField(
+        default=1980,
+        verbose_name=_('Year'),
+    )
+
+    destination = models.IntegerField(
+        default=1980,
+        verbose_name=_('Year'),
+    )
+
+    user_cost=models.ManyToManyField(
+        UserCosts,
+    )
+
+    system_cost=models.ManyToManyField(
+        SystemCosts,
+    )
+
+
+class ExternalInputs(models.Model):
+    year = models.IntegerField(
+        default=1980,
+        verbose_name=_('Year'),
+    )
+
+    water_volume = models.DecimalField(
+        decimal_places=4,
+        max_digits=14,
+        verbose_name=_('Water_volume')
+    )
+
+    sediment = models.DecimalField(
+        decimal_places=4,
+        max_digits=14,
+        verbose_name=_('Extraction value')
+    )
+
+    nitrogen = models.DecimalField(
+        decimal_places=4,
+        max_digits=14,
+        verbose_name=_('Extraction value')
+    )
+
+    phosphorus = models.DecimalField(
+        decimal_places=4,
+        max_digits=14,
+        verbose_name=_('Extraction value')
+    )
+
+    intake = models.ForeignKey(Intake, on_delete=models.CASCADE)
+
+
+class DemandParameters(models.Model):
+
+    initial_year = models.IntegerField(
+        verbose_name=_('Year'),
+    )
+
+    ending_year = models.IntegerField(
+        verbose_name=_('Year'),
+    )
+
+    interpolation_type = models.IntegerField(
+        verbose_name=_('Year'),
+    )
+
+    initial_extraction = models.DecimalField(
+        decimal_places=4,
+        max_digits=14,
+        verbose_name=_('Extraction value')
+    )
+
+    ending_extraction = models.DecimalField(
+        decimal_places=4,
+        max_digits=14,
+        verbose_name=_('Extraction value')
+    )
+
+    is_manual = models.BooleanField(verbose_name=_('Manual'), default=False)
+
+    intake = models.ForeignKey(Intake, on_delete=models.CASCADE)
+
+
+class WaterExtraction(models.Model):
+    year = models.IntegerField(
+        default=1980,
+        verbose_name=_('Year'),
+    )
+
+    value = models.DecimalField(
+        decimal_places=4,
+        max_digits=14,
+        verbose_name=_('Extraction value')
+    )
+
+    demand = models.ForeignKey(DemandParameters, on_delete=models.CASCADE)
