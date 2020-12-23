40c53f661f254741fd1964219e60b8af681adac0
afabiani <alessio.fabiani@gmail.com>
2020-12-23 17:03:16 +0100

    - Test Fixes: CircleCI

--- geonode/tests/csw.py
+++ geonode/tests/csw.py
@@ -17,7 +17,6 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
-
 from .base import GeoNodeBaseTestSupport
 
 import os
@@ -26,10 +25,10 @@ import gisdata
 import logging
 
 from lxml import etree
+from urllib.parse import urljoin
 from defusedxml import lxml as dlxml
 
 from django.conf import settings
-from django.test.utils import override_settings
 
 from geonode import geoserver, qgis_server
 from geonode.utils import check_ogc_backend
@@ -38,84 +37,28 @@ from geonode.catalogue import get_catalogue
 logger = logging.getLogger(__name__)
 
 
-@override_settings(
-    SITEURL='http://localhost:8001/',
-    CATALOGUE={
-        'default': {
-            'ENGINE': 'geonode.catalogue.backends.pycsw_local',
-            'URL': 'http://localhost:8001/catalogue/csw',
-        }
-    },
-    PYCSW={
-        'CONFIGURATION': {
-            'server': {
-                'home': '.',
-                'url': 'http://localhost:8001/catalogue/csw',
-                'encoding': 'UTF-8',
-                'language': 'en',
-                'maxrecords': '20',
-                'pretty_print': 'true',
-                'domaincounts': 'true',
-                'profiles': 'apiso,ebrim',
-            },
-            'manager': {
-                'transactions': 'false',
-                'allowed_ips': '*',
-            },
-            'metadata:main': {
-                'identification_title': 'GeoNode Catalogue',
-                'identification_abstract': 'GeoNode is an open source platform'
-                ' that facilitates the creation, sharing, and collaborative use'
-                ' of geospatial data',
-                'identification_keywords': 'sdi, catalogue, discovery, metadata,'
-                ' GeoNode',
-                'identification_keywords_type': 'theme',
-                'identification_fees': 'None',
-                'identification_accessconstraints': 'None',
-                'provider_name': 'Organization Name',
-                'provider_url': 'http://localhost:8001/',
-                'contact_name': 'Lastname, Firstname',
-                'contact_position': 'Position Title',
-                'contact_address': 'Mailing Address',
-                'contact_city': 'City',
-                'contact_stateorprovince': 'Administrative Area',
-                'contact_postalcode': 'Zip or Postal Code',
-                'contact_country': 'Country',
-                'contact_phone': '+xx-xxx-xxx-xxxx',
-                'contact_fax': '+xx-xxx-xxx-xxxx',
-                'contact_email': 'Email Address',
-                'contact_url': 'Contact URL',
-                'contact_hours': 'Hours of Service',
-                'contact_instructions': 'During hours of service. Off on weekends.',
-                'contact_role': 'pointOfContact',
-            },
-            'metadata:inspire': {
-                'enabled': 'true',
-                'languages_supported': 'eng,gre',
-                'default_language': 'eng',
-                'date': 'YYYY-MM-DD',
-                'gemet_keywords': 'Utility and governmental services',
-                'conformity_service': 'notEvaluated',
-                'contact_name': 'Organization Name',
-                'contact_email': 'Email Address',
-                'temp_extent': 'YYYY-MM-DD/YYYY-MM-DD',
-            }
-        }
-    }
-)
 class GeoNodeCSWTest(GeoNodeBaseTestSupport):
     """Tests geonode.catalogue app/module"""
 
     def test_csw_base(self):
         """Verify that GeoNode works against any CSW"""
+        csw = get_catalogue(
+            backend={
+                'ENGINE': 'geonode.catalogue.backends.pycsw_local',
+                'URL': urljoin('http://localhost:8001/', '/catalogue/csw'),
+            },
+            skip_caps=False)
 
-        csw = get_catalogue(skip_caps=False)
+        self.assertEqual(
+            csw.catalogue.url,
+            urljoin('http://localhost:8001/', '/catalogue/csw')
+        )
 
         # test that OGC:CSW URLs are identical to what is defined in GeoNode
         for op in csw.catalogue.operations:
             for method in op.methods:
                 self.assertEqual(
-                    csw.catalogue.url,
+                    urljoin('http://localhost/', '/catalogue/csw'),
                     method['url'],
                     'Expected GeoNode URL to be equal to all CSW URLs')
 
@@ -149,8 +92,17 @@ class GeoNodeCSWTest(GeoNodeBaseTestSupport):
 
     def test_csw_search_count(self):
         """Verify that GeoNode CSW can handle search counting"""
+        csw = get_catalogue(
+            backend={
+                'ENGINE': 'geonode.catalogue.backends.pycsw_local',
+                'URL': urljoin('http://localhost:8001/', '/catalogue/csw'),
+            },
+            skip_caps=False)
 
-        csw = get_catalogue(skip_caps=False)
+        self.assertEqual(
+            csw.catalogue.url,
+            urljoin('http://localhost:8001/', '/catalogue/csw')
+        )
 
         # get all records
         csw.catalogue.getrecords(typenames='csw:Record')
