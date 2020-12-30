909f7722b9cb57c315fc1c03eb5920275106f048
Luis Saltron <luis.saltron@skaphe.com>
2020-12-30 14:45:33 -0500

    * Added library for polygon editing * Enabled copy polygon intake for editing * Enabled upload shapefile & GeoJSON * Enable geometries validations

--- geonode/waterproof_intake/static/waterproof_intake/js/intake_create.js
+++ geonode/waterproof_intake/static/waterproof_intake/js/intake_create.js
@@ -3,7 +3,7 @@
  * validations & interactions
  * @version 1.0
  */
-var urlParams = (function(url) {
+var urlParams = (function (url) {
     var result = new Object();
     var params = window.location.search.slice(1).split('&');
     for (var i = 0; i < params.length; i++) {
@@ -22,10 +22,18 @@ var snapMarker;
 var snapMarkerMapDelimit;
 var catchmentPoly;
 var catchmentPolyDelimit;
+var editablepolygon;
+var validPolygon;
+var isFile;
+var delimitationFileType;
+const delimitationFileEnum = {
+    GEOJSON: 'geojson',
+    SHP: 'shapefile'
+}
 var mapLoader;
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $("#intakeWECB").click(function() {
+    $("#intakeWECB").click(function () {
         $('#intakeECTAG tr').remove();
         $('#intakeEITA tr').remove();
         $('#autoAdjustHeightF').css("height", "auto");
@@ -42,7 +50,7 @@ $(document).ready(function() {
             for (let index = 0; index <= numberYearsInterpolationValue; index++) {
                 $('#intakeECTAG').append(`<tr>
                 <th class="text-center" scope="row">${index}</th>
-                <td class="text-center">${((m*index)+b).toFixed(2)}</td>
+                <td class="text-center">${((m * index) + b).toFixed(2)}</td>
               </tr>`);
             }
         }
@@ -55,7 +63,7 @@ $(document).ready(function() {
             for (let index = 1; index <= numberYearsInterpolationValue; index++) {
                 $('#intakeECTAG').append(`<tr>
                 <th class="text-center" scope="row">${index}</th>
-                <td class="text-center">${(b*(Math.pow(index,m)))}</td>
+                <td class="text-center">${(b * (Math.pow(index, m)))}</td>
               </tr>`);
             }
         }
@@ -68,7 +76,7 @@ $(document).ready(function() {
             for (let index = 0; index <= numberYearsInterpolationValue; index++) {
                 $('#intakeECTAG').append(`<tr>
                 <th class="text-center" scope="row">${index}</th>
-                <td class="text-center">${(b*(Math.exp(m*index)))}</td>
+                <td class="text-center">${(b * (Math.exp(m * index)))}</td>
               </tr>`);
             }
 
@@ -76,7 +84,7 @@ $(document).ready(function() {
 
         for (let index = 0; index < numberYearsInterpolationValue; index++) {
             $('#intakeEITA').append(`<tr>
-                  <th class="text-center" scope="col">${index+1}</th>
+                  <th class="text-center" scope="col">${index + 1}</th>
                   <td class="text-center" scope="col"><input type="text" class="form-control" required></td>
                   <td class="text-center" scope="col"><input type="text" class="form-control" required></td>
                   <td class="text-center" scope="col"><input type="text" class="form-control" required></td>
@@ -85,18 +93,18 @@ $(document).ready(function() {
         }
     });
 
-    $('#smartwizard').smartWizard("next").click(function() {
+    $('#smartwizard').smartWizard("next").click(function () {
         $('#autoAdjustHeightF').css("height", "auto");
         mapDelimit.invalidateSize();
         map.invalidateSize();
     });
 
-    $('#intakeNIBYMI').click(function() {
+    $('#intakeNIBYMI').click(function () {
         $('#intakeWEMI div').remove();
         intakeNIYMI = Number($("#intakeNIYMI").val());
         for (let index = 0; index < intakeNIYMI; index++) {
             $('#intakeWEMI').append(`<div class="form-group">
-                <label class="col-sm-1 control-label">${index+1}</label>
+                <label class="col-sm-1 control-label">${index + 1}</label>
                 <div class="col-sm-11">
                     <input type="text" class="form-control" required>
                 </div>
@@ -105,7 +113,7 @@ $(document).ready(function() {
     });
 
 
-    $('#smartwizard').smartWizard("next").click(function() {
+    $('#smartwizard').smartWizard("next").click(function () {
         $('#autoAdjustHeightF').css("height", "auto");
         map.invalidateSize();
     });
@@ -124,7 +132,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#smartwizard").on("showStep", function(e, anchorObject, stepIndex, stepDirection) {
+    $("#smartwizard").on("showStep", function (e, anchorObject, stepIndex, stepDirection) {
         if (stepIndex == 3) {
             if (catchmentPoly)
                 mapDelimit.fitBounds(catchmentPoly.getBounds());
@@ -132,7 +140,7 @@ $(document).ready(function() {
         }
     });
     map = L.map('map', {}).setView([4.1, -74.1], 5);
-    mapDelimit = L.map('mapid',{editable: true}).setView([4.1, -74.1], 5);
+    mapDelimit = L.map('mapid', { editable: true }).setView([4.1, -74.1], 5);
     var osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     });
@@ -156,12 +164,13 @@ $(document).ready(function() {
         markerType: L.marker, //optional default L.marker
         markerProps: {}, //optional default {},
         centerUserCoordinates: true,
-        labelFormatterLng: function(lng) { return lng + " lng" }, //optional default none,
-        labelFormatterLat: function(lat) { return lat + " lat" }, //optional default none      
+        labelFormatterLng: function (lng) { return lng + " lng" }, //optional default none,
+        labelFormatterLat: function (lat) { return lat + " lat" }, //optional default none      
     }).addTo(map);
 
     $("#validateBtn").on("click", validateCoordinateWithApi);
-    $('#btnDelimitArea').on("click",delimitIntakeArea)
+    $('#btnDelimitArea').on("click", delimitIntakeArea)
+    $('#btnValidateArea').on("click", validateIntakeArea)
     if (!mapLoader) {
         mapLoader = L.control.loader().addTo(map);
     }
@@ -171,7 +180,7 @@ $(document).ready(function() {
     createEditor(editorUrl);
 
     var menu1Tab = document.getElementById('mapid');
-    var observer2 = new MutationObserver(function() {
+    var observer2 = new MutationObserver(function () {
         if (menu1Tab.style.display != 'none') {
             mapDelimit.invalidateSize();
         }
@@ -180,7 +189,7 @@ $(document).ready(function() {
 
 });
 
-$('#btnaddcost').click(function() {
+$('#btnaddcost').click(function () {
 
     $('#intakeaddcost').append(`<div class="form-group">
                                     <label for="exampleInputEmail1">Costo:</label>
@@ -189,12 +198,75 @@ $('#btnaddcost').click(function() {
 
 });
 
-window.onbeforeunload = function() { return mxResources.get('changesLost'); };
+window.onbeforeunload = function () { return mxResources.get('changesLost'); };
 
-
-function delimitIntakeArea(){
+/** 
+ * Delimit manually the intake polygon
+ */
+function delimitIntakeArea() {
+    isFile = false;
+    var copyCoordinates = [];
     console.log('Delimiting');
-
+    var polygonKeys = Object.keys(catchmentPoly._layers);
+    var keyNamePolygon = polygonKeys[0];
+    var geometryCoordinates = catchmentPoly._layers[keyNamePolygon].feature.geometry.coordinates[0];
+    geometryCoordinates.forEach(function (geom) {
+        var coordinates = [];
+        coordinates.push(geom[1]);
+        coordinates.push(geom[0]);
+        copyCoordinates.push(coordinates);
+    })
+    editablepolygon = L.polygon(copyCoordinates, { color: 'red' });
+    editablepolygon.addTo(mapDelimit)
+    editablepolygon.enableEdit();
+    editablepolygon.on('dblclick', L.DomEvent.stop).on('dblclick', editablepolygon.toggleEdit);
+}
+function validateIntakeArea() {
+    var editablePolygonJson = editablepolygon.toGeoJSON();
+    var intakePolygonJson = catchmentPoly.toGeoJSON();
+    /** 
+    * Get filtered activities by transition id 
+    * @param {String} url   activities URL 
+    * @param {Object} data  transition id  
+    *
+    * @return {String} activities in HTML option format
+    */
+    $.ajax({
+        url: '/intake/validateGeometry/',
+        type: 'POST',
+        data: {
+            'editablePolygon': JSON.stringify(editablePolygonJson),
+            'intakePolygon': JSON.stringify(intakePolygonJson),
+            'isFile': JSON.stringify(isFile),
+            'typeDelimit': delimitationFileType
+        },
+        success: function (result) {
+            if (!result.validPolygon) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error de Geometría',
+                    text: 'El polígono editado no es válido, por favor intente de nuevo',
+                })
+            }
+            else if (!result.polygonContains) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'El polígono debe estar dentro del área de la captación',
+                    text: 'El polígono editado no es válido, por favor intente de nuevo',
+                })
+            }
+            else {
+                Swal.fire(
+                    'Excelente',
+                    'El polígono es válido y está dentro de la captación',
+                    'success'
+                )
+            }
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
 }
 
 /** 
@@ -202,19 +274,30 @@ function delimitIntakeArea(){
  * @param {HTML} dropdown Dropdown selected element
  */
 function changeFileEvent() {
-    $('#intakeArea').change(function(evt) {
+    $('#intakeArea').change(function (evt) {
         var file = evt.currentTarget.files[0];
         var extension = validExtension(file);
         // Validate file's extension
         if (extension.valid) { //Valid
             console.log('Extension valid!');
+            isFile = true;
             // Validate file's extension
             if (extension.extension == 'geojson') { //GeoJSON
                 var readerGeoJson = new FileReader();
-                readerGeoJson.onload = function(evt) {
+                readerGeoJson.onload = function (evt) {
                     var contents = evt.target.result;
                     geojson = JSON.parse(contents);
-                    loadFile(geojson, file.name);
+                    delimitationFileType = delimitationFileEnum.GEOJSON;
+                    let polygonStyle = {
+                        fillColor: "red",
+                        color: "#333333",
+                        weight: 0.2,
+                        fillOpacity: 0.3
+                    };
+                    editablepolygon = L.geoJSON(geojson, { style: polygonStyle })
+                    editablepolygon.addTo(mapDelimit);
+                    mapDelimit.fitBounds(editablepolygon.getBounds())
+                    //loadShapefile(geojson, file.name);
                 }
                 readerGeoJson.readAsText(file);
             } else { //Zip
@@ -225,10 +308,10 @@ function changeFileEvent() {
                     readPrj = false,
                     prj, coord = true;
                 var prjName;
-                reader.onload = function(evt) {
+                reader.onload = function (evt) {
                     var contents = evt.target.result;
-                    JSZip.loadAsync(file).then(function(zip) {
-                        zip.forEach(function(relativePath, zipEntry) {
+                    JSZip.loadAsync(file).then(function (zip) {
+                        zip.forEach(function (relativePath, zipEntry) {
                             filename = zipEntry.name.toLocaleLowerCase();
                             if (filename.indexOf(".shp") != -1) {
                                 readShp = true;
@@ -246,7 +329,7 @@ function changeFileEvent() {
                         });
                         // Valid shapefile with minimum files req
                         if (readShp && readDbf && readPrj && readShx) {
-                            zip.file(prjName).async("string").then(function(data) {
+                            zip.file(prjName).async("string").then(function (data) {
                                 prj = data;
                                 // Validar sistema de referencia
                                 if (!prj) {
@@ -258,19 +341,20 @@ function changeFileEvent() {
                                 }
                                 // Shapefile válido
                                 else {
-                                    shp(contents).then(function(shpToGeojson) {
+                                    shp(contents).then(function (shpToGeojson) {
                                         geojson = shpToGeojson;
+                                        delimitationFileType = delimitationFileEnum.SHP;
                                         let polygonStyle = {
                                             fillColor: "#337ab7",
                                             color: "#333333",
                                             weight: 0.2,
                                             fillOpacity: 0.3
                                         };
-                                        var layer = L.geoJSON(geojson, { style: polygonStyle })
-                                        layer.addTo(mapDelimit);
-                                        mapDelimit.fitBounds(layer.getBounds())
-                                            //loadShapefile(geojson, file.name);
-                                    }).catch(function(e) {
+                                        editablepolygon = L.geoJSON(geojson, { style: polygonStyle })
+                                        editablepolygon.addTo(mapDelimit);
+                                        mapDelimit.fitBounds(editablepolygon.getBounds())
+                                        //loadShapefile(geojson, file.name);
+                                    }).catch(function (e) {
                                         Swal.fire({
                                             icon: 'error',
                                             title: 'Error en shapefile',
@@ -316,7 +400,7 @@ function changeFileEvent() {
                         }
                     });
                 };
-                reader.onerror = function(event) {
+                reader.onerror = function (event) {
                     console.error("File could not be read! Code " + event.target.error.code);
                     //alert("El archivo no pudo ser cargado: " + event.target.error.code);
                 };
