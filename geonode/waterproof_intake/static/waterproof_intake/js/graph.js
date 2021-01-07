0d4ace89ef301dd3b915edad649630d8ab95c381
DanielParra21 <danielparrac26@gmail.com>
2021-01-07 16:25:25 -0500

    modify graph.js send functions to validations.js

--- geonode/waterproof_intake/static/waterproof_intake/js/graph.js
+++ geonode/waterproof_intake/static/waterproof_intake/js/graph.js
@@ -523,54 +523,11 @@ function onInit(editor) {
 
         //Load data from figure to html
         editor.graph.addListener(mxEvent.CLICK, function(sender, evt) {
-            $('#idDiagram').empty();
-            $('#titleDiagram').empty();
-            $('#aguaDiagram').val('');
-            $('#sedimentosDiagram').val('');
-            $('#nitrogenoDiagram').val('');
-            $('#fosforoDiagram').val('');
             selectedCell = evt.getProperty("cell");
-            if (selectedCell.getAttribute('name') == 'River') {
-                $('#aguaDiagram').prop('disabled', true);
-                $('#sedimentosDiagram').prop('disabled', true);
-                $('#nitrogenoDiagram').prop('disabled', true);
-                $('#fosforoDiagram').prop('disabled', true);
-            } else {
-                $('#aguaDiagram').prop('disabled', false);
-                $('#sedimentosDiagram').prop('disabled', false);
-                $('#nitrogenoDiagram').prop('disabled', false);
-                $('#fosforoDiagram').prop('disabled', false);
-            }
-            console.log(selectedCell)
-
-            if (selectedCell != undefined) {
-
-                $('#titleDiagram').text(selectedCell.getAttribute('name'));
-                $('#idDiagram').val(selectedCell.id);
-                if (selectedCell.getAttribute('resultdb') == undefined) return;
-                resultdb = JSON.parse(selectedCell.getAttribute('resultdb'));
-                if (resultdb.length == 0) return;
-                $('#titleDiagram').text(resultdb[0].fields.categorys);
-                // Clear Inputs
-
-                // Add Value to Panel Information Right on HTML
-                $('#aguaDiagram').val(resultdb[0].fields.predefined_transp_water_perc);
-                $('#sedimentosDiagram').val(resultdb[0].fields.predefined_sediment_perc);
-                $('#nitrogenoDiagram').val(resultdb[0].fields.predefined_nitrogen_perc);
-                $('#fosforoDiagram').val(resultdb[0].fields.predefined_phosphorus_perc);
-                // Add Validator 
-                $('#aguaDiagram').attr('min', resultdb[0].fields.minimal_transp_water_perc);
-                $('#aguaDiagram').attr('max', resultdb[0].fields.maximal_transp_water_perc);
-                $('#sedimentosDiagram').attr('min', resultdb[0].fields.minimal_sediment_perc);
-                $('#sedimentosDiagram').attr('max', resultdb[0].fields.maximal_sediment_perc);
-                $('#nitrogenoDiagram').attr('min', resultdb[0].fields.minimal_nitrogen_perc);
-                $('#nitrogenoDiagram').attr('max', resultdb[0].fields.maximal_nitrogen_perc);
-                $('#fosforoDiagram').attr('min', resultdb[0].fields.minimal_phosphorus_perc);
-                $('#fosforoDiagram').attr('max', resultdb[0].fields.maximal_phosphorus_perc);
-                funcost('((11126.6*text(Q)) + 30939.7)*1 + (0.24*((text(Csed) - 56)/56)) + (0.06*((text(CN) - 20)/20))');
-            }
-
-
+            // Clear Inputs
+            clearDataHtml(selectedCell, evt);
+            //console.log(selectedCell)
+            if (selectedCell != undefined) addData(selectedCell);
         });
 
         //Add value entered in sediments in the field resultdb
--- geonode/waterproof_intake/static/waterproof_intake/js/graphValidations.js
+++ geonode/waterproof_intake/static/waterproof_intake/js/graphValidations.js
@@ -1,8 +1,8 @@
-const connectionsType={
-    EC: {name:'Extraction connection', id:'EC', style:'Extraction_connection'},
-    EI: {name:'External input', id:'EI', style:'External_input'},
-    PL: {name:'Pipeline', id:'PL', style:'Pipeline'},
-    CN: {name:'Connection', id:'CN', style:'Connection'},
+const connectionsType = {
+    EC: { name: 'Extraction connection', id: 'EC', style: 'Extraction_connection' },
+    EI: { name: 'External input', id: 'EI', style: 'External_input' },
+    PL: { name: 'Pipeline', id: 'PL', style: 'Pipeline' },
+    CN: { name: 'Connection', id: 'CN', style: 'Connection' },
 }
 
 function customMenuForConnectors() {
@@ -73,4 +73,47 @@ function updateStyleLine(graph, cell, type) {
     cell.setValue(value);
     graph.model.setStyle(cell, type.style);
 
+}
+
+function clearDataHtml(cell, evt) {
+    $('#idDiagram').empty();
+    $('#titleDiagram').empty();
+    $('#aguaDiagram').val('');
+    $('#sedimentosDiagram').val('');
+    $('#nitrogenoDiagram').val('');
+    $('#fosforoDiagram').val('');
+    cell = evt.getProperty("cell");
+    var show = false;
+    if (cell.getAttribute('name') == 'River') show = true;
+    $('#aguaDiagram').prop('disabled', show);
+    $('#sedimentosDiagram').prop('disabled', show);
+    $('#nitrogenoDiagram').prop('disabled', show);
+    $('#fosforoDiagram').prop('disabled', show);
+
+}
+
+function addData(element) {
+
+    $('#titleDiagram').text(element.getAttribute('name'));
+    $('#idDiagram').val(element.id);
+    if (element.getAttribute('resultdb') == undefined) return;
+    resultdb = JSON.parse(element.getAttribute('resultdb'));
+    if (resultdb.length == 0) return;
+    $('#titleDiagram').text(resultdb[0].fields.categorys);
+
+    // Add Value to Panel Information Right on HTML
+    $('#aguaDiagram').val(resultdb[0].fields.predefined_transp_water_perc);
+    $('#sedimentosDiagram').val(resultdb[0].fields.predefined_sediment_perc);
+    $('#nitrogenoDiagram').val(resultdb[0].fields.predefined_nitrogen_perc);
+    $('#fosforoDiagram').val(resultdb[0].fields.predefined_phosphorus_perc);
+    // Add Validator 
+    $('#aguaDiagram').attr('min', resultdb[0].fields.minimal_transp_water_perc);
+    $('#aguaDiagram').attr('max', resultdb[0].fields.maximal_transp_water_perc);
+    $('#sedimentosDiagram').attr('min', resultdb[0].fields.minimal_sediment_perc);
+    $('#sedimentosDiagram').attr('max', resultdb[0].fields.maximal_sediment_perc);
+    $('#nitrogenoDiagram').attr('min', resultdb[0].fields.minimal_nitrogen_perc);
+    $('#nitrogenoDiagram').attr('max', resultdb[0].fields.maximal_nitrogen_perc);
+    $('#fosforoDiagram').attr('min', resultdb[0].fields.minimal_phosphorus_perc);
+    $('#fosforoDiagram').attr('max', resultdb[0].fields.maximal_phosphorus_perc);
+    funcost('((11126.6*text(Q)) + 30939.7)*1 + (0.24*((text(Csed) - 56)/56)) + (0.06*((text(CN) - 20)/20))');
 }
