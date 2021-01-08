009e56231455ab2eb4aa6dc450b9f126057cec31
Edwin Piragauta Vargas <edwin.piragauta@gmail.com>
2021-01-08 14:22:56 -0500

    Add django Translation flags plugin

--- geonode/settings.py
+++ geonode/settings.py
@@ -228,6 +228,12 @@ _DEFAULT_LANGUAGES = """(
     ('ko', '한국어'),
 )"""
 
+_DEFAULT_LANGUAGES = """(
+    ('en', 'English'),
+    ('es', 'Español'),    
+)"""
+
+
 LANGUAGES = ast.literal_eval(os.getenv('LANGUAGES', _DEFAULT_LANGUAGES))
 
 EXTRA_LANG_INFO = {
@@ -541,6 +547,9 @@ INSTALLED_APPS = (
 
     # Documentation
     'django_extensions',
+
+    # translation flags
+    'django_translation_flags',
     
 )
 
