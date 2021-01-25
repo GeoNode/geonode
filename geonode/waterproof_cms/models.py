bd5e082b5b92d5ba481b791c1727db727ed3e514
Edwin Piragauta Vargas <edwin.piragauta@gmail.com>
2021-01-25 15:28:27 -0500

    add waterproof cms from Wagtail

---
+++ geonode/waterproof_cms/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
