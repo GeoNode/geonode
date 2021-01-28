285b5622aed8f72e9daf00791203d0f5468709c1
afabiani <alessio.fabiani@geo-solutions.it>
2021-01-28 17:09:21 +0100

    [Fixes #6886] Swap static files finders

--- geonode/settings.py
+++ geonode/settings.py
@@ -312,8 +312,8 @@ STATICFILES_DIRS = os.getenv('STATICFILES_DIRS', _DEFAULT_STATICFILES_DIRS)
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
