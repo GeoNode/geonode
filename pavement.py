2c4445119d97ec5644c29de8d48736d940f84f25
afabiani <alessio.fabiani@gmail.com>
2021-01-29 13:36:52 +0100

    [Fixes #6897] [Backport to 3.x] Async generation of thumbs in order to avoid timeouts on map saving

--- pavement.py
+++ pavement.py
@@ -612,7 +612,7 @@ def start_django(options):
     sh('%s python -W ignore manage.py runserver %s %s' % (settings, bind, foreground))
 
     if 'django_celery_beat' not in INSTALLED_APPS:
-        sh("{} celery -A geonode.celery_app:app worker -B -E --statedb=worker.state -s celerybeat-schedule --loglevel=INFO --concurrency=10 -n worker1@%h {}".format(  # noqa
+        sh("{} celery -A geonode.celery_app:app worker --without-gossip --without-mingle -Ofair -B -E --statedb=worker.state -s celerybeat-schedule --loglevel=INFO --concurrency=10 -n worker1@%h {}".format(  # noqa
             settings,
             foreground
         ))
