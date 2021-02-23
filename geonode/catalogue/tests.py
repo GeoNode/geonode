8f9110c0e13f060a99ae76192e7c7eb4f28009f6
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2021-02-23 10:18:44 +0100

    [Fixes #6975] GeoNode client catalogue must use the abstract stripped of HTML tags (#6976)

--- geonode/catalogue/tests.py
+++ geonode/catalogue/tests.py
@@ -17,8 +17,15 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
+import logging
+
+from geonode.layers.models import Layer
 from geonode.tests.base import GeoNodeBaseTestSupport
+
 from geonode.catalogue import get_catalogue
+from geonode.catalogue.models import catalogue_post_save
+
+logger = logging.getLogger(__name__)
 
 
 class CatalogueTest(GeoNodeBaseTestSupport):
@@ -31,3 +38,23 @@ class CatalogueTest(GeoNodeBaseTestSupport):
 
         c = get_catalogue()
         self.assertIsNotNone(c)
+
+    def test_update_metadata_records(self):
+        layer = Layer.objects.first()
+        self.assertIsNotNone(layer)
+        layer.abstract = "<p>Test HTML abstract</p>"
+        layer.save()
+        self.assertEqual(layer.abstract, "<p>Test HTML abstract</p>")
+        self.assertEqual(layer.raw_abstract, "Test HTML abstract")
+        # refresh catalogue metadata records
+        catalogue_post_save(instance=layer, sender=layer.__class__)
+        # get all records
+        csw = get_catalogue()
+        record = csw.get_record(layer.uuid)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.identification.title, layer.title)
+        self.assertEqual(record.identification.abstract, layer.raw_abstract)
+        if len(record.identification.otherconstraints) > 0:
+            self.assertEqual(
+                record.identification.otherconstraints[0],
+                layer.raw_constraints_other)
