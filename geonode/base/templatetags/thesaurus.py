b27452f81d1f315fde200ebef1f9eb464c58be9e
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-02-24 11:26:07 +0100

    Resolves #6925: add thesauro title translation in facet, add new tests

--- geonode/base/templatetags/thesaurus.py
+++ geonode/base/templatetags/thesaurus.py
@@ -1,5 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django import template
-from geonode.base.models import Thesaurus
+from django.utils.translation import get_language
+from geonode.base.models import Thesaurus, ThesaurusLabel
 
 register = template.Library()
 
@@ -17,3 +19,19 @@ def get_thesaurus_title(thesaurus_id):
 @register.filter
 def get_thesaurus_date(thesaurus_id):
     return Thesaurus.objects.get(id=thesaurus_id).date
+
+
+@register.filter
+def get_name_translation(tidentifier):
+    lang = get_language()
+    available = Thesaurus.objects.filter(identifier=tidentifier)
+    if not available.exists():
+        raise ObjectDoesNotExist("Selected idenfifier does not exists")
+    lname = (
+        ThesaurusLabel.objects.values_list("label", flat=True)
+        .filter(thesaurus__identifier=tidentifier)
+        .filter(lang=lang)
+    )
+    if not lname:
+        return available.first().title
+    return lname.first()
