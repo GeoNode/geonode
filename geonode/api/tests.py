1c09ae911e9dc53a9d288a8f2ad83126301ec48d
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-03-11 15:12:46 +0100

    [Fixes #6925] Test fix thesaurustest class by adding all_public()

--- geonode/api/tests.py
+++ geonode/api/tests.py
@@ -536,8 +536,7 @@ class ThesaurusKeywordResourceTests(ResourceTestCaseMixin, TestCase):
 
     def setUp(self):
         super(ThesaurusKeywordResourceTests, self).setUp()
-        self.user = 'admin'
-        self.passwd = 'admin'
+        all_public()
         self.list_url = reverse("api_dispatch_list", kwargs={"api_name": "api", "resource_name": "thesaurus/keywords"})
 
     def test_api_will_return_a_valid_json_response(self):
