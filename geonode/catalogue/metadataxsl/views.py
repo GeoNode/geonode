9c76ecfad660aa45679980d95e7c6294f5c8df5b
Emanuele Tajariol <etj@geo-solutions.it>
2021-03-18 10:43:26 +0100

    [Backport to 3.2.x][Fixes #7097] Allow XSL customization (#7100)
    Co-authored-by: Alessio Fabiani <alessio.fabiani@geo-solutions.it>

--- geonode/catalogue/metadataxsl/views.py
+++ geonode/catalogue/metadataxsl/views.py
@@ -66,8 +66,9 @@ def prefix_xsl_line(req, id):
             "Resource Metadata not available!"
         )
     site_url = settings.SITEURL.rstrip('/') if settings.SITEURL.startswith('http') else settings.SITEURL
-    xsl_path = f'{site_url}/static/metadataxsl/metadata.xsl'
-    xsl_line = f'<?xml-stylesheet type="text/xsl" href="{xsl_path}"?>'
+    xsl_static = getattr(settings, 'CATALOG_METADATA_XSL', '/static/metadataxsl/metadata.xsl')
+    xsl_path = f'{site_url}{xsl_static}'
+    xsl_line = '<?xml-stylesheet type="text/xsl" href="{}"?>'.format(xsl_path)
 
     return HttpResponse(
         xsl_line + xml,
