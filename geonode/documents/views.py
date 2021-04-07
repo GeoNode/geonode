6d72f8445aaae88566fb301a844c14f7e8fa218a
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2021-04-07 11:48:10 +0200

    [Fixes #7224] Harmonize/improve metadata editing pages (#7240)
    * [Fixes #7224] Harmonize/improve metadata editing pages
    
    * [Fixes #7224] Harmonize/improve metadata editing pages
    
    * [Fixes #7224] Harmonize/improve metadata editing pages, typos removed
    
    * [Fixes #7224] Question mark missing
    
    Co-authored-by: Alessio Fabiani <alessio.fabiani@geo-solutions.it>
    (cherry picked from commit 6a742ddc7429c0e2b6f7d460561cbb43e29a511d)

--- geonode/documents/views.py
+++ geonode/documents/views.py
@@ -551,6 +551,7 @@ def document_metadata(
         "metadata_author_groups": metadata_author_groups,
         "TOPICCATEGORY_MANDATORY": getattr(settings, 'TOPICCATEGORY_MANDATORY', False),
         "GROUP_MANDATORY_RESOURCES": getattr(settings, 'GROUP_MANDATORY_RESOURCES', False),
+        "UI_MANDATORY_FIELDS": ['title', 'abstract', 'doi', 'attribution', 'data_quality_statement', 'restriction_code_type']
     })
 
 
