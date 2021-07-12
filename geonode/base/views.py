6f4b1dfcdc8e9dd6a54d939e01138111fd2c271a
mattiagiupponi <mattia.giupponi@gmail.com>
2021-07-12 15:31:40 +0200

    Fix flake8 formatting

--- geonode/base/views.py
+++ geonode/base/views.py
@@ -63,9 +63,11 @@ def get_url_for_app_model(model, model_class):
     return reverse(f'admin:{model_class._meta.app_label}_{model}_changelist')
     # was: return f'/admin/{model_class._meta.app_label}/{model}/'
 
+
 def get_url_for_model(model):
     return reverse(f'admin:{model.lower()}s_{model.lower()}_changelist')
     # was: f'/admin/{model.lower()}s/{model.lower()}/'
+
 
 def user_and_group_permission(request, model):
     if not request.user.is_superuser:
