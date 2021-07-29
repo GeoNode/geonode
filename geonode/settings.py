77f217c202e7d3f5d182c295b98bb12b29c8130a
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2021-07-29 09:01:54 +0200

    [Backport 3.3.x][Fixes #7764] Implement GeoApp for Dashboards (#7879)
    * Dashboard skeleton (#1)
    
    Skeleton for dashboards integrations
    
    * Fix req
    
    * Update requirements.txt
    
    * Update requirements
    
    * Fix setupcfg for build
    
    * Fix flake8 issues
    
    * [Fixes #7764] Fix broken tests on geonode.layers.tests.LayersTest
    
    Co-authored-by: Alessio Fabiani <alessio.fabiani@geo-solutions.it>

--- geonode/settings.py
+++ geonode/settings.py
@@ -1514,6 +1514,7 @@ if GEONODE_CLIENT_LAYER_PREVIEW_LIBRARY == 'mapstore':
             'mapstore2_adapter',
             'mapstore2_adapter.geoapps',
             'mapstore2_adapter.geoapps.geostories',
+            'mapstore2_adapter.geoapps.dashboards',
             'geonode_mapstore_client',)
 
     def get_geonode_catalogue_service():
