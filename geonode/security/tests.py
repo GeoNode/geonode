cf0b53eba9b52c5c23c4bc7011fe1b3224211aa9
afabiani <alessio.fabiani@geo-solutions.it>
2021-08-06 15:23:40 +0200

    [CircleCI] Test fixes

--- geonode/security/tests.py
+++ geonode/security/tests.py
@@ -1669,7 +1669,7 @@ class SecurityTests(ResourceTestCaseMixin, GeoNodeBaseTestSupport):
                     },
                     {
                         'id': 3,
-                        'logo': 'http://localhost:8000/static/geonode/img/missing_thumb.png',
+                        'logo': f'{settings.SITEURL}static/geonode/img/missing_thumb.png',
                         'name': 'registered-members',
                         'permissions': 'none',
                         'title': 'Registered Members'
