0453f29d346f4bbf3d12f35211e58d55d827c0ee
giohappy <giohappy@gmail.com>
2021-08-31 18:23:17 +0200

    fix typo

--- geonode/settings.py
+++ geonode/settings.py
@@ -1968,7 +1968,7 @@ THUMBNAIL_BACKGROUND = {
     # example options for a TMS service
     # 'class': 'geonode.thumbs.background.GenericXYZBackground',
     #'options': {
-    #    'url': 'http://backoffice-maps.geosolutionsgroup.com/geoserver/gwc/service/tms/1.0.0/osm%3Aosm_simple_light@EPSG%3A900913@png/{z}/{x}/{y}.png',
+    #    'url': 'http://maps.geosolutionsgroup.com/geoserver/gwc/service/tms/1.0.0/osm%3Aosm_simple_light@EPSG%3A900913@png/{z}/{x}/{y}.png',
     #    'tms': True
     #},
 }
