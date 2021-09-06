81be7e2499a7b0b6fdbede9c9a44af528479713d
afabiani <alessio.fabiani@geo-solutions.it>
2021-09-06 11:05:48 +0200

    [CircleCI] Fix test cases

--- geonode/services/tests.py
+++ geonode/services/tests.py
@@ -726,7 +726,7 @@ class WmsServiceHandlerTestCase(GeoNodeBaseTestSupport):
                 Layer.objects.filter(id=_d.id).delete()
             self.assertEqual(HarvestJob.objects.filter(service=geonode_service,
                                                        resource_id=geonode_layer.alternate).count(), 0)
-            legend_url = handler._create_layer_legend_link(geonode_layer)
+            legend_url = handler._create_layer_legend_link(geonode_service, geonode_layer)
             self.assertTrue('sld_version=1.1.0' in str(legend_url))
         except Service.DoesNotExist as e:
             # In the case the Service URL becomes inaccessible for some reason
