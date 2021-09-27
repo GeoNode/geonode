572dbe275f764c8c7f0018ef6d045fd6bc3da10d
NAGGINDA MARTHA <marthamareal@gmail.com>
2021-09-27 17:04:58 +0300

    [3.2.x][Fixes #8058] GeoStory: missing actions in user profile page (#8067)

--- geonode/static/geonode/js/search/search.js
+++ geonode/static/geonode/js/search/search.js
@@ -611,6 +611,13 @@
                 query_entry = $scope.query[data_filter];
             }
 
+            if (data_filter === 'app_type__in'){
+                delete ($scope.query['type__in']);
+            }
+            else {
+                delete ($scope.query['app_type__in']);
+            }
+
             if (!element.hasClass('selected')) {
                 // Add the entry in the correct query
                 query_entry = value;
@@ -619,7 +626,7 @@
                 element.parents('ul').find('a').removeClass('selected');
 
                 element.addClass('selected');
-
+    
                 //save back the new query entry to the scope query
                 $scope.query[data_filter] = query_entry;
 
