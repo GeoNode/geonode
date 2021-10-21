bf1b2cdffbea6c542f46520f0a9fdbc8e10bcc96
Ricardo Garcia Silva <ricardo@kartoza.com>
2021-10-21 15:55:48 +0100

    Update code

--- geonode/harvesting/tests/test_tasks.py
+++ geonode/harvesting/tests/test_tasks.py
@@ -16,7 +16,6 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
-import datetime as dt
 from unittest import mock
 
 from django.contrib.auth import get_user_model
