4fc774f5f6a33b0356c426021f3afe58cac3ed2c
Ricardo Garcia Silva <ricardo@kartoza.com>
2021-12-23 12:03:22 +0000

    Make the view_or_basicauh() method look for the current user in request
    This shall allow this to work with either GUI or API interactions. The previous implementation relied on django.contrib.gis.auth.get_user(), which assumes we are working in the context of a django HTTP session - and the REST API does not require sessions.

--- geonode/decorators.py
+++ geonode/decorators.py
@@ -65,7 +65,7 @@ def view_or_basicauth(view, request, test_func, realm="", *args, **kwargs):
     are already logged in or if they have provided proper http-authorization
     and returning the view if all goes well, otherwise responding with a 401.
     """
-    if test_func(auth.get_user(request)):
+    if test_func(request.user):
         # Already logged in, just return the view.
         #
         return view(request, *args, **kwargs)
