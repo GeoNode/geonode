3a49b631becafa30488b43586188cf2d5fc5353a
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2022-01-13 09:38:18 +0100

    [Fixes #8551] raster uploads require sending the same file twice (#8586)
    * [Fixes #8551] raster uploads require sending the same file twice
    
    * [Fixes #8551] raster uploads require sending the same file twice

--- geonode/upload/api/tests.py
+++ geonode/upload/api/tests.py
@@ -211,9 +211,6 @@ class UploadApiTests(GeoNodeLiveTestSupport, APITestCase):
                 # allow for that
                 if os.path.exists(file_path):
                     params[spatial_file] = open(file_path, 'rb')
-        elif ext.lower() == '.tif':
-            file_path = base + ext
-            params['tif_file'] = open(file_path, 'rb')
 
         with open(_file, 'rb') as base_file:
             params['base_file'] = base_file
@@ -238,9 +235,6 @@ class UploadApiTests(GeoNodeLiveTestSupport, APITestCase):
         for spatial_file in spatial_files:
             if isinstance(params.get(spatial_file), IOBase):
                 params[spatial_file].close()
-
-        if isinstance(params.get("tif_file"), IOBase):
-            params['tif_file'].close()
 
         try:
             logger.error(f" -- response: {response.status_code} / {response.json()}")
@@ -274,9 +268,6 @@ class UploadApiTests(GeoNodeLiveTestSupport, APITestCase):
                 # allow for that
                 if os.path.exists(file_path):
                     params[spatial_file] = open(file_path, 'rb')
-        elif ext.lower() == '.tif':
-            file_path = base + ext
-            params['tif_file'] = open(file_path, 'rb')
 
         with open(_file, 'rb') as base_file:
             params['base_file'] = base_file
@@ -293,9 +284,6 @@ class UploadApiTests(GeoNodeLiveTestSupport, APITestCase):
         for spatial_file in spatial_files:
             if isinstance(params.get(spatial_file), IOBase):
                 params[spatial_file].close()
-
-        if isinstance(params.get("tif_file"), IOBase):
-            params['tif_file'].close()
 
         try:
             logger.error(f" -- response: {response.status_code} / {response.json()}")
