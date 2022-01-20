fa4536e768522e690e59efbeb153bb95adb75784
marthamareal <marthamareal@gmail.com>
2022-01-20 20:33:55 +0300

    - add unit tests

---
+++ geonode/geoserver/tests/test_tasks.py
@@ -0,0 +1,55 @@
+import os
+
+from unittest.mock import create_autospec
+
+from geonode.base.populate_test_data import all_public, create_models, remove_models
+from geonode.geoserver.tasks import geoserver_create_style
+from geonode.geoserver.signals import geoserver_style_visual_mode
+from geonode.layers.models import Dataset
+from geonode.layers.populate_datasets_data import create_dataset_data
+from geonode.tests.base import GeoNodeBaseTestSupport
+
+
+class TasksTest(GeoNodeBaseTestSupport):
+
+    type = 'dataset'
+
+    fixtures = [
+        'initial_data.json',
+        'group_test_data.json',
+        'default_oauth_apps.json'
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        create_models(type=cls.get_type, integration=cls.get_integration)
+        all_public()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        remove_models(cls.get_obj_ids, type=cls.get_type, integration=cls.get_integration)
+
+    def setUp(self):
+        super().setUp()
+        create_dataset_data()
+
+    def mock_signal_callback(self, instance, **kwargs):
+        pass
+
+    def test_geoserver_style_visual_mode_automatically_with_sld_file(self):
+        dataset = Dataset.objects.first()
+        sld_file = "geonode/base/fixtures/test_sld.sld"
+        handler = create_autospec(self.mock_signal_callback)
+
+        geoserver_create_style(dataset.id, dataset.name,  sld_file=sld_file, tempdir=None)
+        self.assertEqual(handler.call_count, 0)
+
+    def test_geoserver_style_visual_mode_automatically_without_sld_file(self):
+        dataset = Dataset.objects.first()
+        handler = create_autospec(self.mock_signal_callback)
+
+        geoserver_style_visual_mode.connect(handler)
+        geoserver_create_style(dataset.id, dataset.name,  sld_file=None, tempdir=None)
+        handler.assert_called_once_with(signal=geoserver_style_visual_mode, sender=dataset, instance=dataset)
