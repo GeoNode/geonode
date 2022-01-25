a08bb72c64dbddb8723cfe8544b079c4ccc9c4ab
afabiani <alessio.fabiani@geo-solutions.it>
2022-01-25 11:30:45 +0100

    [CircleCi] Test Fixes

--- geonode/upload/api/tests.py
+++ geonode/upload/api/tests.py
@@ -476,7 +476,7 @@ class UploadApiTests(GeoNodeLiveTestSupport, APITestCase):
         # Try to upload a good raster file and check the session IDs
         fname = os.path.join(GOOD_DATA, 'raster', 'relief_san_andres.tif')
         resp, data = self.rest_upload_file(fname)
-        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(resp.status_code, 200)
         resource_id = data['url'].split('/')[-1]
         upload = Upload.objects.get(resource_id=resource_id)
         # temp folder is deleted after successful upload
