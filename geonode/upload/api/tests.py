f86ed8489dc194a24ee8350b2a0ce984397ef8ad
Italo Fernandes <italogsfernandes@gmail.com>
2022-02-17 13:48:34 +0000

    Revert "[Fixes #8736] Minor changes to tests"
    This reverts commit 32e78ff9b1cd5f736d2b63bb5fbab3b877467ff3.

--- geonode/upload/api/tests.py
+++ geonode/upload/api/tests.py
@@ -279,7 +279,8 @@ class UploadApiTests(GeoNodeLiveTestSupport, APITestCase):
             url = urljoin(
                 f"{reverse('uploads-list')}/",
                 'upload/')
-            params["interactive"] = 'false' if non_interactive else 'true'
+            if non_interactive:
+                params["interactive"] = 'false'
             logger.error(f" ---- UPLOAD URL: {url}")
             response = self.client.put(url, data=params)
 
