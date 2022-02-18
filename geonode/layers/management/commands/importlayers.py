36a525e0b45f9e7945521d527f99923717aa942c
Italo Fernandes <italogsfernandes@gmail.com>
2022-02-18 17:38:50 +0000

    [Fixes #8736] Backport importlayers from master

--- geonode/layers/management/commands/importlayers.py
+++ geonode/layers/management/commands/importlayers.py
@@ -1,6 +1,6 @@
 #########################################################################
 #
-# Copyright (C) 2016 OSGeo
+# Copyright (C) 2022 OSGeo
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,229 +16,154 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
+import os
+import argparse
+import datetime
+import requests
+import traceback
+
+from urllib.parse import urljoin
+from io import BufferedReader, IOBase
+from requests.auth import HTTPBasicAuth
 
 from django.utils import timezone
 from django.core.management.base import BaseCommand
-from geonode.layers.utils import upload
-from geonode.people.utils import get_valid_user
-import traceback
-import datetime
+
+parser = argparse.ArgumentParser()
 
 
 class Command(BaseCommand):
-    help = ("Brings a data file or a directory full of data files into a"
-            " GeoNode site.  Layers are added to the Django database, the"
-            " GeoServer configuration, and the pycsw metadata index.")
+    help = (
+        "Brings a data file or a directory full of data files into a"
+        " GeoNode site.  Layers are added to the Django database, the"
+        " GeoServer configuration, and the pycsw metadata index."
+    )
 
     def add_arguments(self, parser):
         # Positional arguments
-        parser.add_argument('path', nargs='*', help='path [path...]')
+        parser.add_argument("path", nargs="*", help="path [path...]")
 
-        # Named (optional) arguments
-        parser.add_argument(
-            '-u',
-            '--user',
-            dest="user",
-            default=None,
-            help="Name of the user account which should own the imported layers")
+        parser.add_argument("-hh", "--host", dest="host", help="Geonode host url")
 
-        parser.add_argument(
-            '-i',
-            '--ignore-errors',
-            action='store_true',
-            dest='ignore_errors',
-            default=False,
-            help='Stop after any errors are encountered.')
+        parser.add_argument("-u", "--username", dest="username", help="Geonode username")
 
-        parser.add_argument(
-            '-o',
-            '--overwrite',
-            dest='overwrite',
-            default=False,
-            action="store_true",
-            help="Overwrite existing layers if discovered (defaults False)")
-
-        parser.add_argument(
-            '-k',
-            '--keywords',
-            dest='keywords',
-            default="",
-            help=("The default keywords, separated by comma, for the imported"
-                  " layer(s). Will be the same for all imported layers"
-                  " if multiple imports are done in one command"))
-
-        parser.add_argument(
-            '-l',
-            '--license',
-            dest='license',
-            default=None,
-            help=("The license for the imported layer(s). Will be the same for"
-                  " all imported layers if multiple imports are done"
-                  " in one command"))
-
-        parser.add_argument(
-            '-c',
-            '--category',
-            dest='category',
-            default=None,
-            help=("The category for the imported layer(s). Will be the same"
-                  " for all imported layers if multiple imports are done"
-                  " in one command"))
-
-        parser.add_argument(
-            '-r',
-            '--regions',
-            dest='regions',
-            default="",
-            help=("The default regions, separated by comma, for the imported"
-                  " layer(s). Will be the same for all imported layers if"
-                  " multiple imports are done in one command"))
-
-        parser.add_argument(
-            '-n',
-            '--name',
-            dest='layername',
-            default=None,
-            help="The name for the imported layer(s). Can not be used with multiple imports")
-
-        parser.add_argument(
-            '-t',
-            '--title',
-            dest='title',
-            default=None,
-            help=("The title for the imported layer(s). Will be the same for"
-                  " all imported layers if multiple imports are done"
-                  " in one command"))
-
-        parser.add_argument(
-            '-a',
-            '--abstract',
-            dest='abstract',
-            default=None,
-            help=("The abstract for the imported layer(s). Will be the same for"
-                  "all imported layers if multiple imports are done"
-                  "in one command"))
-
-        parser.add_argument(
-            '-d',
-            '--date',
-            dest='date',
-            default=None,
-            help=('The date and time for the imported layer(s). Will be the '
-                  'same for all imported layers if multiple imports are done '
-                  'in one command. Use quotes to specify both the date and '
-                  'time in the format \'YYYY-MM-DD HH:MM:SS\'.'))
-
-        parser.add_argument(
-            '-p',
-            '--private',
-            dest='private',
-            default=False,
-            action="store_true",
-            help="Make layer viewable only to owner")
-
-        parser.add_argument(
-            '-m',
-            '--metadata_uploaded_preserve',
-            dest='metadata_uploaded_preserve',
-            default=False,
-            action="store_true",
-            help="Force metadata XML to be preserved")
-
-        parser.add_argument(
-            '-C',
-            '--charset',
-            dest='charset',
-            default='UTF-8',
-            help=("Specify the charset of the data"))
+        parser.add_argument("-p", "--password", dest="password", help="Geonode password")
 
     def handle(self, *args, **options):
-        verbosity = int(options.get('verbosity'))
-        # ignore_errors = options.get('ignore_errors')
-        username = options.get('user')
-        user = get_valid_user(username)
-        overwrite = options.get('overwrite')
-        name = options.get('layername', None)
-        title = options.get('title', None)
-        abstract = options.get('abstract', None)
-        date = options.get('date', None)
-        license = options.get('license', None)
-        category = options.get('category', None)
-        private = options.get('private', False)
-        metadata_uploaded_preserve = options.get('metadata_uploaded_preserve',
-                                                 False)
-        charset = options.get('charset', 'UTF-8')
+        if not len(options["path"]) > 0:
+            self.print_help('manage.py', 'importlayers')
+            return
 
-        if verbosity > 0:
-            console = self.stdout
-        else:
-            console = None
+        host = options.get("host") or "http://localhost:8000"
+        username = options.get("username") or "admin"
+        password = options.get("password") or "admin"
 
-        skip = not overwrite
-
-        keywords = options.get('keywords').split(',')
-        if len(keywords) == 1 and keywords[0] == '':
-            keywords = []
-        else:
-            keywords = [k.strip() for k in keywords]
-        regions = options.get('regions').split(',')
-        if len(regions) == 1 and regions[0] == '':
-            regions = []
-        else:
-            regions = [r.strip() for r in regions]
         start = datetime.datetime.now(timezone.get_current_timezone())
-        output = []
+        for path in options["path"]:
+            success, errors = GeoNodeUploader(
+                host=host, username=username, password=password, folder_path=path
+            ).execute()
 
-        for path in options['path']:
-            out = upload(
-                path,
-                user=user,
-                overwrite=overwrite,
-                skip=skip,
-                name=name,
-                title=title,
-                abstract=abstract,
-                date=date,
-                keywords=keywords,
-                verbosity=verbosity,
-                console=console,
-                license=license,
-                category=category,
-                regions=regions,
-                private=private,
-                metadata_uploaded_preserve=metadata_uploaded_preserve,
-                charset=charset)
+            finish = datetime.datetime.now(timezone.get_current_timezone())
+            td = finish - start
+            duration = td.microseconds / 1000000 + td.seconds + td.days * 24 * 3600
 
-            output.extend(out)
+            print(f"{(duration * 1.0 / len(os.listdir(options['path'][0])))} seconds per layer")
 
-        updated = [dict_['file']
-                   for dict_ in output if dict_['status'] == 'updated']
-        created = [dict_['file']
-                   for dict_ in output if dict_['status'] == 'created']
-        skipped = [dict_['file']
-                   for dict_ in output if dict_['status'] == 'skipped']
-        failed = [dict_['file']
-                  for dict_ in output if dict_['status'] == 'failed']
+            output = {"success": success, "errors": errors}
+            print(f"Output data: {output}")
 
-        finish = datetime.datetime.now(timezone.get_current_timezone())
-        td = finish - start
-        duration = td.microseconds / 1000000 + td.seconds + td.days * 24 * 3600
-        duration_rounded = round(duration, 2)
 
-        if verbosity > 1:
-            print("\nDetailed report of failures:")
-            for dict_ in output:
-                if dict_['status'] == 'failed':
-                    print("\n\n", dict_['file'], "\n================")
-                    traceback.print_exception(dict_['exception_type'],
-                                              dict_['error'],
-                                              dict_['traceback'])
+class GeoNodeUploader:
 
-        if verbosity > 0:
-            print(f"\n\nFinished processing {len(output)} layers in {duration_rounded} seconds.\n")
-            print(f"{len(created)} Created layers")
-            print(f"{len(updated)} Updated layers")
-            print(f"{len(skipped)} Skipped layers")
-            print(f"{len(failed)} Failed layers")
-            if len(output) > 0:
-                print(f"{(duration * 1.0 / len(output))} seconds per layer")
+    def __init__(
+        self,
+        host: str,
+        folder_path: str,
+        username: str,
+        password: str,
+        call_delay: int = 10,
+        **kwargs,
+    ):
+        self.host = host
+        self.folder_path = folder_path
+        self.username = username
+        self.password = password
+        self.call_delay = call_delay
+
+    def execute(self):
+        success = []
+        errors = []
+        for root, subdirs, files in os.walk(self.folder_path):
+            for file in files:
+                _file = os.path.join(root, file)
+                print(f"Scanning: {_file}")
+                if not os.path.exists(_file):
+                    print(f"The selected file path does not exist: {_file}")
+                    continue
+                spatial_files = ("dbf_file", "shx_file", "prj_file")
+                base, ext = os.path.splitext(_file)
+                params = {
+                    # make public since wms client doesn't do authentication
+                    "permissions": '{ "users": {"AnonymousUser": ["view_resourcebase"]} , "groups":{}}',  # to be decided
+                    "time": "false",
+                    "dataset_title": file,
+                    "charset": "UTF-8",
+                }
+
+                if ext.lower() == ".shp":
+                    for spatial_file in spatial_files:
+                        ext, _ = spatial_file.split("_")
+                        file_path = f"{base}.{ext}"
+                        # sometimes a shapefile is missing an extra file,
+                        # allow for that
+                        if os.path.exists(file_path):
+                            params[spatial_file] = open(file_path, "rb")
+                elif ext.lower() == ".tif":
+                    file_path = base + ext
+                    params["tif_file"] = open(file_path, "rb")
+                else:
+                    continue
+
+                files = {}
+
+                client = requests.session()
+
+                with open(_file, "rb") as base_file:
+                    params["base_file"] = base_file
+                    for name, value in params.items():
+                        if isinstance(value, BufferedReader):
+                            files[name] = (os.path.basename(value.name), value)
+                            params[name] = os.path.basename(value.name)
+
+                    params["non_interactive"] = 'true'
+                    response = client.post(
+                        urljoin(self.host, "/api/v2/uploads/upload/"),
+                        auth=HTTPBasicAuth(self.username, self.password),
+                        data=params,
+                        files=files,
+                    )
+                    print(f"{file}: {response.status_code}")
+
+                if isinstance(params.get("tif_file"), IOBase):
+                    params["tif_file"].close()
+
+                try:
+                    data = response.json()
+                    if data['status'] == 'finished':
+                        if data['success']:
+                            success.append(file)
+                        else:
+                            errors.append(file)
+                    else:
+                        errors.append(file)
+                except requests.exceptions.JSONDecodeError:
+                    traceback.print_exc()
+                    errors.append(file)
+        return success, errors
+
+    @staticmethod
+    def _get_upload_id(upload_response, import_id):
+        for item in upload_response.json()["uploads"]:
+            if item.get("import_id", None) == import_id:
+                return item.get("id", None)
