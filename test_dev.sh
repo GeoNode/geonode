92ee9456355409cf6288a04175f17399f68b9f29
afabiani <alessio.fabiani@gmail.com>
2022-02-22 10:48:13 +0100

    [Backport to 3.3.x][Fixes #8844] Typo in valid user invite submission message

--- test_dev.sh
+++ test_dev.sh
@@ -4,5 +4,5 @@ set -a
 set +a
 
 # paver setup_data
-dropdb -U postgres test_geonode
-coverage run --branch --source=geonode manage.py test -v 3 $@
+# dropdb -U postgres test_geonode
+coverage run --branch --source=geonode manage.py test -v 3 --keepdb $@
