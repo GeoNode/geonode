34ac392d3b91124f495768b7720dcb96f6ea369c
mattiagiupponi <mattia.giupponi@gmail.com>
2022-05-09 10:47:52 +0200

    [Fixes #9283] Fix typo

--- geonode/base/templatetags/base_tags.py
+++ geonode/base/templatetags/base_tags.py
@@ -478,7 +478,7 @@ def display_edit_request_button(resource, user, perms):
             resource.BASE_PERMISSIONS.get('write')
         )
 
-        if resource.polymorphic_ctype.model in ['layer', 'documents']:
+        if resource.polymorphic_ctype.model in ['layer', 'document']:
             '''
             The download resource permission should be available only
             if the resource is a layer or Documents. You cant downlod maps
