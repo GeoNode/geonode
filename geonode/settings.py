44d49564a4ddb42ed1867ce4554d67fcd29ca8d4
mattiagiupponi <mattia.giupponi@gmail.com>
2022-07-05 10:39:22 +0200

    [FIxes #9591] Expose supported upload file formats as configurations

--- geonode/settings.py
+++ geonode/settings.py
@@ -1584,56 +1584,6 @@ if GEONODE_CLIENT_LAYER_PREVIEW_LIBRARY == 'mapstore':
     MAPSTORE_EXTENSIONS_FOLDER_PATH = '/static/mapstore/extensions/'
 
     # Supported Dataset file types for uploading Datasets. This setting is being from from the client
-    MAPSTORE_SUPPORTED_DATASET_FILE_TYPES = [
-        {
-            "id": "shp",
-            "label": "ESRI Shapefile",
-            "format": "vector",
-            "ext": ["shp"],
-            "requires": ["shp", "prj", "dbf", "shx"],
-            "optional": ["xml", "sld"]
-        },
-        {
-            "id": "tiff",
-            "label": "GeoTIFF",
-            "format": "raster",
-            "ext": ["tiff", "tif"],
-            "mimeType": ["image/tiff"],
-            "optional": ["xml", "sld"]
-        },
-        {
-            "id": "csv",
-            "label": "Comma Separated Value (CSV)",
-            "format": "vector",
-            "ext": ["csv"],
-            "mimeType": ["text/csv"],
-            "optional": ["xml", "sld"]
-        },
-        {
-            "id": "zip",
-            "label": "Zip Archive",
-            "format": "archive",
-            "ext": ["zip"],
-            "mimeType": ["application/zip"],
-            "optional": ["xml", "sld"]
-        },
-        {
-            "id": "xml",
-            "label": "XML Metadata File",
-            "format": "metadata",
-            "ext": ["xml"],
-            "mimeType": ["application/json"],
-            "needsFiles": ["shp", "prj", "dbf", "shx", "csv", "tiff", "zip", "sld"]
-        },
-        {
-            "id": "sld",
-            "label": "Styled Layer Descriptor (SLD)",
-            "format": "metadata",
-            "ext": ["sld"],
-            "mimeType": ["application/json"],
-            "needsFiles": ["shp", "prj", "dbf", "shx", "csv", "tiff", "zip", "xml"]
-        }
-    ]
 
 # -- END Client Hooksets Setup
 
@@ -2204,3 +2154,54 @@ to evaluate if the file is greater than the limit size defined
 '''
 
 SIZE_RESTRICTED_FILE_UPLOAD_ELEGIBLE_URL_NAMES = ("data_upload", "uploads-upload", "document_upload",)
+
+SUPPORTED_DATASET_FILE_TYPES = [
+        {
+            "id": "shp",
+            "label": "ESRI Shapefile",
+            "format": "vector",
+            "ext": ["shp"],
+            "requires": ["shp", "prj", "dbf", "shx"],
+            "optional": ["xml", "sld"]
+        },
+        {
+            "id": "tiff",
+            "label": "GeoTIFF",
+            "format": "raster",
+            "ext": ["tiff", "tif"],
+            "mimeType": ["image/tiff"],
+            "optional": ["xml", "sld"]
+        },
+        {
+            "id": "csv",
+            "label": "Comma Separated Value (CSV)",
+            "format": "vector",
+            "ext": ["csv"],
+            "mimeType": ["text/csv"],
+            "optional": ["xml", "sld"]
+        },
+        {
+            "id": "zip",
+            "label": "Zip Archive",
+            "format": "archive",
+            "ext": ["zip"],
+            "mimeType": ["application/zip"],
+            "optional": ["xml", "sld"]
+        },
+        {
+            "id": "xml",
+            "label": "XML Metadata File",
+            "format": "metadata",
+            "ext": ["xml"],
+            "mimeType": ["application/json"],
+            "needsFiles": ["shp", "prj", "dbf", "shx", "csv", "tiff", "zip", "sld"]
+        },
+        {
+            "id": "sld",
+            "label": "Styled Layer Descriptor (SLD)",
+            "format": "metadata",
+            "ext": ["sld"],
+            "mimeType": ["application/json"],
+            "needsFiles": ["shp", "prj", "dbf", "shx", "csv", "tiff", "zip", "xml"]
+        }
+    ]
