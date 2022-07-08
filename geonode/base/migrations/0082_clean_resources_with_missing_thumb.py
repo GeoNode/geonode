65a83e24fd0580841359ee6c929118ee602cfe36
marthamareal <marthamareal@gmail.com>
2022-07-08 12:20:31 +0300

    - add command to clean resources thumbs

---
+++ geonode/base/migrations/0082_clean_resources_with_missing_thumb.py
@@ -0,0 +1,33 @@
+import logging
+
+from django.db import migrations
+from django.templatetags.static import static
+
+from geonode.thumbs.utils import MISSING_THUMB
+
+logger = logging.getLogger(__name__)
+
+
+def set_null_thumbnail(apps, _):
+    "Sets thumbnail_url to null for resources with thumbnail_url=missing_thumb"
+    try:
+        resource_model = apps.get_model('base', 'ResourceBase')
+        link_model = apps.get_model('base', 'Link')
+        # update thumbnail urls
+        resource_model.objects.filter(thumbnail_url__icontains=static(MISSING_THUMB)).update(thumbnail_url=None)
+        # Remove thumbnail links
+        link_model.objects.filter(resource__thumbnail_url__isnull=True, name='Thumbnail').delete()
+    except Exception as e:
+        logger.error(f'Error during cleanup of missing thumbnails and links: {e.args[0]}')
+        pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0081_alter_resourcebase_alternate'),
+    ]
+
+    operations = [
+        migrations.RunPython(set_null_thumbnail, migrations.RunPython.noop),
+    ]
