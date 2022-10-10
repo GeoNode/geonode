5bddb39fcdf7a2f055626a4363b27eddf73193ca
NAGGINDA MARTHA <marthamareal@gmail.com>
2022-10-10 11:52:58 +0300

    -[Fixes #10104] Sort resource APIs with created date (#10105)
    Co-authored-by: Alessio Fabiani <alessio.fabiani@geosolutionsgroup.com>

--- geonode/geoapps/api/views.py
+++ geonode/geoapps/api/views.py
@@ -47,6 +47,6 @@ class GeoAppViewSet(DynamicModelViewSet):
         DynamicFilterBackend, DynamicSortingFilter, DynamicSearchFilter,
         ExtentFilter, GeoAppPermissionsFilter
     ]
-    queryset = GeoApp.objects.all().order_by('-last_updated')
+    queryset = GeoApp.objects.all().order_by('-created')
     serializer_class = GeoAppSerializer
     pagination_class = GeoNodeApiPagination
