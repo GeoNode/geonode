81f378e55c6d15c65e5157a738fd4967a5719a28
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2022-11-04 16:55:34 +0100

    [4.0.x][Fixes #10208] Add a custom hook at the end of the permissions assign (#10212)
    * [Fixes #10208] Add a custom hook at the end of the permissions assign

--- geonode/geoserver/manager.py
+++ geonode/geoserver/manager.py
@@ -46,7 +46,7 @@ from geonode.security.permissions import (
 from geonode.resource.manager import (
     ResourceManager,
     ResourceManagerInterface)
-
+from geonode.geoserver.signals import post_set_permissions
 from .tasks import (
     geoserver_set_style,
     geoserver_delete_map,
@@ -472,6 +472,9 @@ class GeoServerResourceManager(ResourceManagerInterface):
         except Exception as e:
             logger.exception(e)
             return False
+
+        post_set_permissions.send_robust(sender=instance, instance=instance)
+
         return True
 
     def set_thumbnail(self, uuid: str, /, instance: ResourceBase = None, overwrite: bool = True, check_bbox: bool = True) -> bool:
