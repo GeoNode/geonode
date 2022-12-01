caad51be9df45bdc88aad3bec7ecba3dca4fefc7
Christian Autermann <c.autermann@52north.org>
2022-12-01 16:13:56 +0100

    [Fixes #10374] UUID resolver endpoint

--- geonode/catalogue/urls.py
+++ geonode/catalogue/urls.py
@@ -18,6 +18,7 @@
 #########################################################################
 
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
@@ -26,5 +27,6 @@ urlpatterns = [
     url(r'^csw_to_extra_format/(?P<layeruuid>[^/]*)/(?P<resname>[^/]*).txt$',
         views.csw_render_extra_format_txt, name="csw_render_extra_format_txt"),
     url(r'^csw_to_extra_format/(?P<layeruuid>[^/]*)/(?P<resname>[^/]*).html$',
-        views.csw_render_extra_format_html, name="csw_render_extra_format_html")
+        views.csw_render_extra_format_html, name="csw_render_extra_format_html"),
+    path(r'uuid/<uuid:uuid>', views.resolve_uuid, name="resolve_uuid")
 ]
