82d023899429e347953214298404cc72d379a9db
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2023-01-09 11:41:01 +0100

    enabled memcahced restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ invoke () {
 
 # Start cron && memcached services
 service cron restart
-#service memcached restart
+service memcached restart
 
 echo $"\n\n\n"
 echo "-----------------------------------------------------"
