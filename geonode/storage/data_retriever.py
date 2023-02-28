6e33c12ec0c862f96432f0d9fb1000f7c098c7ee
Henning Bredel <h.bredel@52north.org>
2023-02-28 14:17:12 +0100

    Make sure to keep all unzipped files as file_paths
    CSV and JSON are both valid `base_file`s. However, the content of a
    ZIP file containing a JSON file along with one or multiple CSV files
    will not be stored as `data_retriever` will override all allowed
    `base_file`s until just one file remain.

--- geonode/storage/data_retriever.py
+++ geonode/storage/data_retriever.py
@@ -211,11 +211,24 @@ class DataRetriever(object):
         not_main_files = ["xml", "sld", "zip", "kmz"]
         base_file_choices = [x for x in available_choices if x not in not_main_files]
         for _file in Path(self.temporary_folder).iterdir():
+            if not zipfile.is_zipfile(str(_file)):
             if any([_file.name.endswith(_ext) for _ext in base_file_choices]):
                 self.file_paths["base_file"] = Path(str(_file))
             elif not zipfile.is_zipfile(str(_file)):
                 ext = _file.name.split(".")[-1]
+                if f"{ext}_file" in self.file_paths:
+                    existing = self.file_paths[f"{ext}_file"]
+                    self.file_paths[f"{ext}_file"] = [ Path(str(_file)), *(existing if type(existing) == list else [existing]) ]
+                else: 
                 self.file_paths[f"{ext}_file"] = Path(str(_file))
+
+        tmp = self.file_paths.copy()
+        for key, value in self.file_paths.items():
+            if type(value) == list:
+                for index, file_path in enumerate(value):
+                    n = f"{key}_{index}" if index > 0 else key
+                    tmp[n] = file_path
+        self.file_paths = tmp
 
         # remiving the zip file
         os.remove(zip_name)
