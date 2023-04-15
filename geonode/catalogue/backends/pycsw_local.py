7d97f93a2aa577909475a02fd5de50ecc181c69d
afabiani <alessio.fabiani@geosolutionsgroup.com>
2023-04-15 20:13:43 +0200

    - Fix OWSLib BREAKING: remove ISO deprecations (https://github.com/geopython/OWSLib/pull/862) (https://github.com/geopython/OWSLib/pull/869)

--- geonode/catalogue/backends/pycsw_local.py
+++ geonode/catalogue/backends/pycsw_local.py
@@ -86,8 +86,8 @@ class CatalogueBackend(GenericCatalogueBackend):
 
         record = MD_Metadata(result)
         record.keywords = []
-        if hasattr(record, "identification") and hasattr(record.identification, "keywords"):
-            for kw in record.identification.keywords:
+        if hasattr(record, "identification[0]") and hasattr(record.identification[0], "keywords"):
+            for kw in record.identification[0].keywords:
                 record.keywords.extend(kw["keywords"])
 
         record.links = {}
