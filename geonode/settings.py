bd72c8a3afdddb01951f0a349dc7104815912004
etj <etj@geo-solutions.it>
2023-05-15 12:14:17 +0200

    [Fixes #10995] Faceting - Some implementations

--- geonode/settings.py
+++ geonode/settings.py
@@ -2314,6 +2314,10 @@ IMPORTER_HANDLERS = ast.literal_eval(
 )
 
 INSTALLED_APPS += ("geonode.facets",)
+GEONODE_APPS += ("geonode.facets",)
 
 FACET_PROVIDERS = (
+    "geonode.facets.providers.category.CategoryFacetProvider",
+    "geonode.facets.providers.users.OwnerFacetProvider",
+    "geonode.facets.providers.thesaurus.ThesaurusFacetProvider",
 )
