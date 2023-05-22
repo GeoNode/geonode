a3ff7533ce05b0b3765245d50b57c49575d9a994
Giovanni Allegri <giohappy@gmail.com>
2023-05-22 13:20:38 +0200

    [Fixes #10792] Improve B/R procedures (#11051)
    * Improve B/R procedures
    
    * fix formatting of geoserver error message
    
    * Fix black formatting
    
    * Fix PEP wanrings
    
    ---------
    
    Co-authored-by: Alessio Fabiani <alessio.fabiani@geosolutionsgroup.com>

--- geonode/settings.py
+++ geonode/settings.py
@@ -682,6 +682,7 @@ LOGGING = {
         "simple": {
             "format": "%(message)s",
         },
+        "br": {"format": "%(levelname)-7s %(asctime)s %(message)s"},
     },
     "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
     "handlers": {
@@ -691,30 +692,32 @@ LOGGING = {
             "filters": ["require_debug_false"],
             "class": "django.utils.log.AdminEmailHandler",
         },
+        "br": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "br"},
     },
     "loggers": {
         "django": {
-            "handlers": ["console"],
             "level": "ERROR",
         },
         "geonode": {
-            "handlers": ["console"],
-            "level": "ERROR",
+            "level": "WARN",
         },
+        "geonode.br": {"level": "INFO", "handlers": ["br"], "propagate": False},
         "geoserver-restconfig.catalog": {
-            "handlers": ["console"],
             "level": "ERROR",
         },
         "owslib": {
-            "handlers": ["console"],
             "level": "ERROR",
         },
         "pycsw": {
-            "handlers": ["console"],
             "level": "ERROR",
         },
         "celery": {
-            "handlers": ["console"],
+            "level": "WARN",
+        },
+        "mapstore2_adapter.plugins.serializers": {
+            "level": "ERROR",
+        },
+        "geonode_logstash.logstash": {
             "level": "ERROR",
         },
     },
