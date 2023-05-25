2ec91adc765cf2423b6cebcd0d5c9c4582b2ffd5
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2023-05-25 16:36:02 +0200

    Remove doc_file and file_path from document serializer (#11117)
    (cherry picked from commit 927a302bb4494a3d7dce5c829d7c6a646247704c)

--- geonode/documents/api/serializers.py
+++ geonode/documents/api/serializers.py
@@ -61,3 +61,10 @@ class DocumentSerializer(ResourceBaseSerializer):
             "doc_url",
             "metadata",
         )
+
+    def to_representation(self, obj):
+        _doc = super(DocumentSerializer, self).to_representation(obj)
+        # better to hide internal server file path
+        _doc.pop("file_path")
+        _doc.pop("doc_file")
+        return _doc
