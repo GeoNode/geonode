264ed5c623e279daf04e4df8c48addc4724f9024
mattiagiupponi <mattia.giupponi@gmail.com>
2023-06-15 16:00:56 +0200

    FIx download_url for WFS url, change settings

--- geonode/layers/models.py
+++ geonode/layers/models.py
@@ -331,8 +331,10 @@ class Dataset(ResourceBase):
             return None
         if self.link_set.filter(resource=self.get_self_resource(), link_type="original").exists():
             original = self.link_set.filter(resource=self.get_self_resource(), link_type="original").first()
-            domain_exists = any(storage_manager.exists(x) for x in self.files) or current_domain not in original.url
-            if original and original.url != "" and original.url is not None and domain_exists:
+            file_exists_or_url = (
+                any(storage_manager.exists(x) for x in self.files) or current_domain not in original.url
+            )
+            if original and original.url != "" and original.url is not None and file_exists_or_url:
                 return original.url
         return build_absolute_uri(reverse("dataset_download", args=(self.alternate,)))
 
