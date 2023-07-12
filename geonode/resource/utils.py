dbc282a13522039fcfd19d933fa60157b53dc846
Giovanni Allegri <giohappy@gmail.com>
2023-07-12 16:37:22 +0200

    [Fixes #11256] Assign regions based on contains and overlaps (#11257)
    * Assign regions based on contains and overlaps
    
    * tests
    
    * fix E501

--- geonode/resource/utils.py
+++ geonode/resource/utils.py
@@ -483,13 +483,7 @@ def metadata_post_save(instance, *args, **kwargs):
             regions_to_add = []
             for region in queryset:
                 try:
-                    srid2, wkt2 = region.geographic_bounding_box.split(";")
-                    srid2 = re.findall(r"\d+", srid2)
-
-                    poly2 = GEOSGeometry(wkt2, srid=int(srid2[0]))
-                    poly2.transform(4326)
-
-                    if not poly2.intersection(poly1).empty:
+                    if region.is_assignable_to_geom(poly1):
                         regions_to_add.append(region)
                     if region.level == 0 and region.parent is None:
                         global_regions.append(region)
@@ -498,7 +492,7 @@ def metadata_post_save(instance, *args, **kwargs):
                     if tb:
                         logger.debug(tb)
             if regions_to_add or global_regions:
-                if regions_to_add and len(regions_to_add) > 0 and len(regions_to_add) <= 30:
+                if regions_to_add:
                     instance.regions.add(*regions_to_add)
                 else:
                     instance.regions.add(*global_regions)
