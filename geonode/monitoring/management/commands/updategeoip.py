ef00ac0df9942508c42866067e62a50baddb41f2
Admire Nyakudya <addloe@gmail.com>
2023-07-30 16:02:45 +0200

    Add new env variable GEOIP_DATABASE_URL

--- geonode/monitoring/management/commands/updategeoip.py
+++ geonode/monitoring/management/commands/updategeoip.py
@@ -27,7 +27,7 @@ from django.utils.translation import ugettext_noop as _
 
 logger = logging.getLogger(__name__)
 
-URL = 'https://build.geo-solutions.it/geonode/geoserver/latest/GeoLite2-City.tar.gz'
+URL = os.environ.get(GEOIP_DATABASE_URL, 'https://build.geo-solutions.it/geonode/geoserver/latest/GeoLite2-City.tar.gz')
 OLD_FORMAT = False
 
 
@@ -65,7 +65,8 @@ class Command(BaseCommand):
         block_size = 1024
         wrote = 0
         with open('output.bin', 'wb') as f:
-            for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size // block_size), unit='KB', unit_scale=False):  # noqa
+            for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size // block_size), unit='KB',
+                             unit_scale=False):  # noqa
                 wrote = wrote + len(data)
                 f.write(data)
         logger.info(f" total_size [{total_size}] / wrote [{wrote}] ")
