bafbb92c18c78dba8ca920e863f9a1e508ef9570
mwallschlaeger <marcel.wallschlaeger@zalf.de>
2023-08-07 15:13:57 +0200

    fixing #11337

--- wait-for-databases.sh
+++ wait-for-databases.sh
@@ -6,17 +6,17 @@ host="$1"
 port="$2"
 shift
 
-until PGPASSWORD=${POSTGRES_PASSWORD} psql -h "$host" -U ${POSTGRES_USER} -p ${port}-P "pager=off" -c '\l'; do
+until PGPASSWORD=${POSTGRES_PASSWORD} psql -h "$host" -U ${POSTGRES_USER} -p "$port" -P "pager=off" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
-until PGPASSWORD=${GEONODE_DATABASE_PASSWORD} psql -h "$host" -U ${GEONODE_DATABASE} -d ${GEONODE_DATABASE} -p ${port} -P "pager=off" -c '\l'; do
+until PGPASSWORD=${GEONODE_DATABASE_PASSWORD} psql -h "$host" -U ${GEONODE_DATABASE} -d ${GEONODE_DATABASE} -p "$port" -P "pager=off" -c '\l'; do
   >&2 echo "${GEONODE_DATABASE} is unavailable - sleeping"
   sleep 1
 done
 
-until PGPASSWORD=${GEONODE_GEODATABASE_PASSWORD} psql -h "$host" -U ${GEONODE_GEODATABASE} -d ${GEONODE_GEODATABASE} -p ${port} -P "pager=off" -c '\l'; do
+until PGPASSWORD=${GEONODE_GEODATABASE_PASSWORD} psql -h "$host" -U ${GEONODE_GEODATABASE} -d ${GEONODE_GEODATABASE} -p "$port" -P "pager=off" -c '\l'; do
   >&2 echo "${GEONODE_GEODATABASE} is unavailable - sleeping"
   sleep 1
 done
