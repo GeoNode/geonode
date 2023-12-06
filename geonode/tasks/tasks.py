050f1c090c61e22d775b6f4ea0f1bdf405433b2b
mattiagiupponi <mattia.giupponi@gmail.com>
2023-12-06 11:46:59 +0100

    [Fixes #11750] Do not send notifications in Celery tasks if notifications are disabled

--- geonode/tasks/tasks.py
+++ geonode/tasks/tasks.py
@@ -168,18 +168,21 @@ def send_queued_notifications(self, *args):
     advantage of this.
 
     """
-    from importlib import import_module
+    from geonode.notifications_helper import has_notifications
 
-    notifications = getattr(settings, "NOTIFICATIONS_MODULE", None)
+    if has_notifications:
+        from importlib import import_module
 
-    if notifications:
-        engine = import_module(f"{notifications}.engine")
-        send_all = getattr(engine, "send_all")
-        # Make sure application can write to location where lock files are stored
-        if not args and getattr(settings, "NOTIFICATION_LOCK_LOCATION", None):
-            send_all(settings.NOTIFICATION_LOCK_LOCATION)
-        else:
-            send_all(*args)
+        notifications = getattr(settings, "NOTIFICATIONS_MODULE", None)
+
+        if notifications:
+            engine = import_module(f"{notifications}.engine")
+            send_all = getattr(engine, "send_all")
+            # Make sure application can write to location where lock files are stored
+            if not args and getattr(settings, "NOTIFICATION_LOCK_LOCATION", None):
+                send_all(settings.NOTIFICATION_LOCK_LOCATION)
+            else:
+                send_all(*args)
 
 
 @app.task(
