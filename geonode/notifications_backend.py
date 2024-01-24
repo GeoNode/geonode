5a8c381d5cd2c03adb9edff16087cbbf86dd1bce
Niji <mathereall@gmail.com>
2024-01-23 23:54:22 -0600

    revert of test_message_notifications.TestSendEmail

--- geonode/notifications_backend.py
+++ geonode/notifications_backend.py
@@ -1,6 +1,6 @@
 #########################################################################
 #
-# Copyright (C) 2020 OSGeo
+# Copyright (C) 2017 OSGeo
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,109 +16,81 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
-from unittest.mock import patch
-from django.contrib.auth.models import Group
+import logging
 
-from user_messages.models import Thread, Message, GroupMemberThread
-from geonode.people.models import Profile
-from geonode.tests.base import GeoNodeBaseTestSupport
-from geonode.messaging.notifications import message_received_notification
+from django.conf import settings
+from django.core.mail import send_mail, get_connection, EmailMessage
+from django.template.loader import render_to_string
+from django.utils.translation import gettext_lazy
+
+from pinax.notifications.backends.base import BaseBackend
+
+logger = logging.getLogger(__name__)
 
 
-class TestSendEmail(GeoNodeBaseTestSupport):
+class EmailBackend(BaseBackend):
+    spam_sensitivity = 2
 
-    def setUp(self):
-        self.p = Profile.objects.create(username='test', email='test1@test.test')
-        self.p1 = Profile.objects.create(username='test1')
-        self.sender = Profile.objects.create(username='sender', email='test1@test.test')
-        self.p = Profile.objects.create(username="test", email="test1@test.test")
-        self.p1 = Profile.objects.create(username="test1")
-        self.sender = Profile.objects.create(username="sender", email="test1@test.test")
+    def can_send(self, user, notice_type, scoping):
+        can_send = super().can_send(user, notice_type, scoping)
+        if can_send and user.email:
+            return True
+        return False
 
-        self.t = Thread.objects.create(subject='test', )
-        self.t2 = Thread.objects.create(subject='test2', )
-        self.t = Thread.objects.create(
-            subject="test",
+    def deliver(self, recipient, sender, notice_type, extra_context):
+        context = self.default_context()
+        context.update(
+            {
+                "recipient": recipient,
+                "sender": sender,
+                "notice": gettext_lazy(notice_type.display),
+            }
         )
-        self.t2 = Thread.objects.create(
-            subject="test2",
+        context.update(extra_context)
+
+        messages = self.get_formatted_messages(("short.txt", "full.txt"), notice_type.label, context)
+
+        context.update(
+            {
+                "message": messages["short.txt"],
+            }
         )
+        subject = "".join(render_to_string("pinax/notifications/email_subject.txt", context).splitlines())
 
-        GroupMemberThread.objects.create(thread=self.t2, group=Group.objects.get(pk=1), user=self.p)
-        GroupMemberThread.objects.create(thread=self.t2, group=Group.objects.get(pk=1), user=self.p1)
+        context.update({"message": messages["full.txt"]})
+        body = render_to_string("pinax/notifications/email_body.txt", context)
 
-        self.m2 = Message.objects.create(content='test', thread=self.t2, sender=self.sender)
-        self.m = Message.objects.create(content='test', thread=self.t, sender=self.sender)
-        self.m2 = Message.objects.create(content="test", thread=self.t2, sender=self.sender)
-        self.m = Message.objects.create(content="test", thread=self.t, sender=self.sender)
+        email = EmailMessage(
+            subject=subject,
+            body=body,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=[
+                recipient.email,
+            ],
+            reply_to=[
+                settings.DEFAULT_FROM_EMAIL,
+            ],
+        )
+        email.content_subtype = "html"
 
-        self.t.single_users.add(self.p)
-        self.t.single_users.add(self.p1)
-        self.t2.group_users.add(self.p)
-        self.t2.group_users.add(self.p1)
+        # TODO: require this to be passed in extra_context
+        connection = get_connection()
 
-    @patch("geonode.notifications_backend.EmailBackend.deliver")
-    def test_email_sent(self, email_message):
-        with self.settings(ASYNC_SIGNALS=False,
-                           NOTIFICATION_ENABLED=True,
-                           NOTIFICATIONS_MODULE='pinax.notifications',
-                           PINAX_NOTIFICATIONS_QUEUE_ALL=False):
-        with self.settings(
-            ASYNC_SIGNALS=False,
-            NOTIFICATION_ENABLED=True,
-            NOTIFICATIONS_MODULE="pinax.notifications",
-            PINAX_NOTIFICATIONS_QUEUE_ALL=False,
-        ):
-            message_received_notification(message=self.m)
-            email_message.assert_called_once()
-
-    @patch("geonode.notifications_backend.EmailBackend.deliver")
-    def test_email_sent_many(self, email_message):
-        with self.settings(ASYNC_SIGNALS=False,
-                           NOTIFICATION_ENABLED=True,
-                           NOTIFICATIONS_MODULE='pinax.notifications',
-                           PINAX_NOTIFICATIONS_QUEUE_ALL=False):
-            self.p1.email = 'test@test.test'
-        with self.settings(
-            ASYNC_SIGNALS=False,
-            NOTIFICATION_ENABLED=True,
-            NOTIFICATIONS_MODULE="pinax.notifications",
-            PINAX_NOTIFICATIONS_QUEUE_ALL=False,
-        ):
-            self.p1.email = "test@test.test"
-            self.p1.save()
-            message_received_notification(message=self.m)
-            self.assertEqual(email_message.call_count, 2)
-
-    @patch("geonode.notifications_backend.EmailBackend.deliver")
-    def test_email_sent_to_group(self, email_message):
-        with self.settings(ASYNC_SIGNALS=False,
-                           NOTIFICATION_ENABLED=True,
-                           NOTIFICATIONS_MODULE='pinax.notifications',
-                           PINAX_NOTIFICATIONS_QUEUE_ALL=False):
-            self.p1.email = 'test@test.test'
-        with self.settings(
-            ASYNC_SIGNALS=False,
-            NOTIFICATION_ENABLED=True,
-            NOTIFICATIONS_MODULE="pinax.notifications",
-            PINAX_NOTIFICATIONS_QUEUE_ALL=False,
-        ):
-            self.p1.email = "test@test.test"
-            self.p1.save()
-            message_received_notification(message=self.m2)
-            self.assertEqual(email_message.call_count, 2)
-
-    @patch("geonode.notifications_backend.EmailBackend.deliver")
-    def test_email_sent_to_group_single(self, email_message):
-        with self.settings(ASYNC_SIGNALS=False,
-                           NOTIFICATION_ENABLED=True,
-                           NOTIFICATIONS_MODULE='pinax.notifications',
-                           PINAX_NOTIFICATIONS_QUEUE_ALL=False):
-        with self.settings(
-            ASYNC_SIGNALS=False,
-            NOTIFICATION_ENABLED=True,
-            NOTIFICATIONS_MODULE="pinax.notifications",
-            PINAX_NOTIFICATIONS_QUEUE_ALL=False,
-        ):
-            message_received_notification(message=self.m2)
-            self.assertEqual(email_message.call_count, 1)
+        try:
+            # Manually open the connection
+            connection.open()
+            connection.send_messages(
+                [
+                    email,
+                ]
+            )
+            # The connection was already open so send_messages() doesn't close it.
+        except Exception as e:
+            logger.error(e)
+            try:
+                send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [email])
+            except Exception as e:
+                logger.error(e)
+        finally:
+            # We need to manually close the connection.
+            connection.close()
