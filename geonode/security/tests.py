bf83eaeb3d78c506f316202a092499e3311efe09
mattiagiupponi <mattia.giupponi@gmail.com>
2024-03-21 10:18:45 +0100

    [Fixes #12046] Remove legacy forms and views for layer upload, replace and append

--- geonode/security/tests.py
+++ geonode/security/tests.py
@@ -198,7 +198,7 @@ class SecurityTests(ResourceTestCaseMixin, GeoNodeBaseTestSupport):
 
         middleware = LoginRequiredMiddleware(None)
 
-        black_listed_url = reverse("get_dataset")
+        black_listed_url = reverse("load_dataset_data")
         white_listed_url = reverse("account_login")
 
         # unauthorized request to black listed URL should be redirected to `redirect_to` URL
@@ -229,7 +229,7 @@ class SecurityTests(ResourceTestCaseMixin, GeoNodeBaseTestSupport):
         """
 
         site_url_settings = [f"{settings.SITEURL}login/custom", "/login/custom", "login/custom"]
-        black_listed_url = reverse("get_dataset")
+        black_listed_url = reverse("load_dataset_data")
 
         for setting in site_url_settings:
             with override_settings(LOGIN_URL=setting):
@@ -1068,18 +1068,6 @@ class SecurityTests(ResourceTestCaseMixin, GeoNodeBaseTestSupport):
         response = self.client.get(reverse("dataset_embed", args=(layer.alternate,)))
         self.assertTrue(response.status_code in (401, 403), response.status_code)
 
-        # 2. change_resourcebase
-        # 2.1 has not change_resourcebase: verify that bobby cannot access the
-        # layer replace page
-        response = self.client.get(reverse("dataset_replace", args=(layer.alternate,)))
-        self.assertTrue(response.status_code in (401, 403), response.status_code)
-        # 2.2 has change_resourcebase: verify that bobby can access the layer
-        # replace page
-        layer.set_permissions({"users": {"bobby": ["change_resourcebase"]}, "groups": []})
-        self.assertTrue(bob.has_perm("change_resourcebase", layer.get_self_resource()))
-        response = self.client.get(reverse("dataset_replace", args=(layer.alternate,)))
-        self.assertEqual(response.status_code, 200, response.status_code)
-
         # 3. change_resourcebase_metadata
         # 3.1 has not change_resourcebase_metadata: verify that bobby cannot
         # access the layer metadata page
@@ -1162,12 +1150,6 @@ class SecurityTests(ResourceTestCaseMixin, GeoNodeBaseTestSupport):
         # access the layer detail page
         layer.set_permissions({"users": {"AnonymousUser": []}, "groups": []})
         response = self.client.get(reverse("dataset_embed", args=(layer.alternate,)))
-        self.assertTrue(response.status_code in (302, 403))
-
-        # 2. change_resourcebase
-        # 2.1 has not change_resourcebase: verify that anonymous user cannot
-        # access the layer replace page but redirected to login
-        response = self.client.get(reverse("dataset_replace", args=(layer.alternate,)))
         self.assertTrue(response.status_code in (302, 403))
 
         # 3. change_resourcebase_metadata
