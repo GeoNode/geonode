39e21b198e451396acfdc4aa3d2f5210e6c9c39a
Mattia <mattia.giupponi@gmail.com>
2024-05-23 10:31:37 +0200

    [Fixes #12255] Harvester is not deleted when its Service is deleted

--- geonode/services/views.py
+++ geonode/services/views.py
@@ -351,6 +351,7 @@ def remove_service(request, service_id):
         return render(request, "services/service_remove.html", {"service": service})
     elif request.method == "POST":
         service.dataset_set.all().delete()
-        service.delete()
+        # by deleting the harvester we delete also the service
+        service.harvester.delete()
         messages.add_message(request, messages.INFO, _(f"Service {service.title} has been deleted"))
         return HttpResponseRedirect(reverse("services"))
