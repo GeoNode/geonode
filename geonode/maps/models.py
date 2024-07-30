a52900dee958b0335b919bc656330d62766612ef
Mattia <mattia.giupponi@gmail.com>
2024-07-30 18:09:19 +0200

    [Fixes #12455] Map thumbnails extent always cover the whole globe

--- geonode/maps/models.py
+++ geonode/maps/models.py
@@ -134,31 +134,32 @@ class Map(ResourceBase):
         Compute bbox for maps by looping on all maplayers and getting the max
         bbox of all the datasets
         """
-        bbox = None
+        bbox = (-8238681.374829309, -8220320.783295829, 4969844.0930337105, 4984363.884452854)
         for layer in self.maplayers.filter(visibility=True).order_by("order").iterator():
             dataset = layer.dataset
-            if dataset.ll_bbox_polygon:
-                dataset_bbox = utils.clean_bbox(dataset.ll_bbox, target_crs)
-            elif (
-                dataset.bbox[-1].upper() != "EPSG:3857"
-                and target_crs.upper() == "EPSG:3857"
-                and utils.exceeds_epsg3857_area_of_use(dataset.bbox)
-            ):
-                # handle exceeding the area of use of the default thumb's CRS
-                dataset_bbox = utils.transform_bbox(utils.crop_to_3857_area_of_use(dataset.bbox), target_crs)
-            else:
-                dataset_bbox = utils.transform_bbox(dataset.bbox, target_crs)
+            if dataset is not None:
+                if dataset.ll_bbox_polygon:
+                    dataset_bbox = utils.clean_bbox(dataset.ll_bbox, target_crs)
+                elif (
+                    dataset.bbox[-1].upper() != "EPSG:3857"
+                    and target_crs.upper() == "EPSG:3857"
+                    and utils.exceeds_epsg3857_area_of_use(dataset.bbox)
+                ):
+                    # handle exceeding the area of use of the default thumb's CRS
+                    dataset_bbox = utils.transform_bbox(utils.crop_to_3857_area_of_use(dataset.bbox), target_crs)
+                else:
+                    dataset_bbox = utils.transform_bbox(dataset.bbox, target_crs)
 
-            if not bbox:
-                bbox = dataset_bbox
-            else:
-                bbox = [
-                    min(bbox[0], dataset_bbox[0]),
-                    min(bbox[2], dataset_bbox[2]),
-                    max(bbox[1], dataset_bbox[1]),
-                    max(bbox[3], dataset_bbox[3]),
-                ]
-        self.set_bbox_polygon(bbox, target_crs)
+                if not bbox:
+                    bbox = dataset_bbox
+                else:
+                    bbox = [
+                        min(bbox[0], dataset_bbox[0]),
+                        max(bbox[1], dataset_bbox[1]),
+                        min(bbox[2], dataset_bbox[2]),
+                        max(bbox[3], dataset_bbox[3]),
+                    ]
+        self.set_bbox_polygon([bbox[0], bbox[2], bbox[1], bbox[3]], target_crs)
         return bbox
 
     @property
