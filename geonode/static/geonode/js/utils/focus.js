b59807a1b2604c3bd9f80f501ea63736b6b0270e
silvadealmeida <igotode@gmail.com>
2024-08-14 13:29:51 +0200

    .

--- geonode/static/geonode/js/utils/focus.js
+++ geonode/static/geonode/js/utils/focus.js
@@ -2,32 +2,10 @@ primaryColor = $('.gn-primary').css('background-color')
 
 projectDiv = $('#related_project')
 selectProj = projectDiv.find('select')
-console.log(selectProj.text())
 
 
 selectProj.filterMultiSelect();
 checkbox = $('.filter-multi-select .dropdown-item .custom-control input[type=checkbox]')
-// checkboxIndeter = $('.filter-multi-select .dropdown-item -checkbox')
+
 selected = $('.filter-multi-select .selected-items')
 checkbox.css('background', 'red');
-// checkboxIndeter.css("background-color", 'blue')
-
-// selected.css('background', 'red');
-
-// selectProj.filterMultiSelect({
-//     placeholderText: "nothing selected",
-//     filterText: "Filter",
-//     selectAllText: "Select All",
-//     labelText: "",
-//     selectionLimit: 0,
-//     caseSensitive: false,
-//     allowEnablingAndDisabling: true,
-// });
-
-// selectProj.find("option:selected").css("color", primaryColor)
-
-
-console.log(selectProj.text())
-console.log(selected)
-console.log(checkbox)
-
