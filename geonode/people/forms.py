a601ef92ccdf8be28d74414ebda239946dec25d7
Silva de Almeida <igo.silvadealmeida@zalf.de>
2024-08-22 17:30:41 +0200

    required false

--- geonode/people/forms.py
+++ geonode/people/forms.py
@@ -57,7 +57,7 @@ class ForgotUsernameForm(forms.Form):
 
 class ProfileForm(forms.ModelForm):
     organization = forms.ModelChoiceField(
-        queryset=FundingReference.objects.values_list("funder_name", flat=True), to_field_name="funder_name"
+        queryset=FundingReference.objects.values_list("funder_name", flat=True), required=False,to_field_name="funder_name"
     )
     keywords = taggit.forms.TagField(
         label=_("Keywords"), required=False, help_text=_("A space or comma-separated list of keywords")
