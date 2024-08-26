1e22a663f4a2075a7f51b01b2593f28df709f5bf
Mattia <mattia.giupponi@gmail.com>
2024-08-26 13:08:42 +0200

    [Fixes #12513] field rename

--- geonode/base/api/serializers.py
+++ geonode/base/api/serializers.py
@@ -552,7 +552,7 @@ class LinksSerializer(DynamicModelSerializer):
         return ret
 
 
-class ResourceSettingsField(serializers.BooleanField):
+class ResourceManagementField(serializers.BooleanField):
     MAPPING = {"is_approved": "can_approve", "is_published": "can_publish", "featured": "can_feature"}
 
     def to_internal_value(self, data):
@@ -608,10 +608,10 @@ class ResourceBaseSerializer(DynamicModelSerializer):
     popular_count = serializers.CharField(required=False)
     share_count = serializers.CharField(required=False)
     rating = serializers.CharField(required=False)
-    featured = ResourceSettingsField(required=False, read_only=False)
+    featured = ResourceManagementField(required=False, read_only=False)
     advertised = serializers.BooleanField(required=False)
-    is_published = ResourceSettingsField(required=False, read_only=False)
-    is_approved = ResourceSettingsField(required=False, read_only=False)
+    is_published = ResourceManagementField(required=False, read_only=False)
+    is_approved = ResourceManagementField(required=False, read_only=False)
     detail_url = DetailUrlField(read_only=True)
     created = serializers.DateTimeField(read_only=True)
     last_updated = serializers.DateTimeField(read_only=True)
