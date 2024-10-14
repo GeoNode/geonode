bafa137d89cde870e3c5f73c17e73ea9c570144f
gpetrak <gkpetrak@gmail.com>
2024-10-14 11:34:22 +0300

    fixing manage.py

--- manage.py
+++ manage.py
@@ -22,7 +22,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geonode.local_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geonode.settings")
 
     from django.core.management import execute_from_command_line
 
