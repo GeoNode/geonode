05976dd6aa8a0084f438c520259d68d439aec0e4
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2025-01-10 11:53:43 +0100

    FILE_UPLOAD_HANDLERS change handlers order

--- geonode/settings.py
+++ geonode/settings.py
@@ -2172,8 +2172,8 @@ MANAGEMENT_COMMANDS_EXPOSED_OVER_HTTP = set(
 
 FILE_UPLOAD_HANDLERS = [
     "geonode.upload.uploadhandler.SizeRestrictedFileUploadHandler",
-    "django.core.files.uploadhandler.MemoryFileUploadHandler",
     "django.core.files.uploadhandler.TemporaryFileUploadHandler",
+    "django.core.files.uploadhandler.MemoryFileUploadHandler",
 ]
 
 DEFAULT_MAX_UPLOAD_SIZE = int(os.getenv("DEFAULT_MAX_UPLOAD_SIZE", 104857600))  # 100 MB
