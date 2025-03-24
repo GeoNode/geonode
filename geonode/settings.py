bf4bd71e6346da3e51c56ec240502e6d60bdc85c
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2025-03-24 10:00:53 +0100

    Fixes #12967 Allow the social signup via social authentication even i… (#12971)
    * Fixes #12967 Allow the social signup via social authentication even if the local signup is closed
    
    ---------
    
    Co-authored-by: Giovanni Allegri <giohappy@gmail.com>

--- geonode/settings.py
+++ geonode/settings.py
@@ -1944,6 +1944,7 @@ MAP_CLIENT_USE_CROSS_ORIGIN_CREDENTIALS = ast.literal_eval(
 )
 
 ACCOUNT_OPEN_SIGNUP = ast.literal_eval(os.environ.get("ACCOUNT_OPEN_SIGNUP", "True"))
+# ref https://github.com/GeoNode/geonode/issues/12967
 ACCOUNT_OPEN_SOCIALSIGNUP = ast.literal_eval(os.environ.get("ACCOUNT_OPEN_SOCIALSIGNUP", "True"))
 ACCOUNT_APPROVAL_REQUIRED = ast.literal_eval(os.getenv("ACCOUNT_APPROVAL_REQUIRED", "False"))
 ACCOUNT_ADAPTER = "geonode.people.adapters.LocalAccountAdapter"
@@ -1959,6 +1960,7 @@ ACCOUNT_MAX_EMAIL_ADDRESSES = int(os.getenv("ACCOUNT_MAX_EMAIL_ADDRESSES", "2"))
 
 SOCIALACCOUNT_AUTO_SIGNUP = ast.literal_eval(os.environ.get("SOCIALACCOUNT_AUTO_SIGNUP", "True"))
 SOCIALACCOUNT_LOGIN_ON_GET = ast.literal_eval(os.environ.get("SOCIALACCOUNT_LOGIN_ON_GET", "True"))
+
 # This will hide or show local registration form in allauth view. True will show form
 SOCIALACCOUNT_WITH_GEONODE_LOCAL_SINGUP = ast.literal_eval(
     os.environ.get("SOCIALACCOUNT_WITH_GEONODE_LOCAL_SINGUP", "True")
