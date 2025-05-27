f1fefe5a7a051ba718974a38e89ec5ead311cb5d
Mattia <mattia.giupponi@gmail.com>
2025-05-27 11:24:06 +0200

    Upgrade to django5

--- geonode/settings.py
+++ geonode/settings.py
@@ -250,9 +250,9 @@ EXTRA_LANG_INFO = {
 AUTH_USER_MODEL = os.getenv("AUTH_USER_MODEL", "people.Profile")
 
 PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.SHA1PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    #"django.contrib.auth.hashers.SHA1PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     # 'django.contrib.auth.hashers.Argon2PasswordHasher',
     # 'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     # 'django.contrib.auth.hashers.BCryptPasswordHasher',
@@ -470,7 +470,7 @@ INSTALLED_APPS = (
     "django_filters",
     "mptt",
     "storages",
-    #"floppyforms",
+    # "floppyforms",
     "tinymce",
     "widget_tweaks",
     "django_celery_results",
