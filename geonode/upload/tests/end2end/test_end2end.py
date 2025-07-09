ce737a0a2b0cbe39d8b3ac27b9bccd775c7af8b4
Mattia <mattia.giupponi@gmail.com>
2025-07-09 14:54:43 +0200

    Fix upsert test

--- geonode/upload/tests/end2end/test_end2end.py
+++ geonode/upload/tests/end2end/test_end2end.py
@@ -44,7 +44,7 @@ logger = logging.getLogger()
 geourl = settings.GEODATABASE_URL
 
 
-@override_settings(FILE_UPLOAD_DIRECTORY_PERMISSIONS=0o777, FILE_UPLOAD_PERMISSIONS=0o7777)
+@override_settings(FILE_UPLOAD_DIRECTORY_PERMISSIONS=0o777, FILE_UPLOAD_PERMISSIONS=0o7777, IMPORTER_ENABLE_DYN_MODELS=False)
 class BaseImporterEndToEndTest(ImporterBaseTestSupport):
     @classmethod
     def setUpClass(cls) -> None:
