3235a1713c55047d2de3d5542ee5277083687bce
Emanuele Tajariol <etj@geo-solutions.it>
2025-08-29 16:12:34 +0200

    [Fixes #13459] Improve sync_geonode_datasets logging

--- geonode/geoserver/management/commands/sync_geonode_datasets.py
+++ geonode/geoserver/management/commands/sync_geonode_datasets.py
@@ -18,13 +18,17 @@
 #########################################################################
 import sys
 import json
+import logging
 
 from django.core.management.base import BaseCommand
 
+from geonode.base.management.command_utils import setup_logger
 from geonode.layers.models import Dataset
 from geonode.security.views import _perms_info_json
 from geonode.base.utils import remove_duplicate_links
 from geonode.geoserver.helpers import create_gs_thumbnail, sync_instance_with_geoserver, set_attributes_from_geoserver
+
+logger = logging.getLogger(__name__)
 
 
 def sync_geonode_datasets(
@@ -46,47 +50,52 @@ def sync_geonode_datasets(
     layers_count = layers.count()
     count = 0
     dataset_errors = []
+
+    if not layers:
+        logger.warning(f"No layers selected by filter '{filter}'")
+        return
+
     for layer in layers:
         try:
             count += 1
-            print(f"Syncing layer {count}/{layers_count}: {layer.name}")
+            logger.info(f"=== Syncing layer {count}/{layers_count}: {layer.name}")
             if updatepermissions:
-                print("Syncing permissions...")
+                logger.info("Syncing permissions...")
                 # sync permissions in GeoFence
                 perm_spec = json.loads(_perms_info_json(layer))
                 # re-sync GeoFence security rules
                 layer.set_permissions(perm_spec)
             if updateattributes:
                 # recalculate the layer statistics
+                logger.info("Setting attributes...")
                 set_attributes_from_geoserver(layer, overwrite=True)
             if updatethumbnails:
-                print("Regenerating thumbnails...")
+                logger.info("Regenerating thumbnails...")
                 create_gs_thumbnail(layer, overwrite=True, check_bbox=False)
             if updatebbox:
-                print("Regenerating BBOX...")
+                logger.info("Regenerating BBOX...")
                 sync_instance_with_geoserver(layer.id, updatemetadata=False, updatebbox=True)
             if updatemetadata:
-                print("Updating metadata...")
+                logger.info("Updating metadata...")
                 sync_instance_with_geoserver(layer.id, updatemetadata=True, updatebbox=False)
             if removeduplicates:
                 # remove duplicates
-                print("Removing duplicate links...")
+                logger.info("Removing duplicate links...")
                 remove_duplicate_links(layer)
         except (Exception, RuntimeError):
             dataset_errors.append(layer.alternate)
             exception_type, error, traceback = sys.exc_info()
-            print(exception_type, error, traceback)
+            logger.info(exception_type, error, traceback)
             if ignore_errors:
                 pass
             else:
-                import traceback
-
-                traceback.print_exc()
-                print("Stopping process because --ignore-errors was not set and an error was found.")
+                logger.error(
+                    "Stopping process because --ignore-errors was not set and an error was found.", stack_info=True
+                )
                 return
-    print(f"There are {len(dataset_errors)} layers which could not be updated because of errors")
+    logger.info(f"There are {len(dataset_errors)} layers which could not be updated because of errors")
     for dataset_error in dataset_errors:
-        print(dataset_error)
+        logger.info(dataset_error)
 
 
 class Command(BaseCommand):
@@ -148,10 +157,23 @@ class Command(BaseCommand):
             action="store_true",
             dest="updatemetadata",
             default=False,
-            help="Update the Geoserver ayer metadata.",
+            help="Update the Geoserver layer metadata.",
+        )
+        parser.add_argument(
+            "--debug",
+            action="store_true",
+            dest="log_debug",
+            default=False,
+            help="Enable debug logging.",
         )
 
     def handle(self, **options):
+        log_level = logging.DEBUG if options.get("log_debug") else logging.INFO
+        setup_logger(__name__, level=log_level)
+        import geonode.geoserver.helpers as helpers
+
+        setup_logger(helpers.__name__, level=log_level)
+
         ignore_errors = options.get("ignore_errors")
         removeduplicates = options.get("removeduplicates")
         updatepermissions = options.get("updatepermissions")
