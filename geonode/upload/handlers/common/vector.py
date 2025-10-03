beb72be91048b11a1371a3f78710723366d1b67b
Mattia <mattia.giupponi@gmail.com>
2025-10-03 16:32:49 +0200

    [Fixes #13563] force pk field namve when creating new layer and for uploads

--- geonode/upload/handlers/common/vector.py
+++ geonode/upload/handlers/common/vector.py
@@ -115,6 +115,11 @@ class BaseVectorFileHandler(BaseHandler):
         return "geometry"
 
     @property
+    def default_pk_column_name(self):
+        # ref https://github.com/GeoNode/geonode/issues/13563
+        return "fid"
+
+    @property
     def supported_file_extension_config(self):
         return NotImplementedError
 
@@ -268,7 +273,7 @@ class BaseVectorFileHandler(BaseHandler):
         # vrt file is aready created in import_resource and vrt will be auto detected by ogr2ogr
         # and also the base_file will work so can be used as alternative for fallback which will also be autodeteced by ogr2ogr.
         input_file = files.get("temp_vrt_file") or files.get("base_file")
-        options += f'"{input_file}"' + " "
+        options += f'"{input_file}"' + " -lco FID=fid "
 
         options += f'-nln {alternate} "{original_name}"'
 
