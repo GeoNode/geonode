4705cd07646d1c3bbdaeb2849d8afc57e972fe2e
sijandh35 <sumoseason35@gmail.com>
2025-11-03 16:59:47 +0000

    [Fixes #13689] Implementation of RequestConfigurationRules endpoint

--- geonode/people/api/serializers.py
+++ geonode/people/api/serializers.py
@@ -8,6 +8,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.conf import settings
 import geonode.base.api.serializers as base_serializers
+from geonode.security.request_configuration_registry import request_configuration_rules_registry
 
 logger = logging.getLogger(__name__)
 
@@ -85,3 +86,17 @@ class UserSerializer(base_serializers.DynamicModelSerializer):
         return data
 
     avatar = base_serializers.AvatarUrlField(240, read_only=True)
+
+
+class RequestConfigurationRulesSerializer(serializers.Serializer):
+    rules = serializers.SerializerMethodField()
+
+    def get_rules(self, obj):
+        if obj.is_superuser:
+            rules = []
+            users = get_user_model().objects.all()
+            for user in users:
+                rules.extend(request_configuration_rules_registry.get_rules(user)["rules"])
+            return rules
+        rules = request_configuration_rules_registry.get_rules(obj)["rules"]
+        return rules
