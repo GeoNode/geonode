4182bd124f2c9af6df37b08c7387eb6de29d2b43
gpetrak <gkpetrak@gmail.com>
2025-11-03 10:35:24 +0200

    register metadata base url to api/v2

--- geonode/metadata/urls.py
+++ geonode/metadata/urls.py
@@ -1,3 +1,6 @@
 from geonode.metadata.api.urls import urlpatterns
+from geonode.api.urls import router
+from .api import views
 
+router.register(r"metadata", views.MetadataViewSet, "metadata")
 urlpatterns += []  # make flake8 happy
