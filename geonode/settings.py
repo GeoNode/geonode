ad1a416e8fe42901cca7f286b0876cdfb3ea0640
G.Allegri <giohappy@gmail.com>
2025-11-25 13:05:58 +0100

    Do not include project fixtures settings in core

--- geonode/settings.py
+++ geonode/settings.py
@@ -2180,7 +2180,3 @@ UPSERT_LOG_LOCATION = os.getenv("UPSERT_LOG_LOCATION", "/tmp")
 
 FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o777
 FILE_UPLOAD_PERMISSIONS = 0o777
-
-PROJECT_FIXTURES = [
-    # List project-related fixture files here, in the order they should be loaded.
-]
