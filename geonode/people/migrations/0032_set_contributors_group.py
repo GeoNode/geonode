40d7c442d0989687ba3b51721fb1dc1a96e3bb79
sijandh35 <sumoseason35@gmail.com>
2026-02-09 13:37:58 +0000

    [Fixes #13935] Updated review comment and allign import on migration

--- geonode/people/migrations/0032_set_contributors_group.py
+++ geonode/people/migrations/0032_set_contributors_group.py
@@ -1,14 +1,14 @@
 # Assign the contributors group to users according to #7364
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
-
 from django.db import migrations
 from django.db.migrations.operations import RunPython
 
 
 def forwards_func(apps, schema_editor):
     from geonode.groups.conf import settings as groups_settings
+    Group = apps.get_model("auth", "Group")
+    Permission = apps.get_model("auth", "Permission")
+    ContentType = apps.get_model("contenttypes", "ContentType")
+    Profile = apps.get_model("people", "Profile")
 
     # assign contributors group to users
     cont_group, _ = Group.objects.get_or_create(name='contributors')
@@ -21,19 +21,21 @@ def forwards_func(apps, schema_editor):
         )
     if perm:
         cont_group.permissions.add(perm)
-    # Exclude admins and anonymous user
-    Profile = apps.get_model("people", "Profile")
-    users_to_update = Profile.objects.filter(pk__gt=0)
+        
+    # Exclude anonymous user
+    users_to_update = Profile.objects.exclude(username="AnonymousUser")
     for user in users_to_update:
         registeredmembers_group.user_set.add(user)
+    # Exclude additional staff and superusers also
     for user in users_to_update.exclude(is_staff=True, is_superuser=True):
         cont_group.user_set.add(user)
 
 def reverse_func(apps, schema_editor):
     # remove contributors group from users
     try:
-        cont_group = Group.objects.get(name='contributors')
+        Group = apps.get_model("auth", "Group")
         Profile = apps.get_model("people", "Profile")
+        cont_group = Group.objects.get(name='contributors')
         users_to_update = Profile.objects.filter(
             pk__gt=0, is_staff=False, is_superuser=False
             )
