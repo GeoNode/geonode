be6757c1612c8f055ba63bdf85dd2f9859f755f5
Mattia Giupponi <mattia.giupponi@gmail.com>
2026-02-27 10:43:10 +0100

    [Fixes #13990] Basic auth is not fully propagated into the WMS Service

--- geonode/harvesting/harvesters/wms.py
+++ geonode/harvesting/harvesters/wms.py
@@ -37,7 +37,6 @@ from geonode.layers.models import Dataset
 from geonode.base.models import Link, ResourceBase
 from geonode.layers.enumerations import GXP_PTYPES
 from geonode.resource.manager import resource_manager
-from geonode.services.serviceprocessors import get_service_handler
 from geonode.thumbs.exceptions import ThumbnailError
 
 from .. import models
@@ -152,7 +151,7 @@ class OgcWmsHarvester(base.BaseHarvesterWorker):
     def get_wms_operations(cls, url, version=None) -> typing.Optional[typing.Dict]:
         operations = {}
         try:
-            _parsed_service = get_service_handler(url, service_type="WMS")
+            _url, _parsed_service = WebMapService(url, version=version)
             for _op in _parsed_service.operations:
                 _methods = []
                 for _op_method in getattr(_op, "methods", []) if hasattr(_op, "methods") else _op.get("methods", []):
