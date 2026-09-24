9c219d4a623ed602a623228fad014fe6325b2cf7
sijandh35 <sumoseason35@gmail.com>
2026-09-24 10:49:01 +0000

    [Fixes #14639] Add test case
    (cherry picked from commit 1b21fe1eeec9b2e37950edcdfe64dc165a833261)

--- geonode/upload/handlers/csv/tests.py
+++ geonode/upload/handlers/csv/tests.py
@@ -144,6 +144,10 @@ class TestCSVHandler(TestCase):
         actual = self.handler.can_handle({"base_file": "random.file"})
         self.assertFalse(actual)
 
+    def test_csv_allowed_lat_long_column_names(self):
+        self.assertListEqual(["latitude", "lat", "y"], self.handler.possible_lat_column)
+        self.assertListEqual(["longitude", "long", "lon", "x"], self.handler.possible_long_column)
+
     @patch("geonode.upload.handlers.common.vector.Popen")
     def test_import_with_ogr2ogr_without_errors_should_call_the_right_command(self, _open):
         _uuid = uuid.uuid4()
